69bd0c6a7c061218c2de50b679ab8d61b4cbfb7a
Jared Kerim <jared.l.kerim@gmail.com>
2016-11-15 17:53:19 +0000

    Add projects fixes #4

--- app/experimenter/urls.py
+++ app/experimenter/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('experimenter.home.urls')),
+    url(r'^', include('experimenter.home.urls')),
 ]
