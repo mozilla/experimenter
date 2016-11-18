83f9505d022ecdc2edf760c23783b7fdde04d805
Jared Kerim <jared.l.kerim@gmail.com>
2016-11-18 20:14:08 +0000

    Add admin branding

--- app/experimenter/urls.py
+++ app/experimenter/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -6,3 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('experimenter.home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
