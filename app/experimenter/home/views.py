fd128306368999773859d348fd3326652d20ad15
Marina Samuel <msamuel@mozilla.com>
2017-04-28 11:46:44 -0400

    Closes #28: Move admin route to default route.

--- app/experimenter/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import View
-from django.http import HttpResponse
-
-
-class LandingView(View):
-
-    def get(self, request):
-        return HttpResponse('Let\'s Experiment!')
--- app/experimenter/settings.py
+++ app/experimenter/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'experimenter.home',
     'experimenter.projects',
     'experimenter.experiments',
 ]
