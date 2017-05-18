f3abf1acd737b7601f0ec69e239c22ad7e7f7c32
Jared Kerim <jkerim@mozilla.com>
2017-05-18 11:25:29 -0400

    Organize experiment admin fields fixes #61

--- app/experimenter/experiments/admin.py
+++ app/experimenter/experiments/admin.py
@@ -4,10 +4,32 @@ from django import forms
 from experimenter.experiments.models import Experiment, ExperimentVariant
 
 
-class BaseVariantInlineAdmin(admin.StackedInline):
+class SlugPrepopulatedMixin(object):
+
+    def get_prepopulated_fields(self, request, obj=None):
+        prepopulated_fields = dict(
+            super().get_prepopulated_fields(request, obj=obj))
+        readonly_fields = self.get_readonly_fields(request, obj=obj)
+
+        if 'slug' not in readonly_fields:
+            prepopulated_fields['slug'] = ('name',)
+
+        return prepopulated_fields
+
+
+class BaseVariantInlineAdmin(SlugPrepopulatedMixin, admin.StackedInline):
     max_num = 1
     model = ExperimentVariant
-    prepopulated_fields = {'slug': ('name',)}
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super().get_readonly_fields(request, obj=obj)
+
+        if obj is not None:
+            db_obj = Experiment.objects.get(pk=obj.pk)
+            if db_obj.is_begun:
+                readonly_fields = self.fields
+
+        return readonly_fields
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -43,36 +65,46 @@ class ExperimentVariantInlineAdmin(BaseVariantInlineAdmin):
         return super().get_queryset(request).filter(is_control=False)
 
 
-class ExperimentAdmin(admin.ModelAdmin):
+class ExperimentAdmin(SlugPrepopulatedMixin, admin.ModelAdmin):
     inlines = (ControlVariantInlineAdmin, ExperimentVariantInlineAdmin,)
-    prepopulated_fields = {'slug': ('name',)}
-    fields = (
-        'status',
-        'created_date',
-        'start_date',
-        'end_date',
-        'project',
-        'name',
-        'slug',
-        'objectives',
-        'success_criteria',
-        'analysis',
-    )
     list_display = (
         'name', 'project', 'status', 'created_date', 'start_date', 'end_date')
 
+    fieldsets = (
+        ('Overview', {
+            'fields': ('project', 'name', 'slug'),
+        }),
+        ('Notes', {
+            'fields': ('objectives', 'success_criteria', 'analysis'),
+        }),
+    )
+
     readonly_fields = ('created_date', 'start_date', 'end_date')
 
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj=obj)
+
+        if obj is not None:
+            fieldsets = (('Status', {
+                'fields': (
+                    'status', ('created_date', 'start_date', 'end_date')),
+            }),) + fieldsets
+
+        return fieldsets
+
     def get_readonly_fields(self, request, obj=None):
-        db_complete = False
+        readonly_fields = super().get_readonly_fields(request, obj=obj)
+
         if obj is not None:
             db_obj = Experiment.objects.get(pk=obj.pk)
-            db_complete = db_obj.is_complete
 
-        if obj is None or db_complete:
-            return super().get_readonly_fields(request, obj=obj) + ('status',)
+            if db_obj.is_complete:
+                readonly_fields += ('status',)
 
-        return super().get_readonly_fields(request, obj=obj)
+            if db_obj.is_begun:
+                readonly_fields += ('project', 'name', 'slug')
+
+        return readonly_fields
 
 
 admin.site.register(Experiment, ExperimentAdmin)
