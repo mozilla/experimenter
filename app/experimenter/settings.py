2a437ccf7ba3ef2f0fdcf22e9fe77b955a816304
Jared Kerim <jkerim@mozilla.com>
2017-06-22 15:51:57 -0400

    Host static assets through nginx fixes #78

--- app/experimenter/settings.py
+++ app/experimenter/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'served')
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'served'), 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
