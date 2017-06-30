8f6cbe403811f65717be4adb75fdce1552dab180
Jared Kerim <jkerim@mozilla.com>
2017-06-30 15:55:19 -0400

    Add dashboard_url, dashboard_image_url, image to projects fixes #104

--- app/experimenter/projects/models.py
+++ app/experimenter/projects/models.py
@@ -5,6 +5,9 @@ class Project(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     slug = models.SlugField(
         max_length=255, unique=True, blank=False, null=False)
+    dashboard_url = models.URLField(blank=True, null=True)
+    dashboard_image_url = models.URLField(blank=True, null=True)
+    image = models.FileField(upload_to='projects/', blank=True, null=True)
 
     def __str__(self):  # pragma: no cover
         return self.name
