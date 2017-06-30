dfa37689a7e7940f3e1c055f6fbafe820c4f58ec
Jared Kerim <jkerim@mozilla.com>
2017-06-30 14:23:00 -0400

    Revert "Host static assets through nginx fixes #78" fixes #126
    This reverts commit ab8b943fda78133d642654bdd5e3a76f1a2a29cd.

--- app/experimenter/settings.py
+++ app/experimenter/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'served'), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'served')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
