5482de0fabc5e0cabd53ac12d3d3e5865ee361e8
Jared Kerim <jkerim@mozilla.com>
2017-07-12 15:30:53 -0400

    Dynamically host static assets when DEBUG=True and statically host them when it's False fixes #140

--- app/experimenter/settings.py
+++ app/experimenter/settings.py
@@ -143,8 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'served')
+if DEBUG: # pragma: no cover
+    STATIC_URL = '/staticdev/'
+else: # pragma: no cover
+    STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, 'served'), 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
