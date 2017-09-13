7d5a058cd973e16f0fd9426d804089674e05e57e
Jared Kerim <jkerim@mozilla.com>
2017-09-13 14:23:06 -0400

    Add Shield Client Filtering Fields fixes #203

--- app/experimenter/experiments/tests/test_serializers.py
+++ app/experimenter/experiments/tests/test_serializers.py
@@ -16,10 +16,10 @@ class TestExperimentVariantSerializer(TestCase):
         variant = ExperimentVariantFactory.create()
         serialized = ExperimentVariantSerializer(variant)
         self.assertEqual(serialized.data, {
-            'name': variant.name,
             'description': variant.description,
-            'slug': variant.slug,
+            'name': variant.name,
             'ratio': variant.ratio,
+            'slug': variant.slug,
             'value': variant.value,
         })
 
@@ -29,21 +29,27 @@ class TestExperimentSerializer(TestCase):
     def test_serializer_outputs_expected_schema(self):
         experiment = ExperimentFactory.create_with_variants()
         serialized = ExperimentSerializer(experiment)
-        self.assertEqual(serialized.data, {
-            'project_name': experiment.project.name,
-            'name': experiment.name,
-            'slug': experiment.slug,
+        expected_data = {
+            'accept_url': experiment.accept_url,
+            'client_matching': experiment.client_matching,
+            'control': ExperimentVariantSerializer(experiment.control).data,
             'experiment_slug': experiment.experiment_slug,
             'experiment_url': experiment.experiment_url,
-            'accept_url': experiment.accept_url,
-            'reject_url': experiment.reject_url,
-            'firefox_version': experiment.firefox_version,
             'firefox_channel': experiment.firefox_channel,
+            'firefox_version': experiment.firefox_version,
+            'name': experiment.name,
+            'objectives': experiment.objectives,
             'population_percent': '{0:.4f}'.format(
                 experiment.population_percent),
-            'objectives': experiment.objectives,
+            'pref_branch': experiment.pref_branch,
             'pref_key': experiment.pref_key,
             'pref_type': experiment.pref_type,
+            'project_name': experiment.project.name,
+            'reject_url': experiment.reject_url,
+            'slug': experiment.slug,
             'variant': ExperimentVariantSerializer(experiment.variant).data,
-            'control': ExperimentVariantSerializer(experiment.control).data,
-        })
+        }
+
+        self.assertEqual(
+            set(serialized.data.keys()), set(expected_data.keys()))
+        self.assertEqual(serialized.data, expected_data)
