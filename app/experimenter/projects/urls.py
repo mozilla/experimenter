55be696e621ea74fe69c23fcfb083f10f22bda43
Jared Kerim <jkerim@mozilla.com>
2017-11-22 13:43:27 -0500

    Update project list page fixes #106

--- app/experimenter/projects/urls.py
+++ app/experimenter/projects/urls.py
@@ -4,5 +4,5 @@ from experimenter.projects.views import ProjectListView
 
 
 urlpatterns = [
-    url(r'', ProjectListView.as_view(), name='projects-list'),
+    url(r'$', ProjectListView.as_view(), name='projects-list'),
 ]
