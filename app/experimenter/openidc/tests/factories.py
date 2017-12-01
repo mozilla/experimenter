9d3b65d48b5557ae531bfdd525dab6257873b9de
Jared Kerim <jkerim@mozilla.com>
2017-12-01 14:48:34 -0500

    Add project detail page fixes #107

--- app/experimenter/openidc/tests/factories.py
+++ app/experimenter/openidc/tests/factories.py
@@ -1,5 +1,4 @@
 import factory
-from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 from faker import Factory as FakerFactory
 
@@ -9,10 +8,8 @@ faker = FakerFactory.create()
 class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.LazyAttribute(lambda o: faker.first_name())
     last_name = factory.LazyAttribute(lambda o: faker.last_name())
-    username = factory.LazyAttribute(
-        lambda o: '{}-{}'.format(slugify(o.first_name), slugify(o.last_name)))
-    email = factory.LazyAttribute(
-        lambda o: '{}@example.com'.format(o.username))
+    email = factory.LazyAttribute(lambda o: faker.company_email())
+    username = factory.LazyAttribute(lambda o: o.email)
 
     class Meta:
         model = get_user_model()
