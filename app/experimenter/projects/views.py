2ba950395df2f721ed8b5e45ea5374a4eee3b55f
Jared Kerim <jkerim@mozilla.com>
2018-01-29 16:58:29 -0500

    Add project create and edit forms fixes #270

--- app/experimenter/projects/views.py
+++ app/experimenter/projects/views.py
@@ -1,6 +1,25 @@
-from django.views.generic import ListView, DetailView
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
+from experimenter.projects.forms import ProjectForm
 from experimenter.projects.models import Project
+
+
+class ProjectEditMixin(object):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/edit.html'
+
+    def get_success_url(self):
+        return reverse('projects-detail', kwargs={'slug': self.object.slug})
+
+
+class ProjectCreateView(ProjectEditMixin, CreateView):
+    pass
+
+
+class ProjectUpdateView(ProjectEditMixin, UpdateView):
+    pass
 
 
 class ProjectListView(ListView):
