bbb3eeeca0922cba727cf596c82000e084afd907
Jared Kerim <jkerim@mozilla.com>
2018-05-09 16:24:12 -0400

    Migrate statuses to new values fixes #338

--- app/experimenter/experiments/migrations/0008_auto_20180503_1747.py
+++ app/experimenter/experiments/migrations/0008_auto_20180503_1747.py
@@ -5,6 +5,23 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
+def update_statuses(apps, schema_editor):
+    Experiment = apps.get_model('experiments', 'Experiment')
+    ExperimentChangeLog = apps.get_model('experiments', 'ExperimentChangeLog')
+
+    Experiment.objects.filter(status='Created').update(status='Draft')
+    ExperimentChangeLog.objects.filter(old_status='Created').update(old_status='Draft')
+    ExperimentChangeLog.objects.filter(new_status='Created').update(new_status='Draft')
+
+    Experiment.objects.filter(status='Pending').update(status='Review')
+    ExperimentChangeLog.objects.filter(old_status='Pending').update(old_status='Review')
+    ExperimentChangeLog.objects.filter(new_status='Pending').update(new_status='Review')
+
+    Experiment.objects.filter(status='Launched').update(status='Live')
+    ExperimentChangeLog.objects.filter(old_status='Launched').update(old_status='Live')
+    ExperimentChangeLog.objects.filter(new_status='Launched').update(new_status='Live')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -27,4 +44,5 @@ class Migration(migrations.Migration):
             name='old_status',
             field=models.CharField(blank=True, choices=[('Draft', 'Draft'), ('Review', 'Review'), ('Accepted', 'Accepted'), ('Live', 'Live'), ('Complete', 'Complete'), ('Rejected', 'Rejected')], max_length=255, null=True),
         ),
+        migrations.RunPython(update_statuses),
     ]
