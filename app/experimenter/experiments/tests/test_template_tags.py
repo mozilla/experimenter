ebdcc63938e5e8cf7292e1ade2541b9287142024
Peter Bengtsson <peterbe@mozilla.com>
2019-02-26 17:33:02 -0500

    Pagination ignores filters fixes #596 (#977)
    Fixes #596

---
+++ app/experimenter/experiments/tests/test_template_tags.py
@@ -0,0 +1,49 @@
+from django.template import Context, Template
+from django.test import RequestFactory, SimpleTestCase
+
+
+class TestPaginationUrl(SimpleTestCase):
+
+    def test_it_works(self):
+        context = Context({"request": RequestFactory().get("/")})
+        template_to_render = Template(
+            "{% load experiment_extras %}" "{% pagination_url 123 %}"
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertEqual("?page=123", rendered_template)
+
+    def test_with_other_keys(self):
+        context = Context(
+            {"request": RequestFactory().get("/", {"foo": "bar"})}
+        )
+        template_to_render = Template(
+            "{% load experiment_extras %}" "{% pagination_url 2 %}"
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertEqual("?foo=bar&amp;page=2", rendered_template)
+
+    def test_next_page(self):
+        context = Context({"request": RequestFactory().get("/", {"page": 1})})
+        template_to_render = Template(
+            "{% load experiment_extras %}" "{% pagination_url 2 %}"
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertEqual("?page=2", rendered_template)
+
+    def test_back_to_first_page(self):
+        context = Context({"request": RequestFactory().get("/", {"page": 2})})
+        template_to_render = Template(
+            "{% load experiment_extras %}" "{% pagination_url 1 %}"
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertEqual(".", rendered_template)
+
+    def test_back_to_first_page_with_other_data(self):
+        context = Context(
+            {"request": RequestFactory().get("/", {"foo": "bar"})}
+        )
+        template_to_render = Template(
+            "{% load experiment_extras %}" "{% pagination_url 1 %}"
+        )
+        rendered_template = template_to_render.render(context)
+        self.assertEqual("?foo=bar", rendered_template)
