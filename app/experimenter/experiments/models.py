7974db97c909a3707c9ba077c782d190f591c2b8
Peter Bengtsson <mail@peterbe.com>
2019-03-20 14:54:25 -0400

    migration check creates files fixes #1091
    Fixes #1091

--- app/experimenter/experiments/models.py
+++ app/experimenter/experiments/models.py
@@ -516,7 +516,7 @@ class ExperimentVariant(models.Model):
     slug = models.SlugField(max_length=255, blank=False, null=False)
     is_control = models.BooleanField(default=False)
     description = models.TextField(default="")
-    ratio = models.PositiveIntegerField(default=1)
+    ratio = models.PositiveIntegerField(default=42)
     value = JSONField(blank=True, null=True)
 
     class Meta:
