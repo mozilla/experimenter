b7eca8855e1a0b02e37e64aab119540320dc61f4
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-03-27 11:39:07 -0400

    Write a simple test for firefox_channel_sort
    Thanks @jaredkerim for pointing me in the right direction.

--- app/experimenter/experiments/tests/test_models.py
+++ app/experimenter/experiments/tests/test_models.py
@@ -838,6 +838,28 @@ class TestExperimentModel(TestCase):
         )
         self.assertEqual(experiment.population, "0.5% of Nightly Firefox 57.0")
 
+    def test_experiment_firefox_channel_sort_does_sorting(self):
+        ExperimentFactory.create(firefox_channel=Experiment.CHANNEL_NIGHTLY)
+        ExperimentFactory.create(firefox_channel=Experiment.CHANNEL_RELEASE)
+        ExperimentFactory.create(firefox_channel=Experiment.CHANNEL_BETA)
+        ExperimentFactory.create(firefox_channel="")
+        sorted_experiments = (
+            Experiment.objects.annotate(
+                firefox_channel_sort=Experiment.firefox_channel_sort()
+            )
+            .order_by("firefox_channel_sort")
+            .values("firefox_channel_sort")
+        )
+        self.assertEqual(
+            [r["firefox_channel_sort"] for r in sorted_experiments],
+            [
+                Experiment.CHANNEL_UNSET_ORDER,
+                Experiment.CHANNEL_NIGHTLY_ORDER,
+                Experiment.CHANNEL_BETA_ORDER,
+                Experiment.CHANNEL_RELEASE_ORDER,
+            ],
+        )
+
 
 class TestExperimentChangeLog(TestCase):
 
