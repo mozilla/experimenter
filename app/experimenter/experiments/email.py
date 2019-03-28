b751a068b452ae4dc83d23a6294ec44fef9c8ee5
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-03-28 17:07:33 -0400

    Clean up email and code
    - No need to pass all these template arguments separately since
      they're largely available on the experiment themselves.
    
    - Do some weird stuff to try to minimize number of extra blank lines
      when risk/technical complexity are absent.
    
    - Disable autoescaping since this is a text email.
    
    - Add tests.

--- app/experimenter/experiments/email.py
+++ app/experimenter/experiments/email.py
@@ -1,9 +1,7 @@
-import datetime
 from urllib.parse import urljoin
 
 from django.conf import settings
 from django.core.mail import send_mail
-from django.urls import reverse
 from django.template.loader import render_to_string
 
 from experimenter.experiments.models import Experiment
@@ -27,60 +25,38 @@ def send_review_email(experiment_name, experiment_url, needs_attention):
     )
 
 
-def send_intent_to_ship_email(experiment_id, experiment_url):
+def send_intent_to_ship_email(experiment_id):
 
     def make_url(uri):
         return urljoin("https://{}".format(settings.HOSTNAME), uri)
 
-    # XXX Is there already a routine for doing this?
-    def format_date(date, format="%Y-%m-%d"):
-        assert isinstance(date, datetime.date)
-        return date.strftime(format)
+    experiment = Experiment.objects.prefetch_related(
+        "locales", "countries"
+    ).get(id=experiment_id)
 
-    experiment = Experiment.objects.get(id=experiment_id)
-
-    version = experiment.firefox_version
-    channel = experiment.firefox_channel
     experiment_url = make_url(experiment.experiment_url)
 
     bug_url = settings.BUGZILLA_DETAIL_URL.format(id=experiment.bugzilla_id)
 
-    # XXX Not all experiments have a Project!
-    project_url = make_url(
-        reverse("projects-detail", kwargs={"slug": experiment.project.slug})
-    )
-
-    proposed_start_date = experiment.proposed_start_date
-    proposed_end_date = proposed_start_date + datetime.timedelta(
-        days=experiment.proposed_duration
-    )
     # Because that's how it's done in Experiment.population (property)
-    percent_of_population = f"{experiment.population_percent:g}%"
-    # XXX https://github.com/mozilla/experimenter/issues/747#issuecomment-468404348
-    platforms = "(unknown)"
-    # XXX https://github.com/mozilla/experimenter/issues/747#issuecomment-468407307
-    locales = "(unknown)"
+    percent_of_population = f"{float(experiment.population_percent):g}%"
 
     content = render_to_string(
-        "my_template.html",
+        "experiments/intent_to_ship.txt",
         {
+            "experiment": experiment,
             "experiment_url": experiment_url,
             "bug_url": bug_url,
-            "project_url": project_url,
-            "experiment_owner": experiment.owner.email,
-            "short_description": experiment.short_description,
-            "version": version,
-            "channel": channel,
-            "proposed_start_date": format_date(proposed_start_date),
-            "proposed_end_date": format_date(proposed_end_date),
             "percent_of_population": percent_of_population,
-            "platforms": platforms,
-            "locales": locales,
-            "qa_status": experiment.qa_status,
-            "feature_bugzilla_url": experiment.feature_bugzilla_url,
-            "related_work": experiment.related_work,
+            "locales": [str(l) for l in experiment.locales.all()],
+            "countries": [str(c) for c in experiment.countries.all()],
         },
-    ).lstrip()
+    )
+    # Strip extra newlines from autoescape tag
+    content = content.strip() + "\n"
+
+    version = experiment.firefox_version
+    channel = experiment.firefox_channel
     send_mail(
         Experiment.INTENT_TO_SHIP_EMAIL_SUBJECT.format(
             name=experiment.name, version=version, channel=channel
