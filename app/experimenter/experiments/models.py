2ade9f547bfaf7188c48edc7e63935572c907d5c
tifran <ttran@mozilla.com>
2019-04-02 13:27:26 -0700

    removed empty line

--- app/experimenter/experiments/models.py
+++ app/experimenter/experiments/models.py
@@ -4,7 +4,6 @@ from urllib.parse import urljoin
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-
 from django.core.validators import MaxValueValidator
 from django.db import models
 from django.db.models import Case, Max, Value, When
