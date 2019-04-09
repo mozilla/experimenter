81e4536a1295756dd9b8aebafc3f5ccede55bb1c
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-04-09 13:41:39 -0400

    Add setting to test configuration

--- app/experimenter/settings_test.py
+++ app/experimenter/settings_test.py
@@ -15,5 +15,6 @@ HOSTNAME = "experimenter.moz"
 EMAIL_REVIEW = "testreview@example.com"
 EMAIL_SHIP = "testship@example.com"
 EMAIL_SENDER = "sender@example.com"
+EMAIL_RELEASE_DRIVERS = "release.drivers@example.com"
 
 USE_GOOGLE_ANALYTICS = True
