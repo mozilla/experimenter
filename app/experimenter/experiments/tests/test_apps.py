47bcb630eaa86c4ac77d559ab91d114ded611284
Rob Hudson <robhudson@mozilla.com>
2019-04-16 10:45:22 -0700

    Fix #597: Add statsd calls to celery tasks (#1174)

---
+++ app/experimenter/experiments/tests/test_apps.py
@@ -0,0 +1,12 @@
+from django.apps import apps
+from django.test import TestCase
+
+from experimenter.experiments.apps import ExperimentsConfig
+
+
+class AppTests(TestCase):
+
+    def test_app_config(self):
+        with self.settings(INSTALLED_APPS=["experimenter.experiments"]):
+            config = apps.get_app_config("experiments")
+        self.assertIsInstance(config, ExperimentsConfig)
