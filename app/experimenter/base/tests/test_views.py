f9f4d6d22f31038170338c4ad97c5e018c174d1f
Jared Kerim <pwbdecker@gmail.com>
2019-04-18 13:38:01 -0400

    Fix google analytics tests fixes #1189 (#1190)

--- app/experimenter/base/tests/test_views.py
+++ app/experimenter/base/tests/test_views.py
@@ -5,16 +5,19 @@ from django.urls import reverse
 
 class TestContextProcessors(TestCase):
 
-    def test_render_google_analytics_or_not(self):
-        headers = {settings.OPENIDC_EMAIL_HEADER: "user@example.com"}
-        response = self.client.get(reverse("home"), **headers)
-        self.assertEqual(response.status_code, 200)
-        html = response.content.decode("utf-8")
-        self.assertTrue("www.googletagmanager.com" in html)
-
+    def test_google_analytics_omitted_if_setting_false(self):
         with self.settings(USE_GOOGLE_ANALYTICS=False):
+            headers = {settings.OPENIDC_EMAIL_HEADER: "user@example.com"}
             response = self.client.get(reverse("home"), **headers)
             self.assertEqual(response.status_code, 200)
             html = response.content.decode("utf-8")
             # Note the 'not'!
-            self.assertTrue("www.googletagmanager.com" not in html)
+            self.assertNotIn("www.googletagmanager.com", html)
+
+    def test_google_analytics_included_if_setting_true(self):
+        with self.settings(USE_GOOGLE_ANALYTICS=True):
+            headers = {settings.OPENIDC_EMAIL_HEADER: "user@example.com"}
+            response = self.client.get(reverse("home"), **headers)
+            self.assertEqual(response.status_code, 200)
+            html = response.content.decode("utf-8")
+            self.assertIn("www.googletagmanager.com", html)
