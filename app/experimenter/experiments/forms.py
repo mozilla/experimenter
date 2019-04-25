83978fb762970e80ff90481ae1340b78475f5007
Tif Tran <ttran@mozilla.com>
2019-04-25 13:54:05 -0700

    added ux risk questions with cond ux review fixes #867 (#1213)

--- app/experimenter/experiments/forms.py
+++ app/experimenter/experiments/forms.py
@@ -660,6 +660,14 @@ class ExperimentRisksForm(ChangeLogMixin, forms.ModelForm):
         coerce=coerce_truthy,
         empty_value=None,
     )
+    risk_ux = forms.TypedChoiceField(
+        label=Experiment.RISK_UX_LABEL,
+        help_text=Experiment.RISK_UX_HELP_TEXT,
+        choices=RADIO_OPTIONS,
+        widget=RadioWidget,
+        coerce=coerce_truthy,
+        empty_value=None,
+    )
     risk_technical = forms.TypedChoiceField(
         label=Experiment.RISK_TECHNICAL_LABEL,
         help_text=Experiment.RISK_TECHNICAL_HELP_TEXT,
@@ -745,6 +753,7 @@ class ExperimentRisksForm(ChangeLogMixin, forms.ModelForm):
             "risk_data_category",
             "risk_external_team_impact",
             "risk_telemetry_data",
+            "risk_ux",
             "risk_technical",
             "risk_technical_description",
             "risks",
@@ -829,7 +838,7 @@ class ExperimentReviewForm(
     review_ux = forms.BooleanField(
         required=False,
         label="UX Review",
-        help_text=Experiment.REVIEW_UX_HELP_TEXT,
+        help_text=Experiment.REVIEW_GENERAL_HELP_TEXT,
     )
     review_security = forms.BooleanField(
         required=False,
