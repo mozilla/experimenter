b93a30d79e4090f387b5734ea110e2adcd631d3a
Tif Tran <ttran@mozilla.com>
2019-05-14 14:03:25 -0700

    Check for fields for request body of bugzilla creation fixes #1258 (#1260)
    * checks for fields for request body of bugzilla creation fixes #1258
    
    * moved building cf_tracking key logic to models.py
    
    * renamed variable to bugzilla_id instead of response_data

--- app/experimenter/experiments/tests/test_bugzilla.py
+++ app/experimenter/experiments/tests/test_bugzilla.py
@@ -12,7 +12,6 @@ from experimenter.experiments.bugzilla import (
     create_experiment_bug,
     format_bug_body,
     make_bugzilla_call,
-    get_firefox_major_version,
 )
 from experimenter.experiments.tests.factories import ExperimentFactory
 from experimenter.experiments.tests.mixins import MockBugzillaMixin
@@ -22,11 +21,9 @@ class TestCreateExperimentBug(MockBugzillaMixin, TestCase):
 
     def test_creating_pref_bugzilla_ticket_returns_ticket_id(self):
         experiment = ExperimentFactory.create_with_status(
-            Experiment.STATUS_DRAFT, name="An Experiment"
-        )
-
-        cf_tracking = "cf_tracking_firefox{}".format(
-            get_firefox_major_version(experiment.firefox_version)
+            Experiment.STATUS_DRAFT,
+            name="An Experiment",
+            firefox_version="56.0",
         )
 
         response_data = create_experiment_bug(experiment)
@@ -53,16 +50,13 @@ class TestCreateExperimentBug(MockBugzillaMixin, TestCase):
                 "blocks": [12345],
                 "url": experiment.experiment_url,
                 "whiteboard": experiment.STATUS_REVIEW_LABEL,
-                cf_tracking: "?",
+                experiment.bugzilla_tracking_key: "?",
             },
         )
 
     def test_create_bugzilla_ticket_retries_with_no_assignee(self):
         experiment = ExperimentFactory.create_with_status(
             Experiment.STATUS_DRAFT, name="An Experiment"
-        )
-        cf_tracking = "cf_tracking_firefox{}".format(
-            get_firefox_major_version(experiment.firefox_version)
         )
 
         self.setUpMockBugzillaInvalidUser()
@@ -90,7 +84,7 @@ class TestCreateExperimentBug(MockBugzillaMixin, TestCase):
             "blocks": [12345],
             "url": experiment.experiment_url,
             "whiteboard": experiment.STATUS_REVIEW_LABEL,
-            cf_tracking: "?",
+            experiment.bugzilla_tracking_key: "?",
         }
 
         self.mock_bugzilla_requests_post.assert_any_call(
@@ -98,6 +92,41 @@ class TestCreateExperimentBug(MockBugzillaMixin, TestCase):
         )
 
         del expected_call_data["assigned_to"]
+
+        self.mock_bugzilla_requests_post.assert_any_call(
+            settings.BUGZILLA_CREATE_URL, expected_call_data
+        )
+
+    def test_create_bugzilla_ticket_retries_with_no_cf_tracking(self):
+        experiment = ExperimentFactory.create_with_status(
+            Experiment.STATUS_DRAFT, name="An Experiment"
+        )
+        self.setUpMockBugzillaInvalidFirefoxVersion()
+
+        bugzilla_id = create_experiment_bug(experiment)
+
+        self.assertEqual(bugzilla_id, self.bugzilla_id)
+        self.assertEqual(self.mock_bugzilla_requests_post.call_count, 2)
+
+        expected_call_data = {
+            "product": "Shield",
+            "component": "Shield Study",
+            "version": "unspecified",
+            "summary": "[Experiment]: {experiment}".format(
+                experiment=experiment
+            ),
+            "description": experiment.BUGZILLA_OVERVIEW_TEMPLATE.format(
+                experiment=experiment
+            ),
+            "assigned_to": experiment.owner.email,
+            "cc": settings.BUGZILLA_CC_LIST,
+            "type": "task",
+            "priority": "P3",
+            "see_also": [12345],
+            "blocks": [12345],
+            "url": experiment.experiment_url,
+            "whiteboard": experiment.STATUS_REVIEW_LABEL,
+        }
 
         self.mock_bugzilla_requests_post.assert_any_call(
             settings.BUGZILLA_CREATE_URL, expected_call_data
