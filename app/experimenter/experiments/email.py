ff564ee4c4123b287de81935d27ccd911025dd26
Tif Tran <ttran@mozilla.com>
2019-06-28 09:06:46 -0700

    remove experiments email fixes #735 (#1400)

--- app/experimenter/experiments/email.py
+++ app/experimenter/experiments/email.py
@@ -1,29 +1,8 @@
-from urllib.parse import urljoin
-
 from django.conf import settings
-from django.core.mail import send_mail
 from django.core.mail.message import EmailMessage
 from django.template.loader import render_to_string
 
 from experimenter.experiments.models import Experiment
-
-
-def send_review_email(experiment_name, experiment_url, needs_attention):
-    attention = Experiment.ATTENTION_MESSAGE if needs_attention else ""
-
-    content = Experiment.REVIEW_EMAIL_TEMPLATE.format(
-        experiment_url=urljoin(
-            "https://{}".format(settings.HOSTNAME), experiment_url
-        ),
-        attention=attention,
-    )
-    send_mail(
-        Experiment.REVIEW_EMAIL_SUBJECT.format(name=experiment_name),
-        content,
-        settings.EMAIL_SENDER,
-        [settings.EMAIL_REVIEW],
-        fail_silently=False,
-    )
 
 
 def send_intent_to_ship_email(experiment_id):
