f9ea6c85f2ad3c618da862885d635985467d10ba
Alissa Sobo <alissasobo@gmail.com>
2019-07-15 12:53:25 -0700

    Remove periods from normandy slug, replace with dashes fixes #1448

--- app/experimenter/experiments/tests/test_forms.py
+++ app/experimenter/experiments/tests/test_forms.py
@@ -1524,7 +1524,7 @@ class TestExperimentStatusForm(
 
         self.assertEqual(
             experiment.normandy_slug,
-            "pref-experiment-slug-nightly-57.0-bug-12345",
+            "pref-experiment-slug-nightly-57-bug-12345",
         )
         self.mock_tasks_update_experiment_bug.delay.assert_called_with(
             self.user.id, experiment.id
