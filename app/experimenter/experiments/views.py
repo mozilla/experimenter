6a2db8818959c4edf39f5dfcb37bde15f2478a43
Alissa Sobo <alissasobo@gmail.com>
2019-08-02 11:44:53 -0700

    Support multiple normandy recipe ids fixes #1407 (#1501)
    * Support multiple normandy recipe ids fixes #1407
    
    * change modal error appearance, address other feedback

--- app/experimenter/experiments/views.py
+++ app/experimenter/experiments/views.py
@@ -525,14 +525,13 @@ class ExperimentNormandyUpdateView(ExperimentFormMixin, UpdateView):
         return response
 
     def form_invalid(self, form):
-        return redirect(
-            "{url}?normandy_id={value}".format(
-                url=reverse(
-                    "experiments-detail", kwargs={"slug": self.kwargs["slug"]}
-                ),
-                value=form.data["normandy_id"],
-            )
+        url = reverse(
+            "experiments-detail", kwargs={"slug": self.kwargs["slug"]}
         )
+        query_parameters = form.data.copy()
+        query_parameters.pop("csrfmiddlewaretoken", None)
+
+        return redirect(f"{url}?{query_parameters.urlencode()}")
 
 
 class ExperimentCommentCreateView(ExperimentFormMixin, CreateView):
