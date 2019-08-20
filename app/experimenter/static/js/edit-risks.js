1a3251b6b6a723725efa761277827f573d7de989
Alissa Sobo <alissasobo@gmail.com>
2019-08-20 08:43:06 -0700

    fix jquery import problem fixes #1561 (#1564)

---
+++ app/experimenter/static/js/edit-risks.js
@@ -0,0 +1,34 @@
+jQuery(function ($) {
+  function updateTechnicalRisk() {
+    // Show/hide the technical risk description field.
+
+    // From the Django point of view, the description is optional. But we make it
+    // mandatory when the technical risk radio is checked.
+    const hasTechnicalRisk = $('[type="radio"][name="risk_technical"][value="True"]:checked').length > 0;
+    $('#id_risk_technical_description').prop("required", hasTechnicalRisk);
+    // We hide the confusing "Optional" label manually here (#888)
+    $('#risks_technical_description_field .text-muted').hide();
+
+    if (hasTechnicalRisk) {
+      $('#risks_technical_description_field').show();
+    } else {
+      $('#risks_technical_description_field').hide();
+    }
+  }
+
+  function updateRisks() {
+    if ($('[type="radio"][value="True"]:checked').length > 0) {
+      $('#risks_field').show();
+    } else {
+      $('#risks_field').hide();
+    }
+  }
+
+  $('[type="radio"]').change(function (e) {
+    updateRisks();
+    updateTechnicalRisk();
+  });
+
+  updateRisks();
+  updateTechnicalRisk();
+});
