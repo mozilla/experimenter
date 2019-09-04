5784ceff782316a187637574eb5b60e6afcf9250
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-09-03 19:51:06 -0500

    Add 3 branch tests. (#1619)
    * Add 3 branch tests.
    
    * Updates.
    
    * Update conftest.

---
+++ tests/integration/pages/experiment_population.py
@@ -0,0 +1,146 @@
+"""Representaion of the Edit Population Page."""
+
+import random
+import string
+
+from selenium.webdriver.common.by import By
+from pypom import Region
+
+from pages.base import Base
+
+
+class PopulationPage(Base):
+
+    _add_branch_btn_locator = (By.CSS_SELECTOR, ".btn-success")
+    _branch_form_root_locator = (
+        By.CSS_SELECTOR,
+        "#formset > div:nth-child(5) > div",
+    )
+    _continue_btn_locator = (By.CSS_SELECTOR, "button.btn:nth-child(3)")
+    _firefox_channel_locator = (
+        By.CSS_SELECTOR,
+        "#id_firefox_channel > option:nth-child(2)",
+    )
+    _firefox_min_version_locator = (By.CSS_SELECTOR, "#id_firefox_min_version > option:nth-child(2)")
+    _firefox_population_percentage_locator = (By.CSS_SELECTOR, "#id_population_percent")
+    _firefox_pref_name_locator = (By.CSS_SELECTOR, "#id_pref_key")
+    _firefox_pref_type_locator = (
+        By.CSS_SELECTOR,
+        "#id_pref_type > option:nth-child(2)",
+    )
+    _firefox_pref_branch_locator = (
+        By.CSS_SELECTOR,
+        "#id_pref_branch > option:nth-child(2)",
+    )
+    _new_branch_locator = (
+        By.CSS_SELECTOR,
+        "#formset > div:nth-child(5) > div:nth-child(3) > div:nth-child(1) > h4:nth-child(1)",
+    )
+    _page_wait_locator = (
+        By.CSS_SELECTOR,
+        "div.form-group:nth-child(2) > h4:nth-child(1)",
+    )
+
+    def wait_for_page_to_load(self):
+        self.wait.until(
+            lambda _: "Population"
+            in self.find_element(*self._page_wait_locator).text
+        )
+        return self
+    
+    def create_new_branch(self):
+        """Creates a new branch."""
+        num_of_branches = len(self.current_branches) + 1
+        self.find_element(*self._add_branch_btn_locator).click()
+        els = self.find_elements(*self._branch_form_root_locator)
+        return self.BranchRegion(self, els[-1], count=num_of_branches)
+    
+    @property
+    def current_branches(self):
+        """Returns list of current branches."""
+        els = self.find_elements(*self._branch_form_root_locator)
+        branches =  [self.BranchRegion(self, root=el, count=count) for count, el in enumerate(els)]
+        for count, item in enumerate(branches):
+            if not item.is_displayed:
+                del branches[count]
+        return branches
+
+    def fill_firefox_population_percentage(self, text=50):
+        element = self.find_element(*self._firefox_population_percentage_locator)
+        element.send_keys(text)
+        return
+
+    def select_firefox_channel(self):
+        self.find_element(*self._firefox_channel_locator).click()
+        return
+
+    def select_firefox_min_version(self):
+        self.find_element(*self._firefox_min_version_locator).click()
+        return
+
+    def input_firefox_pref_name(self, text=None):
+        element = self.find_element(*self._firefox_pref_name_locator)
+        random_chars = "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=6)
+        )
+        element.send_keys(f"{text}-{random_chars}")
+        return
+
+    def select_firefox_pref_type(self):
+        self.find_element(*self._firefox_pref_type_locator).click()
+        return
+
+    def select_firefox_pref_branch(self):
+        self.find_element(*self._firefox_pref_branch_locator).click()
+        return
+
+    def click_continue(self):
+        self.find_element(*self._continue_btn_locator).click()
+        return
+
+    class BranchRegion(Region):
+
+        def __init__(self, page, root=None, count=None, **kwargs):  
+            super().__init__(
+                page=page, root=root, **kwargs
+            )
+            self.number = count
+
+        _remove_branch_btn_locator = (
+            By.CSS_SELECTOR,
+            "div > div > button",
+        )
+        _new_branch_locator = (By.CSS_SELECTOR, "div > div > h4")
+
+        @property
+        def is_displayed(self):
+            """Check if a branch is displayed."""
+            if self.root.get_attribute("data-formset-form-deleted") is not None:
+                return False
+            return True
+
+        def remove_branch(self):
+            self.find_element(*self._remove_branch_btn_locator).click()
+            return
+
+        @property
+        def branch_number(self):
+            return self.find_element(*self._new_branch_locator)
+
+        def set_branch_name(self, text=None):
+            locator = (By.ID, f"id_variants-{self.number}-name")
+            element = self.find_element(*locator)
+            element.send_keys(text)
+            return
+        
+        def set_branch_description(self, text=None):
+            locator = (By.ID, f"id_variants-{self.number}-description")
+            element = self.find_element(*locator)
+            element.send_keys(text)
+            return
+        
+        def set_branch_value(self, text=None):
+            locator = (By.ID, f"id_variants-{self.number}-value")
+            element = self.find_element(*locator)
+            element.send_keys(text)
+            return
