1e81968c2edf2a6a7fef9e27809765fa71f567ae
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-09-12 15:32:31 -0500

    Added tests for remaining form elements on overview page.

---
+++ tests/integration/test_overview.py
@@ -0,0 +1,100 @@
+import pytest
+
+from pages.base import Base
+from pages.home import Home
+from pages.experiment_design import DesignPage
+
+
+@pytest.mark.nondestructive
+def test_overview_type_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert "pref" in experiment.experiment_type
+    exp_type = "addon"
+    experiment.experiment_type = exp_type
+    assert exp_type in experiment.experiment_type
+
+
+@pytest.mark.nondestructive
+def test_overview_owner_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert home.header.current_user in experiment.experiment_owner
+    owner = "admin"
+    experiment.experiment_owner = owner
+    assert owner in experiment.experiment_owner
+
+
+@pytest.mark.nondestructive
+def test_overview_engineering_owner_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.engineering_owner is ""
+    new_owner = "uitester"
+    experiment.engineering_owner = new_owner
+    assert new_owner in experiment.engineering_owner
+
+
+@pytest.mark.nondestructive
+def test_public_name_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.public_name is ""
+    new_public_name = "uitested exp"
+    experiment.public_name = new_public_name
+    assert new_public_name in experiment.public_name
+
+
+@pytest.mark.nondestructive
+def test_public_description_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.public_description is ""
+    new_public_description = "THIS IS A LONG DESCRIPTION..!"
+    experiment.public_description = new_public_description
+    assert new_public_description in experiment.public_description
+
+
+@pytest.mark.nondestructive
+def test_feature_bugzilla_url_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.feature_bugzilla_url is ""
+    new_url = "http://bugzilla.org/1234-new-url"
+    experiment.feature_bugzilla_url = new_url
+    assert new_url in experiment.feature_bugzilla_url
+
+
+@pytest.mark.nondestructive
+def test_related_work_url_changes_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.related_work_urls is ""
+    new_url = "http://someawesomeurl.com"
+    experiment.related_work_urls = new_url
+    assert new_url in experiment.related_work_urls
+
+
+@pytest.mark.nondestructive
+def test_related_experiments_updates_correctly(base_url, selenium):
+    """"""
+    selenium.get(base_url)
+    home = Home(selenium, base_url).wait_for_page_to_load()
+    experiment = home.create_experiment()
+    assert experiment.related_experiments == "Nothing selected"
+    experiment.related_experiments = 1
+    assert "Nothing selected" not in experiment.related_work_urls
