7425e98ae0bb992eff39dca56f26c8675ab87bf4
Alissa Sobo <alissasobo@gmail.com>
2019-10-14 15:20:12 -0700

    fix errors showing in console when branchesDiv was null

--- app/experimenter/static/js/index.js
+++ app/experimenter/static/js/index.js
@@ -4,15 +4,16 @@ import bsSelect from "bootstrap-select";
 import popper from "popper.js";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import DesignForm from "./design-page";
-
-const branchesDiv = document.getElementById("react-branches-form");
-const slug = branchesDiv.dataset.experimentSlug;
-const expType = branchesDiv.dataset.experimentType
 
 window.jQuery = jQuery;
 window.$ = jQuery;
 
+const branchesDiv = document.getElementById("react-branches-form");
 
-ReactDOM.render(<DesignForm slug={slug} expType={expType}/>, branchesDiv);
+if (branchesDiv) {
+  const slug = branchesDiv.dataset.experimentSlug;
+  const expType = branchesDiv.dataset.experimentType;
+
+  ReactDOM.render(<DesignForm slug={slug} expType={expType}/>, branchesDiv);
+}
