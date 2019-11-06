584525f4b79e8f5f9811304e6005958ff182350a
R&D <rehandalal@users.noreply.github.com>
2019-11-06 18:14:04 -0500

    Removing branches no longer clears all data - Fixes #1882 (#1895)
    * addon and generic form changes
    
    * pref changes
    
    * addon generic changes
    
    * Major overhaul of components
    
    * Refactor API requests
    
    * Fix branch sorting
    
    * Use &nbsp; instead of {" "}

--- app/experimenter/static/js/components/DesignForm.js
+++ app/experimenter/static/js/components/DesignForm.js
@@ -1,192 +1,176 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  Row,
-  Col
-} from "react-bootstrap";
-import { boundMethod } from "autobind-decorator";
-import Serialize from "form-serialize";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { boundClass } from "autobind-decorator";
 
 import PrefForm from "experimenter/components/PrefForm";
 import GenericForm from "experimenter/components/GenericForm";
 import AddonForm from "experimenter/components/AddonForm";
-import BranchManager from "experimenter/components/BranchManager";
+import { makeApiRequest } from "experimenter/utils/api";
 
-export default class DesignForm extends React.Component {
+@boundClass
+export default class DesignForm extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      values: {},
+      data: {},
       errors: {},
       loaded: false
     };
   }
 
+  getEndpointUrl() {
+    return `experiments/${this.props.slug}/design-${this.props.expType}/`;
+  }
+
   async componentDidMount() {
-    const response = await this.makeFetchCall("GET");
-
-    const data = await response.json();
-
-    const controlBranchIndex = data.variants.findIndex(element => element.is_control)
-    const controlBranch = data.variants.splice(controlBranchIndex, 1)[0]
-    data.variants.unshift(controlBranch)
-
-    data.loaded = true;
-
-    return this.setState({values: data});
-  }
-
-  @boundMethod
-  addBranch(e) {
-    let stateCopy = { ...this.state.values };
-
-    stateCopy.variants.push({
-      ratio: null,
-      name: "",
-      description: "",
-      value: "",
-      is_control: false
+    const data = await makeApiRequest(this.getEndpointUrl());
+    this.setState({
+      loaded: true,
+      data
     });
-
-    this.setState(stateCopy);
   }
 
-  @boundMethod
-  removeBranch(e) {
-    this.state.values.variants.splice(e.target.dataset.index, 1);
-
-    this.setState({ variants: this.state.values.variants });
-  }
-
-  @boundMethod
-  handleVariantInputChange(e) {
-    let stateCopy = { ...this.state.values.variants };
-    let inputName = e.target.name;
-    stateCopy[e.target.dataset.index][inputName] = e.target.value;
-
-    this.setState(stateCopy);
-  }
-
-  @boundMethod
-  handleInputChange(e) {
-    let stateCopy = { ...this.state.values };
-    stateCopy[e.target.name] = e.target.value;
-
-    this.setState(stateCopy);
-  }
-
-  handleValidationErrors(json) {
-    this.setState({ errors: json });
-  }
-
-  async makeFetchCall(method, body) {
-    const url = `/api/v1/experiments/${this.props.slug}/design-${this.props.expType}/`;
-
-    return await fetch(url, {
-      method: method,
-      body: body,
-      headers: {
-        "Content-Type": "application/json"
+  addBranch() {
+    const { data } = this.state;
+    this.setState({
+      data: {
+        ...data,
+        variants: [
+          ...data.variants,
+          {
+            ratio: "",
+            name: "",
+            description: "",
+            value: "",
+            is_control: false,
+            id: null
+          }
+        ]
       }
     });
   }
 
-  @boundMethod
-  async handleSubmit(e, url) {
-    e.preventDefault();
-
-    const form = document.querySelector("#design-form");
-    let object = Serialize(form, { hash: true });
-
-    //remove undefined/deleted variants
-    object.variants = object.variants.filter(item=>item!=undefined);
-
-    const res = await this.makeFetchCall("PUT", JSON.stringify(object));
-
-    if (res.status == "200") {
-        location.replace(url);
-    }
-
-    const json = await res.json();
-    this.handleValidationErrors(json);
-    document.querySelector('.is-invalid').scrollIntoView();
+  removeBranch(index) {
+    const variants = [...this.state.data.variants];
+    variants.splice(index, 1);
+    this.setState({
+      data: {
+        ...this.state.data,
+        variants
+      }
+    });
   }
 
-  @boundMethod
-  handleSubmitSave(e){
-    this.handleSubmit(e,`/experiments/${this.props.slug}/`)
-
+  handleDataChange(key, value) {
+    this.setState({
+      data: {
+        ...this.state.data,
+        [key]: value
+      }
+    });
   }
 
-  @boundMethod
-  handleSubmitContinue(e){
-    this.handleSubmit(e,`/experiments/${this.props.slug}/edit-objectives/`)
+  async handleSubmit(event, redirectUrl) {
+    event.preventDefault();
 
+    const requestSave = makeApiRequest(this.getEndpointUrl(), {
+      method: "PUT",
+      data: this.state.data,
+    });
+
+    requestSave
+      .then(() => {
+        location.replace(redirectUrl);
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.data,
+        });
+
+        const invalid = document.querySelector(".is-invalid");
+        if (invalid) {
+         invalid.scrollIntoView();
+        }
+      });
+  }
+
+  handleSubmitSave(event) {
+    this.handleSubmit(event, `/experiments/${this.props.slug}/`);
+  }
+
+  handleSubmitContinue(event) {
+    this.handleSubmit(event, `/experiments/${this.props.slug}/edit-objectives/`);
   }
 
   render() {
-    const dataExists = this.state.values.loaded;
-    if (!dataExists) {
+    if (!this.state.loaded) {
       return (
         <Container>
           <div className="fa-5x">
             <Row className="justify-content-center">
-              <i className="fas fa-spinner fa-spin"></i>
+              <i className="fas fa-spinner fa-spin" />
             </Row>
           </div>
         </Container>
       );
-    } else {
-      return (
-        <div>
-          <Container>
-            <form onSubmit={this.handleSubmit} id="design-form">
-              {this.state.values.type == "pref" ? (
-                <PrefForm
-                  handleInputChange={this.handleInputChange}
-                  {...this.state}
-                ></PrefForm>
-              ) : (
-                ""
-              )}
-              {this.state.values.type == "addon" ? (
-                <AddonForm
-                  handleInputChange={this.handleInputChange}
-                  {...this.state}
-                ></AddonForm>
-              ) : (
-                ""
-              )}
-              {this.state.values.type == "generic" ? (
-                <GenericForm
-                  handleInputChange={this.handleInputChange}
-                  {...this.state}
-                ></GenericForm>
-              ) : (
-                ""
-              )}
-              <Row>
-                <Col className="text-right">
-                  <a
-                    className="mr-1 btn btn-default"
-                    href={`/experiments/${this.props.slug}/`}
-                  >
-                    <span className="fas fa-times"></span> Cancel Editing
-                  </a>
-                  <Button variant="primary" type="submit" className="mr-1" onClick={this.handleSubmitSave}>
-                    <span className="fas fa-save"/> Save Draft
-                  </Button>
-                  <Button id="save-continue" variant="primary" type="submit" onClick={this.handleSubmitContinue}>
-                    <span className="fas fa-save"/> Save Draft and Continue
-                  </Button>
-                </Col>
-              </Row>
-            </form>
-          </Container>
-        </div>
-      );
     }
+
+    // Select the appropriate form component to use based on experiment type
+    let Form;
+    switch (this.state.data.type) {
+      case "pref":
+        Form = PrefForm;
+        break;
+      case "addon":
+        Form = AddonForm;
+      default:
+        Form = GenericForm;
+    }
+
+    return (
+      <div>
+        <Container>
+          <form onSubmit={this.handleSubmit} id="design-form">
+            <Form
+              handleDataChange={this.handleDataChange}
+              onAddBranch={this.addBranch}
+              onRemoveBranch={this.removeBranch}
+              data={this.state.data}
+              errors={this.state.errors}
+              loaded={this.state.loaded}
+            />
+            <Row>
+              <Col className="text-right">
+                <a
+                  className="mr-1 btn btn-default"
+                  href={`/experiments/${this.props.slug}/`}
+                >
+                  <span className="fas fa-times" /> Cancel Editing
+                </a>
+
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="mr-1"
+                  onClick={this.handleSubmitSave}
+                >
+                  <span className="fas fa-save" /> Save Draft
+                </Button>
+
+                <Button
+                  id="save-continue"
+                  variant="primary"
+                  type="submit"
+                  onClick={this.handleSubmitContinue}
+                >
+                  <span className="fas fa-save" /> Save Draft and Continue
+                </Button>
+              </Col>
+            </Row>
+          </form>
+        </Container>
+      </div>
+    );
   }
 }
