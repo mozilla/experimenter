c12de0dcf853d19dcf6b6138795b67a7bed5d7e6
Tif Tran <ttran@mozilla.com>
2019-11-07 09:08:55 -0800

    handle empty string results filtering case fixes #1831 (#1885)
    * handle empty string results filtering case fixes #1831
    
    * modified results exclusion filter
    
    * removed extra parens

--- app/experimenter/experiments/filtersets.py
+++ app/experimenter/experiments/filtersets.py
@@ -265,7 +265,9 @@ class ExperimentFilterset(filters.FilterSet):
     def completed_results_filter(self, queryset, name, value):
         if value:
             return queryset.exclude(
-                results_url=None, results_initial=None, results_lessons_learned=None
+                Q(results_url="") | Q(results_url=None),
+                Q(results_initial="") | Q(results_initial=None),
+                Q(results_lessons_learned="") | Q(results_lessons_learned=None),
             )
         return queryset
 
