10df98b21ca8408099c1c4e91e4aa7cc8dc66ebb
Alissa Sobo <alissasobo@gmail.com>
2019-12-27 14:42:03 -0800

    Add react tests for branched addons fixes #2081

--- app/experimenter/static/js/tests/DataFactory.js
+++ app/experimenter/static/js/tests/DataFactory.js
@@ -55,6 +55,38 @@ export class AddonDataFactory extends GenericDataFactory {
       addon_release_url: new Field(faker.internet.url),
       variants: [],
       type: "addon",
+export class BranchedAddonDataFactory extends Factory {
+  getFields() {
+    return {
+      is_branched_addon: true,
+      variants: [],
+    };
+  }
+
+  postGeneration() {
+    const { generateVariants } = this.options;
+    if (generateVariants) {
+      const variants = [];
+      for (let i = 0; i < generateVariants; i++) {
+        variants.push(BranchedAddonVariantFactory.build());
+      }
+      this.data.variants = [...this.data.variants, ...variants];
+    }
+    if (this.data.variants.length) {
+      this.data.variants[0].is_control = true;
+    }
+  }
+}
+
+export class BranchedAddonVariantFactory extends Factory {
+  getFields() {
+    return {
+      id: new AutoIncrementField(),
+      description: new Field(faker.lorem.sentence),
+      name: new Field(faker.lorem.word),
+      ratio: new Field(faker.random.number, { min: 1, max: 100 }),
+      is_control: false,
+      addon_release_url: new Field(faker.internet.url),
     };
   }
 }
