ad1a9f64f23b40d0c45b9aefa0b7b9c184bd2fac
Alissa Sobo <asobo@mozilla.com>
2020-01-14 13:39:43 -0800

    Add react tests for multi pref experiments fixes #2083  (#2116)
    * Add js tests for sing pref form fixes #2082
    
    * Refactor dataFactory
    
    * Revert "Refactor dataFactory"
    
    This reverts commit a72bdcbfd288aa950c6933d5bc4385625b258065.
    
    * remove super from postgeneration
    
    * Fix title of test
    
    * Refactor dataFactory
    
    * Revert "Refactor dataFactory"
    
    This reverts commit a72bdcbfd288aa950c6933d5bc4385625b258065.
    
    * wip
    
    * Add react tests for multi pref experiments fixes #2083
    
    * Add tests for add/remove branch
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>

--- app/experimenter/static/js/tests/PrefForm.test.js
+++ app/experimenter/static/js/tests/PrefForm.test.js
@@ -8,11 +8,13 @@ import {
 import "@testing-library/jest-dom/extend-expect";
 import DesignForm from "experimenter/components/DesignForm";
 import * as Api from "experimenter/utils/api";
-import { PrefDataFactory } from "./DataFactory";
+import { PrefDataFactory, MultiPrefDataFactory } from "./DataFactory";
 import {
   addBranch,
   removeBranch,
   waitForFormToLoad,
+  addPrefBranch,
+  removePrefBranch,
 } from "experimenter/tests/helpers.js";
 
 describe("The `DesignForm` component for Pref Experiments", () => {
@@ -222,5 +224,162 @@ describe("The `DesignForm` component for Pref Experiments", () => {
     expect(getAllByLabelText(/Pref Type/)).toHaveLength(2);
     expect(getAllByLabelText(/Pref Branch/)).toHaveLength(2);
     expect(getAllByLabelText(/Pref Value/)).toHaveLength(2);
+  });
+});
+
+describe("The `DesignForm` component for MultiPrefs", () => {
+  afterEach(() => {
+    Api.makeApiRequest.mockClear();
+    cleanup();
+  });
+
+  const setup = () => {
+    const apiResponse = MultiPrefDataFactory.build({}, { generateVariants: 2 });
+    jest
+      .spyOn(Api, "makeApiRequest")
+      .mockImplementation(async () => apiResponse);
+
+    return apiResponse;
+  };
+
+  const rejectedSetUp = () => {
+    const apiResponse = MultiPrefDataFactory.build({}, { generateVariants: 2 });
+
+    const rejectApiResponse = {
+      data: {
+        variants: [
+          {
+            preferences: [{ pref_name: ["This field may not be blank."] }, {}],
+          },
+          {},
+        ],
+      },
+    };
+
+    jest
+      .spyOn(Api, "makeApiRequest")
+      .mockReturnValueOnce(apiResponse)
+      .mockRejectedValueOnce(rejectApiResponse);
+  };
+
+  it("displays and edits data about multi pref experiments", async () => {
+    const apiResponse = setup();
+
+    const { getByText, getAllByLabelText, container } = await render(
+      <DesignForm slug="the-slug" experimentType={"pref"} isMultiPref={true} />,
+    );
+
+    expect(Api.makeApiRequest).toHaveBeenCalledTimes(1);
+
+    await waitForFormToLoad(container);
+
+    const prefNameInputs = getAllByLabelText(/Pref Name/);
+
+    // check that two pref branches per variant are showing
+    expect(prefNameInputs).toHaveLength(4);
+    expect(prefNameInputs[0].value).toBe(
+      apiResponse.variants[0].preferences[0].pref_name,
+    );
+
+    // Edit some fields
+    location.replace = () => {};
+    fireEvent.change(prefNameInputs[0], {
+      target: { value: "the-new-pref-name" },
+    });
+
+    fireEvent.click(getByText("Save Draft and Continue"));
+
+    // Check that the correct data was sent to the server
+    expect(Api.makeApiRequest).toHaveBeenCalledTimes(2);
+    const [url, { data }] = Api.makeApiRequest.mock.calls[1];
+    expect(url).toBe("experiments/the-slug/design-multi-pref/");
+    expect(data.variants[0].preferences[0].pref_name).toBe("the-new-pref-name");
+  });
+
+  it("displays errors on pref branches", async () => {
+    rejectedSetUp();
+
+    let scrollIntoViewMock = jest.fn();
+    window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
+
+    const { getAllByLabelText, getByText, container } = await render(
+      <DesignForm slug="the-slug" experimentType={"pref"} isMultiPref={true} />,
+    );
+
+    await waitForFormToLoad(container);
+
+    expect(Api.makeApiRequest).toHaveBeenCalledTimes(1);
+
+    const firstPrefBranchPrefNameInput = getAllByLabelText(/Pref Name/)[0];
+
+    fireEvent.change(firstPrefBranchPrefNameInput, { target: { value: "" } });
+
+    fireEvent.click(getByText("Save Draft and Continue"));
+
+    expect(Api.makeApiRequest).toHaveBeenCalled();
+
+    await waitForDomChange(firstPrefBranchPrefNameInput);
+
+    expect(getByText("This field may not be blank.")).toBeInTheDocument();
+  });
+
+  it("adds a pref", async () => {
+    setup();
+    const { getAllByLabelText, container } = await render(
+      <DesignForm slug="the-slug" experimentType={"pref"} isMultiPref={true} />,
+    );
+
+    await waitForFormToLoad(container);
+
+    addPrefBranch(container, 0);
+
+    expect(getAllByLabelText(/Pref Name/)).toHaveLength(5);
+  });
+
+  it("removes a pref", async () => {
+    const apiResponse = setup();
+    const { queryByText, container } = await render(
+      <DesignForm slug="the-slug" experimentType={"pref"} isMultiPref={true} />,
+    );
+
+    await waitForFormToLoad(container);
+
+    removePrefBranch(container, 0, 1);
+
+    expect(
+      queryByText(apiResponse.variants[0].preferences[1].pref_name),
+    ).not.toBeInTheDocument();
+  });
+
+  it("adds a branch", async () => {
+    setup();
+
+    const { getAllByText, container } = await render(
+      <DesignForm experimentType={"pref"} />,
+    );
+
+    await waitForFormToLoad(container);
+
+    addBranch(container);
+
+    expect(getAllByText("Branch Size")).toHaveLength(3);
+    expect(getAllByText("Name")).toHaveLength(3);
+    expect(getAllByText("Description")).toHaveLength(3);
+  });
+
+  it("removes a branch", async () => {
+    setup();
+
+    const { getAllByText, container } = await render(
+      <DesignForm experimentType={"pref"} />,
+    );
+
+    await waitForFormToLoad(container);
+
+    removeBranch(container, 0);
+
+    expect(getAllByText("Branch Size")).toHaveLength(1);
+    expect(getAllByText("Name")).toHaveLength(1);
+    expect(getAllByText("Description")).toHaveLength(1);
   });
 });
