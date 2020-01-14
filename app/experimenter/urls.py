3c1b269b6529c5e5bcb17a07a5602d4d7b04387f
tiftran <ttran@mozilla.com>
2020-01-13 19:40:59 -0800

    added swagger ui api docs fixes #1625

--- app/experimenter/urls.py
+++ app/experimenter/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls import re_path, include
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 from experimenter.experiments.views import ExperimentListView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -11,6 +13,21 @@ urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^experiments/", include("experimenter.experiments.web_urls")),
     re_path(r"^$", ExperimentListView.as_view(), name="home"),
+    re_path(
+        r"^openapi",
+        get_schema_view(
+            title="Experimenter", description="Experimenter API", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
+    re_path(
+        r"^docs/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
 ]
 
 if settings.DEBUG:
