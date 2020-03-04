6a3dbc232bfce98e96f4a0e2792a64403f8be3cd
Tif Tran <ttran@mozilla.com>
2020-03-04 13:58:28 -0800

    separate public and private apis fixes #2250 (#2353)
    * separate public and private apis fixes #2250
    
    * removed unused imports
    
    * added missing slash

--- app/experimenter/static/js/utils/api.js
+++ app/experimenter/static/js/utils/api.js
@@ -1,6 +1,6 @@
 import { request } from "experimenter/utils/request.js";
 
-const API_ROOT = "/api/v1/";
+const API_ROOT = "/api/v2/";
 
 export function makeApiRequest(endpoint, options = {}, extraHeaders = {}) {
   return request(`${API_ROOT}${endpoint}`, options, extraHeaders);
