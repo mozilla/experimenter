4cd9f887bc160043f082f05c56e42b540dc0227f
Kareem Golaub <kareemag@live.com>
2020-03-13 15:25:06 -0400

    Hide date input form group if dates unrestricted fixes #2300 (#2381)

--- app/experimenter/static/js/scripts/experiment-date-filter.js
+++ app/experimenter/static/js/scripts/experiment-date-filter.js
@@ -1,15 +1,12 @@
 let dateRangeSelector = document.getElementById('id_experiment_date_field');
 let rangeElementInput0 = document.getElementById("id_date_range_0");
-let rangeElementInput1 = document.getElementById("id_date_range_1");
-
+let rangeElementGroup = rangeElementInput0.closest(".form-group");
 
 function setUpDateRange () {
   if (dateRangeSelector.value == '') {
-    rangeElementInput0.setAttribute("disabled", "")
-    rangeElementInput1.setAttribute("disabled", "")
+    rangeElementGroup.setAttribute("hidden", "");
   } else {
-    rangeElementInput0.removeAttribute("disabled");
-    rangeElementInput1.removeAttribute("disabled");
+    rangeElementGroup.removeAttribute("hidden");
   }
 }
 
