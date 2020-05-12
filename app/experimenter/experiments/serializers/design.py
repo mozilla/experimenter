16cd9f5de022fc403b4065007baf8f4c717a1a83
Alissa Sobo <alissasobo@gmail.com>
2020-05-12 15:27:51 -0700

    put error message on all fields

--- app/experimenter/experiments/serializers/design.py
+++ app/experimenter/experiments/serializers/design.py
@@ -171,16 +171,17 @@ class ExperimentDesignBranchMultiPrefSerializer(
 
     def validate_preferences(self, data):
         variant = None
-        error_list = [{}] * len(data)
-        for i, pref in enumerate(data):
-            id = pref.get('id')
+        error_list = [{"pref_name": "You cannot swap pref names."}] * len(data)
+        for pref in data:
+            id = pref.get("id")
             if id and not variant:
                 variant = VariantPreferences.objects.get(id=id).variant
-            if (variant and variant.preferences
-                    .filter(pref_name=pref["pref_name"])
-                    .exclude(id=pref.get('id'))
-                    .exists()):
-                error_list[i] = {"pref_name": "You cannot swap pref names."}
+            if (
+                variant
+                and variant.preferences.filter(pref_name=pref["pref_name"])
+                .exclude(id=pref.get("id"))
+                .exists()
+            ):
                 raise serializers.ValidationError(error_list)
 
         if not self.is_pref_valid(data):
