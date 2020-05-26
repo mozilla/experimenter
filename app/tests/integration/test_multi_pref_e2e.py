e2bd851195506805de56326919292af87a8b756d
jrbenny35 <bennyjr169@gmail.com>
2020-05-26 10:48:51 -0500

    Updates to conftest and fixtures for e2e integration test.

--- app/tests/integration/test_multi_pref_e2e.py
+++ app/tests/integration/test_multi_pref_e2e.py
@@ -1,59 +1,61 @@
-import json
 from urllib.parse import urlparse
 
 import pytest
 import requests
 
-experiment_type = "multi-pref-experiment"
+
+@pytest.fixture(name="experiment_type", scope="module")
+def fixture_experiment_type():
+    return "multi-pref-experiment"
+
+
+@pytest.fixture(name="experiment_name", scope="module")
+def fixture_experiment_name():
+    return "Pref-Flip Experiment"
 
 
 @pytest.mark.use_variables
-@pytest.mark.multi_prefs
 @pytest.mark.nondestructive
 def test_multi_pref_e2e(
     base_url,
     selenium,
+    experiment_type,
     fill_timeline_page,
     fill_design_page_multi_prefs,
     fill_analysis_page,
     fill_risks_page,
     signoff_and_ship,
+    variables,
 ):
     url = urlparse(selenium.current_url)
     experiment_url = f"{url.scheme}://{url.netloc}/api/v1{url.path}recipe/"
     experiment_json = requests.get(f"{experiment_url}", verify=False).json()
-    with open("e2e_test_variables.json", "r") as j:
-        test_json = json.load(j)
-    assert test_json["multi-pref-experiment"]["name"] in experiment_json["name"]
+    assert variables[experiment_type]["name"] in experiment_json["name"]
+    assert variables[experiment_type]["action_name"] == experiment_json["action_name"]
     assert (
-        test_json["multi-pref-experiment"]["action_name"]
-        == experiment_json["action_name"]
+        variables[experiment_type]["type"] == experiment_json["filter_object"][1]["type"]
     )
     assert (
-        test_json["multi-pref-experiment"]["type"]
-        == experiment_json["filter_object"][1]["type"]
-    )
-    assert (
-        test_json["multi-pref-experiment"]["channels"].lower()
+        variables[experiment_type]["channels"].lower()
         == experiment_json["filter_object"][1]["channels"][0]
     )
     assert (
-        test_json["multi-pref-experiment"]["min_version"]
+        variables[experiment_type]["min_version"]
         == f"{[item for item in experiment_json['filter_object'][2]['versions']][0]}.0"
     )
     assert (
-        test_json["multi-pref-experiment"]["max_version"]
+        variables[experiment_type]["max_version"]
         == f"{[item for item in experiment_json['filter_object'][2]['versions']][-1]}.0"
     )
     assert (
-        test_json["multi-pref-experiment"]["userFacingName"]
+        variables[experiment_type]["userFacingName"]
         == experiment_json["arguments"]["userFacingName"]
     )
     assert (
-        test_json["multi-pref-experiment"]["userFacingDescription"]
+        variables[experiment_type]["userFacingDescription"]
         == experiment_json["arguments"]["userFacingDescription"]
     )
-    assert len(test_json["multi-pref-experiment"]["branches"]) == len(
+    assert len(variables[experiment_type]["branches"]) == len(
         experiment_json["arguments"]["branches"]
     )
     for item in experiment_json["arguments"][
@@ -63,31 +65,30 @@ def test_multi_pref_e2e(
             len(experiment_json["arguments"]["branches"])
         ):  # Check each branch so we need to do the check for as many branches exis
             if (
-                item["slug"]
-                == test_json["multi-pref-experiment"]["branches"][num]["branch_name"]
+                item["slug"] == variables[experiment_type]["branches"][num]["branch_name"]
             ):  # Start by checking the name
                 for pref_num in range(len(item["preferences"])):
                     assert (
-                        test_json["multi-pref-experiment"]["branches"][num][
-                            "preferences"
-                        ][pref_num]["firefox_pref_name"]
+                        variables[experiment_type]["branches"][num]["preferences"][
+                            pref_num
+                        ]["firefox_pref_name"]
                         in f"{[key for key in item['preferences']]}"
                     )
                     assert (
-                        test_json["multi-pref-experiment"]["branches"][num][
-                            "preferences"
-                        ][pref_num]["firefox_pref_type"].replace("json ", "")
+                        variables[experiment_type]["branches"][num]["preferences"][
+                            pref_num
+                        ]["firefox_pref_type"].replace("json ", "")
                         in f"{[value['preferenceType'] for value in item['preferences'].values()]}"  # noqa: E501
                     )
                     assert (
-                        test_json["multi-pref-experiment"]["branches"][num][
-                            "preferences"
-                        ][pref_num]["firefox_pref_branch"]
+                        variables[experiment_type]["branches"][num]["preferences"][
+                            pref_num
+                        ]["firefox_pref_branch"]
                         in f"{[value['preferenceBranchType'] for value in item['preferences'].values()]}"  # noqa: E501
                     )
                     assert (
-                        test_json["multi-pref-experiment"]["branches"][num][
-                            "preferences"
-                        ][pref_num]["firefox_pref_value"]
+                        variables[experiment_type]["branches"][num]["preferences"][
+                            pref_num
+                        ]["firefox_pref_value"]
                         in f"{[value['preferenceValue'] for value in item['preferences'].values()]}".lower()  # noqa: E501
                     )
