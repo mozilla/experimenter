a3ba7945dd5bf21257dbbb9b576f467869bd9aef
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-05-26 21:36:37 -0400

    Add rapid experiment base template fixes #2685 (#2692)

--- app/experimenter/base/context_processors.py
+++ app/experimenter/base/context_processors.py
@@ -5,3 +5,7 @@ def google_analytics(request):
     """Context processor bits you need related to injecting Google Analytics
     in the rendered templates."""
     return {"USE_GOOGLE_ANALYTICS": settings.USE_GOOGLE_ANALYTICS}
+
+
+def features(request):
+    return {"FEATURE_MESSAGE_TYPE": settings.FEATURE_MESSAGE_TYPE}
