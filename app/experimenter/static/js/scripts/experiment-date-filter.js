ab1a82fbad6865c409771100457b0575fc6435e5
Tif Tran <ttran@mozilla.com>
2020-06-01 14:10:36 -0700

    handle case where experiment_date_field is not selected fixes #2704 (#2705)
    * handle case where experiment_date_field is not selected fixes #2704
    
    * remove newline

--- app/experimenter/static/js/scripts/experiment-date-filter.js
+++ app/experimenter/static/js/scripts/experiment-date-filter.js
@@ -1,16 +1,18 @@
-let dateRangeSelector = document.getElementById('id_experiment_date_field');
+let dateRangeSelector = document.getElementById("id_experiment_date_field");
 let rangeElementInput0 = document.getElementById("id_date_range_0");
+let rangeElementInput1 = document.getElementById("id_date_range_1");
 let rangeElementGroup = rangeElementInput0.closest(".form-group");
 
-function setUpDateRange () {
-  if (dateRangeSelector.value == '') {
+function setUpDateRange() {
+  if (dateRangeSelector.value == "") {
     rangeElementGroup.setAttribute("hidden", "");
+    rangeElementInput0.value = "";
+    rangeElementInput1.value = "";
   } else {
     rangeElementGroup.removeAttribute("hidden");
   }
 }
 
 setUpDateRange();
-
 
 dateRangeSelector.addEventListener("change", setUpDateRange);
