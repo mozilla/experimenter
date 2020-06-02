dbc4a399752fe6ddfa54bf94efb03bb7f7eade5e
Rehan Dalal <rehandalal@gmail.com>
2020-06-01 20:37:04 -0400

    Add skeleton

---
+++ app/experimenter/static/rapid/jest.config.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  globals: {},
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^experimenter-rapid/(.*)$": "<rootDir>/$1",
+    "\\.(less|css)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest-env.setup.js"],
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/**/*.{ts,tsx}", "!<rootDir>/index.tsx"],
+  coverageReporters: ["text"],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+    },
+  },
+};
