7917cf4398501c713436bcc0f04ef2518491f009
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-06-05 12:41:01 -0400

    Explicitly mixin viewset instead of explicitly declaring routes

--- app/experimenter/experiments/api/v3/urls.py
+++ app/experimenter/experiments/api/v3/urls.py
@@ -1,30 +1,8 @@
-from rest_framework.routers import SimpleRouter, Route
+from rest_framework.routers import SimpleRouter
 
 from experimenter.experiments.api.v3.views import ExperimentRapidViewSet
 
 
-class ExperimentRapidRouter(SimpleRouter):
-    routes = [
-        # List route.
-        Route(
-            url=r"^{prefix}{trailing_slash}$",
-            mapping={"post": "create"},
-            name="{basename}-list",
-            detail=False,
-            initkwargs={"suffix": "List"},
-        ),
-        # Detail route.
-        Route(
-            url=r"^{prefix}/{lookup}{trailing_slash}$",
-            mapping={"get": "retrieve", "put": "update"},
-            name="{basename}-detail",
-            detail=True,
-            initkwargs={"suffix": "Instance"},
-        ),
-    ]
-
-
-router = ExperimentRapidRouter()
+router = SimpleRouter()
 router.register(r"experiments", ExperimentRapidViewSet, "experiments-rapid")
-
 urlpatterns = router.urls
--- app/experimenter/experiments/api/v3/views.py
+++ app/experimenter/experiments/api/v3/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from experimenter.experiments.models import Experiment
 from experimenter.experiments.api.v3.serializers import ExperimentRapidSerializer
 
 
-class ExperimentRapidViewSet(viewsets.ModelViewSet):
+class ExperimentRapidViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     serializer_class = ExperimentRapidSerializer
     queryset = Experiment.objects.filter(type=Experiment.TYPE_RAPID)
     lookup_field = "slug"
