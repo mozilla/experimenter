a8ba60ca57cdb44fd5f82ccf1bfb6677b694d46c
Benjamin Forehand Jr <bennyjr169@gmail.com>
2020-06-16 11:39:56 -0500

    Switch to using dataclass instead of JSON for integration tests. (#2829)
    * Switch to using dataclasses instead of JSON for integration tests.
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
    Co-authored-by: Raphael Pierzina <raphael@hackebrot.de>

---
+++ app/tests/integration/models/validation_dataclass.py
@@ -0,0 +1,36 @@
+from typing import List, Any, Optional, Dict
+from pydantic import BaseModel, Field
+
+
+class FilterObjectDataClass(BaseModel):
+    type: Optional[str]
+    channels: Optional[List]
+    versions: Optional[List]
+
+
+class PreferencesDataClass(BaseModel):
+    preferenceBranchType: str
+    preferenceType: str
+    preferenceValue: Any
+
+
+class BranchDataClass(BaseModel):
+    slug: str
+    ratio: Optional[str]
+    addon_url: Optional[str] = None
+    preferences: Optional[Dict[Optional[str], PreferencesDataClass]] = Field(default=None)
+
+
+class ArgumentsDataClass(BaseModel):
+    userFacingName: str
+    userFacingDescription: str
+    branches: List[BranchDataClass]
+
+
+class APIDataClass(BaseModel):
+    action_name: str
+    name: str
+    comment: Optional[str]
+    filter_object: List[FilterObjectDataClass]
+    arguments: ArgumentsDataClass
+    experimenter_slug: Optional[str]
