bbfce5cb68456e09c5f17967152e44c5c5dafb52
Kate Hudson <k88hudson@gmail.com>
2020-06-25 14:03:43 -0400

    Add x-select component for selects with descriptions (#2888)

---
+++ app/experimenter/static/rapid/components/forms/XSelect.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Select, {
+  components,
+  Props,
+  OptionTypeBase,
+  OptionProps,
+  Styles,
+  ValueType,
+  ActionMeta,
+} from "react-select";
+
+export interface XSelectOption extends OptionTypeBase {
+  /** Main label for the option */
+  label: string;
+  /** Optional description that shows up as small text below the label */
+  description?: string;
+  /** Value (id) of the option */
+  value: string;
+}
+
+/**
+ * Custom component for each option display that shows an optional small description
+ * below the label if it is defined.
+ */
+const OptionWithDescription = <T,>(
+  props: OptionProps<T>,
+): ReturnType<React.FC> => {
+  const { children, data } = props;
+  return (
+    <div>
+      <components.Option {...props}>
+        {children}
+        {data.description && (
+          <p style={{ margin: 0 }}>
+            <small style={{ opacity: 0.5, fontSize: "95%" }}>
+              {data.description}
+            </small>
+          </p>
+        )}
+      </components.Option>
+    </div>
+  );
+};
+
+// See https://react-select.com/styles
+const customStyles: Partial<Styles> = {
+  container: (s) => ({
+    ...s,
+    lineHeight: "1.3",
+  }),
+};
+
+interface XSelectCustomProps<OptionType> {
+  /**
+   * This is an alternative to onChange that is called with array of options as the first parameter.
+   * Single selects will return a single item, multis will return the array of options
+   */
+  onOptionChange?: (
+    option: Array<OptionType>,
+    action: ActionMeta<XSelectOption>,
+  ) => void;
+}
+
+// To debug the menu dropdown, add menuIsOpen={true}
+export function XSelect<OptionType extends XSelectOption = XSelectOption>(
+  props: Props<OptionType> & XSelectCustomProps<OptionType>,
+): ReturnType<React.FC> {
+  const renderProps: Props<OptionType> = { ...props };
+  if (props.onOptionChange) {
+    renderProps.onChange = (
+      value: ValueType<XSelectOption>,
+      action: ActionMeta<XSelectOption>,
+    ) => {
+      const options: Array<OptionType> = Array.isArray(value)
+        ? value
+        : value
+        ? [value]
+        : [];
+      if (props.onOptionChange) {
+        props.onOptionChange(options, action);
+      }
+    };
+  }
+
+  return (
+    <Select
+      components={{ Option: OptionWithDescription }}
+      styles={customStyles}
+      {...renderProps}
+    />
+  );
+}
