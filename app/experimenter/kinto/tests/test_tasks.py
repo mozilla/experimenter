a0a7858a1f5d147f874b1cd6b869a75e7c1743ca
Tif Tran <ttran@mozilla.com>
2020-06-29 09:36:30 -0700

    recipe serializer for rapid experiments fixes #2688 (#2893)
    * recipe serializer for rapid experiments fixes #2688
    
    * feedback changes
    
    * json schema
    
    * feedback changes
    
    * moved rapid serializers to api/v1 and feedback changes
    
    * removed maxDiff
    
    * fix imports
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>

--- app/experimenter/kinto/tests/test_tasks.py
+++ app/experimenter/kinto/tests/test_tasks.py
@@ -1,3 +1,4 @@
+import datetime
 from django.conf import settings
 from django.test import TestCase
 
@@ -5,18 +6,23 @@ from experimenter.experiments.models import Experiment
 from experimenter.experiments.tests.factories import ExperimentFactory
 from experimenter.kinto.tests.mixins import MockKintoClientMixin
 from experimenter.kinto import tasks
+from experimenter.experiments.api.v1.serializers import ExperimentRapidRecipeSerializer
 
 
 class TestPushExperimentToKintoTask(MockKintoClientMixin, TestCase):
     def setUp(self):
         super().setUp()
-        self.experiment = ExperimentFactory.create_with_status(Experiment.STATUS_DRAFT)
+        self.experiment = ExperimentFactory.create_with_status(
+            Experiment.STATUS_DRAFT, proposed_start_date=datetime.date(2020, 1, 20)
+        )
 
     def test_push_experiment_to_kinto_sends_experiment_data(self):
         tasks.push_experiment_to_kinto(self.experiment.id)
 
+        data = ExperimentRapidRecipeSerializer(self.experiment).data
+
         self.mock_kinto_client.create_record.assert_called_with(
-            data={"slug": self.experiment.slug},
+            data=data,
             collection=settings.KINTO_COLLECTION,
             bucket=settings.KINTO_BUCKET,
             if_not_exists=True,
