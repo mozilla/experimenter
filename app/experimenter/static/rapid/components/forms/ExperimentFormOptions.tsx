52ad9caaf6fd0c31e84c94880be5cc08935285d7
tiftran <ttran@mozilla.com>
2020-07-02 21:24:23 -0700

    added untrack files

---
+++ app/experimenter/static/rapid/components/forms/ExperimentFormOptions.tsx
@@ -0,0 +1,9 @@
+export const audienceOptions = [
+  { label: "AUDIENCE 1", value: "AUDIENCE 1" },
+  { label: "AUDIENCE 2", value: "AUDIENCE 2" },
+];
+
+export const featureOptions = [
+  { label: "FEATURE 1", value: "FEATURE 1" },
+  { label: "FEATURE 2", value: "FEATURE 2" },
+];
