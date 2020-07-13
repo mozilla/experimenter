672f44bd0a6ca58db0f116339efec1b9457c5d72
tiftran <ttran@mozilla.com>
2020-07-13 12:19:05 -0700

    tweak

--- app/experimenter/static/rapid/components/experiments/ExperimentDetails.tsx
+++ app/experimenter/static/rapid/components/experiments/ExperimentDetails.tsx
@@ -52,8 +52,7 @@ const ExperimentDetails: React.FC = () => {
   if (data.bugzilla_url) {
     bugzilla_url = (
       <div className="my-2">
-        Bugzilla ticket can be found{" "}
-        <a href={data.bugzilla_url ? data.bugzilla_url : "#"}>here</a>.
+        Bugzilla ticket can be found <a href={data.bugzilla_url}>here</a>.
       </div>
     );
   }
