80e06f9937c3c392172152246da4c75edf5dc769
Tif Tran <ttran@mozilla.com>
2020-07-14 13:15:47 -0700

    move api calls into actions fixes #2957 (#3041)
    * move api calls into actions fixes #2957
    
    * remove commented out code
    
    * types
    
    * typing
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>

--- app/experimenter/static/rapid/contexts/experiment/provider.tsx
+++ app/experimenter/static/rapid/contexts/experiment/provider.tsx
@@ -1,44 +1,28 @@
 import React from "react";
 import { useParams } from "react-router";
 
+import { fetchExperiment } from "experimenter-rapid/contexts/experiment/actions";
 import context, {
   INITIAL_CONTEXT,
 } from "experimenter-rapid/contexts/experiment/context";
 import reducer from "experimenter-rapid/contexts/experiment/reducer";
-import {
-  ExperimentData,
-  ExperimentReducerActionType,
-} from "experimenter-types/experiment";
+import { Action, ExperimentData } from "experimenter-types/experiment";
 
 const ExperimentProvider: React.FC<{ initialState?: ExperimentData }> = ({
   children,
   initialState = INITIAL_CONTEXT.state,
 }) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [state, reducerDispatch] = React.useReducer(reducer, initialState);
   const { experimentSlug } = useParams();
   const { Provider } = context;
 
+  const dispatch = (action: Action) => action(state, reducerDispatch);
   React.useEffect(() => {
     if (!experimentSlug) {
       return;
     }
 
-    const fetchData = async () => {
-      const response = await fetch(`/api/v3/experiments/${experimentSlug}/`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      const data = await response.json();
-      dispatch({
-        type: ExperimentReducerActionType.UPDATE_STATE,
-        state: data,
-      });
-    };
-
-    fetchData();
+    dispatch(fetchExperiment(experimentSlug));
   }, [experimentSlug]);
 
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
