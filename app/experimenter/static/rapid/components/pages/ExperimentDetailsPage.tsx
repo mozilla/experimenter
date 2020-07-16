9571763072fcefd9999d317f33092eec955c6ba2
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-07-16 14:08:53 -0400

    Use real experiment status in rapid UI fixes #3053

--- app/experimenter/static/rapid/components/pages/ExperimentDetailsPage.tsx
+++ app/experimenter/static/rapid/components/pages/ExperimentDetailsPage.tsx
@@ -6,16 +6,7 @@ import ExperimentProvider from "experimenter-rapid/contexts/experiment/provider"
 const ExperimentDetailsPage: React.FC = () => {
   return (
     <ExperimentProvider>
-      <div className="col pt-3">
-        <div className="mb-4">
-          <div className="d-flex align-items-center">
-            <h3 className="mr-3">Experiment Summary</h3>
-            <span className="badge badge-secondary mb-1">Draft</span>
-          </div>
-
-          <ExperimentDetails />
-        </div>
-      </div>
+      <ExperimentDetails />
     </ExperimentProvider>
   );
 };
