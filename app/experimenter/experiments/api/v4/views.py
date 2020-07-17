aa87b58ddd1048cd5f609126cf1a04b0d2112a40
Tif Tran <ttran@mozilla.com>
2020-07-17 14:51:35 -0700

    v4 public api fixes #3010 (#3048)
    * v4 public api fixes #3010
    
    * docs
    
    * change to expose recipe only
    
    * docs
    
    * formatting
    
    * v4 tests
    
    * renaming
    
    * unused imports
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>

---
+++ app/experimenter/experiments/api/v4/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, mixins
+
+from experimenter.experiments.models import Experiment
+from experimenter.experiments.api.v4.serializers import ExperimentRapidRecipeSerializer
+
+
+class ExperimentRapidViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet,
+):
+    lookup_field = "slug"
+    queryset = Experiment.objects.get_prefetched().filter(type=Experiment.TYPE_RAPID)
+    serializer_class = ExperimentRapidRecipeSerializer
--- app/experimenter/experiments/tests/api/v1/test_serializers.py
+++ app/experimenter/experiments/tests/api/v1/test_serializers.py
@@ -1,7 +1,4 @@
 import datetime
-import json
-import os
-from jsonschema import validate
 
 from django.test import TestCase
 
@@ -14,7 +11,6 @@ from experimenter.experiments.tests.factories import (
 )
 from experimenter.experiments.api.v1.serializers import (
     ExperimentChangeLogSerializer,
-    ExperimentRapidRecipeSerializer,
     ExperimentSerializer,
     ExperimentVariantSerializer,
     JSTimestampField,
@@ -188,73 +184,3 @@ class TestExperimentChangeLogSerializer(TestCase):
         )
         serializer = ExperimentChangeLogSerializer(change_log)
         self.assertEqual(serializer.data["changed_on"], change_log.changed_on)
-
-
-class TestExperimentRapidSerializer(TestCase):
-    def test_serializer_outputs_expected_schema(self):
-        audience = Experiment.RAPID_AUDIENCE_CHOICES[0][1]
-        features = [feature[0] for feature in Experiment.RAPID_FEATURE_CHOICES]
-        normandy_slug = "experimenter-normandy-slug"
-        today = datetime.datetime.today()
-        experiment = ExperimentFactory.create(
-            type=Experiment.TYPE_RAPID,
-            rapid_type=Experiment.RAPID_AA_CFR,
-            audience=audience,
-            features=features,
-            normandy_slug=normandy_slug,
-            proposed_enrollment=9,
-            proposed_start_date=today,
-        )
-
-        ExperimentVariantFactory.create(
-            experiment=experiment, slug="control", is_control=True
-        )
-        ExperimentVariantFactory.create(experiment=experiment, slug="variant-2")
-
-        serializer = ExperimentRapidRecipeSerializer(experiment)
-        data = serializer.data
-
-        fn = os.path.join(os.path.dirname(__file__), "experimentRecipe.json")
-
-        with open(fn, "r") as f:
-            json_schema = json.load(f)
-        self.assertIsNone(validate(instance=data, schema=json_schema))
-
-        arguments = data.pop("arguments")
-        branches = arguments.pop("branches")
-
-        self.assertDictEqual(
-            data,
-            {"id": normandy_slug, "filter_expression": "AUDIENCE 1", "enabled": True},
-        )
-
-        self.assertDictEqual(
-            dict(arguments),
-            {
-                "userFacingName": experiment.name,
-                "userFacingDescription": experiment.public_description,
-                "slug": normandy_slug,
-                "active": True,
-                "isEnrollmentPaused": False,
-                "endDate": None,
-                "proposedEnrollment": experiment.proposed_enrollment,
-                "features": features,
-                "referenceBranch": "control",
-                "startDate": today.isoformat(),
-                "bucketConfig": {
-                    "count": 0,
-                    "namespace": "",
-                    "randomizationUnit": "normandy_id",
-                    "start": 0,
-                    "total": 10000,
-                },
-            },
-        )
-        converted_branches = [dict(branch) for branch in branches]
-        self.assertEqual(
-            converted_branches,
-            [
-                {"ratio": 33, "slug": "variant-2", "value": None},
-                {"ratio": 33, "slug": "control", "value": None},
-            ],
-        )
