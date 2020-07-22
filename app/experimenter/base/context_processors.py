a30fd98c31bb2fcc5bab719e8e44b6fa25edc544
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-07-22 00:31:16 -0400

    Change WSGIRequest to HttpRequest

--- app/experimenter/base/context_processors.py
+++ app/experimenter/base/context_processors.py
@@ -1,14 +1,14 @@
 from typing import Dict
 
 from django.conf import settings
-from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpRequest
 
 
-def google_analytics(request: WSGIRequest) -> Dict[str, bool]:
+def google_analytics(request: HttpRequest) -> Dict[str, bool]:
     """Context processor bits you need related to injecting Google Analytics
     in the rendered templates."""
     return {"USE_GOOGLE_ANALYTICS": settings.USE_GOOGLE_ANALYTICS}
 
 
-def features(request: WSGIRequest) -> Dict[str, bool]:
+def features(request: HttpRequest) -> Dict[str, bool]:
     return {"FEATURE_MESSAGE_TYPE": settings.FEATURE_MESSAGE_TYPE}
