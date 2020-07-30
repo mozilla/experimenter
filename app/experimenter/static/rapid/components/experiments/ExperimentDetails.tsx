2c6a03c7bfa18b73346b210e0d366c3e3c668c5a
tiftran <ttran@mozilla.com>
2020-07-29 22:25:56 -0700

    react front

--- app/experimenter/static/rapid/components/experiments/ExperimentDetails.tsx
+++ app/experimenter/static/rapid/components/experiments/ExperimentDetails.tsx
@@ -131,11 +131,12 @@ const ExperimentDetails: React.FC = () => {
     );
   }
 
-  const buttonsDisabled = experimentData.status !== ExperimentStatus.DRAFT;
-  let buttonsClass = "btn btn-primary";
-  if (buttonsDisabled) {
-    buttonsClass = "btn btn-secondary";
-  }
+  const approveButtonDisabled =
+    experimentData.status !== ExperimentStatus.DRAFT;
+  const backButtonDisabled = ![
+    ExperimentStatus.DRAFT,
+    ExperimentStatus.REJECTED,
+  ].includes(experimentData.status);
 
   const buttonsShown = ![
     ExperimentStatus.LIVE,
@@ -143,22 +144,22 @@ const ExperimentDetails: React.FC = () => {
   ].includes(experimentData.status);
 
   let changeStatusButtons;
+
   if (buttonsShown) {
     changeStatusButtons = (
       <div className="d-flex mt-4">
         <span>
-          <Link
-            className={buttonsClass}
-            to={buttonsDisabled ? "#" : `/${experimentData.slug}/edit/`}
-          >
-            Back
+          <Link to={`/${experimentData.slug}/edit/`}>
+            <button className="btn btn-primary" disabled={backButtonDisabled}>
+              Back
+            </button>
           </Link>
         </span>
 
         <span className="flex-grow-1 text-right">
           <button
-            className={buttonsClass}
-            disabled={buttonsDisabled}
+            className="btn btn-primary"
+            disabled={approveButtonDisabled}
             type="button"
             onClick={handleClickRequestApproval}
           >
@@ -166,6 +167,21 @@ const ExperimentDetails: React.FC = () => {
           </button>
         </span>
       </div>
+    );
+  }
+
+  let rejectFeedback;
+
+  if (experimentData.reject_feedback) {
+    const messageDate = new Date(experimentData.reject_feedback.changed_on);
+    rejectFeedback = (
+      <>
+        <h3 className="my-4">Review Feedback</h3>
+        <div className="alert alert-secondary" role="alert">
+          <p className="font-weight-bold"> {messageDate.toDateString()}</p>
+          <p>Reject reason: {experimentData.reject_feedback.message}</p>
+        </div>
+      </>
     );
   }
 
@@ -217,6 +233,7 @@ const ExperimentDetails: React.FC = () => {
 
         {analysis_report}
 
+        {rejectFeedback}
         {changeStatusButtons}
       </div>
     </div>
