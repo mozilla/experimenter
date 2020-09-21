ffbc1b3c8f8c05768078cb2547172b3e5284cea8
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-09-21 11:10:05 -0400

    Add descriptions to test cases

--- app/experimenter/experiments/tests/test_models/test_nimbus_models.py
+++ app/experimenter/experiments/tests/test_models/test_nimbus_models.py
@@ -19,6 +19,11 @@ class TestNimbusExperimentModel(TestCase):
 
 class TestNimbusBucketNamespace(TestCase):
     def test_empty_namespace_creates_namespace_and_bucket_range(self):
+        """
+        Common case: A new empty isolation group for an experiment
+        that is orthogonal to all other current experiments.  This will
+        likely describe most experiment launches.
+        """
         experiment = NimbusExperimentFactory.create()
         bucket = NimbusBucketNamespace.request_namespace_buckets(
             experiment.slug, experiment, 100
@@ -35,6 +40,11 @@ class TestNimbusBucketNamespace(TestCase):
         )
 
     def test_existing_namespace_adds_bucket_range(self):
+        """
+        Rare case: An isolation group with no buckets allocated already exists.
+        This may become common when users can create their own isolation groups
+        and then later assign experiments to them.
+        """
         experiment = NimbusExperimentFactory.create()
         namespace = NimbusBucketNamespaceFactory.create(name=experiment.slug)
         bucket = NimbusBucketNamespace.request_namespace_buckets(
@@ -46,6 +56,11 @@ class TestNimbusBucketNamespace(TestCase):
         self.assertEqual(bucket.namespace, namespace)
 
     def test_existing_namespace_with_buckets_adds_next_bucket_range(self):
+        """
+        Common case: An isolation group with experiment bucket allocations exists,
+        and a subsequent bucket allocation is requested.  This will be the common case
+        for any experiments that share an isolation group.
+        """
         experiment = NimbusExperimentFactory.create()
         namespace = NimbusBucketNamespaceFactory.create(name=experiment.slug)
         NimbusBucketRangeFactory.create(namespace=namespace, start=0, count=100)
@@ -58,6 +73,15 @@ class TestNimbusBucketNamespace(TestCase):
         self.assertEqual(bucket.namespace, namespace)
 
     def test_full_namespace_creates_next_namespace_instance_and_adds_bucket_range(self):
+        """
+        Rare case:  An isolation group with experiment bucket allocations exists, and the
+        next requested bucket allocation would overflow its total bucket range, and so a
+        an isolation group with the same name but subsequent instance ID is created.
+
+        This is currently treated naively, ie does not account for possible collisions and
+        overlaps.  When this case becomes more common this will likely need to be given
+        more thought.
+        """
         experiment = NimbusExperimentFactory.create()
         namespace = NimbusBucketNamespaceFactory.create(name=experiment.slug, total=100)
         NimbusBucketRangeFactory(namespace=namespace, count=100)
