45eac29bbbb446ace28b9c7238201e5db3ac6ef9
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-09-21 13:40:06 -0400

    fix #3496: Add bucket logic for Nimbus (#3508)
    * fix #3496: Add bucket logic for Nimbus
    
    * Add NimbusBucketNamespace and NimbusBucketRange to Nimbus models
    * Add related test factories
    * Add related tests to Nimbus model tests
    * Leave the old RapidExperiment versions in place to preserve the functioning of the existing Rapid workflow until Nimbus reaches parity
    
    * I realized I don't have to prefix the related name with nimbus since it's already scoped to the NimbusExperiment model
    
    * Add descriptions to test cases
    
    * Rename Namespace to IsolationGroup

---
+++ app/experimenter/experiments/tests/test_models/test_nimbus_models.py
@@ -0,0 +1,101 @@
+from django.test import TestCase
+
+from experimenter.experiments.models import (
+    NimbusExperiment,
+    NimbusIsolationGroup,
+)
+from experimenter.experiments.tests.factories import (
+    NimbusBucketRangeFactory,
+    NimbusExperimentFactory,
+    NimbusIsolationGroupFactory,
+)
+
+
+class TestNimbusExperimentModel(TestCase):
+    def test_str(self):
+        experiment = NimbusExperimentFactory.create(slug="experiment-slug")
+        self.assertEqual(str(experiment), experiment.name)
+
+
+class TestNimbusIsolationGroup(TestCase):
+    def test_empty_isolation_group_creates_isolation_group_and_bucket_range(self):
+        """
+        Common case: A new empty isolation group for an experiment
+        that is orthogonal to all other current experiments.  This will
+        likely describe most experiment launches.
+        """
+        experiment = NimbusExperimentFactory.create()
+        bucket = NimbusIsolationGroup.request_isolation_group_buckets(
+            experiment.slug, experiment, 100
+        )
+        self.assertEqual(bucket.start, 0)
+        self.assertEqual(bucket.end, 99)
+        self.assertEqual(bucket.count, 100)
+        self.assertEqual(bucket.isolation_group.name, experiment.slug)
+        self.assertEqual(bucket.isolation_group.instance, 1)
+        self.assertEqual(bucket.isolation_group.total, NimbusExperiment.BUCKET_TOTAL)
+        self.assertEqual(
+            bucket.isolation_group.randomization_unit,
+            NimbusExperiment.BUCKET_RANDOMIZATION_UNIT,
+        )
+
+    def test_existing_isolation_group_adds_bucket_range(self):
+        """
+        Rare case: An isolation group with no buckets allocated already exists.
+        This may become common when users can create their own isolation groups
+        and then later assign experiments to them.
+        """
+        experiment = NimbusExperimentFactory.create()
+        isolation_group = NimbusIsolationGroupFactory.create(name=experiment.slug)
+        bucket = NimbusIsolationGroup.request_isolation_group_buckets(
+            experiment.slug, experiment, 100
+        )
+        self.assertEqual(bucket.start, 0)
+        self.assertEqual(bucket.end, 99)
+        self.assertEqual(bucket.count, 100)
+        self.assertEqual(bucket.isolation_group, isolation_group)
+
+    def test_existing_isolation_group_with_buckets_adds_next_bucket_range(self):
+        """
+        Common case: An isolation group with experiment bucket allocations exists,
+        and a subsequent bucket allocation is requested.  This will be the common case
+        for any experiments that share an isolation group.
+        """
+        experiment = NimbusExperimentFactory.create()
+        isolation_group = NimbusIsolationGroupFactory.create(name=experiment.slug)
+        NimbusBucketRangeFactory.create(
+            isolation_group=isolation_group, start=0, count=100
+        )
+        bucket = NimbusIsolationGroup.request_isolation_group_buckets(
+            experiment.slug, experiment, 100
+        )
+        self.assertEqual(bucket.start, 100)
+        self.assertEqual(bucket.end, 199)
+        self.assertEqual(bucket.count, 100)
+        self.assertEqual(bucket.isolation_group, isolation_group)
+
+    def test_full_isolation_group_creates_next_isolation_group_adds_bucket_range(
+        self,
+    ):
+        """
+        Rare case:  An isolation group with experiment bucket allocations exists, and the
+        next requested bucket allocation would overflow its total bucket range, and so a
+        an isolation group with the same name but subsequent instance ID is created.
+
+        This is currently treated naively, ie does not account for possible collisions and
+        overlaps.  When this case becomes more common this will likely need to be given
+        more thought.
+        """
+        experiment = NimbusExperimentFactory.create()
+        isolation_group = NimbusIsolationGroupFactory.create(
+            name=experiment.slug, total=100
+        )
+        NimbusBucketRangeFactory(isolation_group=isolation_group, count=100)
+        bucket = NimbusIsolationGroup.request_isolation_group_buckets(
+            experiment.slug, experiment, 100
+        )
+        self.assertEqual(bucket.start, 0)
+        self.assertEqual(bucket.end, 99)
+        self.assertEqual(bucket.count, 100)
+        self.assertEqual(bucket.isolation_group.name, isolation_group.name)
+        self.assertEqual(bucket.isolation_group.instance, isolation_group.instance + 1)
