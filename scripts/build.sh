ed6337b814528bc0ddb51d5ffc58d5318801d768
Les Orchard <lorchard@mozilla.com>
2020-10-01 10:39:53 -0700

    chore(nimbus-ui): hook up storybook publishing on build (#3579)
    Because
    
    * We want to publish Storybook builds to demo and preview component work
      in nimbus-ui
    
    This commit
    
    * Integrates mozilla-fxa/storybook-gcp-publisher into the CircleCI flow
      so that Storybooks are built and published to a Google Cloud static
      site for every test run
    
    EXP-281
    fixes #3427

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 ./scripts/echo_version_json.sh > ./app/experimenter/version.json
 cp ./app/experimenter/version.json ./app/version.json
+git log -n 1 --no-color --pretty='%s' > ./app/commit-summary.txt
+git log -n 1 --no-color --pretty=medium > ./app/commit-description.txt
 docker build -f app/Dockerfile -t app:build app/
