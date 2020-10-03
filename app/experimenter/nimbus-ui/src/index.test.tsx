aa8371451058f989534a07bddf302d0a4a8616e7
Les Orchard <me@lmorchard.com>
2020-10-02 17:58:13 -0700

    fix #3428 feat(nimbus): add AppErrorBoundary and Sentry for error logging
    Because we want to catch errors within the nimbus-ui React app, this
    commit adds a Logger service wrapper for console.log and a Sentry
    service to report errors back to us.

--- app/experimenter/nimbus-ui/src/index.test.tsx
+++ app/experimenter/nimbus-ui/src/index.test.tsx
@@ -2,25 +2,94 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./lib/config";
+import React, { ReactNode } from "react";
+import "./services/config";
+import "./services/sentry";
 
-jest.mock("react-dom", () => ({ render: jest.fn() }));
-jest.mock("./lib/config", () => ({ readConfig: jest.fn() }));
+// Mock out these components - for testing, we only care that they're used
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  StrictMode: ({ children }: { children: ReactNode }) => (
+    <div data-testid="StrictMode">{children}</div>
+  ),
+}));
+
+jest.mock("./components/AppErrorBoundary", () => ({
+  __esModule: true,
+  default: ({ children }: { children: ReactNode }) => (
+    <div data-testid="AppErrorBoundary">{children}</div>
+  ),
+}));
+
+jest.mock("./components/App", () => ({
+  __esModule: true,
+  default: ({ children }: { children: ReactNode }) => (
+    <div data-testid="App">{children}</div>
+  ),
+}));
 
 describe("index.tsx", () => {
-  it("should render without crashing", () => {
+  const origError = global.console.error;
+  let mockError: any, mockConfig: any, mockSentry: any;
+
+  beforeEach(() => {
+    jest.resetModules();
+
     const div = document.createElement("div");
     div.id = "root";
     document.body.appendChild(div);
+
+    mockConfig = { readConfig: jest.fn() };
+    mockSentry = { configure: jest.fn() };
+
+    jest.mock("./services/config", () => mockConfig);
+    jest.mock("./services/sentry", () => mockSentry);
+
+    mockError = jest.fn();
+    global.console.error = mockError;
+  });
+
+  afterEach(() => {
+    global.console.error = origError;
+  });
+
+  it("should render as expected", () => {
     require("./index");
-    expect(ReactDOM.render).toHaveBeenCalledWith(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      div,
+
+    const root = document.getElementById("root")!;
+    expect(mockConfig.readConfig).toHaveBeenCalledWith(root);
+
+    // Assert that these mock components are nested.
+    let currRoot: any = root;
+    ["StrictMode", "AppErrorBoundary", "App"].forEach((name) => {
+      currRoot = currRoot?.querySelector(`*[data-testid="${name}"]`);
+      expect(currRoot).toBeDefined();
+    });
+  });
+
+  it("should configure sentry if DSN is available", () => {
+    mockConfig.readConfig = jest.fn(() => {
+      Object.assign(mockConfig, {
+        sentry_dsn: "http://example.com/sentry",
+        version: "1.0",
+      });
+    });
+    require("./index");
+    const root = document.getElementById("root")!;
+    expect(mockConfig.readConfig).toHaveBeenCalledWith(root);
+    expect(mockSentry.configure).toHaveBeenCalledWith(
+      mockConfig.sentry_dsn,
+      mockConfig.version,
     );
+  });
+
+  it("should log initialization errors", () => {
+    mockConfig.readConfig = jest.fn(() => {
+      throw new Error("uh oh");
+    });
+    require("./index");
+    const root = document.getElementById("root")!;
+    expect(mockConfig.readConfig).toHaveBeenCalledWith(root);
+    expect(mockError).toHaveBeenCalled();
   });
 });
