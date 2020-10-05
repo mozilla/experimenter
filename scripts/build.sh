148822daeb1f3ec80f1ad9f2811b0d98e5bec9f0
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-10-05 13:54:15 -0400

    fix #3634 chore(project): Use separate build and deploy Docker images (#3642)
    Because
    
    * We want to shrink the size of the deployable Docker image down from its current 2.71gb(1gb compressed)
    
    This commit
    
    * Adds a second stage to our Dockerfile for a deploy image which is a much more reasonable 480mb(133mb compressed)
    * Use deploy image for Django container in local dev compose

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,5 @@
 cp ./app/experimenter/version.json ./app/version.json
 git log -n 1 --no-color --pretty='%s' > ./app/commit-summary.txt
 git log -n 1 --no-color --pretty=medium > ./app/commit-description.txt
-docker build -f app/Dockerfile -t app:build app/
+docker build --target build -f app/Dockerfile -t app:build app/
+docker build --target deploy -f app/Dockerfile -t app:deploy app/
