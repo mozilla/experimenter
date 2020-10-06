080e60bd341febb395e1fc11e22549a6c78dc278
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-10-06 18:17:55 -0400

    fix #3546 feat(nimbus): create NimbusExperiments in load_dummy_experiments (#3685)
    Because
    
    * It'll help with local dev and testing to have dummy NimbusExperiments in the db
    
    This commit
    
    * Creates NimbusExperiments in the load_dummy_experiments management command
    * I took out all the arguments to the management command because it gets really tricky to
      reason about their behaviour with two completely different experiment types, and I don't
      think those arguments get used very often

--- app/experimenter/base/tests/test_initial_data.py
+++ app/experimenter/base/tests/test_initial_data.py
@@ -1,24 +1,15 @@
 from django.core.management import call_command
 from django.test import TestCase
 
-from experimenter.experiments.constants import ExperimentConstants
-from experimenter.experiments.models import Experiment
+from experimenter.experiments.models import Experiment, NimbusExperiment
 
 
 class TestInitialData(TestCase):
     def test_load_dummy_experiments(self):
         self.assertFalse(Experiment.objects.exists())
+        self.assertFalse(NimbusExperiment.objects.exists())
         call_command("load_dummy_experiments")
-        self.assertTrue(Experiment.objects.exists())
-
-    def test_load_dummy_experiments_with_specified_values(self):
-        call_command(
-            "load_dummy_experiments",
-            num_of_experiments=20,
-            status=ExperimentConstants.STATUS_DRAFT,
-        )
-        self.assertTrue(Experiment.objects.exists())
-        self.assertEqual(Experiment.objects.count(), 20)
+        self.assertEqual(Experiment.objects.count(), len(Experiment.STATUS_CHOICES))
         self.assertEqual(
-            Experiment.objects.filter(status=ExperimentConstants.STATUS_DRAFT).count(), 20
+            NimbusExperiment.objects.count(), len(NimbusExperiment.Status.choices)
         )
