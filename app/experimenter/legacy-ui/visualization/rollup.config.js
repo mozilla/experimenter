2839a1a3caf6984d818c1d04ae54271c2467c593
Lauren Zugai <lauren@zugai.com>
2020-10-06 17:56:47 -0500

    fix(visualization): css.write is now relative to destination, not config

--- app/experimenter/legacy-ui/visualization/rollup.config.js
+++ app/experimenter/legacy-ui/visualization/rollup.config.js
@@ -7,6 +7,7 @@ import postcss from "rollup-plugin-postcss";
 import svelte from "rollup-plugin-svelte";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
+import * as path from "path";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -33,7 +34,7 @@ export default [
         // we'll extract any component CSS out into
         // a separate file - better for performance
         css: (css) => {
-          css.write("../assets/visualization/bundle.css");
+          css.write("bundle.css");
         },
       }),
       typescript({ sourceMap: !production }),
