1ef0f9f9db802c38a8320b4f00310f65bf77aea8
Les Orchard <lorchard@mozilla.com>
2020-10-08 15:32:48 -0700

    fix #3428 feat(nimbus): add AppErrorBoundary and Sentry for error logging (#3690)
    Because we want to catch errors within the nimbus-ui React app, this
    commit adds a Logger service wrapper for console.log and a Sentry
    service to report errors back to us.

---
+++ app/experimenter/nimbus-ui/src/components/AppErrorBoundary/index.tsx
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+import { Alert } from "react-bootstrap";
+import sentryMetrics from "../../services/sentry";
+
+class AppErrorBoundary extends React.Component {
+  state: {
+    error: undefined | Error;
+  };
+
+  constructor(props: {}) {
+    super(props);
+    this.state = { error: undefined };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  componentDidCatch(error: Error) {
+    sentryMetrics.captureException(error);
+  }
+
+  render() {
+    const { error } = this.state;
+    return error ? <AppErrorAlert {...{ error }} /> : this.props.children;
+  }
+}
+
+export const AppErrorAlert = ({ error }: { error: Error }) => {
+  return (
+    <Alert variant="warning" data-testid="error-loading-app">
+      <Alert.Heading>General application error</Alert.Heading>
+      <p>Something went wrong. Please try again later.</p>
+    </Alert>
+  );
+};
+
+export default AppErrorBoundary;
--- app/experimenter/nimbus-ui/src/components/AppLayout/index.stories.tsx
+++ app/experimenter/nimbus-ui/src/components/AppLayout/index.stories.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import AppLayout from ".";
 
-storiesOf("AppLayout", module).add("default", () => (
+storiesOf("components/AppLayout", module).add("default", () => (
   <AppLayout>
     <p>App contents go here</p>
   </AppLayout>
