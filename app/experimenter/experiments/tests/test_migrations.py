bfcf49612ceb3063f07844faa17c92e6589be7da
Tif Tran <ttran@mozilla.com>
2020-10-13 09:55:27 -0700

    fix #3716 fix(legacy): Prune empty "add version" changelogs (#3735)
    * fix #3716 fix(legacy): Prune empty "add version" changelogs
    
    Because:
    
    * change from #2538 flooded prod instance with empty changelogs
    
    This commit:
    
    * has a migration that should filter  "Added Version(s)" changelogs that don't have changed_values and delete them
    
    * formatting

---
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -0,0 +1,67 @@
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
+from django.test.testcases import TransactionTestCase
+
+from experimenter.experiments.models import ExperimentChangeLog
+from experimenter.experiments.tests.factories import ExperimentFactory
+from experimenter.openidc.tests.factories import UserFactory
+
+
+class MigrationTestCase(TransactionTestCase):
+    """A Test case for testing migrations"""
+
+    migrate_from = None
+    migrate_to = None
+
+    def setUp(self):
+        super(MigrationTestCase, self).setUp()
+
+        self.executor = MigrationExecutor(connection)
+
+        self.executor.migrate(self.migrate_from)
+
+    def migrate_to_dest(self):
+        self.executor.loader.build_graph()  # reload.
+        self.executor.migrate(self.migrate_to)
+
+
+class TestMigration(MigrationTestCase):
+
+    migrate_from = [("experiments", "0120_add_feature_config")]
+    migrate_to = [("experiments", "0121_prune_add_version_changelogs")]
+
+    def test_migration(self):
+
+        experiment = ExperimentFactory.create()
+        user = UserFactory.create()
+
+        ExperimentChangeLog.objects.create(
+            experiment=experiment,
+            message="Added Version(s)",
+            old_status="Live",
+            new_status="Live",
+            changed_by=user,
+        )
+
+        experiment_something_change_log = ExperimentChangeLog.objects.create(
+            experiment=experiment,
+            message="Added Version(s)",
+            old_status="Live",
+            new_status="Live",
+            changed_by=user,
+            changed_values={
+                "firefox_max_version": {
+                    "old_value": "79.0",
+                    "new_value": "80.0",
+                    "display_name": "Firefox Max Version",
+                }
+            },
+        )
+
+        self.migrate_to_dest()
+
+        self.assertEqual(ExperimentChangeLog.objects.count(), 1)
+        self.assertEqual(
+            ExperimentChangeLog.objects.all().first(),
+            experiment_something_change_log,
+        )
