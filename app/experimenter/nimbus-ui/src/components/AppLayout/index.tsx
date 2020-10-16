0487d3b6f5a8ec37dd79cbd022ab090b4893cd35
Les Orchard <lorchard@mozilla.com>
2020-10-16 09:40:26 -0700

    fix #3465 feat(nimbus): add new experiment form and page (#3765)
    Because
    * We want to accept submissions for new experiments
    
    This commit:
    * Adds a form to the new experiment page to accept experiment details

--- app/experimenter/nimbus-ui/src/components/AppLayout/index.tsx
+++ app/experimenter/nimbus-ui/src/components/AppLayout/index.tsx
@@ -3,16 +3,33 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 type AppLayoutProps = {
+  testid?: string;
   children: React.ReactNode;
 };
 
 // TODO: Depending on the main page ("directory view") design, we may want two
 // AppLayouts - one with a header/footer, and another with a nav sidebar.
 
-export const AppLayout = ({ children }: AppLayoutProps) => {
-  return <main data-testid="main">{children}</main>;
+export const AppLayout = ({ children, testid = "main" }: AppLayoutProps) => {
+  return (
+    <Container
+      fluid
+      as="main"
+      className="h-100"
+      data-testid={testid}
+    >
+      <Row className="h-100">
+        <Col className="ml-auto mr-auto col-md-10 col-lg-8">
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AppLayout;
