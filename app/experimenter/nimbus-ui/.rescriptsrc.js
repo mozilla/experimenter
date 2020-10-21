da38eb7c3409917595fdc94f32cfc95fb2d9ce7c
Jody Heavener <j.heavener@gmail.com>
2020-10-21 15:07:42 -0400

    fix(nimbus): generate and add css to nimbus template in prod

--- app/experimenter/nimbus-ui/.rescriptsrc.js
+++ app/experimenter/nimbus-ui/.rescriptsrc.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 // Rescripts allows us modify the default CRA
 // Webpack config without needing to eject.
 // Check out the README for how to use this file.
@@ -19,6 +21,12 @@ module.exports = [(config) => {
   // contenthash, so we can reliably locate it
   // from within a Django template
   config.output.filename = "static/js/[name].js";
+  // We need to do this for CSS as well
+  config.plugins[
+    config.plugins.findIndex((p) => p.constructor.name === 'MiniCssExtractPlugin')
+  ] = new MiniCssExtractPlugin({
+    filename: 'static/css/[name].css',
+  });
 
   return config;
 }];
