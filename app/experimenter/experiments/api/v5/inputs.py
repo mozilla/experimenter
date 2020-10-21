0b87ca65e8b7233cad274ddc276f13ac491e125f
Ben Bangert <ben@groovie.org>
2020-10-21 12:58:12 -0700

    fix: #3643 feat(nimbus): update experiment branches (#3789)
    Because:
    
    * We want to update an experiment to include a feature config along with
      its control and treatment branches.
    
    This commit:
    
    * Add's a GraphQL mutation that will update the Nimbus experiment to
      reflect the passed in data. Control/Treatment branches will be updated
      to match the GraphQL input, with branches being deleted than set to
      the input.

--- app/experimenter/experiments/api/v5/inputs.py
+++ app/experimenter/experiments/api/v5/inputs.py
@@ -2,7 +2,7 @@ import graphene
 
 
 class ExperimentInput(graphene.InputObjectType):
-    clientMutationId = graphene.String()
+    client_mutation_id = graphene.String()
     name = graphene.String()
     slug = graphene.String()
     application = graphene.String()
@@ -17,3 +17,38 @@ class CreateExperimentInput(ExperimentInput):
 
 class UpdateExperimentInput(ExperimentInput):
     id = graphene.ID(required=True)
+
+
+class BranchType(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    description = graphene.String(required=True)
+    ratio = graphene.Int(required=True)
+    feature_enabled = graphene.Boolean()
+    feature_value = graphene.String()
+
+
+class ControlBranchType(BranchType):
+    class Meta:
+        description = "The control branch"
+
+
+class TreatmentBranchType(BranchType):
+    class Meta:
+        description = (
+            "The treatment branch that should be in this position on the experiment."
+        )
+
+
+class UpdateExperimentBranchesInput(graphene.InputObjectType):
+    client_mutation_id = graphene.String()
+    nimbus_experiment_id = graphene.Int(required=True)
+    feature_config_id = graphene.Int()
+    control_branch = graphene.Field(ControlBranchType, required=True)
+    treatment_branches = graphene.List(
+        TreatmentBranchType,
+        required=True,
+        description=(
+            "List of treatment branches for this experiment. Branches will be created, "
+            "updated, or deleted to match the list provided."
+        ),
+    )
