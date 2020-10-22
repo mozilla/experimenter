a938c3262855bfb7ac8fb9005027ad2da813391e
Lauren Zugai <lauren@zugai.com>
2020-10-22 13:40:25 -0500

    Continue hacking at tests

--- app/experimenter/nimbus-ui/src/components/App/index.test.tsx
+++ app/experimenter/nimbus-ui/src/components/App/index.test.tsx
@@ -3,17 +3,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React, { ReactNode } from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import {
   createMemorySource,
   createHistory,
   LocationProvider,
   Router,
+  Redirect,
 } from "@reach/router";
 import { renderWithRouter } from "../../lib/helpers";
-import { BASE_PATH } from "../../lib/constants";
-import { Redirect as MockRedirect } from "@reach/router";
 import App from ".";
+import PageEditOverview from "../PageEditOverview";
+import PageEditBranches from "../PageEditBranches";
 
 describe("App", () => {
   const Subject = ({ basepath = "/", path = "/" }) => {
@@ -37,8 +38,49 @@ describe("App", () => {
     expect(screen.getByTestId("PageNew")).toBeInTheDocument();
   });
 
-  it("redirects from ':slug/edit' to ':slug/edit/overview'", async () => {
-    // WIP
+  describe(":slug routes", () => {
+    const Root = (props: any) => <>{props.children}</>;
+    const Routes = () => (
+      <Router>
+        <Root path="/:slug/edit">
+          <Redirect from="/" to="overview" noThrow />
+          <PageEditOverview path="overview" />
+          <PageEditBranches path="branches" />
+        </Root>
+      </Router>
+    );
+
+    it("redirects from ':slug/edit' to ':slug/edit/overview'", async () => {
+      const {
+        history: { navigate },
+      } = renderWithRouter(<Routes />, {
+        route: `/my-special-slug/`,
+      });
+
+      await navigate("/my-special-slug/edit");
+      // we have to waitFor the redirect
+      await waitFor(() => {
+        expect(screen.getByTestId("PageEditOverview")).toBeInTheDocument();
+      });
+    });
+
+    it("renders expected active page class in nav", async () => {
+      const {
+        history: { navigate },
+      } = renderWithRouter(<Routes />, {
+        route: `/my-special-slug/`,
+      });
+      // this fails because location.pathname is still set to "/"
+      // and `isCurrent` checks location.pathname against the href
+      await navigate("/my-special-slug/edit/overview");
+      const overviewLink = screen.getByTestId("nav-edit-overview");
+      const branchesLink = screen.getByTestId("nav-edit-branches");
+      expect(overviewLink).toHaveClass("text-primary");
+      expect(branchesLink).not.toHaveClass("text-primary");
+      await navigate("/my-special-slug/edit/branches");
+      expect(branchesLink).toHaveClass("text-primary");
+      expect(overviewLink).not.toHaveClass("text-primary");
+    });
   });
 });
 
