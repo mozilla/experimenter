08c96db5d277fbe8a38b1e03f9ac61b136dac77d
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-10-26 12:26:09 -0400

    fix #3687 chore(project): run integration tests against deploy image (#3822)
    Because
    
    * We split the docker image into two images, one for local dev and one for deploying to prod so that the prod one would be small ~100mb
    * We should find some way to test that the deploy image is working correctly
    * A good candidate is to run the integration tests against it
    
    This commit
    
    * Adds a new make up_prod command that sets up an environment that more closely mirrors prod ie
      - Use the prod image
      - Disable all the yarn watchers
    * Reorganizes the dockerfile to move frontend asset building out of the dev container to increase docker cache hits for local dev

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,4 @@
 cp ./app/experimenter/version.json ./app/version.json
 git log -n 1 --no-color --pretty='%s' > ./app/commit-summary.txt
 git log -n 1 --no-color --pretty=medium > ./app/commit-description.txt
-docker build --target build -f app/Dockerfile -t app:build app/
-docker build --target deploy -f app/Dockerfile -t app:deploy app/
+docker build --target dev -f app/Dockerfile -t app:dev app/
