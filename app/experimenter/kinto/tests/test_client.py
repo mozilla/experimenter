5cd5e7905693809b3ac257db5635d003764dd53d
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-10-28 12:37:09 -0400

    fix #3640 feat(nimbus): add remote settings tasks for nimbus (#3829)
    Because
    
    * We need to send and receive nimbus experiments from a specific remote settings collection
    
    This commit
    
    * Adds tasks to push and read nimbus experiments from the nimbus specific collection
      - Push to collection and mark for review with a changelog
      - Read rejected experiments and mark them as draft with a changelog
      - Check for reviewed experiments that have been accepted and mark them live with a changelog
      - Check for removed experiments and mark them completed with a changelog

--- app/experimenter/kinto/tests/test_client.py
+++ app/experimenter/kinto/tests/test_client.py
@@ -1,13 +1,17 @@
 from django.conf import settings
 from django.test import TestCase
 
-from experimenter.kinto import client
+from experimenter.kinto.client import KintoClient
 from experimenter.kinto.tests.mixins import MockKintoClientMixin
 
 
-class TestPushToKinto(MockKintoClientMixin, TestCase):
+class TestKintoClient(MockKintoClientMixin, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.client = KintoClient(settings.KINTO_COLLECTION)
+
     def test_push_to_kinto_sends_data_updates_collection(self):
-        client.push_to_kinto({"test": "data"})
+        self.client.push_to_kinto({"test": "data"})
 
         self.mock_kinto_client_creator.assert_called_with(
             server_url=settings.KINTO_HOST,
@@ -27,38 +31,31 @@ class TestPushToKinto(MockKintoClientMixin, TestCase):
             bucket=settings.KINTO_BUCKET,
         )
 
-
-class TestHasPendingReview(MockKintoClientMixin, TestCase):
     def test_returns_true_for_pending_review(self):
         self.setup_kinto_pending_review()
-        self.assertTrue(client.has_pending_review())
+        self.assertTrue(self.client.has_pending_review())
 
     def test_returns_false_for_no_pending_review(self):
         self.setup_kinto_no_pending_review()
-        self.assertFalse(client.has_pending_review())
+        self.assertFalse(self.client.has_pending_review())
 
-
-class TestGetMainRecords(MockKintoClientMixin, TestCase):
     def test_returns_records(self):
-        self.setup_kinto_get_main_records()
-        self.assertEqual(len(client.get_main_records()), 1)
+        slug = "test-slug"
+        self.setup_kinto_get_main_records([slug])
+        self.assertEqual(self.client.get_main_records(), [{"id": slug}])
 
     def test_returns_no_records(self):
-        self.setup_kinto_no_main_records()
-        self.assertEqual(client.get_main_records(), [])
+        self.setup_kinto_get_main_records([])
+        self.assertEqual(self.client.get_main_records(), [])
 
-
-class TestGetRejectedCollectionData(MockKintoClientMixin, TestCase):
     def test_returns_nothing_when_not_rejects(self):
         self.setup_kinto_no_pending_review()
-        self.assertIsNone(client.get_rejected_collection_data())
+        self.assertIsNone(self.client.get_rejected_collection_data())
 
     def test_returns_rejected_data(self):
         self.setup_kinto_rejected_review()
-        self.assertTrue(client.get_rejected_collection_data())
+        self.assertTrue(self.client.get_rejected_collection_data())
 
-
-class TestGetRejectedRecords(MockKintoClientMixin, TestCase):
     def test_returns_rejected_record(self):
         self.mock_kinto_client.get_records.side_effect = [
             [{"id": "bug-12345-rapid-test-release-55"}],
@@ -67,4 +64,6 @@ class TestGetRejectedRecords(MockKintoClientMixin, TestCase):
                 {"id": "bug-9999-rapid-test-release-55"},
             ],
         ]
-        self.assertEqual(client.get_rejected_record(), ["bug-9999-rapid-test-release-55"])
+        self.assertEqual(
+            self.client.get_rejected_record(), "bug-9999-rapid-test-release-55"
+        )
