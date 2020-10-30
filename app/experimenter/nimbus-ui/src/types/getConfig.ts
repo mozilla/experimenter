4ed2c112d86e4f2aa54d510ff16da961282d4fda
Jody Heavener <j.heavener@gmail.com>
2020-10-30 14:08:40 -0400

    fixes #3855 feat(nimbus): read config from graphql server (#3851)

---
+++ app/experimenter/nimbus-ui/src/types/getConfig.ts
@@ -0,0 +1,83 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { NimbusFeatureConfigApplication, NimbusProbeKind } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: getConfig
+// ====================================================
+
+export interface getConfig_nimbusConfig_application {
+  __typename: "NimbusLabelValueType";
+  label: string | null;
+  value: string | null;
+}
+
+export interface getConfig_nimbusConfig_channels {
+  __typename: "NimbusLabelValueType";
+  label: string | null;
+  value: string | null;
+}
+
+export interface getConfig_nimbusConfig_featureConfig {
+  __typename: "NimbusFeatureConfigType";
+  id: string;
+  name: string;
+  slug: string;
+  description: string | null;
+  application: NimbusFeatureConfigApplication | null;
+  ownerEmail: string | null;
+  schema: string | null;
+}
+
+export interface getConfig_nimbusConfig_firefoxMinVersion {
+  __typename: "NimbusLabelValueType";
+  label: string | null;
+  value: string | null;
+}
+
+export interface getConfig_nimbusConfig_probeSets_probes {
+  __typename: "NimbusProbeType";
+  id: string;
+  kind: NimbusProbeKind;
+  name: string;
+  eventCategory: string;
+  eventMethod: string | null;
+  eventObject: string | null;
+  eventValue: string | null;
+}
+
+export interface getConfig_nimbusConfig_probeSets {
+  __typename: "NimbusProbeSetType";
+  id: string;
+  name: string;
+  slug: string;
+  probes: getConfig_nimbusConfig_probeSets_probes[];
+}
+
+export interface getConfig_nimbusConfig_targetingConfigSlug {
+  __typename: "NimbusLabelValueType";
+  label: string | null;
+  value: string | null;
+}
+
+export interface getConfig_nimbusConfig {
+  __typename: "NimbusConfigurationType";
+  application: (getConfig_nimbusConfig_application | null)[] | null;
+  channels: (getConfig_nimbusConfig_channels | null)[] | null;
+  featureConfig: (getConfig_nimbusConfig_featureConfig | null)[] | null;
+  firefoxMinVersion: (getConfig_nimbusConfig_firefoxMinVersion | null)[] | null;
+  probeSets: (getConfig_nimbusConfig_probeSets | null)[] | null;
+  targetingConfigSlug:
+    | (getConfig_nimbusConfig_targetingConfigSlug | null)[]
+    | null;
+}
+
+export interface getConfig {
+  /**
+   * Nimbus Configuration Data for front-end usage.
+   */
+  nimbusConfig: getConfig_nimbusConfig | null;
+}
