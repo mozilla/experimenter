af37686686f95d348f409a38407d9837d5393bfb
Jody Heavener <j.heavener@gmail.com>
2020-11-02 15:53:45 -0500

    feat(nimbus): move common experiment edit elements to PageEditContainer component (#3860)

--- app/experimenter/nimbus-ui/src/components/PageEditBranches/index.tsx
+++ app/experimenter/nimbus-ui/src/components/PageEditBranches/index.tsx
@@ -3,60 +3,41 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from "react";
-import { RouteComponentProps, useParams } from "@reach/router";
-import AppLayoutWithSidebar from "../AppLayoutWithSidebar";
-import { useExperiment, useConfig } from "../../hooks";
-import HeaderEditExperiment from "../HeaderEditExperiment";
-import ExperimentNotFound from "../PageExperimentNotFound";
-import PageLoading from "../PageLoading";
+import { RouteComponentProps } from "@reach/router";
+import { useConfig } from "../../hooks";
 import FormBranches from "../FormBranches";
 import LinkExternal from "../LinkExternal";
+import PageEditContainer from "../PageEditContainer";
 
 // TODO: find this doco URL
 const BRANCHES_DOC_URL =
   "https://mana.mozilla.org/wiki/pages/viewpage.action?spaceKey=FJT&title=Project+Nimbus";
 
 const PageEditBranches: React.FunctionComponent<RouteComponentProps> = () => {
-  const { slug } = useParams();
-  const { experiment, notFound, loading } = useExperiment(slug);
   const { featureConfig } = useConfig();
 
-  if (loading) {
-    return <PageLoading />;
-  }
-
-  if (notFound) {
-    return <ExperimentNotFound {...{ slug }} />;
-  }
-
-  const { name, status } = experiment;
   return (
-    <AppLayoutWithSidebar>
-      <section data-testid="PageEditBranches">
-        <HeaderEditExperiment
-          {...{
-            slug,
-            name,
-            status,
-          }}
-        />
-        <h2 className="mt-3 h4">Branches</h2>
-        <p>
-          If you want, you can add a <strong>feature flag</strong> configuration
-          to each branch. Experiments can only change one flag at a time.{" "}
-          <LinkExternal href={BRANCHES_DOC_URL}>Learn more</LinkExternal>
-        </p>
-        {/* TODO: EXP-505 for accepting and saving edits to branches */}
-        <FormBranches
-          {...{
-            experiment,
-            featureConfig,
-            // TODO: supply this as default value, track changes within FormBranches
-            equalRatio: false,
-          }}
-        />
-      </section>
-    </AppLayoutWithSidebar>
+    <PageEditContainer title="Branches" testId="PageEditBranches">
+      {({ experiment }) => (
+        <>
+          <p>
+            If you want, you can add a <strong>feature flag</strong>{" "}
+            configuration to each branch. Experiments can only change one flag
+            at a time.{" "}
+            <LinkExternal href={BRANCHES_DOC_URL}>Learn more</LinkExternal>
+          </p>
+          {/* TODO: EXP-505 for accepting and saving edits to branches */}
+          <FormBranches
+            {...{
+              experiment,
+              featureConfig,
+              // TODO: supply this as default value, track changes within FormBranches
+              equalRatio: false,
+            }}
+          />
+        </>
+      )}
+    </PageEditContainer>
   );
 };
 
