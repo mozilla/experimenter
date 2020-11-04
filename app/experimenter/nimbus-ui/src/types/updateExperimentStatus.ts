5e3d5e09bef7d7b47eb8c2c595ad3e3049ce46a7
Jody Heavener <j.heavener@gmail.com>
2020-11-04 14:51:41 -0500

    fixes #3479 feat(nimbus): add ability to send review request

---
+++ app/experimenter/nimbus-ui/src/types/updateExperimentStatus.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { UpdateExperimentStatusInput, NimbusExperimentStatus } from "./globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: updateExperimentStatus
+// ====================================================
+
+export interface updateExperimentStatus_updateExperimentStatus_nimbusExperiment {
+  __typename: "NimbusExperimentType";
+  status: NimbusExperimentStatus | null;
+}
+
+export interface updateExperimentStatus_updateExperimentStatus {
+  __typename: "UpdateExperimentStatus";
+  clientMutationId: string | null;
+  message: any | null;
+  status: number | null;
+  nimbusExperiment: updateExperimentStatus_updateExperimentStatus_nimbusExperiment | null;
+}
+
+export interface updateExperimentStatus {
+  /**
+   * Mark a Nimubs Experiment as ready for review.
+   */
+  updateExperimentStatus: updateExperimentStatus_updateExperimentStatus | null;
+}
+
+export interface updateExperimentStatusVariables {
+  input: UpdateExperimentStatusInput;
+}
