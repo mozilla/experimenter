daa9e520eb1e74498247d0797bef8b274f16a601
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2020-11-04 20:55:12 -0500

    fix #3894 chore(nimbus): publish storybooks in separate ci task (#3895)
    Because
    
    * It would be nice to run the storybook publishing in parallel, and
    * It would be nice to have a published storybook for a PR even if it hasn't yet got passing tests or complete test coverage
    
    This commit
    
    * Moves storybook publishing to a separate circle ci task

--- scripts/store_git_info.sh
+++ scripts/store_git_info.sh
@@ -1,6 +1,4 @@
-#/bin/sh
 ./scripts/echo_version_json.sh > ./app/experimenter/version.json
 cp ./app/experimenter/version.json ./app/version.json
 git log -n 1 --no-color --pretty='%s' > ./app/commit-summary.txt
 git log -n 1 --no-color --pretty=medium > ./app/commit-description.txt
-docker build --target dev -f app/Dockerfile -t app:dev app/
