9d4e49c116cce12767dfe65be5608ddd468cc191
Les Orchard <lorchard@mozilla.com>
2020-11-10 09:43:56 -0800

    fix #3743 fix #3744 feat(nimbus): feature branch editing UI logic (#3886)
    Because
    
    * we want to manage editing feature branches for an experiment
    
    This commit:
    
    * wires up buttons and form elements in FormBranch and FormBranches
    
    * implements support for adding / removing / changing a feature for configuration (issue #3744)
    
    * implements equal ratio checkbox (issue #3743)
    
    * implements a local reducer for managing editing state

--- app/experimenter/nimbus-ui/src/components/PageEditBranches/index.test.tsx
+++ app/experimenter/nimbus-ui/src/components/PageEditBranches/index.test.tsx
@@ -3,13 +3,23 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from "react";
-import { screen, waitFor, render } from "@testing-library/react";
+import { screen, waitFor, render, fireEvent } from "@testing-library/react";
 import PageEditBranches from ".";
 import FormBranches from "../FormBranches";
 import { RouterSlugProvider } from "../../lib/test-utils";
 import { mockExperimentQuery, MOCK_CONFIG } from "../../lib/mocks";
 
 describe("PageEditBranches", () => {
+  const origConsole = console.log;
+
+  beforeEach(() => {
+    console.log = jest.fn();
+  });
+
+  afterEach(() => {
+    console.log = origConsole;
+  });
+
   it("renders as expected with experiment data", async () => {
     const { mock } = mockExperimentQuery("demo-slug");
     render(<Subject mocks={[mock]} />);
@@ -17,13 +27,37 @@ describe("PageEditBranches", () => {
       expect(screen.getByTestId("PageEditBranches")).toBeInTheDocument();
       expect(screen.getByTestId("header-experiment")).toBeInTheDocument();
     });
-    expect(screen.getByTestId("equal-ratio")).toHaveTextContent("false");
     expect(screen.getByTestId("FormBranches")).toBeInTheDocument();
     expect(screen.getByTestId("feature-config")).toBeInTheDocument();
     for (const feature of MOCK_CONFIG!.featureConfig!) {
       const { slug } = feature!;
       expect(screen.getByText(slug)).toBeInTheDocument();
     }
+  });
+
+  it("handles onSave from FormBranches", async () => {
+    const { mock, data: experiment } = mockExperimentQuery("demo-slug");
+    render(<Subject mocks={[mock]} />);
+    await waitFor(() => {
+      expect(screen.getByTestId("PageEditBranches")).toBeInTheDocument();
+    });
+    fireEvent.click(await screen.findByTestId("save-button"));
+    expect(console.log).toHaveBeenCalledWith("SAVE", {
+      equalRatio: true,
+      featureConfig: experiment!.featureConfig,
+      referenceBranch: experiment!.referenceBranch,
+      treatmentBranches: experiment!.treatmentBranches,
+    });
+  });
+
+  it("handles onNext from FormBranches", async () => {
+    const { mock } = mockExperimentQuery("demo-slug");
+    render(<Subject mocks={[mock]} />);
+    await waitFor(() => {
+      expect(screen.getByTestId("PageEditBranches")).toBeInTheDocument();
+    });
+    fireEvent.click(screen.getByTestId("next-button"));
+    expect(console.log).toHaveBeenCalledWith("NEXT");
   });
 });
 
@@ -41,15 +75,21 @@ jest.mock("../FormBranches", () => ({
   __esModule: true,
   default: ({
     experiment,
-    equalRatio,
     featureConfig,
+    onSave,
+    onNext,
   }: React.ComponentProps<typeof FormBranches>) => {
+    const saveState = {
+      equalRatio: true,
+      featureConfig: experiment.featureConfig,
+      referenceBranch: experiment.referenceBranch,
+      treatmentBranches: experiment.treatmentBranches,
+    };
     return (
       <div data-testid="FormBranches">
         {experiment && (
           <span data-testid="experiment-slug">{experiment.slug}</span>
         )}
-        <span data-testid="equal-ratio">{equalRatio ? "true" : "false"}</span>
         {featureConfig && (
           <ul data-testid="feature-config">
             {featureConfig.map(
@@ -58,6 +98,16 @@ jest.mock("../FormBranches", () => ({
             )}
           </ul>
         )}
+        <button data-testid="next-button" onClick={() => onNext()}>
+          Next
+        </button>
+        <button
+          data-testid="save-button"
+          type="submit"
+          onClick={() => onSave(saveState)}
+        >
+          <span>Save</span>
+        </button>
       </div>
     );
   },
