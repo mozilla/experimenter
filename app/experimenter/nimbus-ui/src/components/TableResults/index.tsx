eb4e97cd483bafaf139242d01640ad1f0f2b2cce
Lauren Zugai <lauren@zugai.com>
2020-11-19 10:52:46 -0600

    fixes #3977 feat(nimbus): Add Results table to Results page (#4045)
    * fixes #3977 feat(nimbus): Add visualization Results table to Results page
    
    This commit:
    * Adds the Results table, copied from the 'rapid' legacy project, into the Results page
    
    Because:
    * We want to be able to see this table when the analysis data is available.
    
    * Add additional TableVisualizationRow stories

--- app/experimenter/nimbus-ui/src/components/TableResults/index.tsx
+++ app/experimenter/nimbus-ui/src/components/TableResults/index.tsx
@@ -3,9 +3,101 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from "react";
+import {
+  RESULTS_METRICS_LIST,
+  METRICS_TIPS,
+  METRIC_TYPE,
+  TABLE_LABEL,
+} from "../../lib/visualization/constants";
+import { AnalysisData } from "../../lib/visualization/types";
+import { getTableDisplayType } from "../../lib/visualization/utils";
+import { getExperiment_experimentBySlug_primaryProbeSets } from "../../types/getExperiment";
+import TableVisualizationRow from "../TableVisualizationRow";
 
-const TableResults: React.FunctionComponent = () => {
-  return <div data-testid="table-results">results table</div>;
+type TableResultsProps = {
+  primaryProbeSets: (getExperiment_experimentBySlug_primaryProbeSets | null)[];
+  results: AnalysisData["overall"];
+};
+
+const getResultMetrics = (
+  probeSets: (getExperiment_experimentBySlug_primaryProbeSets | null)[],
+) => {
+  // Make a copy of `RESULTS_METRICS_LIST` since we modify it.
+  const resultsMetricsList = [...RESULTS_METRICS_LIST];
+  probeSets.forEach((probeSet) => {
+    resultsMetricsList.unshift({
+      value: `${probeSet!.slug.replace(/-/g, "_")}_ever_used`,
+      name: `${probeSet!.name} Conversion`,
+      tooltip: METRICS_TIPS.CONVERSION,
+      type: METRIC_TYPE.PRIMARY,
+    });
+  });
+
+  return resultsMetricsList;
+};
+
+const TableResults = ({
+  primaryProbeSets,
+  results = {},
+}: TableResultsProps) => {
+  const resultsMetricsList = getResultMetrics(primaryProbeSets);
+
+  return (
+    <table className="table text-center mb-5" data-testid="table-results">
+      <thead>
+        <tr>
+          <th scope="col" className="border-bottom-0" />
+          {resultsMetricsList.map((metric, index) => {
+            const badgeClass = `badge ${metric.type?.badge}`;
+            return (
+              <th
+                key={index}
+                scope="col"
+                className="border-bottom-0 align-middle"
+              >
+                <h3 className="h6 mb-0" data-tip={metric.tooltip}>
+                  {metric.name}
+                </h3>
+                {metric.type && (
+                  <span className={badgeClass} data-tip={metric.type.tooltip}>
+                    {metric.type.label}
+                  </span>
+                )}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(results).map((branch) => {
+          return (
+            <tr key={branch}>
+              <th className="align-middle" scope="row">
+                {branch}
+              </th>
+              {resultsMetricsList.map((metric) => {
+                const metricKey = metric.value;
+                const displayType = getTableDisplayType(
+                  metricKey,
+                  TABLE_LABEL.RESULTS,
+                  results[branch]["is_control"],
+                );
+                return (
+                  <TableVisualizationRow
+                    key={metricKey}
+                    results={results[branch]}
+                    tableLabel={TABLE_LABEL.RESULTS}
+                    {...{ metricKey }}
+                    {...{ displayType }}
+                  />
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default TableResults;
