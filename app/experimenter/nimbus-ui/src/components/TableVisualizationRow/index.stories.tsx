e584700697b88bcd2d044cdb8e65ac97f9ff21b6
Marina Samuel <msamuel@mozilla.com>
2020-11-19 14:11:27 -0500

    fix #3974 chore(visualization): Copy primary metric table table from  into TableMetricPrimary component. (#4036)

--- app/experimenter/nimbus-ui/src/components/TableVisualizationRow/index.stories.tsx
+++ app/experimenter/nimbus-ui/src/components/TableVisualizationRow/index.stories.tsx
@@ -9,7 +9,11 @@ import { withLinks } from "@storybook/addon-links";
 import { mockExperimentQuery } from "../../lib/mocks";
 import TableVisualizationRow from ".";
 import { mockAnalysis } from "../../lib/visualization/mocks";
-import { DISPLAY_TYPE, TABLE_LABEL } from "../../lib/visualization/constants";
+import {
+  DISPLAY_TYPE,
+  BRANCH_COMPARISON,
+  TABLE_LABEL,
+} from "../../lib/visualization/constants";
 
 const { mock } = mockExperimentQuery("demo-slug");
 
@@ -47,4 +51,64 @@ storiesOf("visualization/TableVisualizationRow", module)
         displayType={DISPLAY_TYPE.PERCENT}
       />
     </RouterSlugProvider>
-  ));
+  ))
+  .add("Conversion count field", () => {
+    const MOCK_ANALYSIS = mockAnalysis();
+    return (
+      <RouterSlugProvider mocks={[mock]}>
+        <TableVisualizationRow
+          key={DISPLAY_TYPE.CONVERSION_COUNT}
+          branchComparison={BRANCH_COMPARISON.ABSOLUTE}
+          displayType={DISPLAY_TYPE.CONVERSION_COUNT}
+          results={MOCK_ANALYSIS.overall.treatment}
+          tableLabel={TABLE_LABEL.PRIMARY_METRICS}
+          metricKey="picture_in_picture_ever_used"
+        />
+      </RouterSlugProvider>
+    );
+  })
+  .add("Conversion change field (positive)", () => {
+    const MOCK_ANALYSIS = mockAnalysis();
+    return (
+      <RouterSlugProvider mocks={[mock]}>
+        <TableVisualizationRow
+          key={DISPLAY_TYPE.CONVERSION_COUNT}
+          branchComparison={BRANCH_COMPARISON.UPLIFT}
+          displayType={DISPLAY_TYPE.CONVERSION_CHANGE}
+          results={MOCK_ANALYSIS.overall.treatment}
+          tableLabel={TABLE_LABEL.PRIMARY_METRICS}
+          metricKey="picture_in_picture_ever_used"
+        />
+      </RouterSlugProvider>
+    );
+  })
+  .add("Conversion change field (negative)", () => {
+    const MOCK_ANALYSIS = mockAnalysis();
+    return (
+      <RouterSlugProvider mocks={[mock]}>
+        <TableVisualizationRow
+          key={DISPLAY_TYPE.CONVERSION_COUNT}
+          branchComparison={BRANCH_COMPARISON.UPLIFT}
+          displayType={DISPLAY_TYPE.CONVERSION_CHANGE}
+          results={MOCK_ANALYSIS.overall.treatment}
+          tableLabel={TABLE_LABEL.PRIMARY_METRICS}
+          metricKey="feature_b_ever_used"
+        />
+      </RouterSlugProvider>
+    );
+  })
+  .add("Conversion change field (neutral)", () => {
+    const MOCK_ANALYSIS = mockAnalysis();
+    return (
+      <RouterSlugProvider mocks={[mock]}>
+        <TableVisualizationRow
+          key={DISPLAY_TYPE.CONVERSION_COUNT}
+          branchComparison={BRANCH_COMPARISON.UPLIFT}
+          displayType={DISPLAY_TYPE.CONVERSION_CHANGE}
+          results={MOCK_ANALYSIS.overall.treatment}
+          tableLabel={TABLE_LABEL.PRIMARY_METRICS}
+          metricKey="feature_c_ever_used"
+        />
+      </RouterSlugProvider>
+    );
+  });
