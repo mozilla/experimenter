906e6114f4e331feb3811b826578151d997c9799
Jody Heavener <j.heavener@gmail.com>
2020-12-05 12:32:52 -0500

    fixes #4017 #4018 feat(nimbus): support newlines and links in hypothesis text

---
+++ app/experimenter/nimbus-ui/src/components/RichText/index.test.tsx
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+import { render } from "@testing-library/react";
+import RichText from ".";
+
+it("renders text with newlines converted to <br> tags", () => {
+  const text = `Aliens
+
+  Galactic Federation
+
+  We are not ready`;
+
+  const result = render(<RichText {...{ text }} />);
+
+  // Each return should be replaced with a <br />
+  const lineBreaks = result.container.querySelectorAll("br");
+  expect(lineBreaks).toHaveLength(4);
+});
+
+it("renders text with strings that look like links converted to <a> tags", () => {
+  const url1 = "https://www.mozilla.org/about/manifesto";
+  const url2 = "http://foundation.mozilla.org/";
+  const url3 = "www.mozilla.org/contribute";
+  const url4 = "mozilla.org/about/leadership/";
+  const text = `Mozilla Manifesto: ${url1}
+  Mozilla Foundation: ${url2}
+  Get Involved: ${url3}
+  Leadership: ${url4}`;
+
+  const result = render(<RichText {...{ text }} />);
+
+  const anchors = result.container.querySelectorAll("a");
+  expect(anchors).toHaveLength(3);
+
+  expect(anchors[0]).toHaveProperty("href", url1);
+  expect(anchors[0]).toHaveTextContent(url1);
+
+  expect(anchors[1]).toHaveProperty("href", url2);
+  expect(anchors[1]).toHaveTextContent(url2);
+
+  // Non protocol links should be converted, with the text
+  // unchanged but the href prepended with https://
+  expect(anchors[2]).toHaveProperty("href", `https://${url3}`);
+  expect(anchors[2]).toHaveTextContent(url3);
+});
