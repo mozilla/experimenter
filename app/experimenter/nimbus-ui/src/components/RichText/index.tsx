0a573bb69e9d3437915f0e295fe042b7992e07a8
Jody Heavener <j.heavener@gmail.com>
2020-12-09 10:59:33 -0500

    fixes #4017 #4018 feat(nimbus): support newlines and links in hypothesis text (#4125)

---
+++ app/experimenter/nimbus-ui/src/components/RichText/index.tsx
@@ -0,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+import LinkExternal from "../LinkExternal";
+
+// Match http://, https://, www., a combo, but not neither
+const LINK_MATCHER = /((?<!\+)(?:https?:\/\/(?:www\.)?|www\.)(?:[a-zA-Z\d-_.]+(?:(?:\.|@)[a-zA-Z\d]{2,}))(?:(?:[-a-zA-Z\d:%_+.~#*$!?&//=@]*)(?:[,](?![\s]))*)*)/gi;
+const NEWLINE_MATCHER = /(\r\n|\r|\n)/gi;
+
+const RichText = ({ text }: { text: string }) => (
+  <>
+    {text
+      .split(
+        new RegExp(`${LINK_MATCHER.source}|${NEWLINE_MATCHER.source}`, "gi"),
+      )
+      .map((word, idx) => {
+        const linkMatch = word?.match(LINK_MATCHER);
+        const newlineMatch = word?.match(NEWLINE_MATCHER);
+
+        if (linkMatch) {
+          let href = (text = linkMatch[0]);
+          if (!href.startsWith("http")) {
+            href = `https://${href}`;
+          }
+          return <LinkExternal {...{ href, key: idx }}>{text}</LinkExternal>;
+        }
+
+        if (newlineMatch) {
+          return (
+            <React.Fragment key={idx}>
+              {word}
+              <br />
+            </React.Fragment>
+          );
+        }
+
+        return word;
+      })}
+  </>
+);
+
+export default RichText;
