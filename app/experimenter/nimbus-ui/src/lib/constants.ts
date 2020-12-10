1855ecc1403dcd996706a9a85a7ac846a567cf31
Jody Heavener <j.heavener@gmail.com>
2020-12-10 16:09:33 -0500

    fixes #3971 feat(nimbus): promote Nimbus UI to /nimbus, removing /experiments from path (#4226)

--- app/experimenter/nimbus-ui/src/lib/constants.ts
+++ app/experimenter/nimbus-ui/src/lib/constants.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export const BASE_PATH = "/experiments/nimbus";
+export const BASE_PATH = "/nimbus";
 
 export const UNKNOWN_ERROR =
   "Sorry, an error occurred but we don't know why. We're looking into it.";
--- app/experimenter/urls.py
+++ app/experimenter/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from experimenter.experiments.views import ExperimentListView
+from experimenter.experiments.views import ExperimentListView, NimbusUIView
 
 urlpatterns = [
     re_path(r"^api/v1/experiments/", include("experimenter.experiments.api.v1.urls")),
@@ -13,6 +13,8 @@ urlpatterns = [
     re_path(r"^api/v6/", include("experimenter.experiments.api.v6.urls")),
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^experiments/", include("experimenter.experiments.urls")),
+    re_path(r"^nimbus/", NimbusUIView.as_view(), name="nimbus-list"),
+    re_path(r"^nimbus/(?P<slug>[\w-]+)/", NimbusUIView.as_view(), name="nimbus-detail"),
     re_path(r"^$", ExperimentListView.as_view(), name="home"),
 ]
 
