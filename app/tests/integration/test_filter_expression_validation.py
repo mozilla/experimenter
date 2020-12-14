ea43dd041f26924a176d141596186fb8361ddbb2
jrbenny35 <bennyjr169@gmail.com>
2020-12-14 14:11:36 -0600

    Add JEXL filter validation tests.

--- app/tests/integration/test_filter_expression_validation.py
+++ app/tests/integration/test_filter_expression_validation.py
@@ -1,7 +1,18 @@
-def test_validating_filter_expressions(base_url, selenium):
+def test_filter_expressions_with_matching_firefox_versions(
+    base_url, selenium
+):
     selenium.get("about:blank")
     with open("utils/filter_expression.js") as js:
         with selenium.context(selenium.CONTEXT_CHROME):
-            script = selenium.execute_script(js.read(), "filter", 80, "userID")
-    print(script)
-    assert False
+            script = selenium.execute_script(js.read(), 80.1, 80)
+    assert script is True
+
+
+def test_filter_expressions_with_mismatching_firefox_versions(
+    base_url, selenium
+):
+    selenium.get("about:blank")
+    with open("utils/filter_expression.js") as js:
+        with selenium.context(selenium.CONTEXT_CHROME):
+            script = selenium.execute_script(js.read(), 80.1, 100)
+    assert script is False
--- app/tests/integration/utils/filter_expression.js
+++ app/tests/integration/utils/filter_expression.js
@@ -2,38 +2,21 @@ Components.utils.import("resource://gre/modules/components-utils/FilterExpressio
 Components.utils.import("resource://gre/modules/components-utils/ClientEnvironment.jsm");
 Components.utils.import("resource://messaging-system/lib/RemoteSettingsExperimentLoader.jsm");
 
-// Filter expression
-var minFirefoxVersion = arguments[1];
-var audienceFilterExpression = undefined;
-
 // Targeting
-var randomizationUnit = arguments[2];
-var bucketNamespace = "foo-1232ad3";
-var bucketStart = 0;
-var bucketCount = 50;
-var bucketTotal = 100;
-var audienceTargeting = "localeLanguageCode == 'en'";
 
-var recipe = {
-// These are your preset expressions, converted to JS
-filter_expression: `env.version|versionCompare('${minFirefoxVersion}') >= 0 && ${audienceFilterExpression}`,
-targeting: `[${randomizationUnit}, "${bucketNamespace}"]|bucketSample(${bucketStart}, ${bucketCount}, ${bucketTotal}) && ${audienceTargeting}`
-};
-async function myFunction(recipe) {
-    let res = await FilterExpressions.eval(recipe.filter_expression, {env: ClientEnvironmentBase});
-    if (res == undefined) {
-        return "undefined"
-    }
-    return res
-};
-async function remoteSettings(recipe) {
-    let res = await RemoteSettingsExperimentLoader.checkTargeting(recipe);
-    if (res == undefined) {
-        return "undefined"
-    }
-    return res
+async function remoteSettings(arguments) {
+
+    const { TargetingContext } = ChromeUtils.import(
+        "resource://messaging-system/targeting/Targeting.jsm"
+    );
+    const ENVIRONMENT = {version: arguments[0], localeLanguageCode: "en-US"};
+    let targeting = new TargetingContext(ENVIRONMENT);
+    const EXPRESSION = `version|versionCompare('${arguments[1]}.!') >= 0 && localeLanguageCode == 'en'`;
+    console.log(EXPRESSION);
+    const actual = await targeting.evalWithDefault(EXPRESSION, ENVIRONMENT);
+    return actual
 }
 
-let results = {"filter": myFunction(recipe), "remote": remoteSettings(recipe)}
+let results = remoteSettings(arguments)
 
-return results[arguments[0]];
+return results;
