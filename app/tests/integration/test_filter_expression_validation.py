1f3923a2da54f974b00f422d4a4759ba9f796ff9
Benjamin Forehand Jr <bennyjr169@gmail.com>
2020-12-15 11:28:09 -0600

    Add filter expression tests (#4261)
    * Initial commit
    
    * More updates..
    
    * Add JEXL filter validation tests.
    
    * Remove changes.
    
    * Remove debug stuff.

---
+++ app/tests/integration/test_filter_expression_validation.py
@@ -0,0 +1,18 @@
+def test_filter_expressions_with_matching_firefox_versions(
+    base_url, selenium
+):
+    selenium.get("about:blank")
+    with open("utils/filter_expression.js") as js:
+        with selenium.context(selenium.CONTEXT_CHROME):
+            script = selenium.execute_script(js.read(), 80.1, 80)
+    assert script is True
+
+
+def test_filter_expressions_with_mismatching_firefox_versions(
+    base_url, selenium
+):
+    selenium.get("about:blank")
+    with open("utils/filter_expression.js") as js:
+        with selenium.context(selenium.CONTEXT_CHROME):
+            script = selenium.execute_script(js.read(), 80.1, 100)
+    assert script is False
