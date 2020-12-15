95eb5b3f48fa85a90ad09516569291783f07a5e7
Benjamin Forehand Jr <bennyjr169@gmail.com>
2020-12-15 11:28:09 -0600

    Add filter expression tests (#4261)
    * Initial commit
    
    * More updates..
    
    * Add JEXL filter validation tests.
    
    * Remove changes.
    
    * Remove debug stuff.

---
+++ app/tests/integration/utils/filter_expression.js
@@ -0,0 +1,21 @@
+Components.utils.import("resource://gre/modules/components-utils/FilterExpressions.jsm");
+Components.utils.import("resource://gre/modules/components-utils/ClientEnvironment.jsm");
+Components.utils.import("resource://messaging-system/lib/RemoteSettingsExperimentLoader.jsm");
+
+// Targeting
+
+async function remoteSettings(arguments) {
+
+    const { TargetingContext } = ChromeUtils.import(
+        "resource://messaging-system/targeting/Targeting.jsm"
+    );
+    const ENVIRONMENT = {version: arguments[0], localeLanguageCode: "en-US"};
+    let targeting = new TargetingContext(ENVIRONMENT);
+    const EXPRESSION = `version|versionCompare('${arguments[1]}.!') >= 0 && localeLanguageCode == 'en'`;
+    const actual = await targeting.evalWithDefault(EXPRESSION, ENVIRONMENT);
+    return actual
+}
+
+let results = remoteSettings(arguments)
+
+return results;
