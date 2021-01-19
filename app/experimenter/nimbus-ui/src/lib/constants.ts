417537adef942c129a3ba3af842dd18874ee750a
Jody Heavener <j.heavener@gmail.com>
2021-01-19 13:47:22 -0500

    fix #4417 feat(nimbus): move hard-coded external links into constant (#4443)

--- app/experimenter/nimbus-ui/src/lib/constants.ts
+++ app/experimenter/nimbus-ui/src/lib/constants.ts
@@ -8,3 +8,15 @@ export const UNKNOWN_ERROR =
   "Sorry, an error occurred but we don't know why. We're looking into it.";
 export const SUBMIT_ERROR =
   "Sorry, an error occurred while submitting. Please try again.";
+
+export const EXTERNAL_URLS = {
+  RISK_MITIGATION_TEMPLATE_DOC:
+    "https://docs.google.com/document/d/1zfG2g6pYe9aB7ItViQaw8OcOsXXdRUP70zpZoNC2xcA/edit",
+  NIMBUS_MANA_DOC: "https://mana.mozilla.org/wiki/display/FJT/Project+Nimbus",
+  WORKFLOW_MANA_DOC:
+    "https://mana.mozilla.org/wiki/pages/viewpage.action?pageId=109990007",
+  BRANCHES_GOOGLE_DOC:
+    "https://docs.google.com/document/d/155EUgzn22VTX8mFwesSROT3Z6JORSfb5VyoMoLra7ws/edit#heading=h.i8g4ppfvkq0x",
+  METRICS_GOOGLE_DOC:
+    "https://docs.google.com/document/d/155EUgzn22VTX8mFwesSROT3Z6JORSfb5VyoMoLra7ws/edit#heading=h.uq23fsvh16rc",
+};
