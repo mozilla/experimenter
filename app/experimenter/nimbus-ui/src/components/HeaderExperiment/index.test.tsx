42d907fba55d2ec402acb6167e3c92457c00382d
Jody Heavener <j.heavener@gmail.com>
2021-02-02 13:29:18 -0500

    fixes #3972 feat(nimbus): add handy links to header when in summary view

--- app/experimenter/nimbus-ui/src/components/HeaderExperiment/index.test.tsx
+++ app/experimenter/nimbus-ui/src/components/HeaderExperiment/index.test.tsx
@@ -2,12 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import React from "react";
 import HeaderExperiment from ".";
 import { humanDate } from "../../lib/dateUtils";
 import { mockExperimentQuery, mockGetStatus } from "../../lib/mocks";
 import { NimbusExperimentStatus } from "../../types/globalTypes";
+
+const assertStatusLink = async (
+  status: NimbusExperimentStatus,
+  label: string,
+  route: string,
+) => {
+  const { experiment } = mockExperimentQuery("demo-slug", {
+    status,
+  });
+  render(
+    <HeaderExperiment
+      name={experiment.name}
+      slug={experiment.slug}
+      startDate={experiment.startDate}
+      endDate={experiment.endDate}
+      status={mockGetStatus(experiment.status)}
+      summaryView
+    />,
+  );
+  const statusLink = await screen.findByTestId("status-link");
+  expect(statusLink).toHaveTextContent(label);
+  expect(statusLink).toHaveAttribute("href", `/${route}`);
+  cleanup();
+};
 
 describe("HeaderExperiment", () => {
   it("renders as expected", () => {
@@ -54,5 +78,42 @@ describe("HeaderExperiment", () => {
     expect(screen.getByTestId("header-dates")).toHaveTextContent(
       humanDate(experiment.endDate!),
     );
+  });
+
+  describe("summary view", () => {
+    it("displays the return to experiments link", async () => {
+      const { experiment } = mockExperimentQuery("demo-slug");
+      render(
+        <HeaderExperiment
+          name={experiment.name}
+          slug={experiment.slug}
+          startDate={experiment.startDate}
+          endDate={experiment.endDate}
+          status={mockGetStatus(experiment.status)}
+          summaryView
+        />,
+      );
+      await screen.findByTestId("experiment-return");
+    });
+
+    it("displays the correct status links", async () => {
+      await assertStatusLink(
+        NimbusExperimentStatus.DRAFT,
+        "Edit Experiment",
+        "edit",
+      );
+
+      await assertStatusLink(
+        NimbusExperimentStatus.REVIEW,
+        "Go to Review",
+        "request-review",
+      );
+
+      await assertStatusLink(
+        NimbusExperimentStatus.COMPLETE,
+        "Go to Design",
+        "design",
+      );
+    });
   });
 });
