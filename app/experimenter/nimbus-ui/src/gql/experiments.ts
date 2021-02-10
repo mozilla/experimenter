6720222fd254e4b79bc3b18a96065f9984ee47b2
Jody Heavener <j.heavener@gmail.com>
2021-02-10 16:54:40 -0500

    feat(nimbus): remove limit, offset, and status filter from experiments query entirely

--- app/experimenter/nimbus-ui/src/gql/experiments.ts
+++ app/experimenter/nimbus-ui/src/gql/experiments.ts
@@ -191,7 +191,6 @@ export const GET_EXPERIMENT_QUERY = gql`
   }
 `;
 
-// TODO: Only the first 50 until we add pagination.
 export const GET_EXPERIMENTS_QUERY = gql`
   query getAllExperiments {
     experiments {
