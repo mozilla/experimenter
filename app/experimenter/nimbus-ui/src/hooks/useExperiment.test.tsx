f53bd0b42edb51b763c07f4042843f12220ace97
Jody Heavener <j.heavener@gmail.com>
2021-02-22 19:57:27 -0500

    feat(nimbus): refactor useAnalysis and useExperiment hooks to use RTL react-hooks (#4666)

--- app/experimenter/nimbus-ui/src/hooks/useExperiment.test.tsx
+++ app/experimenter/nimbus-ui/src/hooks/useExperiment.test.tsx
@@ -2,163 +2,153 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { render, waitFor } from "@testing-library/react";
+import { MockedResponse } from "@apollo/client/testing";
+import { waitFor } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 import React from "react";
 import { MockedCache, mockExperimentQuery } from "../lib/mocks";
 import { useExperiment } from "./useExperiment";
 
 describe("hooks/useExperiment", () => {
-  describe("useExperiment", () => {
-    let hook: ReturnType<typeof useExperiment>;
+  it("starts by loading", async () => {
+    const { result } = renderHook(() => useExperiment("howdy"), {
+      wrapper,
+    });
+    expect(result.current.loading).toBeTruthy();
+  });
 
-    const TestExperiment = ({ slug }: { slug: string }) => {
-      hook = useExperiment(slug);
-      return <p>Notre-Dame-du-Bon-Conseil, QC</p>;
+  it("loads the experiment", async () => {
+    const { mock, experiment } = mockExperimentQuery("howdy");
+    const { result } = renderHook(() => useExperiment(experiment.slug), {
+      wrapper,
+      initialProps: { mocks: [mock] },
+    });
+    await waitFor(() => expect(result.current.experiment).toEqual(experiment));
+  });
+
+  it("indicates notFound if there's no experiment", async () => {
+    const { mock } = mockExperimentQuery("howdy", null);
+    const { result } = renderHook(() => useExperiment("howdy"), {
+      wrapper,
+      initialProps: { mocks: [mock] },
+    });
+    await waitFor(() => expect(result.current.notFound).toBeTruthy());
+  });
+
+  describe("ready for review", () => {
+    const readyMessages = {
+      public_description: ["This field may not be null."],
+      proposed_duration: ["This field may not be null."],
+      proposed_enrollment: ["This field may not be null."],
+      firefox_min_version: ["This field may not be null."],
+      targeting_config_slug: ["This field may not be null."],
+      reference_branch: ["This field may not be null."],
+      channel: ["This field may not be null."],
+      population_percent: [
+        "Ensure this value is greater than or equal to 0.0001.",
+      ],
     };
 
-    it("returns the experiment", async () => {
-      const { mock, experiment } = mockExperimentQuery("howdy");
+    const pageNames = {
+      public_description: "overview",
+      proposed_duration: "audience",
+      proposed_enrollment: "audience",
+      firefox_min_version: "audience",
+      targeting_config_slug: "audience",
+      reference_branch: "branches",
+      channel: "audience",
+      population_percent: "audience",
+    };
 
-      render(
-        <MockedCache mocks={[mock]}>
-          <TestExperiment slug="howdy" />
-        </MockedCache>,
-      );
-
-      await waitFor(() => expect(hook.experiment).toEqual(experiment));
-    });
-
-    it("returns notFound if no experiment found", async () => {
-      const { mock } = mockExperimentQuery("howdy", null);
-
-      render(
-        <MockedCache mocks={[mock]}>
-          <TestExperiment slug="howdy" />
-        </MockedCache>,
-      );
-
-      await waitFor(() => expect(hook.notFound).toBeTruthy());
-    });
-
-    describe("ready for review", () => {
-      const readyMessages = {
-        public_description: ["This field may not be null."],
-        proposed_duration: ["This field may not be null."],
-        proposed_enrollment: ["This field may not be null."],
-        firefox_min_version: ["This field may not be null."],
-        targeting_config_slug: ["This field may not be null."],
-        reference_branch: ["This field may not be null."],
-        channel: ["This field may not be null."],
-        population_percent: [
-          "Ensure this value is greater than or equal to 0.0001.",
-        ],
-      } as const;
-
-      const pageNames = {
-        public_description: "overview",
-        proposed_duration: "audience",
-        proposed_enrollment: "audience",
-        firefox_min_version: "audience",
-        targeting_config_slug: "audience",
-        reference_branch: "branches",
-        channel: "audience",
-        population_percent: "audience",
-      } as const;
-
-      const commonMissingDetailsTest = (
-        fieldName: keyof typeof readyMessages,
-      ) => async () => {
-        const readyMessage = {
-          [fieldName]: readyMessages[fieldName],
-        };
-        const pageName = pageNames[fieldName];
-
-        Object.defineProperty(window, "location", {
-          value: {
-            search: "?show-errors",
-          },
-        });
-
-        const { mock } = mockExperimentQuery("howdy", {
-          readyForReview: {
-            ready: false,
-            message: readyMessage,
-          },
-        });
-
-        render(
-          <MockedCache mocks={[mock]}>
-            <TestExperiment slug="howdy" />
-          </MockedCache>,
-        );
-
-        await waitFor(() => {
-          const {
-            ready,
-            invalidPages,
-            missingFields,
-            isMissingField,
-          } = hook.review;
-
-          expect(ready).toBeFalsy();
-          expect(invalidPages).toEqual(expect.arrayContaining([pageName]));
-          expect(missingFields).toEqual(
-            expect.arrayContaining(Object.keys(readyMessage)),
-          );
-          Object.keys(readyMessage).forEach((fieldName) => {
-            expect(isMissingField(fieldName)).toBeTruthy();
-          });
-        });
-      };
-
-      let fieldName: keyof typeof readyMessages;
-      for (fieldName in readyMessages) {
-        it(
-          `returns correct review info when missing details for ${fieldName}`,
-          commonMissingDetailsTest(fieldName),
-        );
-      }
-
-      it("returns correct review info when not missing any details", async () => {
-        const { mock } = mockExperimentQuery("howdy", {
-          readyForReview: {
-            ready: true,
-            message: {},
-          },
-        });
-
-        render(
-          <MockedCache mocks={[mock]}>
-            <TestExperiment slug="howdy" />
-          </MockedCache>,
-        );
-
-        await waitFor(() => {
-          const {
-            ready,
-            invalidPages,
-            missingFields,
-            isMissingField,
-          } = hook.review;
-
-          expect(ready).toBeTruthy();
-          expect(invalidPages).toEqual([]);
-          expect(missingFields).toEqual([]);
-          Object.keys(readyMessages).forEach((fieldName) => {
-            expect(isMissingField(fieldName)).toBeFalsy();
-          });
-        });
+    beforeAll(() => {
+      Object.defineProperty(window, "location", {
+        value: {
+          search: "?show-errors",
+        },
       });
     });
 
-    it("starts by loading", async () => {
-      render(
-        <MockedCache mocks={[]}>
-          <TestExperiment slug="howdy" />
-        </MockedCache>,
-      );
+    const missingDetailsTest = (
+      fieldName: keyof typeof readyMessages,
+    ) => async () => {
+      const readyMessage = {
+        [fieldName]: readyMessages[fieldName],
+      };
+      const pageName = pageNames[fieldName];
+      const { mock } = mockExperimentQuery("howdy", {
+        readyForReview: {
+          ready: false,
+          message: readyMessage,
+        },
+      });
+      const { result } = renderHook(() => useExperiment("howdy"), {
+        wrapper,
+        initialProps: { mocks: [mock] },
+      });
 
-      expect(hook.loading).toBeTruthy();
+      await waitFor(() => {
+        const {
+          ready,
+          invalidPages,
+          missingFields,
+          isMissingField,
+        } = result.current.review;
+        expect(ready).toBeFalsy();
+        expect(invalidPages).toEqual(expect.arrayContaining([pageName]));
+        expect(missingFields).toEqual(
+          expect.arrayContaining(Object.keys(readyMessage)),
+        );
+        for (const fieldName of Object.keys(readyMessage)) {
+          expect(isMissingField(fieldName)).toBeTruthy();
+        }
+      });
+    };
+
+    let fieldName: keyof typeof readyMessages;
+    for (fieldName in readyMessages) {
+      it(
+        `returns correct review info when missing details for ${fieldName}`,
+        missingDetailsTest(fieldName),
+      );
+    }
+
+    it("returns correct review info when not missing any details", async () => {
+      const { mock } = mockExperimentQuery("howdy", {
+        readyForReview: {
+          ready: true,
+          message: {},
+        },
+      });
+
+      const { result } = renderHook(() => useExperiment("howdy"), {
+        wrapper,
+        initialProps: { mocks: [mock] },
+      });
+
+      await waitFor(() => {
+        const {
+          ready,
+          invalidPages,
+          missingFields,
+          isMissingField,
+        } = result.current.review;
+        expect(ready).toBeTruthy();
+        expect(invalidPages).toEqual([]);
+        expect(missingFields).toEqual([]);
+        for (const fieldName in readyMessages) {
+          expect(isMissingField(fieldName)).toBeFalsy();
+        }
+      });
     });
   });
 });
+
+const wrapper = ({
+  mocks = [],
+  children,
+}: {
+  mocks?: MockedResponse[];
+  children?: React.ReactNode;
+}) => (
+  <MockedCache {...{ mocks }}>{children as React.ReactElement}</MockedCache>
+);
