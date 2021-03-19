0835d69725fec2593e1df2ccc583e72e4a734c15
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-03-19 16:19:52 -0400

    fix #4785 chore(nimbus): make admin fields optional (#4847)
    Because
    
    * All admin fields for a newly created experiment should be optional
    * Default empty fields being required makes using the admin cumbersome
    
    This commit
    
    * Makes all fields that have default values optional in the admin
    * Updates tests

--- app/experimenter/experiments/tests/test_admin/test_admin_nimbus.py
+++ app/experimenter/experiments/tests/test_admin/test_admin_nimbus.py
@@ -1,13 +1,66 @@
 from django.test import TestCase
 
 from experimenter.experiments.admin.nimbus import NimbusExperimentAdminForm
+from experimenter.experiments.models import NimbusExperiment
 from experimenter.experiments.tests.factories import (
     NimbusBranchFactory,
     NimbusExperimentFactory,
 )
+from experimenter.openidc.tests.factories import UserFactory
 
 
 class TestNimbusExperimentAdminForm(TestCase):
+    def test_form_required_fields(self):
+        experiment = NimbusExperiment.objects.create(
+            owner=UserFactory.create(),
+            status=NimbusExperiment.Status.DRAFT,
+            name="name",
+            slug="slug",
+            application=NimbusExperiment.Application.DESKTOP,
+        )
+        form = NimbusExperimentAdminForm(
+            instance=experiment,
+            data={
+                "owner": experiment.owner,
+                "status": experiment.status,
+                "name": experiment.name,
+                "slug": experiment.slug,
+                "proposed_duration": experiment.proposed_duration,
+                "proposed_enrollment": experiment.proposed_enrollment,
+                "population_percent": experiment.population_percent,
+                "total_enrolled_clients": experiment.total_enrolled_clients,
+                "application": experiment.application,
+                "hypothesis": experiment.hypothesis,
+            },
+        )
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_form_saves_outcomes(self):
+        experiment = NimbusExperimentFactory.create_with_status(
+            NimbusExperiment.Status.DRAFT, primary_outcomes=[], secondary_outcomes=[]
+        )
+        form = NimbusExperimentAdminForm(
+            instance=experiment,
+            data={
+                "owner": experiment.owner,
+                "status": experiment.status,
+                "name": experiment.name,
+                "slug": experiment.slug,
+                "proposed_duration": experiment.proposed_duration,
+                "proposed_enrollment": experiment.proposed_enrollment,
+                "population_percent": experiment.population_percent,
+                "total_enrolled_clients": experiment.total_enrolled_clients,
+                "application": experiment.application,
+                "hypothesis": experiment.hypothesis,
+                "primary_outcomes": "outcome1, outcome2",
+                "secondary_outcomes": "outcome3, outcome4",
+            },
+        )
+        self.assertTrue(form.is_valid(), form.errors)
+        experiment = form.save()
+        self.assertEqual(experiment.primary_outcomes, ["outcome1", "outcome2"])
+        self.assertEqual(experiment.secondary_outcomes, ["outcome3", "outcome4"])
+
     def test_form_rejects_any_branch_if_no_experiment_provided(self):
         branch = NimbusBranchFactory.create()
 
