0f131f20f308ff62449289b5fa5aabb875390c53
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-04-15 11:12:40 -0400

    fix #5038 chore(project): include integration tests in format/check
    Because
    
    * We wanna make sure all paths are included in linting/checking so we don't accidentally miss lint issues and let them sneak into other prs
    
    This commit
    
    * Removes `tests` from .dockerignore to ensure that path is included in the test container

--- app/tests/integration/nimbus/pages/overview.py
+++ app/tests/integration/nimbus/pages/overview.py
@@ -1,5 +1,4 @@
 from nimbus.pages.base import Base
-from nimbus.pages.new_experiment import NewExperiment
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
