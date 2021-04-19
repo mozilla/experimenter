be31943d04a87dd59b5e1aaab7627062679ae67e
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-04-19 17:52:02 -0400

    fix #5067 bug(project): revert django to 3.1.8 (#5068)
    Because
    
    * Django 3.2 introduced a broken check: caches.W002
    * Ticket filed with django: https://code.djangoproject.com/ticket/32665#ticket
    
    This commit
    
    * Reverts Django to 3.1.8
    * Introduces test cases for our heartbeat endpoints to detect this failure in the future

--- app/experimenter/base/tests/test_views.py
+++ app/experimenter/base/tests/test_views.py
@@ -1,6 +1,17 @@
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
+
+
+@override_settings(DEBUG=False)
+class TestHeartbeats(TestCase):
+    def test_heartbeat(self):
+        response = self.client.get("/__heartbeat__")
+        self.assertEqual(response.status_code, 200)
+
+    def test_lbheartbeat(self):
+        response = self.client.get("/__lbheartbeat__")
+        self.assertEqual(response.status_code, 200)
 
 
 class TestContextProcessors(TestCase):
