60de609806100c9f71a21690e4dc6df8815e17c9
Marina Samuel <msamuel@mozilla.com>
2021-06-16 12:59:35 -0400

    fixes #5321: fix(visualization): Make results page load faster. (#5318)
    Because:
    * The results page loads really slowly
    * We're going to need the results available in the db for sorting/filtering
    
    This commit:
    * Caches the results in the db as a json blob w/ a daily task to update it
    * Only embeds graphs when a collapsed section is expanded to speed up page load

---
+++ app/experimenter/jetstream/tasks.py
@@ -0,0 +1,61 @@
+import datetime
+
+import markus
+from celery.utils.log import get_task_logger
+
+from experimenter.celery import app
+from experimenter.experiments.constants import NimbusConstants
+from experimenter.experiments.models import NimbusExperiment
+from experimenter.jetstream.client import get_experiment_data
+
+logger = get_task_logger(__name__)
+metrics = markus.get_metrics("jetstream.tasks")
+
+
+@app.task
+@metrics.timer_decorator("fetch_experiment_data")
+def fetch_experiment_data(experiment_id):
+    metrics.incr("fetch_experiment_data.started")
+    try:
+        experiment = NimbusExperiment.objects.get(id=experiment_id)
+        experiment.results_data = get_experiment_data(experiment)
+        experiment.save()
+        metrics.incr("fetch_experiment_data.completed")
+    except Exception as e:
+        metrics.incr("fetch_experiment_data.failed")
+        logger.info(f"Fetching experiment data for {experiment_id} failed: {e}")
+        raise e
+
+
+@app.task
+@metrics.timer_decorator("fetch_jetstream_data")
+def fetch_jetstream_data():
+    metrics.incr("fetch_jetstream_data.started")
+    try:
+        for experiment in NimbusExperiment.objects.all().exclude(
+            status__in=[NimbusExperiment.Status.DRAFT]
+        ):
+            if (
+                experiment.end_date
+                and (
+                    experiment.end_date
+                    + datetime.timedelta(days=NimbusConstants.DAYS_ANALYSIS_BUFFER)
+                ).date()
+                < datetime.date.today()
+            ):
+                metrics.incr("fetch_jetstream_data.skipped")
+                logger.info(
+                    "Skipping cache refresh for old experiment {name}".format(
+                        name=experiment.name
+                    )
+                )
+
+                continue
+
+            logger.info("Fetching Jetstream data for {name}".format(name=experiment.name))
+            fetch_experiment_data.delay(experiment.id)
+            metrics.incr("fetch_jetstream_data.completed")
+    except Exception as e:
+        metrics.incr("fetch_jetstream_data.failed")
+        logger.info(f"Fetching Jetstream data failed: {e}")
+        raise e
--- app/experimenter/nimbus-ui/src/components/PageResults/GraphsWeekly/index.tsx
+++ app/experimenter/nimbus-ui/src/components/PageResults/GraphsWeekly/index.tsx
@@ -101,18 +101,26 @@ const GraphsWeekly = ({
     group,
     weeklyResults,
   );
-  embedGraphs(outcomeSlug, outcomeName, mergedBranchData);
 
   const [open, setOpen] = useState(false);
+  const [embedded, setEmbedded] = useState(false);
   const graphsVisibleClass = !open ? "d-none" : "";
   const graphsHiddenClass = open ? "d-none" : "";
+
+  const handleCollapse = () => {
+    setOpen(!open);
+    if (!embedded) {
+      embedGraphs(outcomeSlug, outcomeName, mergedBranchData);
+    }
+    setEmbedded(true);
+  };
 
   return (
     <>
       {Object.keys(mergedBranchData).length > 0 && (
         <>
           <span
-            onClick={() => setOpen(!open)}
+            onClick={handleCollapse}
             aria-controls="graphs"
             aria-expanded={open}
             className="text-primary btn"
