2a767635784969ef762a6f04ac45e0c9a6d3b337
Benjamin Forehand Jr <bennyjr169@gmail.com>
2021-06-23 12:30:20 -0600

    e2e nimbus remote settings deny test (#5819)
    * Add e2e rejection test.
    
    * Cleanup.
    
    * Code formatting.
    
    * Remove old code.
    
    * Updates based on review.
    
    * Changes based on review. Also edits to older test.
    
    * Remove print.
    
    * Final updates.
    
    * Format code.

--- app/tests/integration/nimbus/pages/home.py
+++ app/tests/integration/nimbus/pages/home.py
@@ -6,25 +6,31 @@ from selenium.webdriver.support import expected_conditions as EC
 class HomePage(Page):
     """Nimbus Home page."""
 
-    _page_wait_locator = (By.CSS_SELECTOR, "#create-new-button")
+    _create_new_btn_locator = (By.CSS_SELECTOR, "#create-new-button")
+    _page_wait_locator = (By.CSS_SELECTOR, ".table")
 
     def __init__(self, selenium, base_url, **kwargs):
         super(HomePage, self).__init__(selenium, base_url, timeout=30, **kwargs)
 
     def wait_for_page_to_load(self):
-        self.wait.until(EC.presence_of_element_located(self._page_wait_locator))
+        self.wait.until(EC.visibility_of_element_located(self._page_wait_locator))
 
         return self
 
     @property
     def tables(self):
-        _table_locator = (By.CSS_SELECTOR, ".tab-content table")
+        _table_locator = (By.CSS_SELECTOR, ".active .directory-table .table")
         self.wait.until(EC.presence_of_element_located(_table_locator))
         tables = self.find_elements(*_table_locator)
         return [self.Tables(self, el) for el in tables]
 
+    @property
+    def tabs(self):
+        _tabs_locator = (By.CSS_SELECTOR, ".nav-item")
+        return self.find_elements(*_tabs_locator)
+
     def create_new_button(self):
-        el = self.find_element(*self._page_wait_locator)
+        el = self.find_element(*self._create_new_btn_locator)
         el.click()
         from nimbus.pages.new_experiment import NewExperiment
 
