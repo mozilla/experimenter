484b27c35e41bf241a1388c6a955251743a5c5d7
Tif Tran <ttran@mozilla.com>
2021-07-15 12:21:35 -0700

    fixes #5803 feat(reporting): Add celery task to generate reportlogs (#5863)
    * fixes #5803 feat(reporting): Add celery task to generate reportlogs
    
     This commit:
    
        * adds a celery task that generates reportlogs from changelogs and normandy history
    
     Because:
        * This will help aggregate data for stats that will be generated

--- app/experimenter/reporting/constants.py
+++ app/experimenter/reporting/constants.py
@@ -34,12 +34,12 @@ class ReportLogConstants(object):
         COMPLETE = "Complete"
 
     class ExperimentType(models.TextChoices):
-        NORMANDY_PREF = "Normandy Pref"
-        NORMANDY_ADDON = "Normandy Addon"
-        NORMANDY_ROLLOUT = "Normandy Rollout"
-        NIMBUS_DESKTOP = "Nimbus Desktop"
-        NIMBUS_FENIX = "Nimbus Fenix"
-        NIMBUS_IOS = "Nimbus IOS"
+        NORMANDY_PREF = "Normandy-Pref"
+        NORMANDY_ADDON = "Normandy-Addon"
+        NORMANDY_ROLLOUT = "Normandy-Rollout"
+        NIMBUS_DESKTOP = "Nimbus-Firefox-Desktop"
+        NIMBUS_FENIX = "Nimbus-Fenix"
+        NIMBUS_IOS = "Nimbus-Ios"
 
     Event = Event
     EventReason = EventReason
