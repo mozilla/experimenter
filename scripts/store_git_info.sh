43e5a029a74a8b3c11c1268d0a541c02ce79855e
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-07-15 11:37:09 -0400

    fix #5946 chore(project): cache docker layers in dockerhub (#5948)
    Because
    
    * It would save time if we push the docker build layers from circleci to dockerhub to reuse locally
    
    This commit
    
    * Uses the new buildkit --cache-from flag to pull in build layers from dockerhub when applicable
    * Builds all stages and pushes them with their layers to dockerhub on every merge to main
    * Configures local builds to pull those layers from dockerhub
    * Removes the build circle stage since each circle task will now be able to pull from dockerhub rather than the local circle docker cache

--- scripts/store_git_info.sh
+++ scripts/store_git_info.sh
@@ -1,4 +1,3 @@
 ./scripts/echo_version_json.sh > ./app/experimenter/version.json
-cp ./app/experimenter/version.json ./app/version.json
 git log -n 1 --no-color --pretty='%s' > ./app/commit-summary.txt
 git log -n 1 --no-color --pretty=medium > ./app/commit-description.txt
