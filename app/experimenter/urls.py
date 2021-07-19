mozilla/experiment-manager

fixes #5864 feat(reporting): Add serializer/endpoint for reportlog and stats
Because:
* We need this information to be seen in the frontend, therefore we need an endpoint with the info

This commit:
* add a v7 api endpoint which returns reportlogs and stats

app/experimenter/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     re_path(r"^api/v3/", include("experimenter.visualization.api.v3.urls")),
     re_path(r"^api/v5/", include("experimenter.experiments.api.v5.urls")),
     re_path(r"^api/v6/", include("experimenter.experiments.api.v6.urls")),
+    re_path(r"^api/v7/", include("experimenter.experiments.api.v7.urls")),
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^experiments/", include("experimenter.experiments.urls")),
     re_path(r"^nimbus/", NimbusUIView.as_view(), name="nimbus-list"),

mozilla/experimenter

app/experimenter/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     re_path(r"^api/v3/", include("experimenter.visualization.api.v3.urls")),
     re_path(r"^api/v5/", include("experimenter.experiments.api.v5.urls")),
     re_path(r"^api/v6/", include("experimenter.experiments.api.v6.urls")),
+    re_path(r"^api/v7/", include("experimenter.reporting.api.v7.urls")),
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^experiments/", include("experimenter.experiments.urls")),
     re_path(r"^nimbus/", NimbusUIView.as_view(), name="nimbus-list"),