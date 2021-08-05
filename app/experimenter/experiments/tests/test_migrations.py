4271b48536f8c29ac7d765d6c11caefd2e2910ef
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-08-05 17:49:11 -0400

    fix #5476 chore(project): delete non ldap users
    Because
    
    * Long ago our auth gate was misconfigured and ~1k non mozillians accessed the production site
    * This created a User entry for each person
    * A small number (~5) of these users interacted with site elements which created relevant changelogs
    * Even after fixing the auth gate, these user accounts persist in the production database
    * This pollutes the User list with a large number of irrelevant users when selecting users from drop downs
    * We should remove these unnecessary account records
    * Deletion in Django will default to cascading the delete so any foreign keyed objects may also be deleted
    * We need to be careful to filter any users with foreign keys out from the deletion
    
    This commit
    
    * Creates a data migration which deletes all users with non mozilla emails
    * Excludes users with any foreign keys on to any Legacy or Nimbus models

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -1,118 +1,42 @@
-from django.utils.text import slugify
+from django.contrib.auth.models import User
 from django_test_migrations.contrib.unittest_case import MigratorTestCase
-from faker import Factory as FakerFactory
 
-from experimenter.experiments.constants import NimbusConstants
-
-faker = FakerFactory.create()
-
-
-def create_experiments(User, NimbusExperiment, *args):
-    owner = User.objects.create(
-        first_name="foo",
-        last_name="bar",
-        email="foo@example.com",
-        username="foobar",
-    )
-    return [
-        NimbusExperiment.objects.create(
-            name=faker.catch_phrase(),
-            slug=slugify(faker.catch_phrase())[: NimbusConstants.MAX_SLUG_LEN],
-            owner=owner,
-            **props,
-        ).id
-        for props in args
-    ]
+from experimenter.experiments.tests.factories import (
+    ExperimentChangeLogFactory,
+    ExperimentCommentFactory,
+    ExperimentFactory,
+    NimbusChangeLogFactory,
+    NimbusExperimentFactory,
+)
+from experimenter.notifications.tests.factories import NotificationFactory
+from experimenter.openidc.tests.factories import UserFactory
 
 
-class TestMigration0174Forward(MigratorTestCase):
-    migrate_from = ("experiments", "0174_nimbusexperiment_results_data")
-    migrate_to = ("experiments", "0175_switch_to_status_next")
+class TestMigration0181Forward(MigratorTestCase):
+    migrate_from = ("experiments", "0180_auto_20210803_1832")
+    migrate_to = ("experiments", "0181_remove_non_ldap_emails")
 
     def prepare(self):
         """Prepare some data before the migration."""
-        self.experiment_ids = create_experiments(
-            self.old_state.apps.get_model("auth", "User"),
-            self.old_state.apps.get_model("experiments", "NimbusExperiment"),
-            dict(
-                status=NimbusConstants.Status.DRAFT,
-                publish_status=NimbusConstants.PublishStatus.REVIEW,
-            ),
-            dict(
-                status=NimbusConstants.Status.DRAFT,
-                publish_status=NimbusConstants.PublishStatus.IDLE,
-            ),
-            dict(
-                status=NimbusConstants.Status.LIVE,
-                publish_status=NimbusConstants.PublishStatus.REVIEW,
-                is_end_requested=True,
-            ),
-            dict(
-                status=NimbusConstants.Status.LIVE,
-                publish_status=NimbusConstants.PublishStatus.APPROVED,
-                is_end_requested=False,
-            ),
-            dict(
-                status=NimbusConstants.Status.COMPLETE,
-                publish_status=NimbusConstants.PublishStatus.IDLE,
-                is_end_requested=True,
-            ),
-        )
+        self.preserve_users = [
+            ExperimentChangeLogFactory.create().changed_by,
+            ExperimentCommentFactory.create().created_by,
+            ExperimentFactory.create().analysis_owner,
+            ExperimentFactory.create().owner,
+            NimbusChangeLogFactory.create().changed_by,
+            NimbusExperimentFactory.create().owner,
+            NotificationFactory.create().user,
+            UserFactory.create(email="person@mozilla.com"),
+            UserFactory.create(email="person@getpocket.com"),
+        ]
+        self.delete_users = [
+            UserFactory.create(email="person@example.com"),
+        ]
 
     def test_migration(self):
         """Run the test itself."""
-        NimbusExperiment = self.new_state.apps.get_model(
-            "experiments", "NimbusExperiment"
-        )
-        expected_status_next_values = (
-            NimbusConstants.Status.LIVE,
-            None,
-            NimbusConstants.Status.COMPLETE,
-            NimbusConstants.Status.LIVE,
-            None,
-        )
-        for experiment_id, expected_status_next in zip(
-            self.experiment_ids, expected_status_next_values
-        ):
-            self.assertEqual(
-                NimbusExperiment.objects.get(id=experiment_id).status_next,
-                expected_status_next,
-            )
+        for preserved_user in self.preserve_users:
+            self.assertTrue(User.objects.filter(id=preserved_user.id).exists())
 
-
-class TestMigration0174Backward(MigratorTestCase):
-    migrate_from = ("experiments", "0175_switch_to_status_next")
-    migrate_to = ("experiments", "0174_nimbusexperiment_results_data")
-
-    def prepare(self):
-        """Prepare some data before the migration."""
-        self.experiment_ids = create_experiments(
-            self.old_state.apps.get_model("auth", "User"),
-            self.old_state.apps.get_model("experiments", "NimbusExperiment"),
-            dict(
-                status=NimbusConstants.Status.LIVE,
-                status_next=NimbusConstants.Status.COMPLETE,
-            ),
-            # Invalid, but shouldn't match query
-            dict(
-                status=NimbusConstants.Status.COMPLETE,
-                status_next=NimbusConstants.Status.COMPLETE,
-            ),
-        )
-
-    def test_migration(self):
-        """Run the test itself."""
-        NimbusExperiment = self.new_state.apps.get_model(
-            "experiments", "NimbusExperiment"
-        )
-        expected_is_end_requested_values = (
-            True,
-            False,
-        )
-        for experiment_id, expected_is_end_requested in zip(
-            self.experiment_ids, expected_is_end_requested_values
-        ):
-            self.assertEqual(
-                NimbusExperiment.objects.get(id=experiment_id).is_end_requested,
-                expected_is_end_requested,
-            )
+        for deleted_user in self.delete_users:
+            self.assertFalse(User.objects.filter(id=deleted_user.id).exists())
