9ead235b20bdf7b12d3e5a711a38605f385d3189
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-08-12 16:20:08 -0400

    fix #5715 feat(nimbus): attach archive button to mutation (#6187)
    Because
    
    * We want the archive button to do the archiving!
    
    This commit
    
    * Adds a useArchive hook to wrap the mutation
    * Attaches the useArchive hook to the button in the SidebarActions
    * Tests tests tests

--- app/experimenter/nimbus-ui/src/components/SidebarActions/mocks.tsx
+++ app/experimenter/nimbus-ui/src/components/SidebarActions/mocks.tsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { MockedResponse } from "@apollo/client/testing";
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
 import { SidebarActions } from ".";
@@ -11,8 +12,12 @@ import { getExperiment_experimentBySlug } from "../../types/getExperiment";
 
 export const Subject = ({
   experiment: overrides,
+  refetch = async () => {},
+  mocks = [],
 }: RouteComponentProps & {
   experiment?: Partial<getExperiment_experimentBySlug>;
+  refetch?: () => Promise<any>;
+  mocks?: MockedResponse[];
 }) => {
   const { mock, experiment } = mockExperimentQuery(
     "my-special-slug",
@@ -20,8 +25,8 @@ export const Subject = ({
   );
 
   return (
-    <RouterSlugProvider mocks={[mock]} path="/my-special-slug/edit">
-      <SidebarActions {...{ experiment }} />
+    <RouterSlugProvider mocks={[mock, ...mocks]} path="/my-special-slug/edit">
+      <SidebarActions {...{ experiment, refetch }} />
     </RouterSlugProvider>
   );
 };
---
+++ app/experimenter/nimbus-ui/src/hooks/useArchive.tsx
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { useMutation } from "@apollo/client";
+import { useMemo, useState } from "react";
+import { UPDATE_EXPERIMENT_MUTATION } from "../gql/experiments";
+import { SUBMIT_ERROR } from "../lib/constants";
+import { getExperiment_experimentBySlug as Experiment } from "../types/getExperiment";
+import { ExperimentInput } from "../types/globalTypes";
+import { updateExperiment_updateExperiment as UpdateExperiment } from "../types/updateExperiment";
+
+export const ARCHIVING_EXPERIMENT = "Archiving experiment";
+export const UNARCHIVING_EXPERIMENT = "Unarchiving experiment";
+
+export function useArchive(
+  experiment: Partial<Experiment>,
+  refetch: () => Promise<unknown>,
+) {
+  const [submitError, setSubmitError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [updateExperiment, {}] = useMutation<
+    { updateExperiment: UpdateExperiment },
+    { input: ExperimentInput }
+  >(UPDATE_EXPERIMENT_MUTATION);
+
+  const callback = useMemo(
+    () => async () => {
+      setIsLoading(true);
+      setSubmitError(null);
+
+      try {
+        const result = await updateExperiment({
+          variables: {
+            input: {
+              id: experiment?.id,
+              isArchived: !experiment.isArchived,
+              changelogMessage: !experiment.isArchived
+                ? ARCHIVING_EXPERIMENT
+                : UNARCHIVING_EXPERIMENT,
+            },
+          },
+        });
+
+        if (!result.data?.updateExperiment) {
+          throw new Error(SUBMIT_ERROR);
+        }
+
+        const { message } = result.data.updateExperiment;
+
+        if (message && message !== "success" && typeof message === "object") {
+          throw new Error(JSON.stringify(message));
+        }
+
+        await refetch();
+      } catch (error) {
+        setSubmitError(error.message);
+      }
+      setIsLoading(false);
+    },
+
+    [updateExperiment, experiment],
+  );
+
+  return { callback, isLoading, submitError };
+}
