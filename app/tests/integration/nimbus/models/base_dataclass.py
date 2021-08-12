5a54c3238aa2585ddabb96f7dd365551465a151f
Lennart Schoch <lennart.schoch@vw-dilab.com>
2021-08-12 23:52:59 +0100

    fix #6064 chore(nimbus): Cleanup of nimbus end-to-end tests (#6099)
    * fixes #6064 chore(nimbus): Move experiment configuration in nimbus e2e into fixture
    
    * fixes #6064 chore(nimbus): Fix formatting
    
    * fixes #6064 chore(nimbus): Move default_data fixture into conftest.py
    
    * fixes #6064 chore(nimbus): Add metrics to BaseDataClass model
    
    * fixes #6064 chore(nimbus): Remove `fill` functions from pages
    
    * fixes #6064 chore(nimbus): Remove review of experiment from experiment creation test
    
    * fixes #6064 chore(nimbus): Move select options into enums; minor changes in data model
    
    * fixes #6064 chore(nimbus): Re-format
    
    * fixes #6064 chore(nimbus): Move  function into fixture
    
    * fixes #6064 chore(nimbus): Move kinto actions into fixture
    
    Co-authored-by: Lennart Schoch <lennart.schoch@code.berlin>

---
+++ app/tests/integration/nimbus/models/base_dataclass.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional
+
+
+class BaseExperimentApplications(Enum):
+    DESKTOP = ("DESKTOP",)
+    FENIX = ("FENIX",)
+    IOS = "IOS"
+
+
+class BaseExperimentAudienceChannels(Enum):
+    UNBRANDED = ("Unbranded",)
+    NIGHTLY = ("Nightly",)
+    BETA = ("Beta",)
+    RELEASE = "Release"
+
+
+class BaseExperimentAudienceTargetingOptions(Enum):
+    NO_TARGETING = ("NO_TARGETING",)
+    TARGETING_FIRST_RUN = ("TARGETING_FIRST_RUN",)
+    TARGETING_FIRST_RUN_CHROME_ATTRIBUTION = ("TARGETING_FIRST_RUN_CHROME_ATTRIBUTION",)
+    TARGETING_FIRST_RUN_WINDOWS_1903_NEWER = ("TARGETING_FIRST_RUN_WINDOWS_1903_NEWER",)
+    TARGETING_HOMEPAGE_GOOGLE = ("TARGETING_HOMEPAGE_GOOGLE",)
+    TARGETING_URLBAR_FIREFOX_SUGGEST = ("TARGETING_URLBAR_FIREFOX_SUGGEST",)
+    TARGETING_MAC_ONLY = ("TARGETING_MAC_ONLY",)
+    TARGETING_NO_ENTERPRISE = "TARGETING_NO_ENTERPRISE"
+
+
+@dataclass
+class BaseExperimentBranchDataClass:
+    name: str
+    description: str
+    feature_config: str
+
+
+@dataclass
+class BaseExperimentAudienceDataClass:
+    channel: BaseExperimentAudienceChannels
+    min_version: int
+    targeting: BaseExperimentAudienceTargetingOptions
+    percentage: float
+    expected_clients: int
+
+
+@dataclass
+class BaseExperimentMetricsDataClass:
+    primary_outcomes: Optional[List[str]]
+    secondary_outcomes: Optional[List[str]]
+
+
+@dataclass
+class BaseExperimentDataClass:
+    public_name: str
+    hypothesis: str
+    application: BaseExperimentApplications
+    public_description: str
+    branches: Optional[List[BaseExperimentBranchDataClass]]
+    metrics: BaseExperimentMetricsDataClass
+    audience: BaseExperimentAudienceDataClass
