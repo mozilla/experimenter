f283e30f172110e1ddaf9307c3c07e5232f4932d
anthonytran7757 <anthonytran7757@gmail.com>
2021-08-25 08:25:10 -0700

    fixes #5781 refactor(nimbus): reorder middleware (#6304)
    Because
    
    * the whitenoise docs suggests putting the Django Security middleware above the whitenoise middleware
    
    This commit
    
    * reorders the middleware as suggested by whitenoise

--- app/experimenter/settings.py
+++ app/experimenter/settings.py
@@ -85,8 +85,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
