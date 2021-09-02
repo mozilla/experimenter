c40628a0966a9cac9eebbfdb927c170c32b2b3c9
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-09-02 12:51:32 -0400

    chore(nimbus): move integration test logic to base classes (#6341)
    Because
    
    * Now that we have nice clear base classes for the integration tests
    * We don't need to override some shared logic in the child page classes
    
    This commit
    
    * Moves all the shared page logic to the respective base classes

--- app/tests/integration/nimbus/pages/experimenter/metrics.py
+++ app/tests/integration/nimbus/pages/experimenter/metrics.py
@@ -1,22 +1,10 @@
+from nimbus.pages.experimenter.audience import AudiencePage
 from nimbus.pages.experimenter.base import ExperimenterBase
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class MetricsPage(ExperimenterBase):
     """Experiment Metrics Page.."""
 
     _page_wait_locator = (By.CSS_SELECTOR, "#PageEditMetrics")
-    _save_continue_btn_locator = (By.CSS_SELECTOR, "#save-and-continue-button")
-
-    def wait_for_page_to_load(self):
-        self.wait.until(EC.presence_of_element_located(self._page_wait_locator))
-
-        return self
-
-    def save_and_continue(self):
-        element = self.selenium.find_element(*self._save_continue_btn_locator)
-        element.click()
-        from nimbus.pages.experimenter.audience import AudiencePage
-
-        return AudiencePage(self.driver, self.base_url).wait_for_page_to_load()
+    NEXT_PAGE = AudiencePage
