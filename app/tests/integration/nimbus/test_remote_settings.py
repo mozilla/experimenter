53e5767a2f67bb7b000dbd94460f87b888f56dea
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-09-03 16:38:16 -0400

    chore(nimbus): use page.open in integration tests (#6344)
    Because
    
    * The integration test PyPom library provides a handy .open method that opens the page and waits for it to load
    * This can clean up a lot of code in the tests
    
    This commit
    
    * Uses .open in all the integration tests

--- app/tests/integration/nimbus/test_remote_settings.py
+++ app/tests/integration/nimbus/test_remote_settings.py
@@ -1,5 +1,4 @@
 import pytest
-from nimbus.pages.experimenter.home import HomePage
 from nimbus.pages.experimenter.summary import SummaryPage
 from nimbus.pages.remote_settings.dashboard import Dashboard
 from nimbus.pages.remote_settings.login import Login
@@ -15,158 +14,79 @@ def test_create_new_experiment_approve_remote_settings(
     default_data,
     create_experiment,
 ):
-    selenium.get(base_url)
-    home = HomePage(selenium, base_url).wait_for_page_to_load()
+    create_experiment(selenium).launch_and_approve()
 
-    summary = create_experiment(selenium, home, default_data)
-    summary.launch_without_preview.click()
-    summary.request_review.click_launch_checkboxes()
-    summary.request_review.request_launch_button.click()
-    summary.approve()
+    Login(selenium, kinto_url).open().login()
+    Dashboard(selenium, kinto_review_url).open().approve()
 
-    selenium.get(kinto_url)
-    kinto_login = Login(selenium, kinto_url).wait_for_page_to_load()
-    kinto_login.login()
-
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.approve()
-
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
-    summary.wait_for_live_status()
+    SummaryPage(selenium, experiment_url).open().wait_for_live_status()
 
 
 @pytest.mark.run_once
 def test_create_new_experiment_reject_remote_settings(
     selenium,
-    base_url,
     kinto_url,
     kinto_review_url,
     experiment_url,
-    default_data,
     create_experiment,
 ):
-    selenium.get(base_url)
-    home = HomePage(selenium, base_url).wait_for_page_to_load()
+    create_experiment(selenium).launch_and_approve()
 
-    summary = create_experiment(selenium, home, default_data)
-    summary.launch_without_preview.click()
-    summary.request_review.click_launch_checkboxes()
-    summary.request_review.request_launch_button.click()
-    summary.approve()
+    Login(selenium, kinto_url).open().login()
+    Dashboard(selenium, kinto_review_url).open().reject()
 
-    selenium.get(kinto_url)
-    kinto_login = Login(selenium, kinto_url).wait_for_page_to_load()
-    kinto_login.login()
-
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.reject()
-
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
-    summary.wait_for_rejected_alert()
+    SummaryPage(selenium, experiment_url).open().wait_for_rejected_alert()
 
 
 @pytest.mark.run_once
 def test_create_new_experiment_timeout_remote_settings(
     selenium,
-    base_url,
-    default_data,
     create_experiment,
 ):
-    selenium.get(base_url)
-    home = HomePage(selenium, base_url).wait_for_page_to_load()
-
-    summary = create_experiment(selenium, home, default_data)
-    summary.launch_without_preview.click()
-    summary.request_review.click_launch_checkboxes()
-    summary.request_review.request_launch_button.click()
-    summary.approve()
-
+    summary = create_experiment(selenium)
+    summary.launch_and_approve()
     summary.wait_for_timeout_alert()
 
 
 @pytest.mark.run_once
 def test_end_experiment_and_approve_end(
     selenium,
-    base_url,
     kinto_url,
     kinto_review_url,
     experiment_url,
-    default_data,
     create_experiment,
 ):
-    selenium.get(base_url)
-    home = HomePage(selenium, base_url).wait_for_page_to_load()
+    create_experiment(selenium).launch_and_approve()
 
-    summary = create_experiment(selenium, home, default_data)
-    summary.launch_without_preview.click()
-    summary.request_review.click_launch_checkboxes()
-    summary.request_review.request_launch_button.click()
-    summary.approve()
+    Login(selenium, kinto_url).open().login()
+    Dashboard(selenium, kinto_review_url).open().approve()
 
-    selenium.get(kinto_url)
-    kinto_login = Login(selenium, kinto_url).wait_for_page_to_load()
-    kinto_login.login()
-
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.approve()
-
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
+    summary = SummaryPage(selenium, experiment_url).open()
     summary.wait_for_live_status()
-    summary.end_experiment()
-    summary.approve()
+    summary.end_and_approve()
 
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.approve()
+    Dashboard(selenium, kinto_review_url).open().approve()
 
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
-    summary.wait_for_complete_status()
+    SummaryPage(selenium, experiment_url).open().wait_for_complete_status()
 
 
 @pytest.mark.run_once
 def test_end_experiment_and_reject_end(
     selenium,
-    base_url,
     kinto_url,
     kinto_review_url,
     experiment_url,
-    default_data,
     create_experiment,
 ):
-    selenium.get(base_url)
-    home = HomePage(selenium, base_url).wait_for_page_to_load()
+    create_experiment(selenium).launch_and_approve()
 
-    summary = create_experiment(selenium, home, default_data)
-    summary.launch_without_preview.click()
-    summary.request_review.click_launch_checkboxes()
-    summary.request_review.request_launch_button.click()
-    summary.approve()
+    Login(selenium, kinto_url).open().login()
+    Dashboard(selenium, kinto_review_url).open().approve()
 
-    selenium.get(kinto_url)
-    kinto_login = Login(selenium, kinto_url).wait_for_page_to_load()
-    kinto_login.login()
-
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.approve()
-
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
+    summary = SummaryPage(selenium, experiment_url).open()
     summary.wait_for_live_status()
-    summary.end_experiment()
-    summary.approve()
+    summary.end_and_approve()
 
-    selenium.get(kinto_review_url)
-    kinto_dashboard = Dashboard(selenium, kinto_review_url).wait_for_page_to_load()
-    kinto_dashboard.reject()
+    Dashboard(selenium, kinto_review_url).open().reject()
 
-    selenium.get(experiment_url)
-    summary = SummaryPage(selenium, experiment_url).wait_for_page_to_load()
-    summary.wait_for_rejected_alert()
+    SummaryPage(selenium, experiment_url).open().wait_for_rejected_alert()
