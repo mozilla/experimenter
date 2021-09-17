dfad7ffdfe479706b9979adfab3f874afebba8d6
Les Orchard <lorchard@mozilla.com>
2021-09-17 09:04:42 -0700

    fix #6421 feat(nimbus): add support for screenshot upload and in-place update for branches (#6444)
    Because:
    
    * we want to support uploading and managing screenshots associated with
      branches
    
    This commit:
    
    * switches from GraphQLView to FileUploadGraphQLView in v5 API urls to
      add support for multipart file uploads
    
    * reduces length of screenshot filenames by removing the branch slug
    
    * adds BranchScreenshotType under BranchType for GQL experiment input
    
    * enhancements to NimbusBranchSerializer to support for specifying
      branch ID to perform in-place update
    
    * adds NimbusBranchScreenshotSerializer to handle screenshot management
    
    * adds NimbusBranchReadyForReviewSerializer to validate screenshots for
      review readiness without requiring screenshot content
    
    * sets up a new settings_test.py module for testing-specific overrides
      to Django settings
    
    * adds UPLOADS_FILE_STORAGE setting to override file storage for uploads
      with a named class (e.g. inmemorystorage.InMemoryStorage)
    
    * adds NimbusBranchScreenshotFactory to generate screenshots
    
    * updated GQL types to account for new screenshots model, with temporary
      stub for Upload type in nimbus-ui (to be defined in EXP-1766)
    
    * adds graphene-file-upload and dj-inmemorystorage dependencies

--- app/experimenter/experiments/api/v5/urls.py
+++ app/experimenter/experiments/api/v5/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 
 urlpatterns = [
     url(
         r"graphql",
-        csrf_exempt(GraphQLView.as_view(graphiql=True)),
+        csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True)),
         name="nimbus-api-graphql",
     ),
 ]
