83881bacf1bdac1228787edcf394b29bf979d65e
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-11-24 09:21:56 -0500

    fix #6700 bug(nimbus): support null feature_config in multifeature migration (#6701)
    Because
    
    * Not every experiment will have a feature_config set
    * We need to handle that at the model level with the uniqueness constraint in NimbusBranchFeatureValue
    * We need to handle that in the migration
    
    This commit
    
    * Updates the migration and test to cover the case where feature_config is null
    * Updates the uniqueness constraint on the model to use a fancy Django constraint API I didn't know existed to make the uniqueness constraint conditional on feature_config being set
    * Updates the mutation tests to cover the case where branches can be saved even though feature config is not set

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -20,20 +20,44 @@ class TestMigration0181Forward(MigratorTestCase):
 
         self.feature_config = NimbusFeatureConfig.objects.create()
 
-        experiment = NimbusExperiment.objects.create(
-            owner=owner, slug="experiment", feature_config=self.feature_config
+        experiment_with_feature = NimbusExperiment.objects.create(
+            owner=owner,
+            name="experiment_with_feature",
+            slug="experiment_with_feature",
+            feature_config=self.feature_config,
         )
 
-        experiment.reference_branch = NimbusBranch.objects.create(
-            experiment=experiment,
+        experiment_with_feature.reference_branch = NimbusBranch.objects.create(
+            experiment=experiment_with_feature,
             slug="control",
             feature_enabled=False,
             feature_value="control",
         )
-        experiment.save()
+        experiment_with_feature.save()
 
         NimbusBranch.objects.create(
-            experiment=experiment,
+            experiment=experiment_with_feature,
+            slug="treatment",
+            feature_enabled=True,
+            feature_value="treatment",
+        )
+
+        experiment_without_feature = NimbusExperiment.objects.create(
+            owner=owner,
+            name="experiment_without_feature",
+            slug="experiment_without_feature",
+            feature_config=None,
+        )
+        experiment_without_feature.reference_branch = NimbusBranch.objects.create(
+            experiment=experiment_without_feature,
+            slug="control",
+            feature_enabled=False,
+            feature_value="control",
+        )
+        experiment_without_feature.save()
+
+        NimbusBranch.objects.create(
+            experiment=experiment_without_feature,
             slug="treatment",
             feature_enabled=True,
             feature_value="treatment",
@@ -45,22 +69,33 @@ class TestMigration0181Forward(MigratorTestCase):
             "experiments", "NimbusExperiment"
         )
 
-        experiment = NimbusExperiment.objects.get(slug="experiment")
-
-        self.assertEqual(experiment.feature_configs.get().id, self.feature_config.id)
-
-        reference_feature_value = experiment.reference_branch.feature_values.get()
-        self.assertEqual(
-            reference_feature_value.feature_config.id, self.feature_config.id
+        experiment_with_feature = NimbusExperiment.objects.get(
+            slug="experiment_with_feature"
         )
-        self.assertEqual(reference_feature_value.enabled, False)
-        self.assertEqual(reference_feature_value.value, "control")
-
-        treatment_feature_value = experiment.branches.get(
-            slug="treatment"
-        ).feature_values.get()
-        self.assertEqual(
-            treatment_feature_value.feature_config.id, self.feature_config.id
+        experiment_without_feature = NimbusExperiment.objects.get(
+            slug="experiment_without_feature"
         )
-        self.assertEqual(treatment_feature_value.enabled, True)
-        self.assertEqual(treatment_feature_value.value, "treatment")
+
+        for experiment in (experiment_with_feature, experiment_without_feature):
+            reference_feature_value = experiment.reference_branch.feature_values.get()
+
+            self.assertEqual(reference_feature_value.enabled, False)
+            self.assertEqual(reference_feature_value.value, "control")
+
+            treatment_feature_value = experiment.branches.get(
+                slug="treatment"
+            ).feature_values.get()
+
+            self.assertEqual(treatment_feature_value.enabled, True)
+            self.assertEqual(treatment_feature_value.value, "treatment")
+
+            if experiment.feature_configs.exists():
+                self.assertEqual(
+                    experiment.feature_configs.get().id, self.feature_config.id
+                )
+                self.assertEqual(
+                    reference_feature_value.feature_config.id, self.feature_config.id
+                )
+                self.assertEqual(
+                    treatment_feature_value.feature_config.id, self.feature_config.id
+                )
