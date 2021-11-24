2fbcb2e391605ef689eb32a5ac5764780589ca25
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-11-24 10:09:41 -0500

    fix #6703 bug(nimbus): feature_value.value should default to empty string
    Because
    
    * There's no time when we'd want feature_value.value to be None
    * Rather than updating code to handle the None case we can just update the default to empty string
    
    This commit
    
    * Updates default for feature_value.value to empty string
    * Migrates existing data

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -2,8 +2,8 @@ from django_test_migrations.contrib.unittest_case import MigratorTestCase
 
 
 class TestMigration0181Forward(MigratorTestCase):
-    migrate_from = ("experiments", "0197_auto_20211109_2000")
-    migrate_to = ("experiments", "0198_auto_20211110_1527")
+    migrate_from = ("experiments", "0198_auto_20211110_1527")
+    migrate_to = ("experiments", "0199_alter_nimbusbranchfeaturevalue_value")
 
     def prepare(self):
         """Prepare some data before the migration."""
@@ -12,90 +12,36 @@ class TestMigration0181Forward(MigratorTestCase):
             "experiments", "NimbusExperiment"
         )
         NimbusBranch = self.old_state.apps.get_model("experiments", "NimbusBranch")
-        NimbusFeatureConfig = self.old_state.apps.get_model(
-            "experiments", "NimbusFeatureConfig"
+        NimbusBranchFeatureValue = self.old_state.apps.get_model(
+            "experiments", "NimbusBranchFeatureValue"
         )
 
         owner = User.objects.create(email="owner@example.com")
 
-        self.feature_config = NimbusFeatureConfig.objects.create()
-
-        experiment_with_feature = NimbusExperiment.objects.create(
+        experiment = NimbusExperiment.objects.create(
             owner=owner,
-            name="experiment_with_feature",
-            slug="experiment_with_feature",
-            feature_config=self.feature_config,
+            name="experiment",
+            slug="experiment",
         )
 
-        experiment_with_feature.reference_branch = NimbusBranch.objects.create(
-            experiment=experiment_with_feature,
+        branch_with_value = NimbusBranch.objects.create(
+            experiment=experiment,
             slug="control",
-            feature_enabled=False,
-            feature_value="control",
         )
-        experiment_with_feature.save()
+        NimbusBranchFeatureValue.objects.create(branch=branch_with_value, value="value")
 
-        NimbusBranch.objects.create(
-            experiment=experiment_with_feature,
+        branch_without_value = NimbusBranch.objects.create(
+            experiment=experiment,
             slug="treatment",
-            feature_enabled=True,
-            feature_value="treatment",
         )
-
-        experiment_without_feature = NimbusExperiment.objects.create(
-            owner=owner,
-            name="experiment_without_feature",
-            slug="experiment_without_feature",
-            feature_config=None,
-        )
-        experiment_without_feature.reference_branch = NimbusBranch.objects.create(
-            experiment=experiment_without_feature,
-            slug="control",
-            feature_enabled=False,
-            feature_value="control",
-        )
-        experiment_without_feature.save()
-
-        NimbusBranch.objects.create(
-            experiment=experiment_without_feature,
-            slug="treatment",
-            feature_enabled=True,
-            feature_value="treatment",
-        )
+        NimbusBranchFeatureValue.objects.create(branch=branch_without_value, value=None)
 
     def test_migration(self):
         """Run the test itself."""
-        NimbusExperiment = self.new_state.apps.get_model(
-            "experiments", "NimbusExperiment"
+        NimbusBranchFeatureValue = self.new_state.apps.get_model(
+            "experiments", "NimbusBranchFeatureValue"
         )
 
-        experiment_with_feature = NimbusExperiment.objects.get(
-            slug="experiment_with_feature"
-        )
-        experiment_without_feature = NimbusExperiment.objects.get(
-            slug="experiment_without_feature"
-        )
-
-        for experiment in (experiment_with_feature, experiment_without_feature):
-            reference_feature_value = experiment.reference_branch.feature_values.get()
-
-            self.assertEqual(reference_feature_value.enabled, False)
-            self.assertEqual(reference_feature_value.value, "control")
-
-            treatment_feature_value = experiment.branches.get(
-                slug="treatment"
-            ).feature_values.get()
-
-            self.assertEqual(treatment_feature_value.enabled, True)
-            self.assertEqual(treatment_feature_value.value, "treatment")
-
-            if experiment.feature_configs.exists():
-                self.assertEqual(
-                    experiment.feature_configs.get().id, self.feature_config.id
-                )
-                self.assertEqual(
-                    reference_feature_value.feature_config.id, self.feature_config.id
-                )
-                self.assertEqual(
-                    treatment_feature_value.feature_config.id, self.feature_config.id
-                )
+        self.assertFalse(NimbusBranchFeatureValue.objects.filter(value=None).exists())
+        self.assertTrue(NimbusBranchFeatureValue.objects.filter(value="value").exists())
+        self.assertTrue(NimbusBranchFeatureValue.objects.filter(value="").exists())
