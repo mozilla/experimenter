9108da91339f61e77a0c416c734f3a66a9c1ccc1
Les Orchard <lorchard@mozilla.com>
2021-12-01 11:12:33 -0800

    fix #6380 chore(nimbus): add integration test for branch screenshot (#6722)
    Because:
    
    * adding screenshots to a branch could use an integration test
    
    This commit:
    
    * implements a test to add a screenshot to a branch
    
    * adds screenshot-related helpers into BranchesPage and
      SummaryDetailPage
    
    * adds an example.jpg for use as image upload
    
    * refactors some global navigation helpers into ExperimenterBase
    
    * refactors some of the common code to wait for and find elements into
      ExperimenterBase helpers

--- app/tests/integration/nimbus/pages/experimenter/branches.py
+++ app/tests/integration/nimbus/pages/experimenter/branches.py
@@ -15,7 +15,9 @@ class BranchesPage(ExperimenterBase):
     )
     _remove_branch_locator = (By.CSS_SELECTOR, ".bg-transparent")
     _feature_select_locator = (By.CSS_SELECTOR, '[data-testid="feature-config-select"]')
+    _add_screenshot_buttons = (By.CSS_SELECTOR, '[data-testid="add-screenshot"]')
     NEXT_PAGE = MetricsPage
+    PAGE_TITLE = "Edit Branches Page"
 
     @property
     def reference_branch_name(self):
@@ -48,3 +50,31 @@ class BranchesPage(ExperimenterBase):
         el = self.find_element(*self._feature_select_locator)
         select = Select(el)
         select.select_by_visible_text(feature_config)
+
+    @property
+    def add_screenshot_buttons(self):
+        return self.wait_for_and_find_elements(
+            self._add_screenshot_buttons,
+            "branch add screenshot button",
+        )
+
+    def screenshot_description_field(self, branch="referenceBranch", screenshot_idx=0):
+        selector = (
+            f'[data-testid="FormScreenshot"] '
+            f'textarea[data-testid="{branch}.screenshots[{screenshot_idx}].description"]'
+        )
+        return self.wait_for_and_find_element(
+            (By.CSS_SELECTOR, selector),
+            f"screenshot description field for {branch} screenshot {screenshot_idx}",
+        )
+
+    def screenshot_image_field(self, branch="referenceBranch", screenshot_idx=0):
+        selector = (
+            f'[data-testid="FormScreenshot"] '
+            f'input[type="file"]'
+            f'[data-testid="{branch}.screenshots[{screenshot_idx}].image"]'
+        )
+        return self.wait_for_and_find_element(
+            (By.CSS_SELECTOR, selector),
+            f"screenshot image field for {branch} screenshot {screenshot_idx}",
+        )
