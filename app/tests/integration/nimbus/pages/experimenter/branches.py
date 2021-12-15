301a3681a3bcff6a52d795646945c80392d758db
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2021-12-15 16:53:52 -0500

    fix #6762 chore(nimbus): set feature value in integration tests (#6794)
    Because
    
    * We recently broke the branch page feature value submissions without being caught by any tests
    * Unit tests were updated to check that logic but we should also set feature values in the integration tests
    
    This commit
    
    * Sets a feature value in the treatment branch in the integration tests

--- app/tests/integration/nimbus/pages/experimenter/branches.py
+++ app/tests/integration/nimbus/pages/experimenter/branches.py
@@ -13,48 +13,114 @@ class BranchesPage(ExperimenterBase):
         By.CSS_SELECTOR,
         "#referenceBranch-description",
     )
+    _treatment_branch_name_locator = (By.CSS_SELECTOR, "#treatmentBranches\\[0\\]-name")
+    _treatment_branch_description_locator = (
+        By.CSS_SELECTOR,
+        "#treatmentBranches\\[0\\]-description",
+    )
+    _treatment_branch_enable_locator = (
+        By.CSS_SELECTOR,
+        'label[for="treatmentBranches[0].featureEnabled"]',
+    )
+    _treatment_branch_value_locator = (
+        By.CSS_SELECTOR,
+        "#treatmentBranches\\[0\\]-featureValue",
+    )
     _remove_branch_locator = (By.CSS_SELECTOR, ".bg-transparent")
     _feature_select_locator = (By.CSS_SELECTOR, '[data-testid="feature-config-select"]')
-    _add_screenshot_buttons = (By.CSS_SELECTOR, '[data-testid="add-screenshot"]')
+    _add_screenshot_buttons_locator = (By.CSS_SELECTOR, '[data-testid="add-screenshot"]')
     NEXT_PAGE = MetricsPage
     PAGE_TITLE = "Edit Branches Page"
 
     @property
     def reference_branch_name(self):
-        return self.find_element(*self._reference_branch_name_locator).text
+        return self.wait_for_and_find_element(
+            self._reference_branch_name_locator, "reference branch name"
+        ).text
 
     @reference_branch_name.setter
     def reference_branch_name(self, text=None):
-        name = self.find_element(*self._reference_branch_name_locator)
-        name.send_keys(f"{text}")
+        self.wait_for_and_find_element(
+            self._reference_branch_name_locator, "reference branch name"
+        ).send_keys(f"{text}")
 
     @property
     def reference_branch_description(self):
-        return self.find_element(*self._reference_branch_description_locator).text
+        return self.wait_for_and_find_element(
+            self._reference_branch_description_locator, "reference branch description"
+        ).text
 
     @reference_branch_description.setter
     def reference_branch_description(self, text=None):
-        name = self.find_element(*self._reference_branch_description_locator)
-        name.send_keys(f"{text}")
+        self.wait_for_and_find_element(
+            self._reference_branch_description_locator, "reference_branch description"
+        ).send_keys(f"{text}")
+
+    @property
+    def treatment_branch_name(self):
+        return self.wait_for_and_find_element(
+            self._treatment_branch_name_locator, "treatment branch name"
+        ).text
+
+    @treatment_branch_name.setter
+    def treatment_branch_name(self, text=None):
+        self.wait_for_and_find_element(
+            self._treatment_branch_name_locator, "treatment branch name"
+        ).send_keys(f"{text}")
+
+    @property
+    def treatment_branch_description(self):
+        return self.wait_for_and_find_element(
+            self._treatment_branch_description_locator, "treatment branch description"
+        ).text
+
+    @treatment_branch_description.setter
+    def treatment_branch_description(self, text=None):
+        self.wait_for_and_find_element(
+            self._treatment_branch_description_locator, "treatment branch description"
+        ).send_keys(f"{text}")
+
+    @property
+    def treatment_branch_enabled(self):
+        return self.wait_for_and_find_element(
+            self._treatment_branch_enable_locator, "treatment branch enabled"
+        )
+
+    @property
+    def treatment_branch_value(self):
+        return self.wait_for_and_find_element(
+            self._treatment_branch_value_locator, "treatment branch value"
+        ).text
+
+    @treatment_branch_value.setter
+    def treatment_branch_value(self, text=None):
+        self.wait_for_and_find_element(
+            self._treatment_branch_value_locator, "treatment branch value"
+        ).send_keys(f"{text}")
 
     def remove_branch(self):
-        el = self.find_element(*self._remove_branch_locator)
-        el.click()
+        self.wait_for_and_find_element(
+            self._remove_branch_locator, "remove branch button"
+        ).click()
 
     @property
     def feature_config(self):
-        return self.find_element(*self._feature_select_locator).text
+        return self.wait_for_and_find_element(
+            self._feature_select_locator, "feature config"
+        ).text
 
     @feature_config.setter
     def feature_config(self, feature_config="No Feature Firefox Desktop"):
-        el = self.find_element(*self._feature_select_locator)
+        el = self.wait_for_and_find_element(
+            self._feature_select_locator, "feature_config"
+        )
         select = Select(el)
         select.select_by_visible_text(feature_config)
 
     @property
     def add_screenshot_buttons(self):
         return self.wait_for_and_find_elements(
-            self._add_screenshot_buttons,
+            self._add_screenshot_buttons_locator,
             "branch add screenshot button",
         )
 
