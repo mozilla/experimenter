52659591ada9c086a8d820a0db82f693f3e17bb4
Les Orchard <lorchard@mozilla.com>
2021-12-15 12:21:08 -0800

    fix #6638 feat(nimbus): add emergency switch to admin that disables launches (#6772)
    Because:
    
    * we need a way to temporarily disable launching experiments while it's
      being fixed to experiment owners don't inadvertently send invalid data
      to clients.
    
    This commit:
    
    * adds a new SiteFlag model to experiment.base to store flag settings
    
    * adds a new SiteFlagAdmin to manage flag settings
    
    * implements support for a LAUNCHING_DISABLED flag enforced by Nimbus
      API serializer validation to prevent launches
    
    * fixes a small bug in displaying errors for status_next validation when
      requesting launch

--- app/experimenter/base/models.py
+++ app/experimenter/base/models.py
@@ -1,6 +1,36 @@
 from django.db import models
 
 
+class SiteFlagNameChoices(models.TextChoices):
+    LAUNCHING_DISABLED = "LAUNCHING_DISABLED", "Disable launching experiments"
+
+
+class SiteFlagManager(models.Manager):
+    def value(self, choice, defval=False):
+        qs = self.get_queryset().filter(name=choice.name)
+        if qs.exists():
+            return qs.get().value
+        return defval
+
+
+class SiteFlag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    value = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    objects = SiteFlagManager()
+
+    @property
+    def description(self):
+        if self.name in SiteFlagNameChoices:
+            return SiteFlagNameChoices[self.name].label
+        return self.name
+
+    def __str__(self):
+        return f"{self.description}: {self.value}"
+
+
 class Locale(models.Model):
     code = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
