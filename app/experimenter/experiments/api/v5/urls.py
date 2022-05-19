114fbf228fdcdb70a15ae101d746ebe934d9eb09
yashika-khurana <ykhurana@mozilla.com>
2022-05-19 11:46:03 -0700

    fix #7274 feat(nimbus): Url to export csv file

--- app/experimenter/experiments/api/v5/urls.py
+++ app/experimenter/experiments/api/v5/urls.py
@@ -2,10 +2,17 @@ from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
+from experimenter.experiments.api.v5.views import NimbusExperimentCSVListView
+
 urlpatterns = [
     url(
         r"graphql",
         csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True)),
         name="nimbus-api-graphql",
+    ),
+    url(
+        r"^csv/$",
+        NimbusExperimentCSVListView.as_view(),
+        name="nimbus-experiments-api-csv",
     ),
 ]
