01ab76c4d82d9ae448db88340bed7fbff59a683a
yashika-khurana <ykhurana@mozilla.com>
2022-05-19 11:47:04 -0700

    fix #7274 test(nimbus): Model, serializer and view test

---
+++ app/experimenter/experiments/tests/api/v5/test_serializers/test_nimbus_experiment_csv_serializer.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+
+from experimenter.experiments.api.v5.serializers import NimbusExperimentCSVSerializer
+from experimenter.experiments.constants.nimbus import NimbusConstants
+from experimenter.experiments.tests.factories.nimbus import (
+    NimbusExperimentFactory,
+    NimbusFeatureConfigFactory,
+)
+
+
+class TestNimbusExperiment_Csv_Serializer(TestCase):
+    def test_serializer_outputs_expected_schema(self):
+        application = NimbusConstants.Application.DESKTOP
+        feature_config = NimbusFeatureConfigFactory.create(application=application)
+        experiment = NimbusExperimentFactory.create(
+            application=application, feature_configs=[feature_config]
+        )
+        serializer = NimbusExperimentCSVSerializer(experiment)
+        print(serializer)
+        self.assertDictEqual(
+            serializer.data,
+            {
+                "launch_month": experiment.launch_month,
+                "product_area": experiment.application.value,
+                "experiment_name": experiment.name,
+                "owner": experiment.owner.email,
+                "start_date": experiment.start_date,
+                "enrollment_duration": experiment.enrollment_duration,
+                "end_date": experiment.end_date,
+                "monitoring_dashboard_url": experiment.monitoring_dashboard_url,
+                "rollout": experiment.is_rollout,
+                "hypothesis": experiment.hypothesis,
+            },
+        )
