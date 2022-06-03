25f46b4c3d04d03a2b8fd4561fb43820f9469a99
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2022-06-03 15:23:54 -0400

    fix #7357 feat(nimbus): move targeting into its own app (#7360)
    Because
    
    * Targeting has fairly isolated concerns
    * The targeting configurations are very high traffic for changes
    * Having to explicitly configure the targeting dictionaries when making changes is annoying
    
    This commit
    
    * Moves targeting configs into their own app
    * Auto generates the targeting config dictionaries instead of explicitly declaring them
    * Changes the default 'no targeting' to an explicit slug 'no_targeting' instead of an empty string
    * Migrates existing experiments from the old to new default

--- app/experimenter/targeting/tests/test_targeting_configs.py
+++ app/experimenter/targeting/tests/test_targeting_configs.py
@@ -2,20 +2,20 @@ from django.test import TestCase
 from parameterized import parameterized
 from parsimonious.exceptions import ParseError
 
-from experimenter.experiments.constants import NimbusConstants
 from experimenter.experiments.tests import JEXLParser
+from experimenter.targeting.constants import TargetingConstants
 
 
 class TestTargetingConfigs(TestCase):
     def test_all_targeting_configs_defined_in_constants(self):
         self.assertEqual(
-            set([t.value for t in NimbusConstants.TargetingConfig]),
-            set(t for t in NimbusConstants.TARGETING_CONFIGS.keys()),
-            "Targeting Configs must be defined in both NimbusConstants.TargetingConfig "
-            "and NimbusConstants.TARGETING_CONFIGS",
+            set([t.value for t in TargetingConstants.TargetingConfig]),
+            set(t for t in TargetingConstants.TARGETING_CONFIGS.keys()),
+            "Targeting Configs must be defined in both "
+            "TargetingConstants.TargetingConfig and TargetingConstants.TARGETING_CONFIGS",
         )
 
-    @parameterized.expand([(t,) for t in NimbusConstants.TARGETING_CONFIGS.values()])
+    @parameterized.expand([(t,) for t in TargetingConstants.TARGETING_CONFIGS.values()])
     def test_targeting_config_has_valid_jexl(self, targeting_config):
         if targeting_config.targeting:
             try:
