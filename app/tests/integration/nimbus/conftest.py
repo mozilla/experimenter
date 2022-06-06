248422cf3796d28ee881496fcc25dcea75d75c1e
yashika-khurana <ykhurana@mozilla.com>
2022-06-06 10:37:10 -0700

    fix #7150 test(nimbus): Add language field in integration test

--- app/tests/integration/nimbus/conftest.py
+++ app/tests/integration/nimbus/conftest.py
@@ -157,7 +157,7 @@ def default_data(request):
             expected_clients=50,
             locale=None,
             countries=None,
-            languages=None,
+            languages=[1],
         ),
     )
 
