43f579d97e7c7a456934560879309690190cee79
yashika-khurana <ykhurana@mozilla.com>
2022-06-15 15:18:26 -0700

    fix #7338 feat(nimbus): Transform only draft experiments

--- app/experimenter/experiments/migrations/0212_alter_nimbusexperiment_languages_field_for_mobile_client.py
+++ app/experimenter/experiments/migrations/0212_alter_nimbusexperiment_languages_field_for_mobile_client.py
@@ -1,14 +1,18 @@
 from django.db import migrations
 
 from experimenter.base.models import Language
-from experimenter.experiments.models import NimbusExperiment
+from experimenter.experiments.constants import NimbusConstants
+from experimenter.experiments.models import NimbusExperiment as Experiment
 
 
 def update_languages_field_for_mobile_client(apps, schema_editor):
-    NimbusExperiments = apps.get_model("experiments", "NimbusExperiment")
+    NimbusExperiment = apps.get_model("experiments", "NimbusExperiment")
 
-    for experiment in NimbusExperiments.objects.all():
-        if experiment.application != NimbusExperiment.Application.DESKTOP:
+    for experiment in NimbusExperiment.objects.all():
+        if (
+            experiment.application != Experiment.Application.DESKTOP
+            and experiment.status == NimbusConstants.Status.DRAFT
+        ):
             for locale in experiment.locales.all():
                 locale_code = locale.code[:2]
                 language = Language.objects.filter(code=locale_code).first()
@@ -16,7 +20,6 @@ def update_languages_field_for_mobile_client(apps, schema_editor):
 
                     experiment.languages.add(language.id)
             experiment.locales.clear()
-            experiment.save()
 
 
 class Migration(migrations.Migration):
--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -1,6 +1,7 @@
 from django_test_migrations.contrib.unittest_case import MigratorTestCase
 
 from experimenter.base.models import Language, Locale
+from experimenter.experiments.constants import NimbusConstants
 from experimenter.experiments.models import NimbusExperiment as Experiment
 
 
@@ -23,12 +24,14 @@ class TestMigration(MigratorTestCase):
         locale_en = Locale.objects.create(code="en", name="English")
         Language.objects.create(code="en", name="English")
         locale_ts = Locale.objects.create(code="ts", name="does not exist")
+        locale_ca = Locale.objects.create(code="en-CA", name="English Canada")
 
         desktop_experiment = NimbusExperiment.objects.create(
             owner=user,
             name="desktop experiment",
             slug="desktop_experiment",
             application=Experiment.Application.DESKTOP,
+            status=NimbusConstants.Status.DRAFT,
         )
         desktop_experiment.locales.add(locale_en.id)
         mobile_experiments_with_locales = NimbusExperiment.objects.create(
@@ -36,15 +39,28 @@ class TestMigration(MigratorTestCase):
             name="mobile experiment with locales",
             slug="mobile_experiment_with_locales",
             application=Experiment.Application.FOCUS_ANDROID,
+            status=NimbusConstants.Status.DRAFT,
         )
-        mobile_experiments_with_locales.locales.add(locale_en.id, locale_ts.id)
+        mobile_experiments_with_locales.locales.add(locale_en.id, locale_ts.id, locale_ca)
 
         NimbusExperiment.objects.create(
             owner=user,
             name="mobile experiment without locales",
             slug="mobile_experiment_without_locales",
             application=Experiment.Application.FOCUS_IOS,
+            status=NimbusConstants.Status.DRAFT,
         )
+
+        mobile_experiments_with_locales_but_not_in_draft = (
+            NimbusExperiment.objects.create(
+                owner=user,
+                name="mobile experiment not in draft with locales",
+                slug="mobile_experiment_not_in_draft_with_locales",
+                application=Experiment.Application.IOS,
+                status=NimbusConstants.Status.COMPLETE,
+            )
+        )
+        mobile_experiments_with_locales_but_not_in_draft.locales.add(locale_en.id)
 
     def test_migration(self):
         """Run the test itself."""
@@ -77,3 +93,13 @@ class TestMigration(MigratorTestCase):
         )
         self.assertEqual(mobile_experiment_with_locales.locales.count(), 0)
         self.assertEqual(mobile_experiment_with_locales.languages.count(), 0)
+
+        # get mobile experiment without locale field which is not in draft
+        mobile_experiments_with_locales_but_not_in_draft = NimbusExperiment.objects.get(
+            application=Experiment.Application.IOS
+        )
+
+        self.assertEqual(
+            mobile_experiments_with_locales_but_not_in_draft.locales.all()[0].code,
+            Locale.objects.get(code="en").code,
+        )
