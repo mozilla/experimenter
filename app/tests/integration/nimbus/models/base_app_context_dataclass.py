483140c248e191f32e07feae535186edf7d07813
yashika-khurana <ykhurana@mozilla.com>
2022-06-21 16:37:55 -0700

    fix #7386 test(nimbus): Language field for mobile experiments

--- app/tests/integration/nimbus/models/base_app_context_dataclass.py
+++ app/tests/integration/nimbus/models/base_app_context_dataclass.py
@@ -15,7 +15,7 @@ class BaseAppContextDataClass:
     architecture: str
     device_manufacturer: str
     device_model: str
-    locale: str
+    language: str
     os: str
     os_version: str
     android_sdk_version: Optional[str]
