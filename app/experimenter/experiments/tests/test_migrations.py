f7e27ff927fdb4ca1b0262145a41f0e2d5fa3faa
yashika-khurana <104033388+yashika-khurana@users.noreply.github.com>
2022-06-21 10:48:09 -0700

    fix #7338 chore(nimbus): transform locales to languages for mobile experiments (#7405)
    * fix #7338 feat(nimbus): Migration to transform from locale to language for mobile experiments
    
    * fix #7338 test(nimbus): Test locale to languages for mobile clients
    
    * Update app/experimenter/experiments/tests/test_migrations.py
    
    Co-authored-by: Elise Richards <erichards@mozilla.com>
    
    * fix #7338 feat(nimbus): Transform only draft experiments
    
    * fix #7338 feat(nimbus): Make locale field available back for old experiments in ui
    
    * fix #7338 feat(nimbus): Filter query
    
    * Merge branch 'main' of https://github.com/yashika-khurana/experimenter into 7151/prevent_language_min_version
    
    * fix #7338 feat(nimbis): Import model and exclude query
    
    * fix #7338 chore(nimbus): Merge migration
    
    * fix #7338 chore(nimbus): Merge migration
    
    Co-authored-by: Elise Richards <erichards@mozilla.com>

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -1,37 +1,105 @@
 from django_test_migrations.contrib.unittest_case import MigratorTestCase
 
+from experimenter.base.models import Language, Locale
+from experimenter.experiments.constants import NimbusConstants
+from experimenter.experiments.models import NimbusExperiment as Experiment
+
 
 class TestMigration(MigratorTestCase):
-    migrate_from = ("experiments", "0210_alter_nimbusexperiment_channel")
-    migrate_to = ("experiments", "0211_alter_nimbusexperiment_targeting_config_slug")
+    migrate_from = ("experiments", "0212_nimbusexperiment_is_sticky")
+    migrate_to = (
+        "experiments",
+        "0213_alter_nimbusexperiment_languages_field_for_mobile_client",
+    )
 
     def prepare(self):
         """Prepare some data before the migration."""
         User = self.old_state.apps.get_model("auth", "User")
+        Locale = self.old_state.apps.get_model("base", "Locale")
+        Language = self.old_state.apps.get_model("base", "Language")
         NimbusExperiment = self.old_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
         user = User.objects.create(email="test@example.com")
+        locale_en = Locale.objects.create(code="en", name="English")
+        Language.objects.create(code="en", name="English")
+        locale_ts = Locale.objects.create(code="ts", name="does not exist")
+        locale_ca = Locale.objects.create(code="en-CA", name="English Canada")
+
+        desktop_experiment = NimbusExperiment.objects.create(
+            owner=user,
+            name="desktop experiment",
+            slug="desktop_experiment",
+            application=Experiment.Application.DESKTOP,
+            status=NimbusConstants.Status.DRAFT,
+        )
+        desktop_experiment.locales.add(locale_en.id)
+        mobile_experiments_with_locales = NimbusExperiment.objects.create(
+            owner=user,
+            name="mobile experiment with locales",
+            slug="mobile_experiment_with_locales",
+            application=Experiment.Application.FOCUS_ANDROID,
+            status=NimbusConstants.Status.DRAFT,
+        )
+        mobile_experiments_with_locales.locales.add(locale_en.id, locale_ts.id, locale_ca)
+
         NimbusExperiment.objects.create(
             owner=user,
-            name="no targeting experiment",
-            slug="no_targeting_experiment",
-            targeting_config_slug="",
+            name="mobile experiment without locales",
+            slug="mobile_experiment_without_locales",
+            application=Experiment.Application.FOCUS_IOS,
+            status=NimbusConstants.Status.DRAFT,
         )
-        NimbusExperiment.objects.create(
-            owner=user,
-            name="some targeting experiment",
-            slug="some_targeting_experiment",
-            targeting_config_slug="some_targeting",
+
+        mobile_experiments_with_locales_but_not_in_draft = (
+            NimbusExperiment.objects.create(
+                owner=user,
+                name="mobile experiment not in draft with locales",
+                slug="mobile_experiment_not_in_draft_with_locales",
+                application=Experiment.Application.IOS,
+                status=NimbusConstants.Status.COMPLETE,
+            )
         )
+        mobile_experiments_with_locales_but_not_in_draft.locales.add(locale_en.id)
 
     def test_migration(self):
         """Run the test itself."""
         NimbusExperiment = self.new_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
-        self.assertFalse(
-            NimbusExperiment.objects.filter(targeting_config_slug="").exists()
+
+        # get desktop experiment
+        desktop_experiment = NimbusExperiment.objects.get(
+            application=Experiment.Application.DESKTOP
         )
-        NimbusExperiment.objects.get(targeting_config_slug="no_targeting")
-        NimbusExperiment.objects.get(targeting_config_slug="some_targeting")
+
+        self.assertEqual(
+            desktop_experiment.locales.all()[0].code, Locale.objects.get(code="en").code
+        )
+
+        # get mobile experiment included locale field
+        mobile_experiment_with_locales = NimbusExperiment.objects.get(
+            application=Experiment.Application.FOCUS_ANDROID
+        )
+        self.assertEqual(mobile_experiment_with_locales.locales.count(), 0)
+        self.assertEqual(
+            mobile_experiment_with_locales.languages.all()[0].code,
+            Language.objects.get(code="en").code,
+        )
+
+        # get mobile experiment without locale field
+        mobile_experiment_with_locales = NimbusExperiment.objects.get(
+            application=Experiment.Application.FOCUS_IOS
+        )
+        self.assertEqual(mobile_experiment_with_locales.locales.count(), 0)
+        self.assertEqual(mobile_experiment_with_locales.languages.count(), 0)
+
+        # get mobile experiment without locale field which is not in draft
+        mobile_experiments_with_locales_but_not_in_draft = NimbusExperiment.objects.get(
+            application=Experiment.Application.IOS
+        )
+
+        self.assertEqual(
+            mobile_experiments_with_locales_but_not_in_draft.locales.all()[0].code,
+            Locale.objects.get(code="en").code,
+        )
