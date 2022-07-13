d6c949063ed57e176ca9e315ede443f9d3c6bb01
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2022-07-13 13:46:22 -0400

    fix #7510 bug(nimbus): update removed targeting config slug (#7513)
    Because
    
    * We removed a targeting config slug ("urlbar_firefox_suggest_us_en") in a change a while ago but neglected to write a data migration to update references to it
    * Experiments with that removed targeting config slug don't render at all
    
    This commit
    
    * Adds a data migration to update references to that targeting config slug

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -3,67 +3,15 @@ from django_test_migrations.contrib.unittest_case import MigratorTestCase
 from experimenter.experiments.constants import NimbusConstants
 from experimenter.experiments.models import NimbusExperiment as Experiment
 
-DESKTOP_STICKY_TARGETING_SLUGS = [
-    "first_run",
-    "first_run_chrome",
-    "first_run_win1903",
-    "not_tcp_study_first_run",
-    "windows_userchoice_first_run",
-    "infrequent_user_uris",
-    "infrequent_user_need_pin",
-    "infrequent_user_need_default",
-    "infrequent_user_need_default_has_pin",
-    "infrequent_user_has_default_need_pin",
-    "infrequent_windows_user_need_pin",
-    "infrequent_win_user_uris",
-    "infrequent_user_5_bookmarks",
-    "casual_user_uris",
-    "casual_user_need_pin",
-    "casual_user_need_default",
-    "casual_user_need_default_has_pin",
-    "casual_user_has_default_need_pin",
-    "regular_user_uris",
-    "regular_user_need_pin",
-    "regular_user_need_default",
-    "regular_user_need_default_has_pin",
-    "regular_user_has_default_need_pin",
-    "regular_user_uses_fxa",
-    "core_user_uris",
-]
-
-MOBILE_STICKY_TARGETING_SLUGS = [
-    "mobile_new_users",
-    "mobile_recently_updated_users",
-]
-
-NON_STICKY_TARGETING_SLUGS = [
-    "core_user_has_default_need_pin",
-    "core_user_need_default",
-    "core_user_need_default_has_pin",
-    "core_user_need_pin",
-    "fx95_desktop_users",
-    "homepage_google_dot_com",
-    "mac_only",
-    "no_enterprise_or_last_30d_vpn_use",
-    "no_enterprise_or_past_vpn",
-    "no_enterprise_users",
-    "no_targeting",
-    "not_tcp_study",
-    "pocket_common",
-    "rally_core_addon_user",
-    "urlbar_firefox_suggest",
-    "windows_userchoice",
-]
-
 
 class TestMigration(MigratorTestCase):
     migrate_from = (
         "experiments",
-        "0213_alter_nimbusexperiment_languages_field_for_mobile_client",
+        "0214_migrate_sticky",
     )
     migrate_to = (
         "experiments",
-        "0214_migrate_sticky",
+        "0215_update_removed_targeting_config_slugs",
     )
 
     def prepare(self):
@@ -74,56 +22,23 @@ class TestMigration(MigratorTestCase):
         )
         user = User.objects.create(email="test@example.com")
 
-        # Desktop experiments that will keep the same targeting slug
-        # but have is_sticky set to True
-        for desktop_sticky_targeting in DESKTOP_STICKY_TARGETING_SLUGS:
-            NimbusExperiment.objects.create(
-                owner=user,
-                name=desktop_sticky_targeting,
-                slug=desktop_sticky_targeting,
-                application=Experiment.Application.DESKTOP,
-                status=NimbusConstants.Status.DRAFT,
-                targeting_config_slug=desktop_sticky_targeting,
-                is_sticky=False,
-            )
-
-        # Targeting slug 'pip_never_used_sticky' will get replaced with 'pip_never_used'
-        # and is_sticky set to True
         NimbusExperiment.objects.create(
             owner=user,
-            name="pip_never_used_sticky",
-            slug="pip_never_used_sticky",
+            name="urlbar_firefox_suggest_us_en",
+            slug="urlbar_firefox_suggest_us_en",
             application=Experiment.Application.DESKTOP,
             status=NimbusConstants.Status.DRAFT,
-            targeting_config_slug="pip_never_used_sticky",
-            is_sticky=False,
+            targeting_config_slug="urlbar_firefox_suggest_us_en",
         )
 
-        # Mobile experiments that will keep the same targeting slug
-        # but have is_sticky set to True
-        for mobile_sticky_targeting in MOBILE_STICKY_TARGETING_SLUGS:
-            NimbusExperiment.objects.create(
-                owner=user,
-                name=mobile_sticky_targeting,
-                slug=mobile_sticky_targeting,
-                application=Experiment.Application.FOCUS_ANDROID,
-                status=NimbusConstants.Status.DRAFT,
-                targeting_config_slug=mobile_sticky_targeting,
-                is_sticky=False,
-            )
-
-        # Desktop experiments that will keep the same targeting slug
-        # and keep is_sticky set to False
-        for non_sticky_targeting in NON_STICKY_TARGETING_SLUGS:
-            NimbusExperiment.objects.create(
-                owner=user,
-                name=non_sticky_targeting,
-                slug=non_sticky_targeting,
-                application=Experiment.Application.DESKTOP,
-                status=NimbusConstants.Status.DRAFT,
-                targeting_config_slug=non_sticky_targeting,
-                is_sticky=False,
-            )
+        NimbusExperiment.objects.create(
+            owner=user,
+            name="another experiment",
+            slug="another experiment",
+            application=Experiment.Application.DESKTOP,
+            status=NimbusConstants.Status.DRAFT,
+            targeting_config_slug="another_targeting",
+        )
 
     def test_migration(self):
         """Run the test itself."""
@@ -131,59 +46,18 @@ class TestMigration(MigratorTestCase):
             "experiments", "NimbusExperiment"
         )
 
-        # Desktop experiments that keep the same sticky targeting slug
-        # now have is_sticky set to True
-        self.assertEqual(
+        self.assertTrue(
             NimbusExperiment.objects.filter(
-                slug__in=DESKTOP_STICKY_TARGETING_SLUGS, is_sticky=True
-            ).count(),
-            len(DESKTOP_STICKY_TARGETING_SLUGS),
+                targeting_config_slug="urlbar_firefox_suggest"
+            ).exists()
         )
-        self.assertEqual(
-            NimbusExperiment.objects.filter(
-                slug__in=DESKTOP_STICKY_TARGETING_SLUGS, is_sticky=False
-            ).count(),
-            0,
-        )
-
-        # 'pip_never_used_sticky' becomes 'pip_never_used' with is_sticky set to True
         self.assertFalse(
             NimbusExperiment.objects.filter(
-                targeting_config_slug="pip_never_used_sticky"
+                targeting_config_slug="urlbar_firefox_suggest_en_us"
             ).exists()
         )
         self.assertTrue(
             NimbusExperiment.objects.filter(
-                targeting_config_slug="pip_never_used", is_sticky=True
+                targeting_config_slug="another_targeting"
             ).exists()
-        )
-
-        # Mobile experiments that keep the same sticky targeting slug
-        # now have is_sticky set to True
-        self.assertEqual(
-            NimbusExperiment.objects.filter(
-                slug__in=MOBILE_STICKY_TARGETING_SLUGS, is_sticky=True
-            ).count(),
-            len(MOBILE_STICKY_TARGETING_SLUGS),
-        )
-        self.assertEqual(
-            NimbusExperiment.objects.filter(
-                slug__in=MOBILE_STICKY_TARGETING_SLUGS, is_sticky=False
-            ).count(),
-            0,
-        )
-
-        # Desktop experiments that keep the same non-sticky targeting slug
-        # still have is_sticky set to False
-        self.assertEqual(
-            NimbusExperiment.objects.filter(
-                slug__in=NON_STICKY_TARGETING_SLUGS, is_sticky=True
-            ).count(),
-            0,
-        )
-        self.assertEqual(
-            NimbusExperiment.objects.filter(
-                slug__in=NON_STICKY_TARGETING_SLUGS, is_sticky=False
-            ).count(),
-            len(NON_STICKY_TARGETING_SLUGS),
         )
