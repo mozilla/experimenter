61450a517c4f5bb44427ebb6857498a5f7ec9d38
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2022-09-08 12:03:50 -0400

    fix #7709 feat(nimbus): add first run v6 api endpoint (#7710)
    Because
    
    * The mobile first run workflow depends on syncing live experiments into relevant mobile repos
    * When the experiment ends it must be unpublished from the API so it can be removed from the repo
    * The existing V6 api includes both live and complete experiments so we need a new endpoint
    
    This commit
    
    * Adds a new endpoint to V6 API specifically for live first run experiments

--- app/experimenter/experiments/api/v6/urls.py
+++ app/experimenter/experiments/api/v6/urls.py
@@ -1,7 +1,15 @@
 from rest_framework.routers import SimpleRouter
 
-from experimenter.experiments.api.v6.views import NimbusExperimentViewSet
+from experimenter.experiments.api.v6.views import (
+    NimbusExperimentFirstRunViewSet,
+    NimbusExperimentViewSet,
+)
 
 router = SimpleRouter()
 router.register(r"experiments", NimbusExperimentViewSet, "nimbus-experiment-rest")
+router.register(
+    r"experiments-first-run",
+    NimbusExperimentFirstRunViewSet,
+    "nimbus-experiment-rest-first-run",
+)
 urlpatterns = router.urls
