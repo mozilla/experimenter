5c9b14a403e4740563c08494c6a38a10589493f9
yashika-khurana <104033388+yashika-khurana@users.noreply.github.com>
2022-09-09 15:30:37 -0700

    fix #7712 feat(nimbus): Add first run required in targeting (#7722)
    * fix #7712 feat(nimbus): Add first run required in targeting
    
    * fix #7712 fix(nimbus): Pass population as a string value

--- app/experimenter/nimbus-ui/src/types/getAllExperiments.ts
+++ app/experimenter/nimbus-ui/src/types/getAllExperiments.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { NimbusExperimentApplicationEnum, NimbusExperimentFirefoxVersionEnum, NimbusExperimentStatusEnum, NimbusExperimentPublishStatusEnum } from "./globalTypes";
+import { NimbusExperimentApplicationEnum, NimbusExperimentFirefoxVersionEnum, NimbusExperimentStatusEnum, NimbusExperimentPublishStatusEnum, NimbusExperimentChannelEnum } from "./globalTypes";
 
 // ====================================================
 // GraphQL query operation: getAllExperiments
@@ -51,8 +51,8 @@ export interface getAllExperiments_experiments {
   rolloutMonitoringDashboardUrl: string | null;
   resultsReady: boolean | null;
   featureConfig: getAllExperiments_experiments_featureConfig | null;
-  channel: string | null;
-  populationPercent: number | null;
+  channel: NimbusExperimentChannelEnum | null;
+  populationPercent: string | null;
 }
 
 export interface getAllExperiments {
