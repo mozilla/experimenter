f3915e31a0e179aa27e5bffa2fb19f935be6c17b
Benjamin Forehand <bennyjr169@gmail.com>
2022-09-15 16:52:44 -0500

    fix #7740 test(nimbus): Extend targeting integration tests
    Because
    * We currently check each targeting expression validates in the browser, but it is a very basic expression
    
    This Commit
    * Adds an addition test with additional fiends such as channel, locale, region and versions.

--- app/tests/integration/nimbus/test_firefox_targeting.py
+++ app/tests/integration/nimbus/test_firefox_targeting.py
@@ -74,7 +74,7 @@ def targeting_config_slug(request):
 
 
 @pytest.mark.run_targeting
-def test_check_targeting(
+def test_check_advanced_targeting(
     selenium,
     slugify,
     experiment_name,
@@ -116,6 +116,88 @@ def test_check_targeting(
         experiment_slug,
         "desktop",
         targeting_config_slug,
+        data,
+    )
+    experiment_data = load_experiment_data(experiment_slug)
+    targeting = experiment_data["data"]["experimentBySlug"]["jexlTargetingExpression"]
+    recipe = experiment_data["data"]["experimentBySlug"]["recipeJson"]
+
+    # Inject filter expression
+    selenium.get("about:blank")
+    with open("nimbus/utils/filter_expression.js") as js:
+        result = Browser.execute_script(
+            selenium,
+            targeting,
+            json.dumps({"experiment": recipe}),
+            script=js.read(),
+            context="chrome",
+        )
+    assert result is not None, "Invalid Targeting, or bad recipe"
+
+
+@pytest.mark.parametrize(
+    "audience_field", [
+        {"channel": "NIGHTLY"},
+        {"firefoxMinVersion": "FIREFOX_100"},
+        {"firefoxMaxVersion": "FIREFOX_120"},
+        {"locales": [37]},
+        {"countries": [42]},
+        {"proposedEnrollment": "14"},
+        {"proposedDuration": "30"}
+    ],
+    ids=[
+        "channel",
+        "min_version",
+        "max_version",
+        "locales",
+        "countries",
+        "proposed_enrollment",
+        "proposed_duration"
+    ]
+)
+@pytest.mark.run_targeting
+def test_check_audience_targeting(
+    selenium,
+    slugify,
+    experiment_name,
+    create_desktop_experiment,
+    audience_field
+):
+    experiment_slug = str(slugify(experiment_name))
+    data = {
+        "hypothesis": "Test Hypothesis",
+        "application": "DESKTOP",
+        "changelogMessage": "test updates",
+        "targetingConfigSlug": "no_targeting",
+        "publicDescription": "Some sort of Fancy Words",
+        "riskRevenue": False,
+        "riskPartnerRelated": False,
+        "riskBrand": False,
+        "featureConfigId": 1,
+        "referenceBranch": {
+            "description": "reference branch",
+            "name": "Branch 1",
+            "ratio": 50,
+            "featureEnabled": True,
+            "featureValue": "{}",
+        },
+        "treatmentBranches": [
+            {
+                "description": "treatment branch",
+                "name": "Branch 2",
+                "ratio": 50,
+                "featureEnabled": False,
+                "featureValue": "",
+            }
+        ],
+        "populationPercent": "100",
+        "totalEnrolledClients": 55,
+    }
+    data.update(audience_field)
+    create_desktop_experiment(
+        experiment_slug,
+        "desktop",
+        "no_targeting",
         data,
     )
     experiment_data = load_experiment_data(experiment_slug)
