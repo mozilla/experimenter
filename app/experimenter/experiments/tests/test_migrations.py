841fcf12413d2f42ece04de24a909a5d64048922
Mike Williams <mwilliams@mozilla.com>
2022-10-13 16:27:34 -0400

    fix 7839 fix(nimbus): data migration for updated analysis results schema

--- app/experimenter/experiments/tests/test_migrations.py
+++ app/experimenter/experiments/tests/test_migrations.py
@@ -7,11 +7,11 @@ from experimenter.experiments.models import NimbusExperiment as Experiment
 class TestMigration(MigratorTestCase):
     migrate_from = (
         "experiments",
-        "0214_migrate_sticky",
+        "0217_auto_20220901_1914",
     )
     migrate_to = (
         "experiments",
-        "0215_update_removed_targeting_config_slugs",
+        "0218_update_results_data_schema",
     )
 
     def prepare(self):
@@ -22,22 +22,40 @@ class TestMigration(MigratorTestCase):
         )
         user = User.objects.create(email="test@example.com")
 
+        # create experiment without segments
         NimbusExperiment.objects.create(
             owner=user,
-            name="urlbar_firefox_suggest_us_en",
-            slug="urlbar_firefox_suggest_us_en",
+            name="test experiment",
+            slug="test-experiment",
             application=Experiment.Application.DESKTOP,
             status=NimbusConstants.Status.DRAFT,
-            targeting_config_slug="urlbar_firefox_suggest_us_en",
+            results_data={
+                "daily": {"control": [], "treatment": []},
+                "weekly": {"control": {}, "treatment": {}},
+                "overall": {"control": {}, "treatment": {}},
+                "other_metrics": {},
+                "metadata": {},
+                "show_analysis": True,
+                "errors": [],
+            },
         )
 
+        # create experiment with segments
         NimbusExperiment.objects.create(
             owner=user,
             name="another experiment",
-            slug="another experiment",
+            slug="another-experiment",
             application=Experiment.Application.DESKTOP,
             status=NimbusConstants.Status.DRAFT,
-            targeting_config_slug="another_targeting",
+            results_data={
+                "daily": {"all": {"control": [], "treatment": []}},
+                "weekly": {"all": {"control": {}, "treatment": {}}},
+                "overall": {"all": {"control": {}, "treatment": {}}},
+                "other_metrics": {},
+                "metadata": {},
+                "show_analysis": True,
+                "errors": [],
+            },
         )
 
     def test_migration(self):
@@ -46,18 +64,20 @@ class TestMigration(MigratorTestCase):
             "experiments", "NimbusExperiment"
         )
 
-        self.assertTrue(
-            NimbusExperiment.objects.filter(
-                targeting_config_slug="urlbar_firefox_suggest"
-            ).exists()
-        )
-        self.assertFalse(
-            NimbusExperiment.objects.filter(
-                targeting_config_slug="urlbar_firefox_suggest_en_us"
-            ).exists()
-        )
-        self.assertTrue(
-            NimbusExperiment.objects.filter(
-                targeting_config_slug="another_targeting"
-            ).exists()
-        )
+        changed_data = NimbusExperiment.objects.get(slug="test-experiment").results_data
+
+        self.assertTrue("all" in changed_data["daily"])
+        self.assertFalse("control" in changed_data["daily"])
+        self.assertTrue("all" in changed_data["weekly"])
+        self.assertFalse("control" in changed_data["weekly"])
+        self.assertTrue("all" in changed_data["overall"])
+        self.assertFalse("control" in changed_data["overall"])
+        self.assertFalse("all" in changed_data["metadata"])
+
+        unchanged_data = NimbusExperiment.objects.get(
+            slug="another-experiment"
+        ).results_data
+
+        self.assertTrue("all" in unchanged_data["daily"])
+        self.assertFalse("control" in unchanged_data["daily"])
+        self.assertFalse("all" in unchanged_data["other_metrics"])
