a792053cc78f27fd207e47320d1b6cb100924084
Benjamin Forehand Jr <bennyjr169@gmail.com>
2022-11-07 16:55:13 -0600

    fixes #7919 test(Nimbus): Update integration test file names (#7920)
    Because:
    * We have just reoganized our CI jobs to be more descriptive but our
    test suites are still a bit out of order
    
    This Commit:
    * Updates the test suite names to match the tests within them a bit
    better.
    * Moves some tests around

--- app/tests/integration/nimbus/test_remote_settings.py
+++ app/tests/integration/nimbus/test_remote_settings.py
@@ -30,16 +30,6 @@ def test_create_new_experiment_reject_remote_settings(
     SummaryPage(selenium, experiment_url).open().wait_for_rejected_alert()
 
 
-@pytest.mark.nimbus_ui
-def test_create_new_experiment_timeout_remote_settings(
-    selenium,
-    create_experiment,
-):
-    summary = create_experiment(selenium)
-    summary.launch_and_approve()
-    summary.wait_for_timeout_alert()
-
-
 @pytest.mark.remote_settings
 def test_end_experiment_and_approve_end(
     selenium,
@@ -78,3 +68,34 @@ def test_end_experiment_and_reject_end(
     kinto_client.reject()
 
     SummaryPage(selenium, experiment_url).open().wait_for_rejected_alert()
+
+
+@pytest.mark.remote_settings
+@pytest.mark.xdist_group(name="group2")
+def test_takeaways(
+    selenium,
+    experiment_url,
+    create_experiment,
+    kinto_client,
+):
+    create_experiment(selenium).launch_and_approve()
+
+    kinto_client.approve()
+
+    summary = SummaryPage(selenium, experiment_url).open()
+    summary.wait_for_live_status()
+    summary.end_and_approve()
+
+    kinto_client.approve()
+
+    summary = SummaryPage(selenium, experiment_url).open()
+    summary.wait_for_complete_status()
+
+    expected_summary = "Example takeaways summary text"
+    summary.takeaways_edit_button.click()
+    summary.takeaways_summary_field = expected_summary
+    summary.takeaways_recommendation_radio_button("CHANGE_COURSE").click()
+    summary.takeaways_save_button.click()
+
+    assert summary.takeaways_summary_text == expected_summary
+    assert summary.takeaways_recommendation_badge_text == "Change course"
