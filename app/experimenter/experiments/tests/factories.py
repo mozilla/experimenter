2a51f4b2f19a7333682d8016992c7ac189a22126
Elise Richards <erichards@mozilla.com>
2022-12-08 15:57:12 -0600

    For #8022 test(nimbus): Add lifecycle states to test factory

--- app/experimenter/experiments/tests/factories.py
+++ app/experimenter/experiments/tests/factories.py
@@ -104,12 +104,6 @@ class LifecycleStates(Enum):
         "status_next": None,
         "publish_status": NimbusExperiment.PublishStatus.IDLE,
     }
-    LIVE_IDLE_ENROLLING = {
-        "status": NimbusExperiment.Status.LIVE,
-        "status_next": None,
-        "publish_status": NimbusExperiment.PublishStatus.IDLE,
-        "is_paused": False,
-    }
     LIVE_DIRTY = {
         "status": NimbusExperiment.Status.LIVE,
         "status_next": None,
@@ -181,11 +175,6 @@ class LifecycleStates(Enum):
         "status": NimbusExperiment.Status.LIVE,
         "status_next": NimbusExperiment.Status.COMPLETE,
         "publish_status": NimbusExperiment.PublishStatus.REVIEW,
-    }
-    LIVE_IDLE_REJECT_ENDING = {
-        "status": NimbusExperiment.Status.LIVE,
-        "status_next": None,
-        "publish_status": NimbusExperiment.PublishStatus.IDLE,
     }
     LIVE_APPROVED_ENDING = {
         "status": NimbusExperiment.Status.LIVE,
