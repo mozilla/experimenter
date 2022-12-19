8ae6c77f1bb216eff0a85d4baa5cc5e1b1ec0a90
Benjamin Forehand Jr <bennyjr169@gmail.com>
2022-12-19 11:41:46 -0600

    fixes #8045 tests(nimbus): Add an integration test for sticky targeting (#8077)
    Because:
    * Sticky targeting is alive and well within nimbus
    
    This Commit:
    * Ensures we have proper automated testing of it.

--- app/tests/integration/nimbus/pages/browser/__init__.py
+++ app/tests/integration/nimbus/pages/browser/__init__.py
@@ -21,6 +21,7 @@ class AboutConfig(Page):
 
     _search_bar_locator = (By.ID, "about-config-search")
     _row_locator = (By.CSS_SELECTOR, "tr > td > span > span")
+    _pref_locator = (By.CSS_SELECTOR, "tr > th > span")
 
     def __init__(self, selenium, **kwargs):
         super().__init__(selenium, timeout=80, **kwargs)
@@ -47,3 +48,45 @@ class AboutConfig(Page):
             else:
                 return True
         raise (error)
+
+    def flip_pref(self, pref):
+        timeout = time.time() + 15
+        error = None
+        while time.time() < timeout:
+            try:
+                search_bar = self.find_element(*self._search_bar_locator)
+                search_bar.send_keys(pref)
+                self.wait.until(EC.presence_of_element_located(self._row_locator))
+                actual_row = (By.CSS_SELECTOR, "table > tr")
+                new_pref_locator = (By.CSS_SELECTOR, "th > span")
+                elements = self.find_elements(*actual_row)
+                el = [
+                    element
+                    for element in elements
+                    if pref in element.find_element(*new_pref_locator).text
+                ][0]
+                el.find_element(By.CSS_SELECTOR, ".cell-edit").click()
+            except Exception as e:
+                error = e
+                time.sleep(2)
+                self.selenium.get("about:config")
+                self.wait_for_page_to_load()
+            else:
+                return True
+        raise (error)
+
+    def get_pref_value(self, pref):
+        self.selenium.get("about:config")
+        self = self.wait_for_page_to_load()
+        search_bar = self.find_element(*self._search_bar_locator)
+        search_bar.send_keys(pref)
+        self.wait.until(EC.presence_of_element_located(self._row_locator))
+        actual_row = (By.CSS_SELECTOR, "table > tr")
+        new_pref_locator = (By.CSS_SELECTOR, "th > span")
+        elements = self.find_elements(*actual_row)
+        el = [
+            element
+            for element in elements
+            if pref in element.find_element(*new_pref_locator).text
+        ][0]
+        return el.find_element(By.CSS_SELECTOR, ".cell-value").text
