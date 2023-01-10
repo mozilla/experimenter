c37bc5a6d8ef6fd769499c8a4d61f9f81514a78e
Elise Richards <erichards@mozilla.com>
2023-01-10 15:53:15 -0600

    For #8012 feat(nimbus): Support rollouts and experiments for status validation (#8081)
    Because...
    
    * Our v5 serializer validates which fields can be modified against
    "locked" statuses
    * And we are going to be allowing Rollouts to be modified while `LIVE`
    
    This commit...
    
    * Changes `constants.py` to include lists for experiments, rollouts, and
    shared ("all") for:
       * status: `STATUS_ALLOWS_UPDATE`
       * publish status: `PUBLISH_STATUS_ALLOWS_UPDATE`
       * modification exempt fields: `STATUS_UPDATE_EXEMPT_FIELDS`
    * Uses these new lists in the v5 serializer to check if an edit is valid
    * Adds a new test for rollouts for serializer status mixin for codecov

--- app/experimenter/experiments/tests/api/v5/test_serializers/test_nimbus_status_validation_mixin.py
+++ app/experimenter/experiments/tests/api/v5/test_serializers/test_nimbus_status_validation_mixin.py
@@ -17,6 +17,7 @@ class TestNimbusStatusValidationMixin(TestCase):
     def test_update_experiment_with_invalid_status_error(self):
         experiment = NimbusExperimentFactory.create(
             status=NimbusExperiment.Status.PREVIEW,
+            is_rollout=False,
         )
         serializer = NimbusExperimentSerializer(
             experiment,
@@ -29,9 +30,10 @@ class TestNimbusStatusValidationMixin(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertIn("experiment", serializer.errors)
 
-    def test_unable_to_update_experiment_in_publish_status(self):
+    def test_update_rollout_with_invalid_status_error(self):
         experiment = NimbusExperimentFactory.create(
-            publish_status=NimbusExperiment.PublishStatus.REVIEW,
+            status=NimbusExperiment.Status.PREVIEW,
+            is_rollout=True,
         )
         serializer = NimbusExperimentSerializer(
             experiment,
@@ -43,6 +45,43 @@ class TestNimbusStatusValidationMixin(TestCase):
         )
         self.assertFalse(serializer.is_valid())
         self.assertIn("experiment", serializer.errors)
+
+    @parameterized.expand(
+        [
+            ["public_description", NimbusExperiment.Status.PREVIEW, False, False, False],
+            ["public_description", NimbusExperiment.Status.DRAFT, False, True, True],
+            ["takeaways_summary", NimbusExperiment.Status.DRAFT, True, True, True],
+        ]
+    )
+    def test_locked_fields_and_status_errors(
+        self, field_to_change, status, field_valid, status_valid, serializer_valid
+    ):
+        fields = (
+            NimbusExperiment.STATUS_UPDATE_EXEMPT_FIELDS["all"]
+            + NimbusExperiment.STATUS_UPDATE_EXEMPT_FIELDS["experiments"]
+        )
+        status_allowed = (
+            NimbusExperiment.STATUS_ALLOWS_UPDATE["all"]
+            + NimbusExperiment.STATUS_ALLOWS_UPDATE["experiments"]
+        )
+
+        experiment = NimbusExperimentFactory.create(
+            status=status,
+            is_rollout=True,
+        )
+
+        serializer = NimbusExperimentSerializer(
+            experiment,
+            data={
+                field_to_change: "who knows, really",
+                "changelog_message": "test changelog message",
+            },
+            context={"user": self.user},
+        )
+
+        self.assertEquals(field_to_change in fields, field_valid)
+        self.assertEquals(status in status_allowed, status_valid)
+        self.assertEquals(serializer.is_valid(), serializer_valid)
 
     @parameterized.expand(
         [
