1c5f58b6563916d6c64429b77c2d2f76dce518ae
Benjamin Forehand <bennyjr169@gmail.com>
2023-01-25 15:31:49 -0600

    Try a fix for projects.

--- app/tests/integration/nimbus/conftest.py
+++ app/tests/integration/nimbus/conftest.py
@@ -244,7 +244,7 @@ def create_experiment(base_url, default_data):
         overview.select_risk_brand_false()
         overview.select_risk_revenue_false()
         overview.select_risk_partner_false()
-        overview.projects = ["Advanced hybrid forecast"]
+        overview.projects = ["A"]
         overview.set_additional_links(value="DESIGN_DOC")
         overview.add_additional_links()
         overview.set_additional_links(value="DS_JIRA", url="https://jira.jira.com")
