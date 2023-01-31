fc6d71dcb9eaf9d26586744a3c7fe8d4bc34a367
Elise Richards <erichards@mozilla.com>
2023-01-31 13:30:22 -0600

    For #7823 feat(nimbus): Add form for live reviews (#8203)
    📢📢📢 Requires #8202 to be merged first
    
    Because...
    
    * We want to be able to make edits while a rollout is live
    * And we want to send those edits through the review process before we
    launch the edits
    
    This commit...
    
    * Creates the Live -> Review form which will appear at the top of the
    summary page
    * Adds tests
    
    <img width="306" alt="image"
    src="https://user-images.githubusercontent.com/43795363/215853952-3efa6943-1217-4880-8fe2-d4d8a1cec078.png">
    
    ^ Let's change the wording to "update" instead of "launch"

---
+++ app/experimenter/nimbus-ui/src/components/PageSummary/FormLaunchLiveToReview.tsx
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+
+const FormLaunchLiveToReview = ({
+  isLoading,
+  onSubmit,
+  onCancel,
+}: {
+  isLoading: boolean;
+  onSubmit: () => void;
+  onCancel: () => void;
+}) => {
+  return (
+    <Alert
+      variant="secondary"
+      id="request-live-launch-alert"
+      data-testid="request-live-launch-alert"
+    >
+      <Form className="text-body">
+        <div className="d-flex bd-highlight">
+          <div className="py-1">
+            <p>Review and update live rollout:</p>
+            <button
+              data-testid="launch-live-to-review"
+              id="request-launch-button"
+              type="button"
+              className="mr-2 btn btn-primary"
+              disabled={isLoading}
+              onClick={onSubmit}
+            >
+              Request Update
+            </button>
+            <button
+              data-testid="cancel"
+              type="button"
+              className="btn btn-secondary"
+              disabled={isLoading}
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Form>
+    </Alert>
+  );
+};
+
+export default FormLaunchLiveToReview;
