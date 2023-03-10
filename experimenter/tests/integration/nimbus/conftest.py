2ec6e8dea12a9f532290d87db083c39c45d711dc
USER <user@Apples-MacBook-Pro.local>
2023-03-10 01:22:58 +0000

    modify the assertion to check if the first item in the metrics.primary_outcomes list is not empty

--- experimenter/tests/integration/nimbus/conftest.py
+++ experimenter/tests/integration/nimbus/conftest.py
@@ -274,7 +274,7 @@ def create_experiment(base_url, default_data):
         metrics = branches.save_and_continue()
         if default_data.metrics.primary_outcomes:
             metrics.set_primary_outcomes(values=default_data.metrics.primary_outcomes[0])
-            assert metrics.primary_outcomes.text != "", "The primary outcome was not set"
+            assert metrics.primary_outcomes[0] != "", "The primary outcome was not set"
             metrics.set_secondary_outcomes(
                 values=default_data.metrics.secondary_outcomes[0]
             )
