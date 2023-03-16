69635ecdaef62fecceaeeb8dfcab525b6a1b3ef4
Elise Richards <erichards@mozilla.com>
2023-03-16 11:20:04 -0400

    For #8514 fix(nimbus): Check feature configs when looking for duplicate rollouts

--- experimenter/experimenter/experiments/api/v5/serializers.py
+++ experimenter/experimenter/experiments/api/v5/serializers.py
@@ -1507,18 +1507,20 @@ class NimbusReviewSerializer(serializers.ModelSerializer):
         return data
 
     def _validate_bucket_duplicates(self, data):
-        if not self.instance or not self.instance.is_rollout:
-            return data
         is_rollout = self.instance.is_rollout
+        if not self.instance or not is_rollout:
+            return data
+
         count = NimbusExperiment.objects.filter(
             status=NimbusExperiment.Status.LIVE,
             channel=self.instance.channel,
             application=self.instance.application,
             targeting_config_slug=self.instance.targeting_config_slug,
+            feature_configs__in=self.instance.feature_configs.all(),
             is_rollout=is_rollout,
         ).count()
 
-        if is_rollout and count > 0:
+        if count > 0:
             self.warnings["bucketing"] = [NimbusConstants.ERROR_BUCKET_EXISTS]
 
         return data
