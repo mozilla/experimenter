00dfcbc043d6b6a997b18ac28aa9b7425e03552f
USER <user@Apples-MacBook-Pro.local>
2023-03-20 01:57:43 +0000

    Add is-invalid and border-danger classes to select before an item is chosen

--- experimenter/experimenter/nimbus-ui/src/components/PageEditMetrics/FormMetrics/index.tsx
+++ experimenter/experimenter/nimbus-ui/src/components/PageEditMetrics/FormMetrics/index.tsx
@@ -179,7 +179,11 @@ const FormMetrics = ({
           {...formSelectAttrs("primaryOutcomes", setPrimaryOutcomes)}
           options={primaryOutcomeOptions}
           isOptionDisabled={() => primaryOutcomes.length >= maxPrimaryOutcomes!}
-          className={`select-control ${primaryValid ? "is-valid" : ""}`}
+          className={`${
+            primaryValid
+              ? "is-valid border border-success rounded"
+              : "is-invalid border border-danger rounded"
+          }`}
           onChange={(selectedOptions) => {
             setPrimaryOutcomes(selectedOptions.map((option) => option.value));
           }}
@@ -213,7 +217,11 @@ const FormMetrics = ({
           isMulti
           {...formSelectAttrs("secondaryOutcomes", setSecondaryOutcomes)}
           options={secondaryOutcomeOptions}
-          className={`select-control ${secondaryValid ? "is-valid" : ""}`}
+          className={`${
+            secondaryValid
+              ? "is-valid border border-success rounded"
+              : "is-invalid border border-danger rounded"
+          }`}
           onChange={(selectedOptions) => {
             setSecondaryOutcomes(selectedOptions.map((option) => option.value));
           }}
