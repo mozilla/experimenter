387eff3a019509695d74f3eefbbc5c74e59669de
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-03-22 13:19:41 -0700

    Revert "fixes #8498 fix(nimbus): change the search bar to check if input contains space at the end, if input does not have space add space at the end and also change the test cases" (#8567)
    Because
    - we are getting intermittent tests because of changes in the search-
    https://github.com/mozilla/experimenter/issues/8557
    
    This Commit
    - Reverts this PR https://github.com/mozilla/experimenter/pull/8547
    because this PR is build on top of this pr
    changes-https://github.com/mozilla/experimenter/pull/8418

--- experimenter/experimenter/nimbus-ui/src/components/PageHome/SearchBar/index.tsx
+++ experimenter/experimenter/nimbus-ui/src/components/PageHome/SearchBar/index.tsx
@@ -78,19 +78,9 @@ const SearchBar: React.FunctionComponent<SearchBarProps> = ({
   const handleChange = (event: {
     target: { value: React.SetStateAction<string> };
   }) => {
-    let inputValue = event.target.value as string;
-    // Check if the input value already has a space at the end
-    if (
-      inputValue.length > 0 &&
-      inputValue.charAt(inputValue.length - 1) !== " "
-    ) {
-      // If not, add a space at the end
-      inputValue = inputValue + " ";
-    }
-
     // add the search query to history state
     const url = new URL(`${window.location}`);
-    url.searchParams.set("search", inputValue);
+    url.searchParams.set("search", event.target.value as string);
     window.history.pushState({}, "", `${url}`);
 
     setSearchTerms(event.target.value);
@@ -101,7 +91,7 @@ const SearchBar: React.FunctionComponent<SearchBarProps> = ({
       setClearIcon(true);
 
       const newTimer = setTimeout(() => {
-        const results = fuse.search(inputValue);
+        const results = fuse.search(event.target.value as string);
 
         const searchResults = results.map((character) => character.item);
         onChange(searchResults);
