a53b85720938423416d8b8d5f9e014fa6a1dd47b
prernadabi23 <prernadabi24@gmail.com>
2023-03-25 13:28:32 +0530

    error

--- experimenter/experimenter/nimbus-ui/src/components/SidebarActions/index.tsx
+++ experimenter/experimenter/nimbus-ui/src/components/SidebarActions/index.tsx
@@ -200,7 +200,6 @@ export const SidebarActions = ({
             Preview Recipe JSON
           </Link>
         )}
-
         <div className="edit-divider position-relative small my-3">
           <span className="position-relative bg-light pl-1 pr-2 text-muted">
             Help
