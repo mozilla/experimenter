0eab9fca39760d2ed8b81125e9d784ea4d3c9869
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-03-31 10:47:23 -0700

    fix #8584 feat(cirrus): Genereate openapi.json for cirrus (#8619)
    Because
    
    - FastAPI generates the openapi.json file automatically based on the
    code of the FastAPI application. This file can be used by tools to
    generate client libraries, documentation, and other API-related
    artifacts.
    
    This commit
    
    - Generates the openapi.json file for cirrus
    - included in the `make_cirrus_check` command to make sure to update if
    there are any changes in the app too

---
+++ cirrus/server/cirrus/generate_docs.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+from fastapi.openapi.utils import get_openapi
+from main import app
+
+DOCS_DIR = "cirrus/server/cirrus/docs"
+OPENAPI_PATH = os.path.join(DOCS_DIR, "openapi.json")
+
+
+def generate_openapi_schema():
+    # Generate new OpenAPI schema
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+
+    with open(OPENAPI_PATH, "r") as f:
+        if not os.path.isfile(OPENAPI_PATH) or openapi_schema != json.load(f):
+            with open(OPENAPI_PATH, "w") as f:
+                json.dump(openapi_schema, f)
+            print("openapi.json has been updated!")
+        else:
+            print("openapi.json is already up-to-date.")
+
+
+if __name__ == "__main__":
+    generate_openapi_schema()
