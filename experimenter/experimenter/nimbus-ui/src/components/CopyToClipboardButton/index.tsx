68083edd24f6255f64185c5c06b7ac7fc68863a7
Avishkar <79744258+Avi-88@users.noreply.github.com>
2023-04-06 20:34:38 +0530

    fixes #8530 feat(nimbus): Added a copy-to-clipboard button next to experiment slug (#8611)
    Because
    
    - it is a handy feature to have so users can copy the experiment slug
    effortlessly
    
    This commit
    
    - adds a copy-to-clipboard button next to the experiment slug with all
    required tests in place
    
    [Video demo of the feature in
    action](https://drive.google.com/file/d/1cz9jj_0YS8HQnlJlus0SADhAyoAKxOX-/view)
    
    ---------
    
    Co-authored-by: Yashika Khurana <yashikakhuranayashika@gmail.com>

---
+++ experimenter/experimenter/nimbus-ui/src/components/CopyToClipboardButton/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { ReactComponent as Copy } from "src/components/CopyToClipboardButton/copy.svg";
+
+interface Props {
+  text: string;
+}
+
+const CopyToClipboardButton = ({ text }: Props) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setIsClicked(true);
+      setMessage("Copied to clipboard!");
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 3000);
+    } catch (error) {
+      setIsClicked(true);
+      setMessage("Failed to copy slug");
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 3000);
+      console.error(error);
+    }
+  };
+
+  return (
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip id="tooltip-copy">{message}</Tooltip>}
+      show={isClicked}
+    >
+      <Button
+        data-testid="copy-button"
+        variant="outlined"
+        className="rounded-circle border-0 mb-2 shadow-none"
+        onClick={handleCopy}
+      >
+        <Copy
+          data-testid="copy-to-clipboard-icon"
+          onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+          width="18"
+          height="18"
+          title="Copy to Clipboard"
+        />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+export default CopyToClipboardButton;
