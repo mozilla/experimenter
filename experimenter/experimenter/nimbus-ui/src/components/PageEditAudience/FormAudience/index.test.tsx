9a7b2755d27d63145ce1969d68df515c26f175ac
Cherish <saicherish90@gmail.com>
2023-04-08 00:57:15 +0530

    renaming text inside queryByText

--- experimenter/experimenter/nimbus-ui/src/components/PageEditAudience/FormAudience/index.test.tsx
+++ experimenter/experimenter/nimbus-ui/src/components/PageEditAudience/FormAudience/index.test.tsx
@@ -121,7 +121,7 @@ describe("FormAudience", () => {
     );
 
     expect(screen.getByTestId("isSticky")).toBeChecked();
-    expect(screen.queryByText("isFirstRun")).not.toBeInTheDocument();
+    expect(screen.queryByText("First Run Experiment")).not.toBeInTheDocument();
   });
 
   it("expect sticky enrollment to be not selected as sticky is not required for the selected targeting", async () => {
