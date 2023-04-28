346cec763f9e0110dd0fa99f68754ede5a153503
Benjamin Forehand <bennyjr169@gmail.com>
2023-04-27 20:01:23 -0500

    Updates.

--- .circleci/create_parallel_steps.py
+++ .circleci/create_parallel_steps.py
@@ -6,18 +6,19 @@ if __name__ == "__main__":
     tests = []
     final_string = "()"
 
-    with open("../tmp_test_names.txt") as r:
+    with open("tmp_test_names.txt") as r:
         for item in r:
             if "test_" in item and "pytest_" not in item and ".py" not in item:
-                item = item.split()[1].split("[")[0]
+                item = item.split()[1].split(">")[0]
                 item = f"{item}"
                 tests.append(item)
-    with open("../_test_names.txt", "+w") as _:
+    with open("_test_names.txt", "+w") as _:
         for item in tests:
-            _.write(f"{item} ")
+            _.write(f"{item} \n")
     # split using circleci
-    stream = os.popen("circleci tests split ../_test_names.txt --total=2")
-    output = stream.read().strip("\n").split(" ")
-    length_of_tests = len(tests)
-    print(f"-k '({' and '.join(output)})'")
+    stream = os.popen("circleci tests split _test_names.txt")
+    output = stream.read().strip(" ").splitlines()[0].split(" ")
+    output = ' or '.join(output)
+    print(f"-k '{''.join(output)}'")
+
 
