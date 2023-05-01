14cc80a6d1fede968cf7080981e661870e175b0f
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-05-01 13:58:31 -0700

    fix #87731 fix(cirrus): Requested changes

--- cirrus/server/tests/test_experiment_recipes.py
+++ cirrus/server/tests/test_experiment_recipes.py
@@ -1,13 +1,13 @@
-from ..cirrus.experiment_recipes import RemoteSetting
+from ..cirrus.experiment_recipes import RemoteSettings
 
 
 def test_get_recipes_is_empty():
-    rs = RemoteSetting()
+    rs = RemoteSettings()
     assert rs.get_recipes() == []
 
 
 def test_update_recipes():
-    rs = RemoteSetting()
+    rs = RemoteSettings()
     new_recipes = [{"experiment1": True}, {"experiment2": False}]
     rs.update_recipes(new_recipes)
     assert rs.get_recipes() == new_recipes
