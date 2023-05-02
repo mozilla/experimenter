9c7b29bdd99e02a8a0a7ab1f3c5eba2132232fa2
Ed Lee <edilee@mozilla.com>
2023-05-01 22:11:24 -0700

    feat(nimbus): add targeting to exclude import infrequent rollouts (#8743)
    Because we want to exclude any user in either the original
    https://experimenter.services.mozilla.com/nimbus/import-infrequent-rollout-make-yourself-at-home
    or relaunched
    https://experimenter.services.mozilla.com/nimbus/updated-import-infrequent-rollout-make-yourself-at-home-copy
    so that a new experiment
    https://experimenter.services.mozilla.com/nimbus/new-import-interface-firefox-114-launch
    for a separate feature doesn't interact poorly (import spotlight modal
    could be stuck when some new wizard branches opens about:preferences in
    a tab)
    
    This commit adds targeting to exclude (and unenroll -- i.e., don't want
    sticky) from the experiment anybody in the rollout. It's insufficient to
    add targeting that is opposite of the rollouts (i.e., `!(infrequent 5
    bookmarks)`) because the rollouts have sticky enrollment.
    
    This also updates the tests to handle "intersect" binary operator in the
    jexl tests to match
    https://searchfox.org/mozilla-central/rev/26790fecfcda622dab234b28859da721b80f3a35/toolkit/components/utils/FilterExpressions.sys.mjs#35,84-96
    
    @dmose @jwayn

--- experimenter/experimenter/experiments/tests/__init__.py
+++ experimenter/experimenter/experiments/tests/__init__.py
@@ -1,8 +1,15 @@
 from pyjexl.jexl import JEXLConfig
-from pyjexl.operators import default_binary_operators, default_unary_operators
+from pyjexl.operators import Operator, default_binary_operators, default_unary_operators
 from pyjexl.parser import Parser, jexl_grammar
 
-jexl_config = JEXLConfig({}, default_unary_operators, default_binary_operators)
+jexl_config = JEXLConfig(
+    {},
+    default_unary_operators,
+    {
+        **default_binary_operators,
+        "intersect": Operator("intersect", 40, lambda x, y: set(x).intersection(y)),
+    },
+)
 
 
 class JEXLParser(Parser):
