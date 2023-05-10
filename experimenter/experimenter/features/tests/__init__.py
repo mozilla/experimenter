f082069a35e6894fab67aca69c4deb01bfebc3e2
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2023-05-10 16:35:10 -0400

    fix #8798 feat(nimbus): fetch remote schemas in circle task (#8806)
    Because
    
    * We have been prefetching feature manifests in a circle task and
    storing them in the repo
    * Feature manifests can refer to remote feature schemas
    * We have been remotely fetching those schemas at container startup time
    * As the number of remote feature schemas increases, this can delay
    container startup and introduces the risk of remote schemas failing to
    fetch during container start
    * We have been seeing intermittent deploy failures which may be caused
    by slow/failing remote schema fetches
    
    This commit
    
    * Fetches remote schemas as part of the circle ci task
    * Stores remote schemas in the repo
    * Loads remote schemas from disk at container startup time

--- experimenter/experimenter/features/tests/__init__.py
+++ experimenter/experimenter/features/tests/__init__.py
@@ -15,16 +15,23 @@ mock_invalid_features = override_settings(
     )
 )
 
+MOCK_REMOTE_FEATURE_MANIFESTS_PATH = os.path.join(
+    pathlib.Path(__file__).parent.absolute(), "fixtures", "remote_schema_features"
+)
 mock_remote_schema_features = override_settings(
-    FEATURE_MANIFESTS_PATH=os.path.join(
-        pathlib.Path(__file__).parent.absolute(), "fixtures", "remote_schema_features"
-    )
+    FEATURE_MANIFESTS_PATH=MOCK_REMOTE_FEATURE_MANIFESTS_PATH,
+    FEATURE_SCHEMAS_PATH=os.path.join(MOCK_REMOTE_FEATURE_MANIFESTS_PATH, "schemas"),
+)
+
+MOCK_INVALID_REMOTE_FEATURE_MANIFESTS_PATH = os.path.join(
+    pathlib.Path(__file__).parent.absolute(),
+    "fixtures",
+    "invalid_remote_schema_features",
 )
 
 mock_invalid_remote_schema_features = override_settings(
-    FEATURE_MANIFESTS_PATH=os.path.join(
-        pathlib.Path(__file__).parent.absolute(),
-        "fixtures",
-        "invalid_remote_schema_features",
-    )
+    FEATURE_MANIFESTS_PATH=MOCK_INVALID_REMOTE_FEATURE_MANIFESTS_PATH,
+    FEATURE_SCHEMAS_PATH=os.path.join(
+        MOCK_INVALID_REMOTE_FEATURE_MANIFESTS_PATH, "schemas"
+    ),
 )
