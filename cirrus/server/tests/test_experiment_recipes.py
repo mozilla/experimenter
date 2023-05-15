5fe12b1f5155f7767fe4cf053a215739683ffb12
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-05-15 16:26:22 -0700

    fix #8732 feat(cirrus): Sync Remote setting to get collection of experiments (#8797)
    Because
    
    - We want to fetch experiments from RS and keep a copy of those
    experiments in memory
    
    This commit
    
    - Uses [Apscheduler
    library](https://apscheduler.readthedocs.io/en/3.x/userguide.html) to
    sync [RS
    collection](https://firefox.settings.services.mozilla.com/v1/buckets/main/collections/nimbus-web-experiments/records)
    - uses  `.env` values to configure remote setting URL and refresh rate 
    - provide sample env file as `.env.sample` file inside cirrus/server
    directory to configure/set value
    - Retry failed job up to 3 times
    - test cases to verify changes
    - Rename API to `v1/features/` and post request
    Note: The JSON response will be validated in a different ticket, the
    scope of this ticket is to get the ability to sync it time to time and
    get list in memory, also logger will be improved in another ticket

--- cirrus/server/tests/test_experiment_recipes.py
+++ cirrus/server/tests/test_experiment_recipes.py
@@ -1,13 +1,82 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+import requests
+
 from cirrus.experiment_recipes import RemoteSettings
 
 
-def test_get_recipes_is_empty():
-    rs = RemoteSettings()
-    assert rs.get_recipes() == []
+class TestFetchRecipes(unittest.TestCase):
+    def get_remote_settings(self):
+        return RemoteSettings()
 
+    def test_get_recipes_is_empty(self):
+        rs = self.get_remote_settings()
+        assert rs.get_recipes() == []
 
-def test_update_recipes():
-    rs = RemoteSettings()
-    new_recipes = [{"experiment1": True}, {"experiment2": False}]
-    rs.update_recipes(new_recipes)
-    assert rs.get_recipes() == new_recipes
+    def test_update_recipes(self):
+        rs = self.get_remote_settings()
+        new_recipes = [{"experiment1": True}, {"experiment2": False}]
+        rs.update_recipes(new_recipes)
+        assert rs.get_recipes() == new_recipes
+
+    @patch("cirrus.experiment_recipes.requests.get")
+    def test_empty_data_key(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {"data": []}
+        mock_get.return_value = mock_response
+
+        rs = self.get_remote_settings()
+        rs.fetch_recipes()
+        self.assertEqual(rs.get_recipes(), [])
+
+    @patch("cirrus.experiment_recipes.requests.get")
+    def test_non_empty_data_key(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "data": [{"experiment1": True}, {"experiment2": False}]
+        }
+        mock_get.return_value = mock_response
+
+        rs = self.get_remote_settings()
+        rs.fetch_recipes()
+        self.assertEqual(
+            rs.get_recipes(), [{"experiment1": True}, {"experiment2": False}]
+        )
+
+    @patch("cirrus.experiment_recipes.requests.get")
+    def test_successful_response(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {"data": []}
+        mock_get.return_value = mock_response
+
+        rs = self.get_remote_settings()
+        rs.fetch_recipes()
+        mock_get.assert_called_once_with(rs.url)
+
+    @patch("cirrus.experiment_recipes.requests.get")
+    def test_failed_request(self, mock_get):
+        mock_get.side_effect = requests.exceptions.RequestException("Failed request")
+
+        rs = self.get_remote_settings()
+
+        with self.assertRaises(requests.exceptions.RequestException) as context:
+            rs.fetch_recipes()
+
+        self.assertEqual(str(context.exception), "Failed request")
+        self.assertEqual(rs.get_recipes(), [])
+
+    @patch("cirrus.experiment_recipes.requests.get")
+    def test_empty_data_key_with_non_empty_recipes(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {"data": []}
+        mock_get.return_value = mock_response
+
+        rs = self.get_remote_settings()
+        rs.update_recipes([{"experiment1": True}])
+        rs.fetch_recipes()
+        self.assertEqual(rs.get_recipes(), [{"experiment1": True}])
