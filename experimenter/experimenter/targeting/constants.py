0b7fe176ea58b7a022ed0d645209aea610dbb0bb
Rebecca BurWei <rebecca.burwei@gmail.com>
2023-05-15 10:24:06 -0500

    Add targeting for Google Trending Experiment (#8825)
    Because
    
    the Google Trending experiment requires custom advanced targeting
    
    This commit
    
    adds the advanced targeting
    
    ---------
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -1067,6 +1067,17 @@ HAS_GOOGLE_BING_DDG_AS_CURRENT_DEFAULT_SEARCH_ENGINE = NimbusTargetingConfig(
     application_choice_names=(Application.DESKTOP.name,),
 )
 
+HAS_GOOGLE_AS_CURRENT_DEFAULT_SEARCH_ENGINE = NimbusTargetingConfig(
+    name="Has Google as current default search engine",
+    slug="has_google_as_current_default_search_engine",
+    description="Users with Google as current default engine",
+    targeting=("'google' in searchEngines.current"),
+    desktop_telemetry="",
+    sticky_required=True,
+    is_first_run_required=False,
+    application_choice_names=(Application.DESKTOP.name,),
+)
+
 NEW_ANDROID_13_USERS = NimbusTargetingConfig(
     name="New Android 13 Users",
     slug="new_android_13_users",
