c44d3746047a8586b1b1727d3ae728f52e4eabfc
Mike Williams <mwilliams@mozilla.com>
2023-05-26 16:42:55 -0400

    test more of metadata object parsing

--- schemas/mozilla_nimbus_schemas/tests/jetstream/test_metadata.py
+++ schemas/mozilla_nimbus_schemas/tests/jetstream/test_metadata.py
@@ -61,7 +61,14 @@ def test_parse_metadata():
     metadata_json = """
         {
             "analysis_start_time": "2023-05-01 01:02:03.000000+00:00",
-            "external_config": null,
+            "external_config": {
+                "reference_branch": "test-branch",
+                "end_date": null,
+                "start_date": "2023-05-15",
+                "enrollment_period": 14,
+                "skip": false,
+                "url": "https://example.com/test/config"
+            },
             "metrics": {
                 "active_hours": {
                     "analysis_bases": [
@@ -82,12 +89,24 @@ def test_parse_metadata():
                     "friendly_name": "Ad clicks"
                 }
             },
-            "outcomes": {},
+            "outcomes": {
+                "test_outcome": {
+                    "commit_hash": "abcd1234",
+                    "default_metrics": ["test-metric1"],
+                    "description": "outcome for testing",
+                    "friendly_name": "Test Outcome",
+                    "metrics": ["test-metric1", "test-metric2"],
+                    "slug": "test-outcome"
+                }
+            },
             "schema_version": 4
         }
     """
     metadata = Metadata.parse_raw(metadata_json)
-    assert metadata.external_config is None
+    assert metadata.external_config is not None
+    assert not metadata.external_config.skip
+    assert metadata.outcomes != {}
+    assert metadata.outcomes.get("test_outcome").slug == "test-outcome"
     assert metadata.schema_version == 4
     assert metadata.analysis_start_time == dt.datetime(
         2023, 5, 1, 1, 2, 3, tzinfo=dt.timezone.utc
