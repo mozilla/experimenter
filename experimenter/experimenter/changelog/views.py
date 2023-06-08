8692fb031e79f0366b5c72acff123e96ad9fa702
Avi <avishkar.22011115@viit.ac.in>
2023-06-08 05:58:35 +0530

    fixes #8394 feat(nimbus):added tests to maintain 100% coverage along with a test template
    Signed-off-by: Avi <avishkar.22011115@viit.ac.in>

--- experimenter/experimenter/changelog/views.py
+++ experimenter/experimenter/changelog/views.py
@@ -1,36 +1,32 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView
-
+from django.http import HttpResponse
+from django.views.generic import ListView
 from experimenter.experiments.models import NimbusChangeLog, NimbusExperiment
 
 
-class NimbusChangeLogsView(DetailView):
+
+class NimbusChangeLogsView(ListView):
     model = NimbusChangeLog
-    context_object_name = "experiment"
+    template_name = 'changelog/test.html'
+    context_object_name = 'experiment'
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         return super().get(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
-        slug = self.kwargs.get("slug")
+        slug = self.kwargs.get('slug')
         experiment = get_object_or_404(NimbusExperiment, slug=slug)
         return experiment
 
     def get_context_data(self, **kwargs):
-        slug = self.kwargs.get("slug")
+        slug = self.kwargs.get('slug')
         context = super().get_context_data(**kwargs)
         experiment = self.object
 
         # Retrieve the changelogs for the experiment
         changelogs = experiment.changes.all()
 
-        context["slug"] = slug
-        context["changelogs"] = changelogs
+        context['slug'] = slug
+        context['changelogs'] = changelogs
         return context
-
-    def render_to_response(
-        self, context, **response_kwargs
-    ):  # rendering simple text until templates are implemented
-        return HttpResponse("test")
