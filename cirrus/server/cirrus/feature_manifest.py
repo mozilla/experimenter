bf653b78653d5fdc54fa7eaf694eb3b79e5cfca5
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-06-09 09:15:47 -0700

    fix #8874 feat(cirrus): partial config and fml test

--- cirrus/server/cirrus/feature_manifest.py
+++ cirrus/server/cirrus/feature_manifest.py
@@ -1,14 +1,18 @@
 import json
 from typing import Any, Dict
+import logging
 
 from fml_sdk import FmlClient  # type: ignore
 
 from .settings import channel, fml_path
 
+logger = logging.getLogger(__name__)
+
 
 class FeatureManifestLanguage:
     def __init__(self):
         self.fml_client = FmlClient(fml_path, channel)
+        self.merge_errors = []
 
     def compute_feature_configurations(
         self,
@@ -23,11 +27,13 @@ class FeatureManifestLanguage:
         merged_res: Dict[str, Any] = self.fml_client.merge(  # type: ignore
             feature_configs
         )
-        if len(merged_res.errors) > 0:
-            print(merged_res.errors)
+        self.merge_errors = merged_res.errors
+        if self.merge_errors:
+            logger.error(
+                f"An error occurred during enrolled partial config and FML: {self.merge_errors}"
+            )
 
         merged_res_json = json.loads(merged_res.json)
-        print("merged res", merged_res)
         return merged_res_json
 
 
--- cirrus/server/tests/test_feature_manifest.py
+++ cirrus/server/tests/test_feature_manifest.py
@@ -1,29 +1,116 @@
 import json
 import unittest
+from parameterized import parameterized
 
 from cirrus.feature_manifest import FeatureManifestLanguage
 from cirrus.sdk import SDK
+from fml_sdk import FmlError
 
 
 class FeatureManifestLanguageTestCase(unittest.TestCase):
-    maxDiff = None
+    def setUp(self):
+        self.fml = FeatureManifestLanguage()
+        self.sdk = SDK()
 
-    def test_compute_feature_configurations(self):
-        fml = FeatureManifestLanguage()
-
+    def test_compute_feature_configurations_always_return_default_config(self):
         enrolled_partial_configuration = {
             "enrolledFeatureConfigMap": {},
             "enrollments": [],
             "events": [],
         }
 
-        result = fml.compute_feature_configurations(enrolled_partial_configuration)
+        result = self.fml.compute_feature_configurations(enrolled_partial_configuration)
 
         self.assertEqual(
             result, {"example-feature": {"enabled": False, "something": "wicked"}}
         )
 
-    def test_compute_feature_configurations_with_partial_config(self):
+    @parameterized.expand(
+        [
+            [
+                "imported-module-1-included-feature-1",
+                False,
+            ],
+            ["example-feature", True],
+            ["", False],
+        ]
+    )
+    def test_compute_feature_configurations_feature(
+        self, feature_name, expected_feature_status
+    ):
+        enrolled_partial_configuration = {
+            "enrolledFeatureConfigMap": {
+                feature_name: {
+                    "branch": "treatment",
+                    "feature": {
+                        "featureId": feature_name,
+                        "value": {"enabled": True},
+                    },
+                    "featureId": feature_name,
+                    "slug": "experiment_slug",
+                }
+            },
+            "enrollments": [
+                {
+                    "slug": "experiment_slug",
+                    "status": {
+                        "Enrolled": {
+                            "branch": "treatment",
+                            "enrollment_id": "enrollment_id",
+                            "reason": "Qualified",
+                        }
+                    },
+                }
+            ],
+            "events": [
+                {
+                    "branch_slug": "treatment",
+                    "change": "Enrollment",
+                    "enrollment_id": "enrollment_id",
+                    "experiment_slug": "experiment_slug",
+                    "reason": None,
+                }
+            ],
+        }
+
+        result = self.fml.compute_feature_configurations(enrolled_partial_configuration)
+
+        self.assertEqual(
+            result,
+            {
+                "example-feature": {
+                    "enabled": expected_feature_status,
+                    "something": "wicked",
+                }
+            },
+        )
+
+    def test_compute_feature_configurations_invalid_key_merge_errors(self):
+        enrolled_partial_configuration = {
+            "enrolledFeatureConfigMap": {
+                "example-feature": {
+                    "branch": "treatment",
+                    "feature": {
+                        "featureId": "example-feature",
+                        "value": {"enabled1": True},  # invalid
+                    },
+                    "featureId": "example-feature",
+                    "slug": "experiment-slug",
+                }
+            },
+            "enrollments": [],
+            "events": [],
+        }
+
+        result = self.fml.compute_feature_configurations(enrolled_partial_configuration)
+
+        self.assertEqual(
+            result, {"example-feature": {"enabled": False, "something": "wicked"}}
+        )  # returns default config
+        self.assertEqual(len(self.fml.merge_errors), 1)
+        assert isinstance(self.fml.merge_errors[0], FmlError)
+
+    def test_compute_feature_configurations_targeting_doesnt_match(self):
         bucket_config = {
             "randomizationUnit": "user_id",
             "count": 100,
@@ -37,7 +124,7 @@ class FeatureManifestLanguageTestCase(unittest.TestCase):
                 "slug": "control",
                 "ratio": 1,
                 "feature": {
-                    "featureId": "imported-module-1-included-feature-1",
+                    "featureId": "example-feature",
                     "value": {"enabled": False},
                 },
             },
@@ -45,115 +132,53 @@ class FeatureManifestLanguageTestCase(unittest.TestCase):
                 "slug": "treatment",
                 "ratio": 1,
                 "feature": {
-                    "featureId": "imported-module-1-included-feature-1",
+                    "featureId": "example-feature",
                     "value": {"enabled": True},
                 },
             },
         ]
 
-        experiment_slug = "experiment-slug"
-        enrollment_id = "c3d0aa7b-c52c-4548-8a9a-13475e445cf2"
-
         experiment = {
             "schemaVersion": "1.0.0",
-            "slug": experiment_slug,
+            "slug": "experiment-slug",
             "userFacingName": "",
             "userFacingDescription": "",
             "appId": "test_app_id",
             "appName": "test_app_name",
             "channel": "developer",
-            "targeting": '(is_already_enrolled) || (username in ["test", "jeddai"])',
+            "targeting": '(is_already_enrolled) || (username in ["test", "test2"])',
             "bucketConfig": bucket_config,
             "isRollout": False,
             "isEnrollmentPaused": False,
             "proposedEnrollment": 10,
             "branches": branches,
-            "featureIds": ["imported-module-1-included-feature-1"],
+            "featureIds": ["example-feature"],
         }
 
         data = json.dumps({"data": [experiment]})
-        targeting_context = {"clientId": "test", "requestContext": {"username": "test"}}
-        sdk = SDK()
-        sdk.set_experiments(data)
-        result = sdk.compute_enrollments(targeting_context)
-
-        expected_result = {
-            "enrolledFeatureConfigMap": {
-                "imported-module-1-included-feature-1": {
-                    "branch": "treatment",
-                    "feature": {
-                        "featureId": "imported-module-1-included-feature-1",
-                        "value": {"enabled": True},
-                    },
-                    "featureId": "imported-module-1-included-feature-1",
-                    "slug": experiment_slug,
-                }
-            },
-            "enrollments": [
-                {
-                    "slug": experiment_slug,
-                    "status": {
-                        "Enrolled": {
-                            "branch": "treatment",
-                            "enrollment_id": enrollment_id,
-                            "reason": "Qualified",
-                        }
-                    },
-                }
-            ],
-            "events": [
-                {
-                    "branch_slug": "treatment",
-                    "change": "Enrollment",
-                    "enrollment_id": enrollment_id,
-                    "experiment_slug": experiment_slug,
-                    "reason": None,
-                }
-            ],
+        targeting_context = {
+            "clientId": "test",
+            "requestContext": {"username": "test1"},
         }
-
-        # self.assertEqual(result, expected_result)
+        self.sdk.set_experiments(data)
+        enrolled_partial_configuration = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(
-            result["enrolledFeatureConfigMap"]["imported-module-1-included-feature-1"][
-                "branch"
-            ],
-            expected_result["enrolledFeatureConfigMap"][
-                "imported-module-1-included-feature-1"
-            ]["branch"],
+            enrolled_partial_configuration,
+            {
+                "enrolledFeatureConfigMap": {},
+                "enrollments": [
+                    {
+                        "slug": "experiment-slug",
+                        "status": {"NotEnrolled": {"reason": "NotTargeted"}},
+                    }
+                ],
+                "events": [],
+            },
         )
 
-        self.assertEqual(
-            result["enrolledFeatureConfigMap"]["imported-module-1-included-feature-1"][
-                "feature"
-            ]["value"],
-            expected_result["enrolledFeatureConfigMap"][
-                "imported-module-1-included-feature-1"
-            ]["feature"]["value"],
-        )
-        self.assertEqual(
-            result["enrollments"][0]["status"]["Enrolled"]["branch"],
-            expected_result["enrollments"][0]["status"]["Enrolled"]["branch"],
-        )
-        self.assertEqual(
-            result["enrollments"][0]["status"]["Enrolled"]["reason"],
-            expected_result["enrollments"][0]["status"]["Enrolled"]["reason"],
-        )
-        self.assertEqual(
-            result["events"][0]["branch_slug"],
-            expected_result["events"][0]["branch_slug"],
-        )
-        self.assertEqual(
-            result["events"][0]["change"],
-            expected_result["events"][0]["change"],
-        )
-        self.assertEqual(
-            result["events"][0]["reason"],
-            expected_result["events"][0]["reason"],
-        )
-        fml = FeatureManifestLanguage()
-
-        result = fml.compute_feature_configurations(result)
+        result = self.fml.compute_feature_configurations(enrolled_partial_configuration)
 
         self.assertEqual(
             result, {"example-feature": {"enabled": False, "something": "wicked"}}
-        )
+        )  # default config
+        self.assertEqual(len(self.fml.merge_errors), 0)
