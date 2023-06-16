1255986d8d6b7fd9070e18cd8f200422c8c04355
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-06-16 10:28:52 -0700

    fix #8940 feat(cirrus): Move objects into main file 🧹  (#8978)
    Because
    
    - Python `parses` the file line by line and it may cause an issue when
    creating several objects like` fml, SDK,` etc, currently, we are
    creating objects into their individual class file and importing them
    into the `main`
    
    This commit
    
    - Deletes unnecessary files
    - move objects into the main file and separate method for setup
    - FML- Exception handling and exit the container` if the path or channel
    is invalid `
    - SDK- Exception handling and exit the container if `context is missing
    any required argument`
    - Removes file path from docker volumes for feature manifest
    
    UPDATE: uses the concept of `lifespan` instead of using `startup and
    shutdown events`, thanks to @jaredlockhart
    - This PR also updates the version of Fast API to support `lifespan`
    closes #8940

--- cirrus/server/cirrus/experiment_recipes.py
+++ cirrus/server/cirrus/experiment_recipes.py
@@ -4,23 +4,24 @@ from typing import Any, Dict, List
 
 import requests
 
-from .sdk import sdk
+from .sdk import SDK
 from .settings import remote_setting_url
 
 logger = logging.getLogger(__name__)
 
 
 class RemoteSettings:
-    def __init__(self):
+    def __init__(self, sdk: SDK):
         self.recipes: Dict[str, List[Any]] = {"data": []}
         self.url: str = remote_setting_url
+        self.sdk = sdk
 
     def get_recipes(self) -> Dict[str, List[Any]]:
         return self.recipes
 
     def update_recipes(self, new_recipes: Dict[str, List[Any]]) -> None:
         self.recipes = new_recipes
-        sdk.set_experiments(json.dumps(self.recipes))
+        self.sdk.set_experiments(json.dumps(self.recipes))
 
     def fetch_recipes(self):
         try:
@@ -36,6 +37,3 @@ class RemoteSettings:
             logger.info("Fetched resources")
         else:
             logger.warning("No recipes found in the response")
-
-
-remote_setting = RemoteSettings()
--- cirrus/server/tests/test_sdk.py
+++ cirrus/server/tests/test_sdk.py
@@ -1,86 +1,121 @@
+import json
 import unittest
 
 from cirrus_sdk import NimbusError
+from parameterized import parameterized
 
 from cirrus.sdk import SDK
+from cirrus.settings import context
 
 
 class SDKTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sdk = SDK(context=context)
+
+    @parameterized.expand(
+        [
+            [
+                json.dumps(
+                    {  # missing app_id
+                        "app_name": "test_name",
+                        "channel": "test_channel",
+                    }
+                ),
+                "NimbusError.JsonError('JSON Error: missing field `app_id`",
+            ],
+            [
+                json.dumps(
+                    {  # missing app_name
+                        "app_id": "test_id",
+                        "channel": "test_channel",
+                    }
+                ),
+                "NimbusError.JsonError('JSON Error: missing field `app_name`",
+            ],
+            [
+                json.dumps(
+                    {  # missing channel
+                        "app_id": "test_id",
+                        "app_name": "test_app_name",
+                    }
+                ),
+                "NimbusError.JsonError('JSON Error: missing field `channel`",
+            ],
+        ]
+    )
+    def test_invalid_context(self, context, expected_error_message):
+        with self.assertRaises(NimbusError) as e:
+            SDK(context=context)
+
+        self.assertTrue(str(e.exception).startswith(expected_error_message))
+
     def test_compute_enrollments(self):
-        sdk = SDK()
         targeting_context = {"clientId": "test", "requestContext": {}}
 
-        result = sdk.compute_enrollments(targeting_context)
+        result = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(
             result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
         )
 
     def test_failure_case_no_client_id_key(self):
-        sdk = SDK()
         targeting_context = {"requestContext": {}}
 
         try:
-            result = sdk.compute_enrollments(targeting_context)
+            result = self.sdk.compute_enrollments(targeting_context)
         except NimbusError:
             self.fail("NimbusError was raised when it should not have been")
         self.assertEqual(result, {})
 
     def test_failure_case_no_client_id_value(self):
-        sdk = SDK()
         targeting_context = {"clientId": None, "requestContext": {}}
 
         try:
-            result = sdk.compute_enrollments(targeting_context)
+            result = self.sdk.compute_enrollments(targeting_context)
         except NimbusError:
             self.fail("NimbusError was raised when it should not have been")
         self.assertEqual(result, {})
 
     def test_failure_case_no_request_context(self):
-        sdk = SDK()
         targeting_context = {"clientId": "test"}
 
         try:
-            result = sdk.compute_enrollments(targeting_context)
+            result = self.sdk.compute_enrollments(targeting_context)
         except NimbusError:
             self.fail("NimbusError was raised when it should not have been")
         self.assertEqual(result, {})
 
     def test_set_experiments_recipe_both_empty(self):
-        sdk = SDK()
         recipes = "{}"
         targeting_context = {}
-        sdk.set_experiments(recipes)
-        result = sdk.compute_enrollments(targeting_context)
+        self.sdk.set_experiments(recipes)
+        result = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(result, {})
 
     def test_set_experiments_success(self):
-        sdk = SDK()
         recipes = '{"data": [{"experiment1": True}, {"experiment2": False}]}'
         targeting_context = {"clientId": "test", "requestContext": {}}
-        sdk.set_experiments(recipes)
-        result = sdk.compute_enrollments(targeting_context)
+        self.sdk.set_experiments(recipes)
+        result = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(
             result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
         )
 
     def test_set_experiments_failure_missing_data_key(self):
-        sdk = SDK()
         recipes = '{"invalid": []}'
 
         targeting_context = {"clientId": "test", "requestContext": {}}
-        sdk.set_experiments(recipes)
-        result = sdk.compute_enrollments(targeting_context)
+        self.sdk.set_experiments(recipes)
+        result = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(
             result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
         )
 
     def test_set_experiments_failure_malform_key(self):
-        sdk = SDK()
         recipes = '{"invalid}'
 
         targeting_context = {"clientId": "test", "requestContext": {}}
-        sdk.set_experiments(recipes)
-        result = sdk.compute_enrollments(targeting_context)
+        self.sdk.set_experiments(recipes)
+        result = self.sdk.compute_enrollments(targeting_context)
         self.assertEqual(
             result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
         )
