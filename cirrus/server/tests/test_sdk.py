887af009a15f71f141d32d29c7feeb879a76df6b
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-06-20 23:03:47 -0700

    fix #8993 feat(cirrus): Consistent use of pytest in test cases 🧪  (#9000)
    Because
    
    - In some test cases we are using `unittest` and in some cases `pytest`
    
    This commit
    
    - Convert all the test cases to use consistent `pytest`
    - Removed unnecessary
    [parameterized](https://pypi.org/project/parameterized/) package and
    replaces with the pytest parameterization
    - Fixtures to reuse object creation
    
    closes #8993

--- cirrus/server/tests/test_sdk.py
+++ cirrus/server/tests/test_sdk.py
@@ -1,121 +1,124 @@
 import json
-import unittest
 
+import pytest
 from cirrus_sdk import NimbusError
-from parameterized import parameterized
 
 from cirrus.sdk import SDK
-from cirrus.settings import context
 
 
-class SDKTestCase(unittest.TestCase):
-    def setUp(self):
-        self.sdk = SDK(context=context)
+@pytest.mark.parametrize(
+    ("context", "expected_error_message"),
+    [
+        (
+            json.dumps(
+                {  # missing app_id
+                    "app_name": "test_name",
+                    "channel": "test_channel",
+                }
+            ),
+            "NimbusError.JsonError('JSON Error: missing field `app_id`",
+        ),
+        (
+            json.dumps(
+                {  # missing app_name
+                    "app_id": "test_id",
+                    "channel": "test_channel",
+                }
+            ),
+            "NimbusError.JsonError('JSON Error: missing field `app_name`",
+        ),
+        (
+            json.dumps(
+                {  # missing channel
+                    "app_id": "test_id",
+                    "app_name": "test_app_name",
+                }
+            ),
+            "NimbusError.JsonError('JSON Error: missing field `channel`",
+        ),
+    ],
+)
+def test_invalid_context(context, expected_error_message):
+    with pytest.raises(NimbusError) as e:
+        SDK(context=context)
 
-    @parameterized.expand(
-        [
-            [
-                json.dumps(
-                    {  # missing app_id
-                        "app_name": "test_name",
-                        "channel": "test_channel",
-                    }
-                ),
-                "NimbusError.JsonError('JSON Error: missing field `app_id`",
-            ],
-            [
-                json.dumps(
-                    {  # missing app_name
-                        "app_id": "test_id",
-                        "channel": "test_channel",
-                    }
-                ),
-                "NimbusError.JsonError('JSON Error: missing field `app_name`",
-            ],
-            [
-                json.dumps(
-                    {  # missing channel
-                        "app_id": "test_id",
-                        "app_name": "test_app_name",
-                    }
-                ),
-                "NimbusError.JsonError('JSON Error: missing field `channel`",
-            ],
-        ]
-    )
-    def test_invalid_context(self, context, expected_error_message):
-        with self.assertRaises(NimbusError) as e:
-            SDK(context=context)
+        assert str(e.value).startswith(expected_error_message)
 
-        self.assertTrue(str(e.exception).startswith(expected_error_message))
 
-    def test_compute_enrollments(self):
-        targeting_context = {"clientId": "test", "requestContext": {}}
+def test_compute_enrollments(sdk):
+    targeting_context = {"clientId": "test", "requestContext": {}}
 
-        result = self.sdk.compute_enrollments(targeting_context)
-        self.assertEqual(
-            result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
-        )
+    result = sdk.compute_enrollments(targeting_context)
+    assert result == {
+        "enrolledFeatureConfigMap": {},
+        "enrollments": [],
+        "events": [],
+    }
 
-    def test_failure_case_no_client_id_key(self):
-        targeting_context = {"requestContext": {}}
 
-        try:
-            result = self.sdk.compute_enrollments(targeting_context)
-        except NimbusError:
-            self.fail("NimbusError was raised when it should not have been")
-        self.assertEqual(result, {})
+def test_failure_case_no_client_id_key(sdk):
+    targeting_context = {"requestContext": {}}
 
-    def test_failure_case_no_client_id_value(self):
-        targeting_context = {"clientId": None, "requestContext": {}}
+    try:
+        result = sdk.compute_enrollments(targeting_context)
+    except NimbusError:
+        pytest.fail("NimbusError was raised when it should not have been")
+    assert result == {}
 
-        try:
-            result = self.sdk.compute_enrollments(targeting_context)
-        except NimbusError:
-            self.fail("NimbusError was raised when it should not have been")
-        self.assertEqual(result, {})
 
-    def test_failure_case_no_request_context(self):
-        targeting_context = {"clientId": "test"}
+def test_failure_case_no_client_id_value(sdk):
+    targeting_context = {"clientId": None, "requestContext": {}}
 
-        try:
-            result = self.sdk.compute_enrollments(targeting_context)
-        except NimbusError:
-            self.fail("NimbusError was raised when it should not have been")
-        self.assertEqual(result, {})
+    try:
+        result = sdk.compute_enrollments(targeting_context)
+    except NimbusError:
+        pytest.fail("NimbusError was raised when it should not have been")
+    assert result == {}
 
-    def test_set_experiments_recipe_both_empty(self):
-        recipes = "{}"
-        targeting_context = {}
-        self.sdk.set_experiments(recipes)
-        result = self.sdk.compute_enrollments(targeting_context)
-        self.assertEqual(result, {})
 
-    def test_set_experiments_success(self):
-        recipes = '{"data": [{"experiment1": True}, {"experiment2": False}]}'
-        targeting_context = {"clientId": "test", "requestContext": {}}
-        self.sdk.set_experiments(recipes)
-        result = self.sdk.compute_enrollments(targeting_context)
-        self.assertEqual(
-            result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
-        )
+def test_failure_case_no_request_context(sdk):
+    targeting_context = {"clientId": "test"}
 
-    def test_set_experiments_failure_missing_data_key(self):
-        recipes = '{"invalid": []}'
+    try:
+        result = sdk.compute_enrollments(targeting_context)
+    except NimbusError:
+        pytest.fail("NimbusError was raised when it should not have been")
+    assert result == {}
 
-        targeting_context = {"clientId": "test", "requestContext": {}}
-        self.sdk.set_experiments(recipes)
-        result = self.sdk.compute_enrollments(targeting_context)
-        self.assertEqual(
-            result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
-        )
 
-    def test_set_experiments_failure_malform_key(self):
-        recipes = '{"invalid}'
+def test_set_experiments_recipe_both_empty(sdk):
+    recipes = "{}"
+    targeting_context = {}
+    sdk.set_experiments(recipes)
+    result = sdk.compute_enrollments(targeting_context)
+    assert result == {}
 
-        targeting_context = {"clientId": "test", "requestContext": {}}
-        self.sdk.set_experiments(recipes)
-        result = self.sdk.compute_enrollments(targeting_context)
-        self.assertEqual(
-            result, {"enrolledFeatureConfigMap": {}, "enrollments": [], "events": []}
-        )
+
+def test_set_experiments_success(sdk):
+    recipes = '{"data": [{"experiment1": True}, {"experiment2": False}]}'
+    targeting_context = {"clientId": "test", "requestContext": {}}
+    sdk.set_experiments(recipes)
+    result = sdk.compute_enrollments(targeting_context)
+    assert result == {
+        "enrolledFeatureConfigMap": {},
+        "enrollments": [],
+        "events": [],
+    }
+
+
+@pytest.mark.parametrize(
+    ("recipes"),
+    [
+        ('{"invalid": []}'),
+        ('{"invalid}'),
+    ],
+)
+def test_set_experiments_failure_invalid_malform_key(sdk, recipes):
+    targeting_context = {"clientId": "test", "requestContext": {}}
+    sdk.set_experiments(recipes)
+    result = sdk.compute_enrollments(targeting_context)
+    assert result == {
+        "enrolledFeatureConfigMap": {},
+        "enrollments": [],
+        "events": [],
+    }
