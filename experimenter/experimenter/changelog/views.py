4bdea37cadad7958c6dcfcc7d1d7eeae9a3dc0b6
Avi <avishkar.22011115@viit.ac.in>
2023-06-23 23:14:27 +0530

    fixes #8394 feat(nimbus):change moves the get changelogs method into the NimbusExperiment model

--- experimenter/experimenter/changelog/views.py
+++ experimenter/experimenter/changelog/views.py
@@ -6,8 +6,3 @@ from experimenter.experiments.models import NimbusExperiment
 class NimbusChangeLogsView(DetailView):
     model = NimbusExperiment
     template_name = "changelog/overview.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["changelogs"] = list(self.object.changes.all())
-        return context
