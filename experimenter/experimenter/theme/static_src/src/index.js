67cc23035c4446ef7f87e88e91a5c3e7b8386e4a
Avi <avishkar.22011115@viit.ac.in>
2023-07-31 22:26:30 +0530

    Fixes #8886 feat(nimbus): switching rollup with webpack - test

--- experimenter/experimenter/theme/static_src/src/index.js
+++ experimenter/experimenter/theme/static_src/src/index.js
@@ -1,14 +1,14 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
 
 Alpine.data('dropdown', () => ({
     open: false,
  
     toggle() {
-        this.open = ! this.open
+        this.open = ! this.open;
     }
-}))
+}));
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
  
-Alpine.start()
+Alpine.start();
