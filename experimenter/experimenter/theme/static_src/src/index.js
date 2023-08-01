03874bb2de38cd6c2ee7093e2ee92418f767d4ae
Avi <avishkar.22011115@viit.ac.in>
2023-08-02 01:25:35 +0530

    Fixes #8886 feat(nimbus): updated code based on reviews

--- experimenter/experimenter/theme/static_src/src/index.js
+++ experimenter/experimenter/theme/static_src/src/index.js
@@ -1,14 +1,13 @@
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
-Alpine.data('dropdown', () => ({
-    open: false,
- 
-    toggle() {
-        this.open = ! this.open;
-    }
+Alpine.data("dropdown", () => ({
+  open: false,
+
+  toggle() {
+    this.open = !this.open;
+  },
 }));
 
 window.Alpine = Alpine;
 
- 
 Alpine.start();
