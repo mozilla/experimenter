6f2b929b2feff3e726ebbdb46aa22da1bcdaefd9
Yashika Khurana <yashikakhuranayashika@gmail.com>
2023-08-03 14:02:31 -0700

    fix #9205 feat(cirrus): Call glean shutdown event (#9209)
    Because
    
    - We want to gracefully shutdown the Glean app when Fastapi app shutdown
    
    This commit
    
    - Update to the new Glean SDK version `53.2.0`
    - Call Glean.shutdown() on app termination
    - Fixes some test cases
    fixes #9205

--- cirrus/server/tests/test_main.py
+++ cirrus/server/tests/test_main.py
@@ -87,9 +87,16 @@ def test_get_features_with_required_field(client):
             status.HTTP_422_UNPROCESSABLE_ENTITY,
             [
                 {
+                    "type": "missing",
                     "loc": ["body", "client_id"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
+                    "msg": "Field required",
+                    "input": {
+                        "context": {
+                            "key1": "value1",
+                            "key2": {"key2.1": "value2", "key2.2": "value3"},
+                        }
+                    },
+                    "url": "https://errors.pydantic.dev/2.1/v/missing",
                 }
             ],
         ),
@@ -103,9 +110,11 @@ def test_get_features_with_required_field(client):
             status.HTTP_422_UNPROCESSABLE_ENTITY,
             [
                 {
+                    "type": "missing",
                     "loc": ["body", "context"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
+                    "msg": "Field required",
+                    "input": {"client_id": "4a1d71ab-29a2-4c5f-9e1d-9d9df2e6e449"},
+                    "url": "https://errors.pydantic.dev/2.1/v/missing",
                 }
             ],
         ),
@@ -114,14 +123,18 @@ def test_get_features_with_required_field(client):
             status.HTTP_422_UNPROCESSABLE_ENTITY,
             [
                 {
+                    "type": "missing",
                     "loc": ["body", "client_id"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.1/v/missing",
                 },
                 {
+                    "type": "missing",
                     "loc": ["body", "context"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.1/v/missing",
                 },
             ],
         ),
