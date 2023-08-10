3f8c220bd9b7512468d8def7ee4d0b577387a2bb
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-10 15:00:20 -0700

    fix #9210 chore(nimbus): Fix migration issue

--- experimenter/experimenter/experiments/migrations/0244_auto_20230810_2150.py
+++ experimenter/experimenter/experiments/migrations/0244_auto_20230810_2150.py
@@ -6,23 +6,61 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('experiments', '0243_nimbusexperiment_takeaways_metric_gain'),
+        ("experiments", "0243_nimbusexperiment_takeaways_metric_gain"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='nimbusexperiment',
-            name='application',
-            field=models.CharField(choices=[('firefox-desktop', 'Firefox Desktop'), ('fenix', 'Firefox for Android (Fenix)'), ('ios', 'Firefox for iOS'), ('focus-android', 'Focus for Android'), ('klar-android', 'Klar for Android'), ('focus-ios', 'Focus for iOS'), ('klar-ios', 'Klar for iOS'), ('monitor-web', 'Monitor Web')], max_length=255),
+            model_name="nimbusexperiment",
+            name="application",
+            field=models.CharField(
+                choices=[
+                    ("firefox-desktop", "Firefox Desktop"),
+                    ("fenix", "Firefox for Android (Fenix)"),
+                    ("ios", "Firefox for iOS"),
+                    ("focus-android", "Focus for Android"),
+                    ("klar-android", "Klar for Android"),
+                    ("focus-ios", "Focus for iOS"),
+                    ("klar-ios", "Klar for iOS"),
+                    ("monitor-web", "Monitor Web"),
+                ],
+                max_length=255,
+            ),
         ),
         migrations.AlterField(
-            model_name='nimbusfeatureconfig',
-            name='application',
-            field=models.CharField(blank=True, choices=[('firefox-desktop', 'Firefox Desktop'), ('fenix', 'Firefox for Android (Fenix)'), ('ios', 'Firefox for iOS'), ('focus-android', 'Focus for Android'), ('klar-android', 'Klar for Android'), ('focus-ios', 'Focus for iOS'), ('klar-ios', 'Klar for iOS'), ('monitor-web', 'Monitor Web')], max_length=255, null=True),
+            model_name="nimbusfeatureconfig",
+            name="application",
+            field=models.CharField(
+                blank=True,
+                choices=[
+                    ("firefox-desktop", "Firefox Desktop"),
+                    ("fenix", "Firefox for Android (Fenix)"),
+                    ("ios", "Firefox for iOS"),
+                    ("focus-android", "Focus for Android"),
+                    ("klar-android", "Klar for Android"),
+                    ("focus-ios", "Focus for iOS"),
+                    ("klar-ios", "Klar for iOS"),
+                    ("monitor-web", "Monitor Web"),
+                ],
+                max_length=255,
+                null=True,
+            ),
         ),
         migrations.AlterField(
-            model_name='nimbusisolationgroup',
-            name='application',
-            field=models.CharField(choices=[('firefox-desktop', 'Firefox Desktop'), ('fenix', 'Firefox for Android (Fenix)'), ('ios', 'Firefox for iOS'), ('focus-android', 'Focus for Android'), ('klar-android', 'Klar for Android'), ('focus-ios', 'Focus for iOS'), ('klar-ios', 'Klar for iOS'), ('monitor-web', 'Monitor Web')], max_length=255),
+            model_name="nimbusisolationgroup",
+            name="application",
+            field=models.CharField(
+                choices=[
+                    ("firefox-desktop", "Firefox Desktop"),
+                    ("fenix", "Firefox for Android (Fenix)"),
+                    ("ios", "Firefox for iOS"),
+                    ("focus-android", "Focus for Android"),
+                    ("klar-android", "Klar for Android"),
+                    ("focus-ios", "Focus for iOS"),
+                    ("klar-ios", "Klar for iOS"),
+                    ("monitor-web", "Monitor Web"),
+                ],
+                max_length=255,
+            ),
         ),
     ]
