bf6083580135b47f2f91d98c3d7c9488c015ade2
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-10 11:54:02 -0700

    fix #9243 feat(cirrus): Node 18, proxy and remove extra files

--- demo-app/frontend/src/index.js
+++ demo-app/frontend/src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,20 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
--- demo-app/server/index.js
+++ demo-app/server/index.js
@@ -1,19 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // Allow CORS by setting appropriate headers
- res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.url === '/api/data' && req.method === 'GET') {
 
-    res.end(JSON.stringify({ message: 'Hello from the server!' }));
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+if (req.url === '/api/data' && req.method === 'GET') {
+  res.setHeader('Content-Type', 'application/json');
+  const jsonResponse = JSON.stringify({ message: 'Hello from the server!' });
+  console.log('Server Response:', jsonResponse);
+  res.end(jsonResponse);
+} else {
+  res.statusCode = 404;
+  res.end('Not Found');
+}
 });
 
 const PORT = 3002;
