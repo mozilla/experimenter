987b5ec1fdcec9b45f2c8966a47c307256f0f456
Mike Williams <102263964+mikewilli@users.noreply.github.com>
2023-08-10 15:31:43 -0400

    fix #9235 bug(nimbus): fix pop sizing test with factory (#9238)
    Because
    
    - a sizing schemas change had an incompatible change that led to a test
    failing
    - schemas tests should live in the package and not be concerned with the
    specific structure of the data
    - we still want to test the task itself
    
    This commit
    
    - changes the static test data in experimenter for pop sizing to a
    factory

--- experimenter/experimenter/jetstream/tests/test_tasks.py
+++ experimenter/experimenter/jetstream/tests/test_tasks.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from django.core.cache import cache
 from django.test import TestCase, override_settings
+from mozilla_nimbus_schemas.jetstream import SampleSizesFactory
 from parameterized import parameterized
 
 from experimenter.experiments.models import NimbusExperiment
@@ -1122,82 +1123,7 @@ class TestFetchJetstreamDataTask(TestCase):
     @patch("django.core.files.storage.default_storage.open")
     @patch("django.core.files.storage.default_storage.exists")
     def test_sizing_data_parsed_and_stored(self, mock_exists, mock_open):
-        sizing_test_data = """
-            {
-                "firefox_desktop:release:['EN-US']:US:108": {
-                    "new": {
-                        "target_recipe": {
-                            "app_id": "firefox_desktop",
-                            "channel": "release",
-                            "locale": "('EN-US')",
-                            "country": "US",
-                            "new_or_existing": "new",
-                            "minimum_version": "108"
-                        },
-                        "sample_sizes": {
-                            "Power0.8EffectSize0.05": {
-                                "metrics": {
-                                    "active_hours": {
-                                        "number_of_clients_targeted": 35,
-                                        "sample_size_per_branch": 3.0,
-                                        "population_percent_per_branch": 8.571428571
-                                    },
-                                    "search_count": {
-                                        "number_of_clients_targeted": 35,
-                                        "sample_size_per_branch": 5.0,
-                                        "population_percent_per_branch": 14.285714285
-                                    },
-                                    "days_of_use": {
-                                        "number_of_clients_targeted": 35,
-                                        "sample_size_per_branch": 20.0,
-                                        "population_percent_per_branch": 57.142857142
-                                    }
-                                },
-                                "parameters": {
-                                    "power": 0.8,
-                                    "effect_size": 0.05
-                                }
-                            }
-                        }
-                    },
-                    "existing": {
-                        "target_recipe": {
-                            "app_id": "firefox_desktop",
-                            "channel": "release",
-                            "locale": "('EN-US')",
-                            "country": "US",
-                            "new_or_existing": "existing",
-                            "minimum_version": "108"
-                        },
-                        "sample_sizes": {
-                            "Power0.8EffectSize0.05": {
-                                "metrics": {
-                                    "active_hours": {
-                                        "number_of_clients_targeted": 10000,
-                                        "sample_size_per_branch": 100000.0,
-                                        "population_percent_per_branch": 1000.0
-                                    },
-                                    "tagged_search_count": {
-                                        "number_of_clients_targeted": 10000,
-                                        "sample_size_per_branch": 100.0,
-                                        "population_percent_per_branch": 1.0
-                                    },
-                                    "days_of_use": {
-                                        "number_of_clients_targeted": 10000,
-                                        "sample_size_per_branch": 10000.0,
-                                        "population_percent_per_branch": 100.0
-                                    }
-                                },
-                                "parameters": {
-                                    "power": 0.8,
-                                    "effect_size": 0.05
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            """
+        sizing_test_data = SampleSizesFactory.build().json()
 
         class File:
             def __init__(self, filename):
@@ -1245,9 +1171,7 @@ class TestFetchJetstreamDataTask(TestCase):
 
     @patch("django.core.files.storage.default_storage.open")
     @patch("django.core.files.storage.default_storage.exists")
-    def test_exception_for_fetch_population_sizing_data_empty(
-        self, mock_exists, mock_open
-    ):
+    def test_fetch_population_sizing_data_invalid(self, mock_exists, mock_open):
         class File:
             def __init__(self, filename):
                 self.name = filename
