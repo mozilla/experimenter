84218318c96a20612a219d40db6ebb0c30e7b0d4
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-09-11 09:46:21 -0700

    fix #9377 feat(cirrus): Adjust profile sample rate

--- cirrus/server/cirrus/main.py
+++ cirrus/server/cirrus/main.py
@@ -62,7 +62,7 @@ def initialize_sentry():
             # Set profiles_sample_rate to 1.0 to profile 100%
             # of sampled transactions.
             # We recommend adjusting this value in production.
-            profiles_sample_rate=1.0,
+            profiles_sample_rate=0.1,
         )
 
 
