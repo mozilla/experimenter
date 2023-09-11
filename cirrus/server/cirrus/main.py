edfdf72ca99edbd9b34e29ee0a34493101407a8e
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-09-11 09:50:24 -0700

    fix #9377 feat(cirrus): Adjust trace sample rate

--- cirrus/server/cirrus/main.py
+++ cirrus/server/cirrus/main.py
@@ -58,7 +58,7 @@ def initialize_sentry():
             # Set traces_sample_rate to 1.0 to capture 100%
             # of transactions for performance monitoring.
             # We recommend adjusting this value in production.
-            traces_sample_rate=1.0,
+            traces_sample_rate=0.1,
             # Set profiles_sample_rate to 1.0 to profile 100%
             # of sampled transactions.
             # We recommend adjusting this value in production.
