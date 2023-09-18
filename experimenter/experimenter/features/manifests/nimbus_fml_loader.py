461265472fdba3156d727dd00f434f168878e3ed
Elise Richards <erichards@mozilla.com>
2023-09-18 15:55:33 -0700

    For #9388 feat(project): Fetch manifest paths and versions in fml loader

--- experimenter/experimenter/features/manifests/nimbus_fml_loader.py
+++ experimenter/experimenter/features/manifests/nimbus_fml_loader.py
@@ -1,38 +1,78 @@
-import logging
 import os
 
 import yaml
-from rust_fml import FmlClient
+from packaging import version
 
 from experimenter.settings import BASE_DIR
 
-logger = logging.getLogger()
+from rust_fml import FmlClient
 
 
 class NimbusFmlLoader:
+    __base_path = os.path.join(BASE_DIR, "features", "manifests", "apps.yaml")
+    __major_release = "major_release_branch"
+    __minor_release = "minor_release_tag"
+
     def __init__(self, application: str, channel: str):
         self.application: str = application
         self.channel: str = channel
 
-        path = os.path.join(BASE_DIR, "features", "manifests", "apps.yaml")
-        fml_local = ""
-        if os.path.exists(path):
-            with open(path) as application_yaml_file:
-                application_data = yaml.load(
-                    application_yaml_file.read(), Loader=yaml.Loader
-                )
-                for feature_slug in application_data:
-                    if feature_slug in application:
-                        fml_local = application_data[feature_slug]["fml_path"]
-                        logger.info(str(fml_local))
-                        break
-        if fml_local != "":
-            self.fml_loader = self.create(path, channel)
-            logger.info("FML loader created")
-        else:
-            logger.error("Failed to find fml path for application: " + application)
+    @staticmethod
+    def parse_version(version_str: str):
+        return version.parse(version_str)
 
     def create(self, path: str, channel: str) -> FmlClient:
         return FmlClient.new_with_ref(
             "@mozilla-mobile/firefox-android/fenix/app/nimbus.fml.yaml", "release", "main"
+        )
+
+    @staticmethod
+    def load_yaml(file):
+        return yaml.load(file.read(), Loader=yaml.Loader)
+
+    def get_repo_location(self, file_location=__base_path):
+        if os.path.exists(file_location):
+            with open(file_location) as application_yaml_file:
+                application_data = self.load_yaml(application_yaml_file)
+                for feature_slug in application_data:
+                    if feature_slug in self.application:
+                        return application_data[feature_slug]["repo"]
+
+    def get_fm_path(self, file_location=__base_path):
+        if os.path.exists(file_location):
+            with open(file_location) as application_yaml_file:
+                application_data = self.load_yaml(application_yaml_file)
+                for feature_slug in application_data:
+                    if feature_slug in self.application:
+                        return application_data[feature_slug]["fml_path"]
+
+    def get_manifest_paths(self, versions: list[str]):
+        manifest_paths = []
+        for v in versions:
+            version = self.parse_version(v)
+            if os.path.exists(self.__base_path):
+                with open(self.__base_path) as application_yaml_file:
+                    application_data = self.load_yaml(application_yaml_file)
+
+                    for feature_slug in application_data:
+                        if feature_slug in self.application:
+                            # Todo: this can be expanded later to fetch both the
+                            # branch/major version and the tagged minor version.
+                            manifest_path_minor = self.get_minor_release_path(
+                                version, application_data, feature_slug
+                            )
+                            manifest_paths.append(manifest_path_minor)
+                            break
+        return manifest_paths
+
+    def get_major_release_path(self, version, application_data, feature_slug):
+        return application_data[feature_slug][self.__major_release].format(
+            major=version.major
+        )
+
+    def get_minor_release_path(self, version, application_data, feature_slug):
+        return application_data[feature_slug][self.__minor_release].format(
+            major=version.major,
+            minor=version.minor,
+            patch=version.micro,
         )
--- experimenter/experimenter/features/tests/test_nimbus_fml_loader.py
+++ experimenter/experimenter/features/tests/test_nimbus_fml_loader.py
@@ -1,46 +1,127 @@
-from unittest import mock
+import os
 
 from django.test import TestCase
 
 from experimenter.features.manifests.nimbus_fml_loader import NimbusFmlLoader
+from experimenter.settings import BASE_DIR
 
 
 class TestNimbusFmlLoader(TestCase):
-    def test_intiate_new_fml_client_no_error(self):
-        application = "fenix"
-        channel = "production"
+    @staticmethod
+    def create_loader(
+        application: str = "fenix",
+        channel: str = "production",
+    ):
+        return NimbusFmlLoader(application, channel)
 
-        with mock.patch(
-            "experimenter.features.manifests.nimbus_fml_loader.NimbusFmlLoader.create",
-        ) as create:
-            loader = NimbusFmlLoader(application, channel)
-
-            self.assertEqual(loader.application, application)
-            self.assertEqual(loader.channel, channel)
-            create.assert_called_once()
-
-    def test_intiate_invalid_fml_client_errors(self):
-        application = "rats"
-        channel = "production"
-
-        with self.assertLogs(level="ERROR") as log:
-            with mock.patch(
-                "experimenter.features.manifests.nimbus_fml_loader.NimbusFmlLoader.create",
-            ) as create:
-                loader = NimbusFmlLoader(application, channel)
-                self.assertIn(
-                    "Failed to find fml path for application: rats", log.output[0]
-                )
-
-            self.assertEqual(loader.application, application)
-            self.assertEqual(loader.channel, channel)
-            create.assert_not_called()
-
-    def test_create(self):
-        application = "fenix"
+    def test_intiate_new_fml_loader(self):
+        application = "firefox_ios"
         channel = "production"
 
         loader = NimbusFmlLoader(application, channel)
-        response = loader.create("my/favorite/path", "ziggy")
-        # Todo: Connect FML https://mozilla-hub.atlassian.net/browse/EXP-3791
-        self.assertIsNotNone(response)
+
+        self.assertEqual(loader.application, application)
+        self.assertEqual(loader.channel, channel)
+
+    def test_parse_versions(self):
+        version = "112.1.0"
+        parse = NimbusFmlLoader.parse_version(version)
+        self.assertEqual(str(parse), version)
+        self.assertEqual(parse.major, 112)
+        self.assertEqual(parse.minor, 1)
+        self.assertEqual(parse.micro, 0)
+
+    def test_load_yaml(self):
+        path = os.path.join(BASE_DIR, "features", "tests", "test.yaml")
+        loader = self.create_loader()
+
+        if os.path.exists(path):
+            with open(path) as file:
+                yaml = loader.load_yaml(file)
+                self.assertIsNotNone(yaml)
+
+    def test_get_repo_location(self):
+        path = os.path.join(BASE_DIR, "features", "tests", "test.yaml")
+        expected = r"\@mozilla-mobile/firefox-android"
+
+        loader = self.create_loader()
+        result = loader.get_repo_location(path)
+        self.assertEqual(result, expected)
+
+    def test_get_fm_path(self):
+        path = os.path.join(BASE_DIR, "features", "tests", "test.yaml")
+        expected = "fenix/app/nimbus.fml.yaml"
+
+        loader = self.create_loader()
+        result = loader.get_fm_path(path)
+        self.assertEqual(result, expected)
+
+    def test_fetch_single_manifest_version(self):
+        application = "firefox_ios"
+        channel = "release"
+        version = ["112.1.0"]
+
+        client = self.create_loader(application, channel)
+        manifests = client.get_manifest_paths(version)
+
+        self.assertEqual(client.application, application)
+        self.assertEqual(client.channel, channel)
+        self.assertEqual(len(manifests), 1)
+
+    def test_fetch_multiple_manifest_versions(self):
+        application = "firefox_ios"
+        channel = "release"
+        versions = [
+            "112.1.0",
+            "112.1.1",
+            "113.0.0",
+            "118.7.3",
+        ]
+
+        client = self.create_loader(application, channel)
+        manifests = client.get_manifest_paths(versions)
+
+        self.assertEqual(client.application, application)
+        self.assertEqual(client.channel, channel)
+        self.assertEqual(len(manifests), 4)
+
+    def test_get_major_version_fml_paths(self):
+        path = os.path.join(BASE_DIR, "features", "tests", "test.yaml")
+        loader = self.create_loader()
+        version = loader.parse_version("112.1.0")
+        expected = "releases_v112"
+
+        if os.path.exists(path):
+            with open(path) as application_yaml_file:
+                application_data = loader.load_yaml(application_yaml_file)
+                result = loader.get_major_release_path(
+                    version,
+                    application_data,
+                    "fenix",
+                )
+                self.assertEqual(result, expected)
+
+    def test_get_minor_version_fml_paths(self):
+        path = os.path.join(BASE_DIR, "features", "tests", "test.yaml")
+        loader = self.create_loader()
+        version = loader.parse_version("112.1.0")
+        expected = "fenix-v112.1.0"
+
+        if os.path.exists(path):
+            with open(path) as application_yaml_file:
+                application_data = loader.load_yaml(application_yaml_file)
+                result = loader.get_minor_release_path(
+                    version,
+                    application_data,
+                    "fenix",
+                )
+
+                self.assertEqual(result, expected)
+
+    def test_create(self):
+        loader = self.create_loader()
+        fml_path = "fenix/app/nimbus.fml.yaml"
+        release_version = "v112.1.0"
+
+        result = loader.create(fml_path, release_version, loader.channel)
+        self.assertIsNotNone(result)
