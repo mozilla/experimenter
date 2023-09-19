9286c17dd50972c6b0fb181b2f55f1eb7fcbed08
Mike Williams <102263964+mikewilli@users.noreply.github.com>
2023-09-19 12:41:06 -0400

    fix #9437 feat(schemas): add languages to pop sizing schema (#9438)
    Because
    
    - mobile experiments use language instead of locale
    
    This commit
    
    - adds languages to the recipe schema for pop sizing data

--- schemas/mozilla_nimbus_schemas/tests/jetstream/test_population_sizing.py
+++ schemas/mozilla_nimbus_schemas/tests/jetstream/test_population_sizing.py
@@ -90,11 +90,84 @@ class TestPopulationSizing(TestCase):
                         }
                     }
                 }
+            },
+            "firefox_ios:release:['EN-US']:US": {
+                "new": {
+                    "target_recipe": {
+                        "app_id": "firefox_desktop",
+                        "channel": "release",
+                        "language": "('EN-US')",
+                        "country": "US",
+                        "new_or_existing": "new"
+                    },
+                    "sample_sizes": {
+                        "Power0.8EffectSize0.05": {
+                            "metrics": {
+                                "active_hours": {
+                                    "number_of_clients_targeted": 35,
+                                    "sample_size_per_branch": 3.0,
+                                    "population_percent_per_branch": 8.571428571
+                                },
+                                "search_count": {
+                                    "number_of_clients_targeted": 35,
+                                    "sample_size_per_branch": 5.0,
+                                    "population_percent_per_branch": 14.285714285
+                                },
+                                "days_of_use": {
+                                    "number_of_clients_targeted": 35,
+                                    "sample_size_per_branch": 20.0,
+                                    "population_percent_per_branch": 57.142857142
+                                }
+                            },
+                            "parameters": {
+                                "power": 0.8,
+                                "effect_size": 0.05
+                            }
+                        }
+                    }
+                },
+                "existing": {
+                    "target_recipe": {
+                        "app_id": "firefox_desktop",
+                        "channel": "release",
+                        "language": "('EN-US')",
+                        "country": "US",
+                        "new_or_existing": "existing"
+                    },
+                    "sample_sizes": {
+                        "Power0.8EffectSize0.05": {
+                            "metrics": {
+                                "active_hours": {
+                                    "number_of_clients_targeted": 10000,
+                                    "sample_size_per_branch": 100000.0,
+                                    "population_percent_per_branch": 1000.0
+                                },
+                                "tagged_search_count": {
+                                    "number_of_clients_targeted": 10000,
+                                    "sample_size_per_branch": 100.0,
+                                    "population_percent_per_branch": 1.0
+                                },
+                                "days_of_use": {
+                                    "number_of_clients_targeted": 10000,
+                                    "sample_size_per_branch": 10000.0,
+                                    "population_percent_per_branch": 100.0
+                                }
+                            },
+                            "parameters": {
+                                "power": 0.8,
+                                "effect_size": 0.05
+                            }
+                        }
+                    }
+                }
             }
         }
         """
         sample_sizes = SampleSizes.parse_raw(sizing_test_data)
-        self.assertEqual(json.dumps(json.loads(sizing_test_data)), sample_sizes.json())
+        self.assertEqual(
+            json.dumps(json.loads(sizing_test_data)),
+            sample_sizes.json(exclude_unset=True),
+        )
 
     def test_parse_population_sizing_factory(self):
         sample_sizes = SampleSizesFactory.build()
