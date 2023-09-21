fca7eb21c25912c521605c94789cdc88655c5cfd
Barret Rennie <barret@brennie.ca>
2023-09-21 12:44:18 -0400

    Add manifest-tool

---
+++ experimenter/manifesttool/appconfig.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Any, Optional
+
+from pydantic import BaseModel
+import yaml
+
+
+class AppConfig(BaseModel):
+    slug: str
+    repo: str
+    fml_path: str
+    channels: list[str]
+    major_release_branch: Optional[str]
+    minor_release_tag: Optional[str]
+
+
+class AppConfigs(BaseModel):
+    __root__: dict[str, AppConfig]
+
+    @classmethod
+    def load_from_file(cls, filename: Path) -> "AppConfigs":
+        with filename.open() as f:
+            app_configs: Any = yaml.safe_load(f)
+
+        return cls.parse_obj(app_configs)
