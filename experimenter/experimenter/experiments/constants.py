21f0a25042d40503478f3ccde50198fd70654bc3
Chris Bellini <cbellini@mozilla.com>
2023-09-27 13:47:34 -0400

    add support to target 118.0.1 users (#9506)
    Because
    
    - we will be updating a frecency experiment to target users with 118.0.1
    
    This commit
    
    - adds 118.0.1 as a selectable version target

--- experimenter/experimenter/experiments/constants.py
+++ experimenter/experimenter/experiments/constants.py
@@ -420,6 +420,7 @@ class NimbusConstants:
         FIREFOX_116_3_0 = "116.3.0"
         FIREFOX_117 = "117.!"
         FIREFOX_118 = "118.!"
+        FIREFOX_118_0_1 = "118.0.1"
         FIREFOX_119 = "119.!"
         FIREFOX_120 = "120.!"
 
