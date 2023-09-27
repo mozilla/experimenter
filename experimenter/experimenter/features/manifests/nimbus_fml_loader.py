59d5925392c191d373897550fac34fa272516190
Elise Richards <erichards@mozilla.com>
2023-09-27 10:48:16 -0700

    For #9388 feat(project): Get manifest paths and release versions in fml loader (#9454)
    Because
    
    - We want to get the repos, fml paths, and formatted versions from the
    `apps.yaml` file
    
    This commit
    
    - Adds methods to the loader that:
       - Gets the repo paths from the yaml file
       - Gets the manifest path from the yaml file
       - Formats the versions according to the format in the yaml file
       - Sets up the loader methods to be hooked up to the FML

--- experimenter/experimenter/features/manifests/nimbus_fml_loader.py
+++ experimenter/experimenter/features/manifests/nimbus_fml_loader.py
@@ -1,38 +1,72 @@
-import logging
 import os
+from pathlib import Path
 
 import yaml
+from packaging import version as version_packaging
 from rust_fml import FmlClient
 
 from experimenter.settings import BASE_DIR
 
-logger = logging.getLogger()
-
 
 class NimbusFmlLoader:
-    def __init__(self, application: str, channel: str):
+    BASE_PATH = Path(BASE_DIR) / "features" / "manifests" / "apps.yaml"
+
+    def __init__(self, application: str, channel: str, file_location=BASE_PATH):
         self.application: str = application
         self.channel: str = channel
+        self.application_data = self.get_application_data(application, file_location)
 
-        path = os.path.join(BASE_DIR, "features", "manifests", "apps.yaml")
-        fml_local = ""
-        if os.path.exists(path):
-            with open(path) as application_yaml_file:
-                application_data = yaml.load(
-                    application_yaml_file.read(), Loader=yaml.Loader
-                )
-                for feature_slug in application_data:
-                    if feature_slug in application:
-                        fml_local = application_data[feature_slug]["fml_path"]
-                        logger.info(str(fml_local))
-                        break
-        if fml_local != "":
-            self.fml_loader = self.create(path, channel)
-            logger.info("FML loader created")
-        else:
-            logger.error("Failed to find fml path for application: " + application)
+    def get_fml_clients(self, versions: list[str]) -> list[FmlClient]:
+        if not self.application_data:
+            return None
+        refs = self.get_version_refs(versions)
+        file_path = self.get_file_path()
+        return [self.create_client(file_path, self.channel, r) for r in refs]
 
-    def create(self, path: str, channel: str) -> FmlClient:
-        return FmlClient.new_with_ref(
-            "@mozilla-mobile/firefox-android/fenix/app/nimbus.fml.yaml", "release", "main"
+    @staticmethod
+    def get_application_data(application_name, file_location=BASE_PATH):
+        if os.path.exists(file_location):
+            with open(file_location) as application_yaml_file:
+                file = yaml.safe_load(application_yaml_file.read())
+                if application_name in file:
+                    return file[application_name]
+                else:
+                    return None
+
+    def get_file_path(self):
+        if not self.application_data:
+            return None
+        return Path(self.application_data["repo"]) / self.application_data["fml_path"]
+
+    def get_version_refs(self, versions):
+        if versions == []:
+            return ["main"]
+        refs = []
+        for v in versions:
+            version = version_packaging.parse(v)
+            # Todo: this can be expanded later to fetch both the
+            # branch/major version and the tagged minor version.
+            version_ref_minor = self.get_minor_version_ref(
+                version,
+            )
+            refs.append(version_ref_minor)
+        return refs
+
+    def get_major_version_ref(self, version):
+        if not self.application_data:
+            return None
+        return self.application_data["major_release_branch"].format(
+            major=version.major,
         )
+
+    def get_minor_version_ref(self, version):
+        if not self.application_data:
+            return None
+        return self.application_data["minor_release_tag"].format(
+            major=version.major,
+            minor=version.minor,
+            patch=version.micro,
+        )
+
+    def create_client(self, path: str, channel: str, ref: str) -> FmlClient:
+        return FmlClient.new_with_ref(path, channel, ref)
