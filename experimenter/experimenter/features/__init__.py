daddf21a59f56c9dada20a1493704f44e2096f31
Barret Rennie <barret@brennie.ca>
2023-10-12 14:02:30 -0400

    Use FeatureManifest schema from schemas

--- experimenter/experimenter/features/__init__.py
+++ experimenter/experimenter/features/__init__.py
@@ -1,22 +1,19 @@
 import json
 import os
-from enum import Enum
-from typing import Literal, Optional, Union
+from dataclasses import dataclass
+from typing import Union
 
 import yaml
 from django.conf import settings
 from django.core.checks import Error, register
-from pydantic import BaseModel, Field
+from mozilla_nimbus_schemas.experiments.feature_manifests import (
+    FeatureManifest,
+    FeatureVariableType,
+    FeatureWithExposure,
+    FeatureWithoutExposure,
+)
 
 from experimenter.experiments.constants import NimbusConstants
-
-
-class FeatureVariableType(Enum):
-    INT = "int"
-    STRING = "string"
-    BOOLEAN = "boolean"
-    JSON = "json"
-
 
 FEATURE_SCHEMA_TYPES = {
     FeatureVariableType.INT: "integer",
@@ -31,35 +28,19 @@ FEATURE_PYTHON_TYPES = {
 }
 
 
-class FeatureVariable(BaseModel):
-    description: Optional[str]
-    enum: Optional[list[str]]
-    fallbackPref: Optional[str]
-    type: Optional[FeatureVariableType]
-    setPref: Optional[str]
-
-
-class FeatureSchema(BaseModel):
-    uri: Optional[str]
-    path: Optional[str]
-
-
-class Feature(BaseModel):
-    applicationSlug: str
-    description: Optional[str]
-    exposureDescription: Optional[Union[Literal[False], str]]
-    isEarlyStartup: Optional[bool]
+@dataclass
+class Feature:
     slug: str
-    variables: Optional[dict[str, FeatureVariable]]
-    schema_paths: Optional[FeatureSchema] = Field(alias="schema")
+    application_slug: str
+    model: Union[FeatureWithExposure, FeatureWithoutExposure]
 
     def load_remote_jsonschema(self):
-        if self.schema_paths:
+        if self.model.json_schema is not None:
             schema_path = os.path.join(
                 settings.FEATURE_MANIFESTS_PATH,
-                self.applicationSlug,
+                self.application_slug,
                 "schemas",
-                self.schema_paths.path,
+                self.model.json_schema.path,
             )
 
             with open(schema_path) as schema_file:
@@ -76,25 +57,24 @@ class Feature(BaseModel):
             "additionalProperties": False,
         }
 
-        if self.variables:
-            for variable_slug, variable in self.variables.items():
-                variable_schema = {
-                    "description": variable.description,
-                }
+        for variable_slug, variable in self.model.variables.items():
+            variable_schema = {
+                "description": variable.description,
+            }
 
-                if variable.type in FEATURE_SCHEMA_TYPES:
-                    variable_schema["type"] = FEATURE_SCHEMA_TYPES[variable.type]
+            if variable.type in FEATURE_SCHEMA_TYPES:
+                variable_schema["type"] = FEATURE_SCHEMA_TYPES[variable.type]
 
-                if variable.enum:
-                    python_type = FEATURE_PYTHON_TYPES[variable.type]
-                    variable_schema["enum"] = [python_type(e) for e in variable.enum]
+            if variable.enum:
+                python_type = FEATURE_PYTHON_TYPES[variable.type]
+                variable_schema["enum"] = [python_type(e) for e in variable.enum]
 
-                schema["properties"][variable_slug] = variable_schema
+            schema["properties"][variable_slug] = variable_schema
 
         return json.dumps(schema, indent=2)
 
     def get_jsonschema(self):
-        if self.schema_paths is not None:
+        if self.model.json_schema is not None:
             return self.load_remote_jsonschema()
         return self.generate_jsonschema()
 
@@ -114,11 +94,16 @@ class Features:
             if os.path.exists(application_yaml_path):
                 with open(application_yaml_path) as application_yaml_file:
                     application_data = yaml.safe_load(application_yaml_file)
-                    for feature_slug in application_data:
-                        feature_data = application_data[feature_slug]
-                        feature_data["slug"] = feature_slug
-                        feature_data["applicationSlug"] = application.slug
-                        features.append(Feature.parse_obj(feature_data))
+                    manifest = FeatureManifest.parse_obj(application_data)
+
+                    for feature_slug, feature_model in manifest.__root__.items():
+                        features.append(
+                            Feature(
+                                slug=feature_slug,
+                                application_slug=application.slug,
+                                model=feature_model.__root__,
+                            )
+                        )
 
         return features
 
@@ -135,7 +120,7 @@ class Features:
 
     @classmethod
     def by_application(cls, application):
-        return [f for f in cls.all() if f.applicationSlug == application]
+        return [f for f in cls.all() if f.application_slug == application]
 
 
 @register()
