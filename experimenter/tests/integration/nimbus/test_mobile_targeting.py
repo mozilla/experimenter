ce1d458a476204312b0921bf22dfe7a2661f9116
Charlie Humphreys <chumphreys@mozilla.com>
2023-10-27 12:15:16 -0500

    fix #9564, #9640 feature(nimbus): add targeting for new and later users for Firefox iOS and Android (#9642)
    Because
    
    - New advanced targeting configurations are needed for Fakespot
    experiments
    
    This commit
    
    - Adds the four advanced targeting options required

--- experimenter/tests/integration/nimbus/test_mobile_targeting.py
+++ experimenter/tests/integration/nimbus/test_mobile_targeting.py
@@ -78,6 +78,7 @@ def test_check_mobile_targeting(
             "days_since_install": 1,
             "isFirstRun": "true",
             "is_first_run": True,
+            "is_phone": True,
         }
     )
     client = sdk_client(load_app_context(context))
