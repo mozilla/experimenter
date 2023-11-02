d3ea6bb8b24683997438ef26cea0ad50848d4b91
Barret Rennie <barret@brennie.ca>
2023-11-02 11:46:58 -0400

    feat(nimbus): Explicitly require a manifest path in AppConfig (#9623)
    Because
    
    - we want to ensure that apps.yaml is always valid; and
    - we want to ensure that each app specifies exactly one manifest file
    
    This commit
    
    - adds a test that loads the production apps.yaml;
    - updates the AppConfig model with a validator to ensure only one
      manifest path can be specified

---
+++ experimenter/manifesttool/tests/test_appconfig.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from manifesttool.cli import MANIFESTS_DIR
+from manifesttool.appconfig import AppConfigs
+
+
+class AppConfigTests(TestCase):
+    def test_parse_apps_yaml(self):
+        """Testing that we can parse apps.yaml."""
+        apps_yaml_path = MANIFESTS_DIR / "apps.yaml"
+        AppConfigs.load_from_file(apps_yaml_path)
+
+    def test_parse_experimenter_and_fml_paths(self):
+        """Testing that parsing apps.yaml fails if an app contains both the
+        fml_path and experimenter_yaml_path keys.
+        """
+        with self.assertRaises(ValueError):
+            AppConfigs.parse_obj(
+                {
+                    "app": {
+                        "slug": "app",
+                        "repo": {"type": "github", "name": "owner/repo"},
+                        "fml_path": "nimbus.fml.yaml",
+                        "experimenter_yaml_path": "experimenter.yaml",
+                    },
+                }
+            )
+
+    def test_parse_no_paths(self):
+        """Testing that parsing apps.yaml fails if an app is missing both the
+        fml_path and experimenter_yaml_path keys.
+        """
+        with self.assertRaises(ValueError):
+            AppConfigs.parse_obj(
+                {
+                    "app": {
+                        "slug": "app",
+                        "repo": {"type": "github", "name": "owner/repo"},
+                    },
+                }
+            )
