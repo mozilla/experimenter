7ca3837f5228edea84751b4cfa3ea45df2cff2f7
Barret Rennie <barret@brennie.ca>
2023-11-13 19:09:10 -0500

    feat(nimbus): List GitHub branches and tags in manifesttool (#9692)
    Because
    
    - we need to discover versions based on GitHub branches and tags
    
    This commit
    
    - adds methods to manifesttool's github_api to find all branches and
      tags.

--- experimenter/manifesttool/github_api.py
+++ experimenter/manifesttool/github_api.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any
+from typing import Any, Generator
 
 import requests
 
@@ -15,11 +15,34 @@ if bearer_token := os.getenv("GITHUB_BEARER_TOKEN"):  # pragma: no cover
     GITHUB_API_HEADERS["Authorization"] = f"Bearer {bearer_token}"
 
 
-def api_request(path: str, **kwargs: dict[str, Any]) -> dict[str, Any]:
+def api_request(path: str, **kwargs: dict[str, Any]) -> Any:
     """Make a request to the GitHub API."""
-    return requests.get(
-        f"{GITHUB_API_URL}/{path}", headers=GITHUB_API_HEADERS, **kwargs
-    ).json()
+    url = f"{GITHUB_API_URL}/{path}"
+    rsp = requests.get(url, headers=GITHUB_API_HEADERS, **kwargs)
+
+    if rsp.status_code == 403:
+        if rsp.headers.get("X-RateLimit-Remaining") == "0":
+            raise Exception(f"Could not fetch {url}: GitHub API rate limit exceeded")
+
+    rsp.raise_for_status()
+
+    return rsp.json()
+
+
+def paginated_api_request(path: str, per_page: int = 100) -> Generator[Any, None, None]:
+    """Make several reqeusts to a paginated API resource and yield each page of results."""
+    page = 1
+
+    while True:
+        results = api_request(path, params={"page": page, "per_page": per_page})
+
+        # When there are no more results, the API returns an empty list.
+        if results:
+            yield results
+        else:
+            break
+
+        page += 1
 
 
 def get_main_ref(repo: str) -> Ref:
@@ -31,3 +54,30 @@ def get_main_ref(repo: str) -> Ref:
     """
     rsp = api_request(f"repos/{repo}/branches/main")
     return Ref("main", rsp["commit"]["sha"])
+
+
+def get_branches(repo: str) -> list[Ref]:
+    """Return all the branches in a repository."""
+    return _get_refs(repo, "branches")
+
+
+def get_tags(repo: str) -> list[Ref]:
+    """Return all the tags in a repository."""
+    return _get_refs(repo, "tags")
+
+
+def _get_refs(repo: str, kind: str) -> list[Ref]:
+    """Return all the refs of a given kind.
+
+    Args:
+        repo: The name of the repository, including the owner.
+        kind: Either ``"branches"`` or ``"tags"``.
+
+    Returns:
+        The list of refs.
+    """
+    return [
+        Ref(ref["name"], ref["commit"]["sha"])
+        for page in paginated_api_request(f"repos/{repo}/{kind}")
+        for ref in page
+    ]
