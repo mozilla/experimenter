0a1ddc07abd523c1340b128ab062ff39ff52c3e0
Barret Rennie <barret@brennie.ca>
2023-12-15 11:03:48 -0500

    feat(nimbus): use the GitHub contents API to find download URLs
    We can't directly access raw.githubusercontent.com paths for private
    repositories, even with a GitHub API key. Instead, we first have to hit
    the file contents API, which will return a download URL that includes a
    token.
    
    Fixes #9946

--- experimenter/manifesttool/tests/test_github_api.py
+++ experimenter/manifesttool/tests/test_github_api.py
@@ -7,8 +7,10 @@ import responses
 from responses import matchers
 
 from manifesttool import github_api
-from manifesttool.github_api import GITHUB_RAW_URL, GITHUB_API_URL
+from manifesttool.github_api import GITHUB_API_URL
 from manifesttool.repository import Ref
+
+GITHUB_RAW_URL = "https://raw.githubusercontent.com"
 
 
 def _add_paginated_responses(
@@ -164,7 +166,17 @@ class GitHubApiTests(TestCase):
     @responses.activate
     def test_fetch_file_download(self):
         """Testing github_api.fetch_file."""
-        responses.get(f"{GITHUB_RAW_URL}/repo/ref/file/path.txt", body=b"hello, world\n")
+        api_rsp = responses.get(
+            f"{GITHUB_API_URL}/repos/repo/contents/file/path.txt",
+            match=[matchers.query_param_matcher({"ref": "ref"})],
+            json={
+                "download_url": f"{GITHUB_RAW_URL}/repo/ref/file/path.txt",
+            },
+        )
+        file_rsp = responses.get(
+            f"{GITHUB_RAW_URL}/repo/ref/file/path.txt",
+            body=b"hello, world\n",
+        )
 
         with TemporaryDirectory() as tmp_dir:
             tmp_filename = Path(tmp_dir, "file.txt")
@@ -177,6 +189,9 @@ class GitHubApiTests(TestCase):
                 contents = f.read()
 
             self.assertEqual(contents, b"hello, world\n")
+
+        self.assertEqual(api_rsp.call_count, 1)
+        self.assertEqual(file_rsp.call_count, 1)
 
         contents = github_api.fetch_file("repo", "file/path.txt", "ref")
         self.assertEqual(contents, "hello, world\n")
