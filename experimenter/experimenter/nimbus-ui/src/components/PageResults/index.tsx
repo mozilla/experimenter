3f6d0f2f0ee5c92fe42d8edb2fcb21cf66c4023e
Mike Williams <mwilliams@mozilla.com>
2024-01-05 14:30:09 -0500

    better fix for this

--- experimenter/experimenter/nimbus-ui/src/components/PageResults/index.tsx
+++ experimenter/experimenter/nimbus-ui/src/components/PageResults/index.tsx
@@ -238,7 +238,6 @@ const PageResults: React.FunctionComponent<RouteComponentProps> = () => {
                 marginRight: "1rem",
                 marginTop: "1.5rem",
                 top: "1.5rem",
-                zIndex: 1,
               }}
             >
               <Card.Header as="h5" style={{ paddingLeft: "0.5rem" }}>
