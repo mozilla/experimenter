8fd42497123c3304aefa1c90d0fbad550cc65b59
Mike Williams <102263964+mikewilli@users.noreply.github.com>
2024-01-08 12:34:34 -0500

    feat(schemas): add results ingestion enums to schemas package (#10003)
    Because
    
    - Some of these enums could be useful in the UI as typescript types
    
    This commit
    
    - Copies the enums defined in Experimenter's Jetstream results ingestion
    client to the schemas package
    
    Fixes #10001

---
+++ schemas/mozilla_nimbus_schemas/jetstream/misc.py
@@ -0,0 +1,50 @@
+from enum import StrEnum
+
+
+class AnalysisSegment(StrEnum):
+    ALL = "all"
+
+
+class AnalysisSignificance(StrEnum):
+    POSITIVE = "positive"
+    NEGATIVE = "negative"
+    NEUTRAL = "neutral"
+
+
+class AnalysisWindow(StrEnum):
+    DAILY = "daily"
+    WEEKLY = "weekly"
+    OVERALL = "overall"
+
+
+class BranchComparison(StrEnum):
+    ABSOLUTE = "absolute"
+    DIFFERENCE = "difference"
+    UPLIFT = "relative_uplift"
+
+
+# TODO: Consider a "guardrail_metrics" group containing "days_of_use",
+# "retained", and "search_count".
+class MetricGroup(StrEnum):
+    SEARCH = "search_metrics"
+    USAGE = "usage_metrics"
+    OTHER = "other_metrics"
+
+
+class MetricIngestEnum(StrEnum):
+    RETENTION = "retained"
+    SEARCH = "search_count"
+    DAYS_OF_USE = "days_of_use"
+    USER_COUNT = "identity"
+
+
+class StatisticIngestEnum(StrEnum):
+    """
+    This is the list of statistics supported in Experimenter,
+    not a complete list of statistics available in Jetstream.
+    """
+
+    PERCENT = "percentage"
+    BINOMIAL = "binomial"
+    MEAN = "mean"
+    COUNT = "count"
