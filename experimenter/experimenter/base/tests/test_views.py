2992ae6bc5fb01350a3a591c1edd8b9946b28076
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-01-09 14:33:57 -0500

    chore(project): remove google analytics (#10021)
    Because
    
    * We have Google Analytics enabled but don't use it
    
    This commit
    
    * Removes Google Analytics
    
    fixes #10019

--- experimenter/experimenter/base/tests/test_views.py
+++ experimenter/experimenter/base/tests/test_views.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.test import TestCase, override_settings
-from django.urls import reverse
 
 
 @override_settings(DEBUG=False)
@@ -12,22 +10,3 @@ class TestHeartbeats(TestCase):
     def test_lbheartbeat(self):
         response = self.client.get("/__lbheartbeat__")
         self.assertEqual(response.status_code, 200)
-
-
-class TestContextProcessors(TestCase):
-    def test_google_analytics_omitted_if_setting_false(self):
-        with self.settings(USE_GOOGLE_ANALYTICS=False):
-            headers = {settings.OPENIDC_EMAIL_HEADER: "user@example.com"}
-            response = self.client.get(reverse("home"), **headers)
-            self.assertEqual(response.status_code, 200)
-            html = response.content.decode("utf-8")
-            # Note the 'not'!
-            self.assertNotIn("www.googletagmanager.com", html)
-
-    def test_google_analytics_included_if_setting_true(self):
-        with self.settings(USE_GOOGLE_ANALYTICS=True):
-            headers = {settings.OPENIDC_EMAIL_HEADER: "user@example.com"}
-            response = self.client.get(reverse("home"), **headers)
-            self.assertEqual(response.status_code, 200)
-            html = response.content.decode("utf-8")
-            self.assertIn("www.googletagmanager.com", html)
