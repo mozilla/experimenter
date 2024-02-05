909c8560b82734e8fa7fdeb4f7988c016b6fb2fa
Elise Richards <erichards@mozilla.com>
2024-02-05 11:56:44 -0800

    For #10094 feat(nimbus): Check if there are other live multifeature experiments for targeted features (#10210)
    Because
    
    - Audience overlap can happen if there are other live multifeature
    experiments for features being targeted by your experiment
    
    This commit
    
    - Adds a query to the model to check for other live multifeature
    experiments for your features
    - This query returns a list of the slugs of any matching experiments, so
    that we will be able to say "these are the experiments that might cause
    overlap with yours" once we connect this to the frontend
    
    Fixes #10094

--- experimenter/experimenter/experiments/api/v5/types.py
+++ experimenter/experimenter/experiments/api/v5/types.py
@@ -494,6 +494,9 @@ class NimbusExperimentType(DjangoObjectType):
         lambda: graphene.List(graphene.NonNull(NimbusExperimentBranchThroughExcludedType))
     )
     feature_configs = DjangoListField(NimbusFeatureConfigType)
+    feature_has_live_multifeature_experiments = graphene.NonNull(
+        lambda: graphene.List(graphene.NonNull(graphene.String))
+    )
     firefox_max_version = NimbusExperimentFirefoxVersionEnum()
     firefox_min_version = NimbusExperimentFirefoxVersionEnum()
     hypothesis = graphene.String()
@@ -571,6 +574,7 @@ class NimbusExperimentType(DjangoObjectType):
             "documentation_links",
             "enrollment_end_date",
             "feature_configs",
+            "feature_has_live_multifeature_experiments",
             "firefox_max_version",
             "firefox_min_version",
             "hypothesis",
