a2e5061db3638eb08326b6da77f89dc48a39a558
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-02-26 16:10:31 -0500

    chore(project): update to django 5.0 (#10317)
    Because
    
    * Django 5.0 is out now
    * We should stay up to date with the latest Django
    
    This commit
    
    * Updates to Django 5.0
    
    fixes #9024

--- experimenter/experimenter/urls.py
+++ experimenter/experimenter/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, re_path
 from django.views.generic.base import RedirectView
 
 from experimenter.legacy.legacy_experiments.views import (
