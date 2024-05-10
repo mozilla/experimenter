ad90c6b96d8bfd720980ca2df62dcebdf9ab23f8
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-05-10 11:43:04 -0400

    feat(nimbus): new list page skeleton (#10680)
    Because
    
    * We are rewriting Nimbus UI in django templates/bootstrap5/htmx
    
    This commit
    
    * Introduces the skeleton of the new list page
    
    fixes #10537
    
    
    ![image](https://github.com/mozilla/experimenter/assets/119884/ac63b64b-de56-44c2-a648-51a5402fbdd1)
    
    ![image](https://github.com/mozilla/experimenter/assets/119884/20d15aae-59d4-445f-9520-863cde34f83c)

--- experimenter/experimenter/nimbus_ui_new/urls.py
+++ experimenter/experimenter/nimbus_ui_new/urls.py
@@ -1,11 +1,19 @@
 from django.urls import re_path
 
-from experimenter.nimbus_ui_new.views import NimbusChangeLogsView
+from experimenter.nimbus_ui_new.views import (
+    NimbusChangeLogsView,
+    NimbusExperimentsListView,
+)
 
 urlpatterns = [
     re_path(
         r"^(?P<slug>[\w-]+)/history/$",
         NimbusChangeLogsView.as_view(),
-        name="nimbus-history",
+        name="nimbus-new-history",
+    ),
+    re_path(
+        r"^",
+        NimbusExperimentsListView.as_view(),
+        name="nimbus-new-list",
     ),
 ]
