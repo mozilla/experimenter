8bd82c4f4bedaa52505c2ecab021dfd4324d4868
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-05-10 11:43:04 -0400

    feat(nimbus): new list page skeleton (#10680)
    Because
    
    * We are rewriting Nimbus UI in django templates/bootstrap5/htmx
    
    This commit
    
    * Introduces the skeleton of the new list page
    
    fixes #10537
    
    
    ![image](https://github.com/mozilla/experimenter/assets/119884/ac63b64b-de56-44c2-a648-51a5402fbdd1)
    
    ![image](https://github.com/mozilla/experimenter/assets/119884/20d15aae-59d4-445f-9520-863cde34f83c)

--- experimenter/experimenter/nimbus_ui_new/views.py
+++ experimenter/experimenter/nimbus_ui_new/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import DetailView
+from collections import defaultdict
+
+from django.views.generic import DetailView, ListView
 
 from experimenter.experiments.models import NimbusExperiment
 
@@ -7,3 +9,24 @@ class NimbusChangeLogsView(DetailView):
     model = NimbusExperiment
     context_object_name = "experiment"
     template_name = "changelog/overview.html"
+
+
+class NimbusExperimentsListView(ListView):
+    model = NimbusExperiment
+    queryset = NimbusExperiment.objects.with_owner_features()
+    context_object_name = "experiments"
+    template_name = "nimbus_experiments/list.html"
+
+    def get_context_data(self, **kwargs):
+        queryset = self.get_queryset()
+        status_counts = defaultdict(int)
+        for experiment in queryset:
+            status_counts[experiment.status] += 1
+            if experiment.is_archived:
+                status_counts["Archived"] += 1
+            if experiment.owner == self.request.user:
+                status_counts["MyExperiments"] += 1
+            if experiment.publish_status != NimbusExperiment.PublishStatus.IDLE:
+                status_counts["Review"] += 1
+
+        return super().get_context_data(status_counts=status_counts, **kwargs)
