0c6c67016aa0a5db0822db875a112c0500b96520
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-05-15 12:17:50 -0400

    feat(nimbus): filter by status on new nimbus list page (#10704)
    Because
    
    * We are implementing a new Nimbus list page in django templates and
    htmx
    * We need to be able to filter by status using tabs like the old page
    does
    * We can use django-filters for the filtering
    * We can use htmx for the first time to handle the page updates
    
    This commit
    
    * Adds a status filter to the list page
    * Uses htmx to update the page in place
    
    fixes #10681
    
    
    https://github.com/mozilla/experimenter/assets/119884/dc27cc11-6d50-4a29-a3d6-8dd7d5ed842f

--- experimenter/experimenter/nimbus_ui_new/views.py
+++ experimenter/experimenter/nimbus_ui_new/views.py
@@ -1,8 +1,48 @@
 from collections import defaultdict
 
-from django.views.generic import DetailView, ListView
+import django_filters
+from django.db import models
+from django.views.generic import DetailView
+from django_filters.views import FilterView
 
 from experimenter.experiments.models import NimbusExperiment
+
+
+class StatusChoices(models.TextChoices):
+    DRAFT = NimbusExperiment.Status.DRAFT.value
+    PREVIEW = NimbusExperiment.Status.PREVIEW.value
+    LIVE = NimbusExperiment.Status.LIVE.value
+    COMPLETE = NimbusExperiment.Status.COMPLETE.value
+    REVIEW = "Review"
+    ARCHIVED = "Archived"
+    MY_EXPERIMENTS = "MyExperiments"
+
+
+class NimbusExperimentFilter(django_filters.FilterSet):
+    status = django_filters.ChoiceFilter(
+        choices=StatusChoices.choices, method="filter_status"
+    )
+
+    class Meta:
+        model = NimbusExperiment
+        fields = ["status"]
+
+    def filter_status(self, queryset, name, value):
+        match value:
+            case StatusChoices.REVIEW:
+                return queryset.filter(
+                    status=NimbusExperiment.Status.DRAFT,
+                    publish_status=NimbusExperiment.PublishStatus.REVIEW,
+                )
+            case StatusChoices.ARCHIVED:
+                return queryset.filter(is_archived=True)
+            case StatusChoices.MY_EXPERIMENTS:
+                return queryset.filter(owner=self.request.user)
+            case _:
+                return queryset.filter(
+                    status=value,
+                    is_archived=False,
+                ).exclude(publish_status=NimbusExperiment.PublishStatus.REVIEW)
 
 
 class NimbusChangeLogsView(DetailView):
@@ -11,22 +51,38 @@ class NimbusChangeLogsView(DetailView):
     template_name = "changelog/overview.html"
 
 
-class NimbusExperimentsListView(ListView):
+class NimbusExperimentsListView(FilterView):
     model = NimbusExperiment
     queryset = NimbusExperiment.objects.with_owner_features()
+    filterset_class = NimbusExperimentFilter
     context_object_name = "experiments"
     template_name = "nimbus_experiments/list.html"
+
+    def get_filterset_kwargs(self, filterset_class):
+        kwargs = super().get_filterset_kwargs(filterset_class)
+        if kwargs["data"] is None:
+            kwargs["data"] = {"status": StatusChoices.LIVE.value}
+        return kwargs
 
     def get_context_data(self, **kwargs):
         queryset = self.get_queryset()
         status_counts = defaultdict(int)
         for experiment in queryset:
-            status_counts[experiment.status] += 1
-            if experiment.is_archived:
-                status_counts["Archived"] += 1
             if experiment.owner == self.request.user:
-                status_counts["MyExperiments"] += 1
-            if experiment.publish_status != NimbusExperiment.PublishStatus.IDLE:
-                status_counts["Review"] += 1
+                status_counts[StatusChoices.MY_EXPERIMENTS.value] += 1
+            if experiment.is_archived:
+                status_counts[StatusChoices.ARCHIVED.value] += 1
+                continue
+            if (
+                experiment.status == NimbusExperiment.Status.DRAFT
+                and experiment.publish_status == NimbusExperiment.PublishStatus.REVIEW
+            ):
+                status_counts[StatusChoices.REVIEW.value] += 1
+            else:
+                status_counts[experiment.status] += 1
 
-        return super().get_context_data(status_counts=status_counts, **kwargs)
+        return super().get_context_data(
+            active_status=kwargs["filter"].data["status"],
+            status_counts=status_counts,
+            **kwargs,
+        )
