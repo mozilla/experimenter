c37a65cb27693858c5282b9645678466ca1513f9
Yashika Khurana <yashikakhuranayashika@gmail.com>
2024-05-31 15:52:06 -0700

    feat(nimbus): Update conclusion recommendation in changelogs (#10782)
    Because
    
    - Since we have removed old field `conclusion_recommendation`, but in
    the changelogs we still have that field in experiment_data
    
    This commit
    
    - Changes the reference to new field `conclusion_recommendations` in the
    changelogs
    
    Fixes #10780

--- experimenter/experimenter/experiments/tests/test_migrations.py
+++ experimenter/experimenter/experiments/tests/test_migrations.py
@@ -4,11 +4,11 @@ from django_test_migrations.contrib.unittest_case import MigratorTestCase
 class TestMigrations(MigratorTestCase):
     migrate_from = (
         "experiments",
-        "0269_nimbusexperiment_conclusion_recommendations",
+        "0271_remove_nimbusexperiment_conclusion_recommendation",
     )
     migrate_to = (
         "experiments",
-        "0270_alter_conclusion_recommendations",
+        "0272_update_conclusion_recommendation_in_changelogs",
     )
 
     def prepare(self):
@@ -17,14 +17,22 @@ class TestMigrations(MigratorTestCase):
         NimbusExperiment = self.old_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
-        owner = User.objects.create()
+        NimbusChangeLog = self.old_state.apps.get_model("experiments", "NimbusChangeLog")
+        owner = User.objects.create(username="testuser")
 
-        # Create NimbusExperiment objects with old values for conclusion_recommendation
-        NimbusExperiment.objects.create(
+        experiment = NimbusExperiment.objects.create(
             owner=owner,
             slug="should_change",
             name="should_change",
-            conclusion_recommendation="RERUN",
+            conclusion_recommendations=[],
+        )
+
+        NimbusChangeLog.objects.create(
+            experiment=experiment,
+            changed_by=owner,
+            experiment_data={"conclusion_recommendation": "RERUN"},
+            message="Initial creation",
+            changed_on="2023-05-12T17:00:48.454269Z",
         )
 
     def test_migration(self):
@@ -32,8 +40,10 @@ class TestMigrations(MigratorTestCase):
         NimbusExperiment = self.new_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
-
+        NimbusChangeLog = self.new_state.apps.get_model("experiments", "NimbusChangeLog")
+        experiment = NimbusExperiment.objects.get(slug="should_change")
+        changelog = NimbusChangeLog.objects.get(experiment=experiment)
         self.assertEqual(
-            NimbusExperiment.objects.get(slug="should_change").conclusion_recommendations,
-            ["RERUN"],
+            changelog.experiment_data.get("conclusion_recommendations"), ["RERUN"]
         )
+        self.assertNotIn("conclusion_recommendation", changelog.experiment_data)
