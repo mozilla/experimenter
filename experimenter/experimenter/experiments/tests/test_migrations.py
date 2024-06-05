3a80976546baa03dbe8c525e6801edcd62f6fdf5
yashikakhurana <yashikakhuranayashika@gmail.com>
2024-06-05 15:56:12 -0700

    feat(nimbus): Handle if the field does not exist

--- experimenter/experimenter/experiments/tests/test_migrations.py
+++ experimenter/experimenter/experiments/tests/test_migrations.py
@@ -4,11 +4,11 @@ from django_test_migrations.contrib.unittest_case import MigratorTestCase
 class TestMigrations(MigratorTestCase):
     migrate_from = (
         "experiments",
-        "0271_remove_nimbusexperiment_conclusion_recommendation",
+        "0269_nimbusexperiment_conclusion_recommendations",
     )
     migrate_to = (
         "experiments",
-        "0272_update_conclusion_recommendation_in_changelogs",
+        "0270_alter_conclusion_recommendations",
     )
 
     def prepare(self):
@@ -17,22 +17,14 @@ class TestMigrations(MigratorTestCase):
         NimbusExperiment = self.old_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
-        NimbusChangeLog = self.old_state.apps.get_model("experiments", "NimbusChangeLog")
-        owner = User.objects.create(username="testuser")
+        owner = User.objects.create()
 
-        experiment = NimbusExperiment.objects.create(
+        # Create NimbusExperiment objects with old values for conclusion_recommendation
+        NimbusExperiment.objects.create(
             owner=owner,
             slug="should_change",
             name="should_change",
-            conclusion_recommendations=[],
-        )
-
-        NimbusChangeLog.objects.create(
-            experiment=experiment,
-            changed_by=owner,
-            experiment_data={"conclusion_recommendation": "RERUN"},
-            message="Initial creation",
-            changed_on="2023-05-12T17:00:48.454269Z",
+            conclusion_recommendation="RERUN",
         )
 
     def test_migration(self):
@@ -40,10 +32,8 @@ class TestMigrations(MigratorTestCase):
         NimbusExperiment = self.new_state.apps.get_model(
             "experiments", "NimbusExperiment"
         )
-        NimbusChangeLog = self.new_state.apps.get_model("experiments", "NimbusChangeLog")
-        experiment = NimbusExperiment.objects.get(slug="should_change")
-        changelog = NimbusChangeLog.objects.get(experiment=experiment)
+
         self.assertEqual(
-            changelog.experiment_data.get("conclusion_recommendations"), ["RERUN"]
+            NimbusExperiment.objects.get(slug="should_change").conclusion_recommendations,
+            ["RERUN"],
         )
-        self.assertNotIn("conclusion_recommendation", changelog.experiment_data)
