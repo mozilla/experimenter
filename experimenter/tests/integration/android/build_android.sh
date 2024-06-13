c23918af8ce17d363cfb54ff446e060240593c08
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-13 11:34:35 -0500

    Change system image and apk archh.

--- experimenter/tests/integration/android/build_android.sh
+++ experimenter/tests/integration/android/build_android.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 cd mobile/android/fenix \
 && ./gradlew clean app:assembleFenixDebug \
-&& mv app/build/outputs/apk/fenix/debug/app-fenix-x86-debug.apk ./ \
+&& mv app/build/outputs/apk/fenix/debug/app-fenix-x86_64-debug.apk ./ \
 && ./gradlew clean app:assembleFenixDebugAndroidTest \
 && mv app/build/outputs/apk/androidTest/fenix/debug/app-fenix-debug-androidTest.apk ./
