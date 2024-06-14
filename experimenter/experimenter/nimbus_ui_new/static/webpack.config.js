affe0d287d665db2b053f6405bcff8407b033a0a
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-06-14 11:46:52 -0400

    chore(nimbus): format config files in nimbus ui new (#10847)
    Because
    
    * It's helperful to autoformat all the js config files in nimbus ui new
    to keep things consistent
    
    This commit
    
    * Updates the paths and ignores to autoformat all the files in nimbus ui
    new static
    
    fixes #10846

--- experimenter/experimenter/nimbus_ui_new/static/webpack.config.js
+++ experimenter/experimenter/nimbus_ui_new/static/webpack.config.js
@@ -1,52 +1,48 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        app: './js/index.js',
-        experiment_list: './js/scripts/experiment_list.js',
-        theme: './js/scripts/theme.js',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default'],
-            bootstrap: 'bootstrap',
-        }),
+  entry: {
+    app: "./js/index.js",
+    experiment_list: "./js/scripts/experiment_list.js",
+    theme: "./js/scripts/theme.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-    resolve: {
-        alias: {
-            jquery: "jquery/src/jquery"
-        }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"],
+      bootstrap: "bootstrap",
+    }),
+  ],
+  resolve: {
+    alias: {
+      jquery: "jquery/src/jquery",
     },
-    mode: 'production'
+  },
+  mode: "production",
 };
