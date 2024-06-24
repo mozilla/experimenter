a1489bc58d056b0e691ead40af2f62db888d51e5
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-24 13:56:10 -0500

    test(nimbus): Add android e2e test config. (#10844)
    Because
    
    It would be awesome to extend our coverage to android devices to make
    sure those stay healthy.
    
    This commit
    
    * Adds a job to build the fenix test APKs from mozilla central.
    * Adds a job to run a small suite of tests on fenix
    
    Fixes #10843

---
+++ experimenter/tests/integration/nimbus/android/build_android.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+hg pull && hg update
+
+./mach build
+cd mobile/android/fenix
+./gradlew clean app:assembleFenixDebug
+mv app/build/outputs/apk/fenix/debug/app-fenix-x86_64-debug.apk ./
+./gradlew clean app:assembleFenixDebugAndroidTest
+mv app/build/outputs/apk/androidTest/fenix/debug/app-fenix-debug-androidTest.apk ./
---
+++ experimenter/tests/integration/nimbus/android/test_fenix_integration.py
@@ -0,0 +1,46 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+
+
+@pytest.fixture
+def experiment_slug():
+    return "firefox-fenix-test-experiment"
+
+
+@pytest.mark.generic_test
+def test_experiment_unenrolls_via_studies_toggle(
+    setup_experiment, gradlewbuild, open_app
+):
+    setup_experiment()
+    open_app()
+    gradlewbuild.test("GenericExperimentIntegrationTest#disableStudiesViaStudiesToggle")
+    gradlewbuild.test("GenericExperimentIntegrationTest#testExperimentUnenrolled")
+
+
+@pytest.mark.generic_test
+def test_experiment_enrolls(
+    setup_experiment,
+    gradlewbuild,
+    open_app,
+    check_ping_for_experiment,
+):
+    setup_experiment()
+    open_app()
+    gradlewbuild.test("GenericExperimentIntegrationTest#testExperimentEnrolled")
+    assert check_ping_for_experiment(reason="enrollment", branch="control")
+
+
+@pytest.mark.generic_test
+def test_experiment_unenrolls_via_secret_menu(
+    setup_experiment, gradlewbuild, open_app, check_ping_for_experiment
+):
+    setup_experiment()
+    open_app()
+    gradlewbuild.test(
+        "GenericExperimentIntegrationTest#testExperimentUnenrolledViaSecretMenu"
+    )
+    gradlewbuild.test("GenericExperimentIntegrationTest#testExperimentUnenrolled")
+    assert check_ping_for_experiment(reason="unenrollment", branch="control")
