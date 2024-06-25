0f158b2c0c26b7024f0e458cb41386fe42f3d5f3
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-25 18:57:45 -0500

    Change script steps.

--- experimenter/tests/nimbus_integration_tests.sh
+++ experimenter/tests/nimbus_integration_tests.sh
@@ -11,8 +11,8 @@ fi
 
 sudo apt-get -qqy update && sudo apt-get -qqy install python3-venv python3-pip
 pip install poetry --break-system-packages
-mkdir -m a+rwx /code/experimenter/tests/integration/test-reports
 sudo chmod -R a+rwx /code/experimenter/tests/integration/
+mkdir -m a+rwx /code/experimenter/tests/integration/test-reports
 
 poetry -C experimenter/tests/integration -vvv install --no-root
 poetry -C experimenter/tests/integration -vvv run pytest --html=experimenter/tests/integration/test-reports/report.htm --self-contained-html --reruns-delay 30 --driver Firefox --verify-base-url --base-url https://nginx/nimbus/ experimenter/tests/integration/nimbus -vvv $(PYTEST_ARGS)
