25de45a4fa85cc5cac36616803e329938d5114cd
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-25 19:24:14 -0500

    Fix failing command.

--- experimenter/tests/experimenter_legacy_tests.sh
+++ experimenter/tests/experimenter_legacy_tests.sh
@@ -11,4 +11,4 @@ sudo chmod -R a+rwx /code/experimenter/tests/integration/
 mkdir -m a+rwx /code/experimenter/tests/integration/test-reports
 
 poetry -C experimenter/tests/integration -vvv install --no-root
-poetry -C experimenter/tests/integration -vvv run pytest --html=experimenter/tests/integration/test-reports/report.htm --self-contained-html --reruns-delay 30 --driver Firefox --verify-base-url --base-url https://nginx/nimbus/ experimenter/tests/integration/legacy/ -vvv $(PYTEST_ARGS)
+poetry -C experimenter/tests/integration -vvv run pytest --html=experimenter/tests/integration/test-reports/report.htm --self-contained-html --reruns-delay 30 --driver Firefox --verify-base-url --base-url https://nginx/nimbus/ experimenter/tests/integration/legacy/ -vvv $PYTEST_ARGS
