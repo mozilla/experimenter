b1bc5babfaa170b122a99b16d5b532a1741cc69b
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-26 14:10:13 -0500

    chore(tests): Change integration test suite to use poetry (#10908)
    Because
    
    - We use poetry for most other package management
    
    This commit
    
    - Updates our integration tests to use poetry too.
    
    Fixes #10907

---
+++ experimenter/tests/nimbus_rust_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+set +x
+
+mkdir -m a+rwx /code/experimenter/tests/integration/test-reports
+
+poetry -C experimenter/tests/integration -vvv install --no-root
+poetry -C experimenter/tests/integration \
+    -vvv \
+    run \
+    pytest \
+    --verify-base-url \
+    --base-url https://nginx/nimbus/ \
+    --html=test-reports/report.htm \
+    --self-contained-html \
+    experimenter/tests/integration/nimbus/test_mobile_targeting.py \
+    -vvv
