0ed489822dd8b066f5def6fe9089e769794dacbb
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-06-26 14:10:13 -0500

    chore(tests): Change integration test suite to use poetry (#10908)
    Because
    
    - We use poetry for most other package management
    
    This commit
    
    - Updates our integration tests to use poetry too.
    
    Fixes #10907

--- experimenter/tests/integration/legacy/test_filter_expression_validation.py
+++ experimenter/tests/integration/legacy/test_filter_expression_validation.py
@@ -1,14 +1,29 @@
-def test_filter_expressions_with_matching_firefox_versions(base_url, selenium):
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def filter_expression_path():
+    path = Path(__file__).parent / "utils" / "filter_expression.js"
+    return path.absolute()
+
+
+def test_filter_expressions_with_matching_firefox_versions(
+    base_url, selenium, filter_expression_path
+):
     selenium.get("about:blank")
-    with open("legacy/utils/filter_expression.js") as js:
+    with filter_expression_path.open() as js:
         with selenium.context(selenium.CONTEXT_CHROME):
             script = selenium.execute_script(js.read(), 80.1, 80)
     assert script is True
 
 
-def test_filter_expressions_with_mismatching_firefox_versions(base_url, selenium):
+def test_filter_expressions_with_mismatching_firefox_versions(
+    base_url, selenium, filter_expression_path
+):
     selenium.get("about:blank")
-    with open("legacy/utils/filter_expression.js") as js:
+    with filter_expression_path.open() as js:
         with selenium.context(selenium.CONTEXT_CHROME):
             script = selenium.execute_script(js.read(), 80.1, 150)
     assert script is False
--- experimenter/tests/integration/nimbus/test_desktop_targeting.py
+++ experimenter/tests/integration/nimbus/test_desktop_targeting.py
@@ -13,12 +13,19 @@ def targeting_config_slug(request):
     return request.param
 
 
+@pytest.fixture
+def filter_expression_path():
+    path = Path(__file__).parent / "utils" / "filter_expression.js"
+    return path.absolute()
+
+
 @pytest.mark.run_targeting
 def test_check_advanced_targeting(
     selenium,
     targeting_config_slug,
     experiment_slug,
     default_data_api,
+    filter_expression_path,
 ):
     helpers.create_experiment(
         experiment_slug,
@@ -32,7 +39,7 @@ def test_check_advanced_targeting(
 
     # Inject filter expression
     selenium.get("about:blank")
-    with Path("nimbus", "utils", "filter_expression.js").open() as js:
+    with filter_expression_path.open() as js:
         result = Browser.execute_script(
             selenium,
             targeting,
@@ -70,6 +77,7 @@ def test_check_audience_targeting(
     audience_field,
     experiment_slug,
     default_data_api,
+    filter_expression_path,
 ):
     default_data_api.update(audience_field)
     helpers.create_experiment(
@@ -84,7 +92,7 @@ def test_check_audience_targeting(
 
     # Inject filter expression
     selenium.get("about:blank")
-    with Path("nimbus", "utils", "filter_expression.js").open() as js:
+    with filter_expression_path.open() as js:
         result = Browser.execute_script(
             selenium,
             targeting,
