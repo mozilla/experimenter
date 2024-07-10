d0bc7482df75d4981403ebbf3e7f60bacb83f848
Yashika Khurana <yashikakhuranayashika@gmail.com>
2024-07-10 10:57:51 -0700

    feat(nimbus): New QA form on summary page (#10952)
    Because
    
    - QA should be able to edit its status and comment on new summary page
    in HTMX
    
    This commit
    
    - New QA form on the new summary page
    
    Fixes #10913 
    <img width="1451" alt="Screenshot 2024-07-04 at 11 24 49 AM"
    src="https://github.com/mozilla/experimenter/assets/25848231/9fe4eeec-395c-4154-9991-c08e5fea2852">
    <img width="1451" alt="Screenshot 2024-07-04 at 11 24 59 AM"
    src="https://github.com/mozilla/experimenter/assets/25848231/ea422e4b-42ae-48ef-b3c8-3738ed79a253">
    <img width="1451" alt="Screenshot 2024-07-04 at 11 25 15 AM"
    src="https://github.com/mozilla/experimenter/assets/25848231/f076270d-651f-4b2d-a4b3-7e493e7fffdf">

---
+++ experimenter/experimenter/experiments/forms.py
@@ -0,0 +1,14 @@
+# forms.py
+
+from django import forms
+
+from experimenter.experiments.models import NimbusExperiment
+
+
+class QAStatusForm(forms.ModelForm):
+    class Meta:
+        model = NimbusExperiment
+        fields = ["qa_status", "qa_comment"]
+        widgets = {
+            "qa_status": forms.Select(choices=NimbusExperiment.QAStatus),
+        }
