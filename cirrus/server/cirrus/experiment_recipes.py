d7e4b45e910884cf5427eb970c478af843a808fb
yashikakhurana <yashikakhuranayashika@gmail.com>
2024-07-12 13:09:08 -0700

    feat(nimbus): live recipes

--- cirrus/server/cirrus/experiment_recipes.py
+++ cirrus/server/cirrus/experiment_recipes.py
@@ -19,7 +19,7 @@ class RecipeType(Enum):
 
 class RemoteSettings:
     def __init__(self, sdk_live: SDK, sdk_preview: SDK):
-        self.recipes: dict[str, list[Any]] = {"data": []}
+        self.live_recipes: dict[str, list[Any]] = {"data": []}
         self.preview_recipes: dict[str, list[Any]] = {"data": []}
         self.url: str = remote_setting_url
         self.preview_url: str = remote_setting_preview_url
@@ -27,7 +27,7 @@ class RemoteSettings:
         self.sdk_preview = sdk_preview
 
     def get_recipes(self) -> dict[str, list[Any]]:
-        return self.recipes
+        return self.live_recipes
 
     def get_preview_recipes(self) -> dict[str, list[Any]]:
         return self.preview_recipes
@@ -71,8 +71,8 @@ class RemoteSettings:
             raise e
 
     def update_live_recipes(self, new_recipes: dict[str, list[Any]]) -> None:
-        self.recipes = new_recipes
-        self.sdk_live.set_experiments(json.dumps(self.recipes))
+        self.live_recipes = new_recipes
+        self.sdk_live.set_experiments(json.dumps(self.live_recipes))
 
     def update_preview_recipes(self, new_recipes: dict[str, list[Any]]) -> None:
         self.preview_recipes = new_recipes
