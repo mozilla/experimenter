c8db8ffce514822fb6d386d036d2c4d8ab8760f5
Benjamin Forehand Jr <bforehand@mozilla.com>
2024-07-29 12:53:50 -0500

    test(nimbus): Add Firefox for iOS integration tests.
        Because:
    
        - We have integration tests for fenix and desktop, we should add ios
    
        This commit:
    
        - Adds ios integration tests to the circleci workflow.
    
        This test creates an experiment on experimenter and copies the recipe from that experiment.
        It then uses nimbus cli to enroll the device into that experiment and does a few checks, namely
        making sure the experiment is enrolled and that it can be unenrolled.
    
        Fixes #11028

---
+++ experimenter/tests/integration/nimbus/ios/test_ios_integration.py
@@ -0,0 +1,91 @@
+import logging
+import os
+import subprocess
+import time
+from pathlib import Path
+
+import pytest
+
+here = Path(__file__)
+
+
+@pytest.fixture
+def experiment_slug():
+    return "firefox-ios-integration-test"
+
+
+@pytest.fixture(name="device_control", scope="module", autouse=True)
+def fixture_device_control(xcrun):
+    ...
+
+
+@pytest.fixture(name="experiment_url", scope="module")
+def fixture_experiment_url(request, variables):
+    ...
+
+
+@pytest.fixture(name="experiment_data")
+def fixture_experiment_data(experiment_url, request):
+    ...
+
+
+@pytest.fixture(name="set_env_variables", autouse=True)
+def fixture_set_env_variables(experiment_slug):
+    """Set any env variables XCUITests might need"""
+    os.environ["EXPERIMENT_NAME"] = experiment_slug
+
+
+@pytest.fixture(name="setup_experiment")
+def setup_experiment(experiment_slug, nimbus_cli_args):
+    def _setup_experiment():
+        logging.info(f"Testing experiment {experiment_slug}")
+        command = [
+            "nimbus-cli",
+            "--app",
+            "firefox_ios",
+            "--channel",
+            "developer",
+            "enroll",
+            f"{experiment_slug}",
+            "--branch",
+            "control",
+            "--file",
+            "/tmp/experimenter/tests/integration/ios_recipe.json",
+            "--reset-app",
+            "--",
+            f"{nimbus_cli_args}",
+        ]
+        logging.info(f"Nimbus CLI Command: {' '.join(command)}")
+        out = subprocess.check_output(
+            " ".join(command),
+            cwd=here.parent,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+            shell=True,
+        )
+        logging.info(out)
+
+    return _setup_experiment
+
+
+@pytest.mark.ios_enrollment
+def test_experiment_unenrolls_after_studies_toggle(
+    xcodebuild, setup_experiment, start_app
+):
+    xcodebuild.install(boot=False)
+    xcodebuild.test(
+        "XCUITests/ExperimentIntegrationTests/testAppStartup", build=False, erase=False
+    )
+    setup_experiment()
+    time.sleep(5)
+    xcodebuild.test(
+        "XCUITests/ExperimentIntegrationTests/testVerifyExperimentEnrolled",
+        build=False,
+        erase=False,
+    )
+    start_app()
+    xcodebuild.test(
+        "XCUITests/ExperimentIntegrationTests/testStudiesToggleDisablesExperiment",
+        build=False,
+        erase=False,
+    )
