51467d86a20a589581d7ff906b38136c158932a7
yashikakhurana <yashikakhuranayashika@gmail.com>
2024-08-23 11:12:12 -0700

    fix(cirrus): uncomment demo tests

--- experimenter/tests/integration/nimbus/pages/demo_app/frontend.py
+++ experimenter/tests/integration/nimbus/pages/demo_app/frontend.py
@@ -23,8 +23,7 @@ class DemoAppPage(Base):
         context_input = self.wait_for_and_find_element(
             By.XPATH, context_xpath, description=None
         )
-        time.sleep(10)
-        
+        time.sleep(10)   
         client_id_input.send_keys(client_id)
         context_input.send_keys(context)
 
