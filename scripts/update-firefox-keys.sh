2cb61a4b2144cc026757a4c18f60b4fe73d27e25
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-09-09 12:52:22 -0500

    chore(ci): Add steps to download firefox beta build for testing from specific tasks.

--- scripts/update-firefox-keys.sh
+++ scripts/update-firefox-keys.sh
@@ -17,14 +17,14 @@ case $input in
     fenix)
         INDEX_BASE="gecko.v2.mozilla-beta.latest.mobile"
         TASK_ID=$(curl ${CURLFLAGS[@]} "${TASKCLUSTER_API}/tasks/${INDEX_BASE}" | jq '.tasks[] | select(.namespace == "gecko.v2.mozilla-beta.latest.mobile.fenix-beta") | .taskId')
-        echo TASK ID "${TASK_ID}"
+        echo FIREFOX_FENIX_TASK_ID TASK ID "${TASK_ID}"
         echo "FIREFOX_FENIX_TASK_ID=${TASK_ID}" > firefox-fenix-build.env
         mv firefox-fenix-build.env experimenter/tests
         ;;
     desktop-beta)
         INDEX_BASE="gecko.v2.mozilla-beta.latest.firefox"
-        TASK_ID=$(curl ${CURLFLAGS[@]} "${TASKCLUSTER_API}/tasks/${INDEX_BASE}" | jq '.tasks[] | select(.namespace == "gecko.v2.mozilla-beta.latest.firefox.linux-debug") | .taskId')
-        echo TASK ID "${TASK_ID}"
+        TASK_ID=$(curl ${CURLFLAGS[@]} "${TASKCLUSTER_API}/tasks/${INDEX_BASE}" | jq '.tasks[] | select(.namespace == "gecko.v2.mozilla-beta.latest.firefox.linux64-debug") | .taskId')
+        echo FIREFOX_BETA_TASK_ID "${TASK_ID}"
         echo "FIREFOX_BETA_TASK_ID=${TASK_ID}" > firefox-desktop-beta-build.env
         mv firefox-desktop-beta-build.env experimenter/tests
         ;;
