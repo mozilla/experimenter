756b274acae6ebe763151d424293555561938259
Mike Williams <mwilliams@mozilla.com>
2024-09-09 14:03:25 -0400

    version bump; remove commented code

--- schemas/mozilla_nimbus_schemas/__init__.py
+++ schemas/mozilla_nimbus_schemas/__init__.py
@@ -1,8 +1,2 @@
 from mozilla_nimbus_schemas.experiments import *
 from mozilla_nimbus_schemas.jetstream import *
-
-# from mozilla_nimbus_schemas import experiments, jetstream
-
-# __all__ = [f"experiments.{m}" for m in experiments.__all__] + [
-#     f"jetstream.{m}" for m in jetstream.__all__
-# ]
