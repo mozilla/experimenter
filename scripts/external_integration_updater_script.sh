3c48a942945014867d0d6f674b4007b62b20e6b5
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-09-18 12:45:08 -0500

    chore(ci): Fix branch name in external integration script. (#11387)
    Because
    
    - The branch that was being created did not match the branch being
    pushed within our external integration script
    
    This commit
    
    - Fixes the branch names
    
    Fixes #11363

--- scripts/external_integration_updater_script.sh
+++ scripts/external_integration_updater_script.sh
@@ -2,7 +2,7 @@
 
 git checkout main
 git pull origin main
-git checkout -B check-external-firefox-integrations
+git checkout -B check_external_firefox_integrations
 firefox_types=("fenix" "desktop-beta")
 for name in "${firefox_types[@]}"
 do
@@ -18,7 +18,7 @@ if (($(git status --porcelain | wc -c) > 0)); then
     git add .
     git commit -m "chore(nimbus): Check external firefox integrations and update keys"
     git push origin -f check_external_firefox_integrations
-    gh pr create -t "chore(nimbus): Check external firefox integrations and update keys" -b "" --base main --head check-external-firefox-integrations --repo mozilla/experimenter || echo "PR already exists, skipping"
+    gh pr create -t "chore(nimbus): Check external firefox integrations and update keys" -b "" --base main --head check_external_firefox_integrations --repo mozilla/experimenter || echo "PR already exists, skipping"
 else
     echo "No config changes, skipping"
 fi
