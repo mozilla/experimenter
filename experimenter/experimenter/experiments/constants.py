a184ae9d76419846e21006e26932b1ebed8e227a
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2024-10-08 13:29:34 -0400

    chore(nimbus): add version 131.2.0 (#11512)
    Becuase
    
    * We need to target version 131.2.0
    
    This commit
    
    * Adds version 131.2.0
    
    fixes #11511

--- experimenter/experimenter/experiments/constants.py
+++ experimenter/experimenter/experiments/constants.py
@@ -572,6 +572,7 @@ class NimbusConstants:
         FIREFOX_131 = "131.!"
         FIREFOX_131_B4 = "131.0b4"
         FIREFOX_131_1_0 = "131.1.0"
+        FIREFOX_131_2_0 = "131.2.0"
         FIREFOX_132 = "132.!"
         FIREFOX_133 = "133.!"
         FIREFOX_134 = "134.!"
