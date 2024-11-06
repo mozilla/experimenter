26ec2327d582bd57d13dacf630045a49720d5de6
yashikakhurana <yashikakhuranayashika@gmail.com>
2024-11-06 12:17:11 -0800

    feat(nimbus): Enrollment end date

--- experimenter/experimenter/experiments/constants.py
+++ experimenter/experimenter/experiments/constants.py
@@ -571,7 +571,7 @@ class NimbusConstants:
         FIREFOX_130_0_1 = "130.0.1"
         FIREFOX_131 = "131.!"
         FIREFOX_131_B4 = "131.0b4"
-        FIREFOX_131_0_3 = "13.0.3"
+        FIREFOX_131_0_3 = "131.0.3"
         FIREFOX_131_1_0 = "131.1.0"
         FIREFOX_131_2_0 = "131.2.0"
         FIREFOX_132 = "132.!"
