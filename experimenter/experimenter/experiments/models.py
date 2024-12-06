1a792441c43d1b8a6f3076cb841c24de8aaa2e4a
Rana Al-Khulaidi <69090627+RJAK11@users.noreply.github.com>
2024-12-06 11:09:06 -0500

    feat(nimbus): Add audience overlap warnings to new summary page (#11888)
    Because
    
    * The summary page did not display warnings related to audience overlap
    
    This commit
    
    * Incorporates the warnings "Excluding experiments warning", "Live
    experiments bucket warning", and "Live multifeature warning" into the
    summary page based on the experiment's status and overlap conditions
    
    Fixes #11725
    
    <img width="1504" alt="Screenshot 2024-12-03 at 3 48 43 PM"
    src="https://github.com/user-attachments/assets/4b7be908-a974-454f-be97-a0ce5e11f809">
    <img width="1507" alt="Screenshot 2024-12-03 at 3 49 59 PM"
    src="https://github.com/user-attachments/assets/f3ca6f7d-ca3d-480d-954b-ccb4b0f75a08">
    <img width="1503" alt="Screenshot 2024-12-03 at 3 59 40 PM"
    src="https://github.com/user-attachments/assets/48efceff-a966-4f7c-b957-07fe53444ce3">

--- experimenter/experimenter/experiments/models.py
+++ experimenter/experimenter/experiments/models.py
@@ -31,6 +31,7 @@ from experimenter.experiments.constants import (
     NimbusConstants,
     TargetingMultipleKintoCollectionsError,
 )
+from experimenter.nimbus_ui_new.constants import NimbusUIConstants
 from experimenter.projects.models import Project
 from experimenter.targeting.constants import TargetingConstants
 
@@ -1186,6 +1187,62 @@ class NimbusExperiment(NimbusConstants, TargetingConstants, FilterMixin, models.
             timezone.now() - self.changes.latest_change().changed_on
         ) >= datetime.timedelta(seconds=settings.KINTO_REVIEW_TIMEOUT)
         return self.publish_status == self.PublishStatus.WAITING and review_expired
+
+    @property
+    def audience_overlap_warnings(self):
+        warnings = []
+        excluded_live_deliveries = ""
+        if self.excluded_live_deliveries:
+            excluded_live_deliveries = ", ".join(self.excluded_live_deliveries)
+
+        feature_has_live_multifeature_experiments = ""
+        if self.feature_has_live_multifeature_experiments:
+            feature_has_live_multifeature_experiments = ", ".join(
+                self.feature_has_live_multifeature_experiments
+            )
+
+        live_experiments_in_namespace = ""
+        if self.live_experiments_in_namespace:
+            live_experiments_in_namespace = ", ".join(self.live_experiments_in_namespace)
+
+        overlapping_warnings = (
+            feature_has_live_multifeature_experiments
+            and live_experiments_in_namespace
+            and feature_has_live_multifeature_experiments in live_experiments_in_namespace
+        )
+
+        if self.status in [NimbusConstants.Status.DRAFT, NimbusConstants.Status.PREVIEW]:
+            if excluded_live_deliveries:
+                warnings.append(
+                    {
+                        "text": NimbusUIConstants.EXCLUDING_EXPERIMENTS_WARNING,
+                        "slugs": self.excluded_live_deliveries,
+                        "variant": "warning",
+                        "learn_more_link": NimbusUIConstants.AUDIENCE_OVERLAP_WARNING,
+                    }
+                )
+
+            if live_experiments_in_namespace and not overlapping_warnings:
+                warnings.append(
+                    {
+                        "text": NimbusUIConstants.LIVE_EXPERIMENTS_BUCKET_WARNING,
+                        "slugs": self.live_experiments_in_namespace,
+                        "variant": "warning",
+                        "learn_more_link": NimbusUIConstants.AUDIENCE_OVERLAP_WARNING,
+                    }
+                )
+
+            if feature_has_live_multifeature_experiments:
+                warnings.append(
+                    {
+                        "text": NimbusUIConstants.LIVE_MULTIFEATURE_WARNING,
+                        "slugs": self.feature_has_live_multifeature_experiments,
+                        "variant": "warning",
+                        "learn_more_link": NimbusUIConstants.AUDIENCE_OVERLAP_WARNING,
+                    }
+                )
+
+        return warnings
 
     def clone(self, name, user, rollout_branch_slug=None, changed_on=None):
         # Inline import to prevent circular import
