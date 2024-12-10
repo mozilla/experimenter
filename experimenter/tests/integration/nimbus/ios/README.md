9c15845cd4630cc1ab9c20db741f9c88d9a139a7
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-12-10 13:34:26 -0600

    chore(ci): Update iOS integration test xcode/ios versions. (#11885)
    Because
    
    - Our iOS beta integration test is failing to build due to an out of
    date xcode version.
    
    This commit
    
    - Updates the xcode and iOS version of the integration test CI runner
    
    Fixes #11884

---
+++ experimenter/tests/integration/nimbus/ios/README.md
@@ -0,0 +1,3 @@
+# Nimbus iOS Integration test files
+
+This folder contains test files used on our CI jobs to test the Nimbus integration with iOS. These files are used to replace the files within the `firefox-ios` repo when it is cloned during the job. This is done so that we can easily update the test configuration for testing against older releases.
---
+++ experimenter/tests/integration/nimbus/ios/xcodebuild.py
@@ -0,0 +1,76 @@
+import logging
+import os
+import subprocess
+from pathlib import Path
+
+from .xcrun import XCRun
+
+HERE = Path(__file__).resolve()
+LOGGER = logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+class XCodeBuild:
+    def __init__(self, log, **kwargs):
+        self.device = os.getenv("IOS_DEVICE", "iPhone 16")
+        self.ios_version = os.getenv("IOS_VERSION", "18.1")
+        self.binary = "xcodebuild"
+        self.destination = (
+            f"platform=iOS Simulator,name={self.device},OS={self.ios_version}"
+        )
+        self.scheme = "Fennec"
+        self.testPlan = "SyncIntegrationTestPlan"
+        self.xcrun = XCRun()
+        self.scheme = kwargs.get("scheme", self.scheme)
+        self.test_plan = kwargs.get("test_plan", self.testPlan)
+        self.log = log
+        self.logger = logging.getLogger()
+        self.firefox_app_path = next(
+            Path("/Users").glob(
+                "**/Library/Developer/Xcode/DerivedData/Client-*/Build/Products/Fennec_Testing-*/Client.app"
+            )
+        )
+
+    def install(self, boot=True):
+        if boot:
+            self.xcrun.boot()
+        try:
+            out = subprocess.check_output(
+                ["xcrun", "simctl", "install", "booted", self.firefox_app_path],
+                cwd=f"{HERE.parent}",
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+            )
+        except subprocess.CalledProcessError as e:
+            out = e.output
+            raise
+        finally:
+            with self.log.open("w") as f:
+                f.write(out)
+
+    def test(self, identifier, build=True, erase=True):
+        run_args = "test" if build else "test-without-building"
+        args = [
+            self.binary,
+            run_args,
+            "-scheme",
+            self.scheme,
+            "-destination",
+            self.destination,
+            f"-only-testing:{identifier}",
+            "-testPlan",
+            self.testPlan,
+        ]
+        self.logger.info("Running: {}".format(" ".join(args)))
+        try:
+            out = subprocess.check_output(
+                args,
+                cwd=f"{HERE.parents[3]}",
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+            )
+        except subprocess.CalledProcessError as e:
+            out = e.output
+            raise
+        finally:
+            with self.log.open("w") as f:
+                f.write(out)
