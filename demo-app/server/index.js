aa1b6b91b930db19988a89457f137783f8402eaa
yashikakhurana <yashikakhuranayashika@gmail.com>
2025-01-08 11:47:57 -0800

    feat(cirrus): V2 api

--- demo-app/server/index.js
+++ demo-app/server/index.js
@@ -29,7 +29,7 @@ const server = http.createServer(async (req, res) => {
       nimbus_preview: nimbusPreview,
     };
 
-    let pathName = '/v1/features/'
+    let pathName = '/v2/features/'
     if(nimbusPreview) {
       pathName= pathName+"?nimbus_preview=" + nimbusPreview;
     }
@@ -51,7 +51,18 @@ const server = http.createServer(async (req, res) => {
       });
 
       responseFromAPI.on('end', () => {
-        res.end(responseData);
+         try {
+            const parsedResponse = JSON.parse(responseData);
+            // Get the features
+            const clientFeatureConfig = parsedResponse['Features'] || parsedResponse;
+            // Send back the raw clientFeatureConfig to the frontend
+            res.end(JSON.stringify(clientFeatureConfig));
+        } catch (error) {
+            console.error('Error parsing API response:', error);
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        }
+
       });
     });
 
