3cd7d276220b67e82c725a70c695649c3c6cfb95
RJAK11 <ralkhulaidi@gmail.com>
2025-01-19 21:22:18 -0500

    feat: add enrolling/observing/complete status indicators to experiment list

--- experimenter/experimenter/nimbus_ui_new/views.py
+++ experimenter/experimenter/nimbus_ui_new/views.py
@@ -94,6 +94,8 @@ class NimbusExperimentsListView(NimbusExperimentViewMixin, FilterView):
         return filterset_class(**kwargs)
 
     def get_context_data(self, **kwargs):
+        selected_tab = self.request.GET.get("status", "Live")
+
         queryset = self.filterset_no_status.qs
 
         status_counts = {
@@ -106,6 +108,7 @@ class NimbusExperimentsListView(NimbusExperimentViewMixin, FilterView):
             status_counts=status_counts,
             sort_choices=SortChoices,
             create_form=NimbusExperimentCreateForm(),
+            selected_tab=selected_tab,
             **kwargs,
         )
 
