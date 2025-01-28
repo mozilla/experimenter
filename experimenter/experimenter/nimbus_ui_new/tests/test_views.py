97cc1ada89f528910e1e83edf1194988c203d462
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2025-01-28 15:31:46 -0500

    Revert "feat: split start/end dates into separate columns on main list (#11989)" (#12114)
    This reverts commit 4be0b3e8157c59dbe223f8f79c32b010163cf7ca.
    
    Because
    
    - Dynamically computing the end dates for sorting takes too long to
    process
    - We will need to further performance optimize this before we can re
    deploy it
    
    This commit
    
    - Reverts splitting the date into separate start and end dates
    
    Fixes #12113

--- experimenter/experimenter/nimbus_ui_new/tests/test_views.py
+++ experimenter/experimenter/nimbus_ui_new/tests/test_views.py
@@ -847,7 +847,7 @@ class NimbusExperimentsListViewTest(AuthTestCase):
             [experiment2.slug, experiment1.slug],
         )
 
-    def test_sort_by_start_date(self):
+    def test_sort_by_dates(self):
         experiment1 = NimbusExperimentFactory.create_with_lifecycle(
             NimbusExperimentFactory.Lifecycles.LIVE_ENROLLING,
             start_date=datetime.date(2024, 1, 1),
@@ -860,7 +860,7 @@ class NimbusExperimentsListViewTest(AuthTestCase):
         response = self.client.get(
             reverse("nimbus-list"),
             {
-                "sort": SortChoices.START_DATE_UP,
+                "sort": SortChoices.DATES_UP,
             },
         )
 
@@ -872,67 +872,13 @@ class NimbusExperimentsListViewTest(AuthTestCase):
         response = self.client.get(
             reverse("nimbus-list"),
             {
-                "sort": SortChoices.START_DATE_DOWN,
+                "sort": SortChoices.DATES_DOWN,
             },
         )
 
         self.assertEqual(
             [e.slug for e in response.context["experiments"]],
             [experiment2.slug, experiment1.slug],
-        )
-
-    def test_sort_by_end_date(self):
-        experiment1 = NimbusExperimentFactory.create_with_lifecycle(
-            NimbusExperimentFactory.Lifecycles.ENDING_APPROVE,
-            start_date=datetime.date(2024, 1, 1),
-            end_date=datetime.date(2024, 2, 1),
-        )
-        experiment2 = NimbusExperimentFactory.create_with_lifecycle(
-            NimbusExperimentFactory.Lifecycles.ENDING_APPROVE,
-            start_date=datetime.date(2024, 1, 2),
-            end_date=datetime.date(2024, 2, 2),
-        )
-        experiment3 = NimbusExperimentFactory.create_with_lifecycle(
-            NimbusExperimentFactory.Lifecycles.LIVE_ENROLLING,
-            start_date=datetime.date(2024, 1, 2),
-            proposed_duration=10,
-        )
-        experiment4 = NimbusExperimentFactory.create(
-            status=NimbusExperiment.Status.LIVE,
-        )
-
-        response = self.client.get(
-            reverse("nimbus-list"),
-            {
-                "sort": SortChoices.END_DATE_UP,
-            },
-        )
-
-        expected_order_up = sorted(
-            [experiment1, experiment2, experiment3], key=lambda exp: exp.computed_end_date
-        )
-
-        self.assertEqual(
-            [e.slug for e in response.context["experiments"]],
-            ([exp.slug for exp in expected_order_up] + [experiment4.slug]),
-        )
-
-        response = self.client.get(
-            reverse("nimbus-list"),
-            {
-                "sort": SortChoices.END_DATE_DOWN,
-            },
-        )
-
-        expected_order_down = sorted(
-            [experiment1, experiment2, experiment3],
-            key=lambda exp: exp.computed_end_date,
-            reverse=True,
-        )
-
-        self.assertEqual(
-            [e.slug for e in response.context["experiments"]],
-            ([exp.slug for exp in expected_order_down] + [experiment4.slug]),
         )
 
 
