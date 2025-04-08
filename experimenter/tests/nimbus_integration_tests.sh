5cd32aae189ce1ab9128ecadc63b7df5687ec5c1
Benjamin Forehand Jr <bennyjr169@gmail.com>
2025-04-08 16:14:29 -0500

    chore(tests): Add chown command to fix integration tests docker permission error. (#12460)
    Because
    
    - Selenium recently added a python virtual environment as part of their
    docker images but it is owned by `root` and the docker images are ran by
    `seluser`.
    
    This commit
    
    - Changes ownership of that directory so that the running user can
    access it.
    - We also removed an environment variable in the docker image needed for
    testing against firefox release, this adds that back.
    
    Fixes #12459

--- experimenter/tests/nimbus_integration_tests.sh
+++ experimenter/tests/nimbus_integration_tests.sh
@@ -47,6 +47,7 @@ esac
 curl -sSL https://install.python-poetry.org | python3 - --version 1.8.4
 sudo chmod -R a+rwx /code/experimenter/tests/integration/
 mkdir -m a+rwx -p /code/experimenter/tests/integration/test-reports
+sudo chown -R seluser /opt/venv/
 
 firefox --version
 
