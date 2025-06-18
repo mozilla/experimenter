ba639fb84e7ab5845f6a7073e76f73860c2fc8e7
Shane Hughes <shughes@mozilla.com>
2025-06-18 15:04:14 -0700

    Advanced targeting config: Signed out, not new user, eligible for VPN promotions on newtab
    Fixes #12813

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -924,6 +924,31 @@ NO_ENTERPRISE_OR_RECENT_VPN = NimbusTargetingConfig(
     application_choice_names=(Application.DESKTOP.name,),
 )
 
+WIN10_VPN_PROMOTION_ELIGIBLE = NimbusTargetingConfig(
+    name="Windows 10 users eligible for VPN promotion",
+    slug="win10_vpn_promotion_eligible",
+    description=(
+        "Windows 10 users who are signed out, at least 7 days old, "
+        "no enterprise, default newtab, no adblock"
+    ),
+    targeting=(
+        "os.isWindows && os.windowsVersion >= 10 && "
+        "os.windowsBuildNumber < 22000 && "
+        "isFxAEnabled && !isFxASignedIn && "
+        f"{NO_ENTERPRISE.targeting} && "
+        "newtabSettings.isDefault && "
+        f"{PROFILEMORETHAN7DAYS} && "
+        "!(['uBlock0@raymondhill.net','{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}',"
+        "'adblockultimate@adblockultimate.net','jid1-NIfFY2CA8fy1tg@jetpack',"
+        "'adguardadblocker@adguard.com','firefox@ghostery.com'] "
+        "intersect addonsInfo.addons|keys)|length"
+    ),
+    desktop_telemetry="",
+    sticky_required=False,
+    is_first_run_required=False,
+    application_choice_names=(Application.DESKTOP.name,),
+)
+
 INFREQUENT_USER_URIS = NimbusTargetingConfig(
     name="Infrequent user (uris)",
     slug="infrequent_user_uris",
