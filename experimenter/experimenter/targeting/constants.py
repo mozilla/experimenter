aba78fa3d5822a3fa354d61c30aa3b24264166c3
negin <nsauermann@mozilla.com>
2025-06-18 13:30:17 -0400

    feat(nimbus): Adds one-off advanced targeting - Whats New Notification Sidebar Vertical Tabs Rollout (excluding experiment treatment)

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -1577,6 +1577,27 @@ WINDOWS_10_PLUS_BACKGROUND_TASK_NOTIFICATION_1HR_INACTIVITY_CFR_ONLY = (
     )
 )
 
+WHATS_NEW_NOTIFICATION_SIDEBAR_VERTICAL_TABS_ROLLOUT = NimbusTargetingConfig(
+    name=(
+        "Windows 10+ users with 1hr+ of inactivity in the past day "
+        "who are running a background task with CFR enabled and "
+        "not enrolled in treatment-a of WNN sidebar/vertical tabs experiment"
+    ),
+    slug="whats_new_notification_sidebar_vertical_tabs_rollout",
+    description=(
+        "Windows 10+ users with 1hr+ of inactivity in the past day "
+        "who are running a background task with CFR enabled and "
+        "not enrolled in treatment-a of WNN sidebar/vertical tabs experiment"
+    ),
+    targeting=(
+        f"{WINDOWS_10_PLUS_BACKGROUND_TASK_NOTIFICATION_1HR_INACTIVITY_CFR_ONLY.targeting} && ((defaultProfile.enrollmentsMap['whats-new-notification-sidebarvertical-tabs'] == 'treatment-a') == false)"
+    ),
+    desktop_telemetry="",
+    sticky_required=True,
+    is_first_run_required=False,
+    application_choice_names=(Application.DESKTOP.name,),
+)
+
 NEWTAB_SPONSORED_TOPSITES_ENABLED = NimbusTargetingConfig(
     name="Newtab has Sponsored TopSites enabled ",
     slug="newtab_sponsored_topsites_enabled",
