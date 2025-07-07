ec310c130b4f811a7c96bdbf1ff5d5dd801e2af2
negin <nsauermann@mozilla.com>
2025-07-07 14:00:43 -0400

    feat(nimbus): Updates advanced targeting - Windows 10+ and backgroudTaskMode and 1hr+ of inactivity

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -1605,13 +1605,13 @@ WINDOWS_10_PLUS_BACKGROUND_TASK_NOTIFICATION_1HR_INACTIVITY_CFR_ONLY = (
     )
 )
 
-WHATS_NEW_NOTIFICATION_SIDEBAR_VERTICAL_TABS_ROLLOUT = NimbusTargetingConfig(
+WHATS_NEW_NOTIFICATION_SIDEBAR_VERTICAL_TABS_ROLLOUT_V2 = NimbusTargetingConfig(
     name=(
         "Windows 10+ users with 1hr+ of inactivity in the past day "
         "who are running a background task with CFR enabled and "
         "not enrolled in treatment-a of WNN sidebar/vertical tabs experiment"
     ),
-    slug="whats_new_notification_sidebar_vertical_tabs_rollout",
+    slug="whats_new_notification_sidebar_vertical_tabs_rollout_v2",
     description=(
         "Windows 10+ users with 1hr+ of inactivity in the past day "
         "who are running a background task with CFR enabled and "
@@ -1630,8 +1630,6 @@ WHATS_NEW_NOTIFICATION_SIDEBAR_VERTICAL_TABS_ROLLOUT = NimbusTargetingConfig(
         )
         &&
         isBackgroundTaskMode
-        &&
-        'browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features'|preferenceValue
         &&
         ((defaultProfile.enrollmentsMap['whats-new-notification-sidebarvertical-tabs']
         == 'treatment-a') == false)
