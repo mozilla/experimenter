ecbe2eae6c79c796e4a662aceb07d10b6dba378a
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2025-07-14 17:16:25 -0400

    feat(nimbus): save whole branch form on branch/screenshot change
    Becuase
    
    * When a user clicks the create/delete branch/screenshot button, the rest of the form state is being lost
    * In HTMX, the state lives only in the database
    * To preserve the state of the branch forms when a branch/screenshot is created/deleted, we must submit the entire form and update the db
    
    This commit
    
    * Changes the branch/screenshot create/delete buttons in the template to submit the entire branch form
    * Updates branch/screenshot create/delete forms to subclass the entire branch form
    * Updates tests to cover the full branch form case in each test class for branch/screenshot create/delete
    
    fixes #12884

--- experimenter/experimenter/nimbus_ui_new/tests/test_views.py
+++ experimenter/experimenter/nimbus_ui_new/tests/test_views.py
@@ -1736,7 +1736,10 @@ class TestBranchCreateView(AuthTestCase):
         experiment.save()
 
         response = self.client.post(
-            reverse("nimbus-new-create-branch", kwargs={"slug": experiment.slug}), {}
+            reverse("nimbus-new-create-branch", kwargs={"slug": experiment.slug}),
+            {
+                "feature_configs": [feature_config1.id, feature_config2.id],
+            },
         )
 
         self.assertEqual(response.status_code, 200)
