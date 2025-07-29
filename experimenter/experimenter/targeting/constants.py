53ea1ce50b4f73db87432907d51720b90f486ef8
t-p-white <towhite@mozilla.com>
2025-07-29 10:44:43 +0100

    feat(nimbus): Add advanced targeting for tou users

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -2383,6 +2383,28 @@ ANDROID_LARGE_SCREEN_USERS_ONLY = NimbusTargetingConfig(
     application_choice_names=(Application.FENIX.name,),
 )
 
+TOU_TARGETING_ANDROID_NOT_ACCEPTED = NimbusTargetingConfig(
+    name="Users that have not accepted the Terms of Use",
+    slug="users_not_accepted_tou",
+    description="Targeting users who have NOT accepted the Terms of Use",
+    targeting="user_accepted_tou == false",
+    desktop_telemetry="",
+    sticky_required=False,
+    is_first_run_required=False,
+    application_choice_names=(Application.FENIX.name,),
+)
+
+TOU_TARGETING_ANDROID_ACCEPTED = NimbusTargetingConfig(
+    name="Users that have accepted the Terms of Use",
+    slug="users_accepted_tou",
+    description="Targeting users who have accepted the Terms of Use",
+    targeting="user_accepted_tou == true",
+    desktop_telemetry="",
+    sticky_required=False,
+    is_first_run_required=False,
+    application_choice_names=(Application.FENIX.name,),
+)
+
 CHATBOT_IS_HUGGINGCHAT = NimbusTargetingConfig(
     name="Chatbot provider is HuggingChat",
     slug="chatbot_is_huggingchat",
