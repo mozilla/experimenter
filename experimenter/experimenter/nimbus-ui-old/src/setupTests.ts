482d34da3ab1607e52ae01736818326dd7eeacab
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2025-08-05 12:44:27 -0400

    chore(nimbus): remove nimbus-ui-old (#13202)
    Becuase
    
    * We have launched the new nimbus ui
    * We can safely remove the old nimbus ui
    
    This commit
    
    * Removes nimbus-ui-old
    
    fixes #13201

--- experimenter/experimenter/nimbus-ui-old/src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-import "@testing-library/jest-dom/extend-expect";
-import "jest-canvas-mock";
-import "mutationobserver-shim";
-jest.mock("vega-embed");
