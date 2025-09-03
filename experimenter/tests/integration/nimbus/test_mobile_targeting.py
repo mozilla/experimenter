633b588e4ef1455f1133dee006bf256e7a8847ec
t-p-white <1833156+t-p-white@users.noreply.github.com>
2025-09-03 16:45:49 +0100

    feat(nimbus): Add advanced targeting for ToU users who have interacted with links or remind me later option (#13290)
    Because
    
    - We want to be able to target users that have used any of the Terms
    of Use (ToU) prompt links or clicked the "remind me later" option.
    
    This commit
    
    - Adds the advanced targeting for users who have interacted with links
    or clicked the remind me later option.
    
    Fixes https://bugzilla.mozilla.org/show_bug.cgi?id=1981877
    Fixes https://mozilla-hub.atlassian.net/browse/FXDROID-5329
    
    Fixes #github_issue_number
    
    ---------
    
    Co-authored-by: Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
    Co-authored-by: experimenter-github-app[bot] <225274952+experimenter-github-app[bot]@users.noreply.github.com>
    Co-authored-by: Experimenter CircleCI Bot <experimenter-ci-bot@mozilla.com>
    Co-authored-by: Beth Rennie <beth@brennie.ca>
    Co-authored-by: Yashika Khurana <yashikakhuranayashika@gmail.com>
    Co-authored-by: Benjamin Forehand Jr <bennyjr169@gmail.com>

--- experimenter/tests/integration/nimbus/test_mobile_targeting.py
+++ experimenter/tests/integration/nimbus/test_mobile_targeting.py
@@ -109,6 +109,8 @@ def test_check_mobile_targeting(
             "is_large_device": True,
             "user_accepted_tou": True,
             "no_shortcuts_or_stories_opt_outs": True,
+            "user_clicked_tou_prompt_link": True,
+            "user_clicked_tou_prompt_remind_me_later": True,
         }
     )
     client = sdk_client(load_app_context(context))
