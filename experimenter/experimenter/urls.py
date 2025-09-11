b250cf51019d41b3c0d6776ddd2f5ab0daa82fd1
yashikakhurana <yashikakhuranayashika@gmail.com>
2025-09-11 10:17:45 -0700

    feat(nimbus): Add home page on header

--- experimenter/experimenter/urls.py
+++ experimenter/experimenter/urls.py
@@ -2,15 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, re_path
-from django.views.generic.base import RedirectView
 
 from experimenter.legacy.legacy_experiments.views import (
     ExperimentListView,
     PageNotFoundView,
 )
-from experimenter.nimbus_ui.views import NimbusExperimentsListView
+from experimenter.nimbus_ui.views import (
+    NimbusExperimentsHomeView,
+    NimbusExperimentsListView,
+)
 
 urlpatterns = [
+    re_path(r"^$", NimbusExperimentsHomeView.as_view(), name="nimbus-ui-home"),
     re_path(
         r"^api/v1/experiments/",
         include("experimenter.legacy.legacy_experiments.api.v1.urls"),
@@ -26,14 +29,9 @@ urlpatterns = [
     re_path(r"^api/v8/", include("experimenter.experiments.api.v8.urls")),
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^experiments/", include("experimenter.legacy.legacy_experiments.urls")),
-    re_path(r"^nimbus/", include("experimenter.nimbus_ui.urls")),
-    re_path(r"^nimbus/$", NimbusExperimentsListView.as_view(), name="nimbus-list"),
+    re_path(r"^all/", include("experimenter.nimbus_ui.urls")),
+    re_path(r"^all/$", NimbusExperimentsListView.as_view(), name="nimbus-list"),
     re_path(r"^legacy/$", ExperimentListView.as_view(), name="home"),
-    re_path(
-        r"^$",
-        RedirectView.as_view(pattern_name="nimbus-list"),
-        name="redirect-to-nimbus",
-    ),
 ]
 
 handler404 = PageNotFoundView.as_404_view()
