25dd5158e10cd0c11321b72093cf0fda143fa718
Meg Viar <lmegviar@gmail.com>
2025-10-09 11:13:32 -0400

    feat(nimbus) Make SPONSORED_SEARCH_SUGGESTIONS_DISABLED region agnostic

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -81,7 +81,7 @@ RECOMMENDED_OR_SPONSORED_STORIES_DISABLED = """
 )
 """
 SPONSORED_SEARCH_SUGGESTIONS_DISABLED = (
-    "!'browser.urlbar.suggest.quicksuggest.sponsored'|preferenceValue"
+    "'browser.urlbar.suggest.quicksuggest.sponsored'|preferenceIsUserSet && !'browser.urlbar.suggest.quicksuggest.sponsored'|preferenceValue"
 )
 ADS_DISABLED = f"""
 (
