82e0551d13a73f5c992c387c53396779a66ceabe
t-p-white <1833156+t-p-white@users.noreply.github.com>
2025-10-16 14:36:55 +0100

    Revert "fix(nimbus): Update the `TOU_TARGETING_ANDROID_NOT_ACCEPTED_AND_NO_SPONSORED_OPT_OUTS` configuration to be first run" (#13694)
    Reverts mozilla/experimenter#13681
    
    Need to re-verify the reported issue without this change.

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -2538,7 +2538,7 @@ TOU_TARGETING_ANDROID_NOT_ACCEPTED_AND_NO_SPONSORED_OPT_OUTS = NimbusTargetingCo
     targeting="user_accepted_tou == false && no_shortcuts_or_stories_opt_outs == true",
     desktop_telemetry="",
     sticky_required=False,
-    is_first_run_required=True,
+    is_first_run_required=False,
     application_choice_names=(Application.FENIX.name,),
 )
 
