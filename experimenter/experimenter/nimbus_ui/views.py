9626d2889c5ecad3492f3081a02281737756a6a6
Benjamin Forehand Jr <bennyjr169@gmail.com>
2025-10-31 12:02:30 -0500

    feat(nimbus): Add sorting to feature changes table.

--- experimenter/experimenter/nimbus_ui/views.py
+++ experimenter/experimenter/nimbus_ui/views.py
@@ -723,11 +723,16 @@ class NimbusFeaturesView(TemplateView):
 
         feature_id = self.request.GET.get("feature_configs")
         if feature_id:
-            qs = (
-                qs.filter(feature_configs=feature_id)
-                .distinct()
-                .order_by(self.request.GET.get("sort", "name"))
-            )
+            sort_param = self.request.GET.get("sort", "name")
+            if sort_param in [
+                "change_version",
+                "-change_version",
+                "change_size",
+                "-change_size",
+            ]:
+                sort_param = "name"
+
+            qs = qs.filter(feature_configs=feature_id).distinct().order_by(sort_param)
         else:
             return qs.none()
         return qs
@@ -779,17 +784,51 @@ class NimbusFeaturesView(TemplateView):
         )
         qa_runs_non_sortable_fields = {field for field, _ in qa_runs_non_sortable_headers}
 
+        feature_changes_sortable_headers = FeaturesPageSortChoices.sortable_headers(
+            FeaturesPageSortChoices.FeatureChanges
+        )
+        feature_changes_non_sortable_headers = [
+            ("show_diff", "Change Diff from Prev. (Previous | Current)")
+        ]
+
+        feature_change_headers = (
+            feature_changes_sortable_headers + feature_changes_non_sortable_headers
+        )
+
         # Get feature schema versions with their diffs
         feature_schemas = []
         feature_id = self.request.GET.get("feature_configs")
         thresholds = SCHEMA_DIFF_SIZE_CONFIG["thresholds"]
         labels = SCHEMA_DIFF_SIZE_CONFIG["labels"]
+        total_changes = 0
 
         if feature_id:
+            sort = self.request.GET.get("sort", "")
+
+            schema_order_by = [
+                "-version__major",
+                "-version__minor",
+                "-version__patch",
+            ]
+
+            # Map version sort
+            if sort == "change_version":
+                schema_order_by = [
+                    "version__major",
+                    "version__minor",
+                    "version__patch",
+                ]
+            elif sort == "-change_version":
+                schema_order_by = [
+                    "-version__major",
+                    "-version__minor",
+                    "-version__patch",
+                ]
+
             schemas = list(
                 NimbusVersionedSchema.objects.filter(feature_config_id=feature_id)
                 .select_related("version")
-                .order_by("-version__major", "-version__minor", "-version__patch")
+                .order_by(*schema_order_by)
             )
 
             schema_cache = []
@@ -829,6 +868,7 @@ class NimbusFeaturesView(TemplateView):
                     if total_changes > 0:
                         schemas_with_changes += 1
                 else:
+                    total_changes = 0
                     size_label = labels["first_version"]
 
                 feature_schemas.append(
@@ -838,8 +878,14 @@ class NimbusFeaturesView(TemplateView):
                         "previous_json": previous_json,
                         "size_label": size_label.get("text"),
                         "size_badge": size_label.get("badge_class"),
+                        "total_changes": total_changes,
                     }
                 )
+
+            if sort == "change_size":
+                feature_schemas.sort(key=lambda x: x["total_changes"])
+            elif sort == "-change_size":
+                feature_schemas.sort(key=lambda x: x["total_changes"], reverse=True)
 
         feature_changes_pagination = Paginator(feature_schemas, 5)
         feature_changes_page_number = self.request.GET.get("feature_changes") or 1
@@ -865,6 +911,8 @@ class NimbusFeaturesView(TemplateView):
             "feature_schemas": feature_changes_page_obj.object_list,
             "feature_changes_page_obj": feature_changes_page_obj,
             "schemas_with_changes": schemas_with_changes,
+            "feature_changes_headers": feature_change_headers,
+            "feature_changes_non_sortable_headers": feature_changes_non_sortable_headers,
         }
         return context
 
