22e870c07bb5207cbdfe010f8f526aa66b53eb19
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2025-11-18 16:44:58 -0500

    feat(nimbus): add loading spinner to list page
    Becuase
    
    * Updates on the list page can be very slow
    * It's not clear to a user if a request is in flight
    * On other pages we put a loading overlay overtop the loading elements
    * We don't need to put an overlay on top of the list becuase we don't need to prevent user input
    * We can just use a spinner
    
    This commit
    
    * Adds a loading spinner to the list page
    
    fixes #13944

--- experimenter/experimenter/nimbus_ui/static/js/experiment_list.js
+++ experimenter/experimenter/nimbus_ui/static/js/experiment_list.js
@@ -1,3 +1,17 @@
+document.body.addEventListener("htmx:beforeRequest", function () {
+  const spinner = document.querySelector("#htmx-table-spinner");
+  if (spinner) {
+    spinner.style.opacity = "1";
+  }
+});
+
+document.body.addEventListener("htmx:afterRequest", function () {
+  const spinner = document.querySelector("#htmx-table-spinner");
+  if (spinner) {
+    spinner.style.opacity = "0";
+  }
+});
+
 document.body.addEventListener("htmx:afterSwap", function () {
   // After reloading the table, we must update the values for status and sort in the sidebar form
   // since it will not have been reloaded in the htmx get call
