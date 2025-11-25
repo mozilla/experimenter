1880697e2dde2ada6aa4ee40b0451e4b7569152e
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2025-11-25 17:30:59 -0500

    bug(nimbus): line wrap readonly codemirrors (#14049)
    Becuase
    
    * Long strings inside JSON values like feature values or JEXL targeting
    can cause the read only codemirrors to be too wide
    
    This commit
    
    * Moves all read only codemirror setup into a single file
    * Adds the codemirror linewrapping extension
    * Updates the outer element styling to account for fixed width
    
    fixes #13977

---
+++ experimenter/experimenter/nimbus_ui/static/js/codemirror_utils.js
@@ -0,0 +1,73 @@
+import { basicSetup } from "codemirror";
+import { EditorView } from "@codemirror/view";
+import { EditorState } from "@codemirror/state";
+import { json, jsonParseLinter } from "@codemirror/lang-json";
+import { linter } from "@codemirror/lint";
+import {
+  themeCompartment,
+  getThemeExtensions,
+  registerView,
+} from "./theme_utils.js";
+import $ from "jquery";
+
+const VISIBLE_LINE_COUNT = 5;
+
+export const createReadonlyJsonEditor = (textarea) => {
+  if (!textarea || textarea.dataset.is_rendered) return null;
+
+  textarea.dataset.is_rendered = true;
+
+  const extensions = [
+    basicSetup,
+    json(),
+    linter(jsonParseLinter()),
+    EditorState.readOnly.of(true),
+    EditorView.editable.of(false),
+    EditorView.lineWrapping,
+    themeCompartment.of(getThemeExtensions()),
+  ];
+
+  const view = new EditorView({
+    doc: textarea.value,
+    extensions,
+    parent: textarea.parentNode,
+  });
+
+  view.dom.style.border = "1px solid #ccc";
+  view.dom.style.maxHeight = "inherit";
+  textarea.parentNode.insertBefore(view.dom, textarea);
+  textarea.style.display = "none";
+
+  registerView(view);
+
+  return view;
+};
+
+export const setupCodemirrorCollapsibleDisplay = (textarea) => {
+  const lines = textarea.value.split("\n").length;
+
+  if (lines > VISIBLE_LINE_COUNT) {
+    textarea.parentNode.nextElementSibling?.classList.remove("d-none");
+    textarea.nextElementSibling?.classList.remove("d-none");
+
+    $(".show-btn").on("click", (e) => {
+      e.target.parentNode.classList.add("d-none");
+      e.target.parentNode.nextElementSibling.classList.remove("d-none");
+    });
+
+    $(".hide-btn").on("click", (e) => {
+      e.target.parentNode.classList.add("d-none");
+      e.target.parentNode.previousElementSibling.classList.remove("d-none");
+    });
+  }
+};
+
+export const setupReadonlyJsonEditors = () => {
+  const textareas = document.querySelectorAll(".readonly-json");
+  textareas.forEach((textarea) => {
+    const view = createReadonlyJsonEditor(textarea);
+    if (view) {
+      setupCodemirrorCollapsibleDisplay(textarea);
+    }
+  });
+};
