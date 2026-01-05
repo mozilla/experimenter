4914522c598767372ec822e0330b6ea97befc1e1
Yashika Khurana <yashikakhuranayashika@gmail.com>
2026-01-05 13:06:44 -0800

    Update experimenter/experimenter/nimbus_ui/static/js/experiment_home.js
    Co-authored-by: Beth Rennie <beth@brennie.ca>

--- experimenter/experimenter/nimbus_ui/static/js/experiment_home.js
+++ experimenter/experimenter/nimbus_ui/static/js/experiment_home.js
@@ -1,8 +1,9 @@
 document.body.addEventListener("htmx:beforeRequest", function () {
-  const spinners = document.querySelectorAll(".htmx-spinner");
-  spinners.forEach((spinner) => {
-    spinner.style.opacity = "1";
-  });
+  document
+    .querySelectorAll(".htmx-spinner")
+    .forEach((spinner) => {
+      spinner.style.opacity = "1";
+    });
 });
 
 document.body.addEventListener("htmx:afterRequest", function () {
