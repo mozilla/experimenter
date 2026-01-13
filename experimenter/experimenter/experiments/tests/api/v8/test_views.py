48aacd0dad546ace767d1bf23a80d0f5e424a7a8
Jared Lockhart <119884+jaredlockhart@users.noreply.github.com>
2026-01-13 10:46:57 -0500

    chore(nimbus): remove focus/klar (#14328)
    Becuase
    
    * Nimbus was removed from the Focus/Klar clients
    * They no longer need to be accessible from Experimenter
    
    This commit
    
    * Removes Focus/Klar and all related code/configuration
    
    fixes #12804

--- experimenter/experimenter/experiments/tests/api/v8/test_views.py
+++ experimenter/experimenter/experiments/tests/api/v8/test_views.py
@@ -146,8 +146,8 @@ class NimbusExperimentFilterMixin:
             reverse(self.LIST_VIEW),
             {
                 "application": [
-                    NimbusExperiment.Application.FOCUS_ANDROID,
-                    NimbusExperiment.Application.KLAR_ANDROID,
+                    NimbusExperiment.Application.FENIX,
+                    NimbusExperiment.Application.IOS,
                 ]
             },
         )
@@ -157,8 +157,8 @@ class NimbusExperimentFilterMixin:
             [
                 f"{application}-experiment"
                 for application in (
-                    NimbusExperiment.Application.FOCUS_ANDROID,
-                    NimbusExperiment.Application.KLAR_ANDROID,
+                    NimbusExperiment.Application.FENIX,
+                    NimbusExperiment.Application.IOS,
                 )
             ],
         )
