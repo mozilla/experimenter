dee03d118c9ed96690be1b48108f5b0b269fe9bc
t-p-white <1833156+t-p-white@users.noreply.github.com>
2026-01-14 11:36:10 +0000

    Update constants.py

--- experimenter/experimenter/targeting/constants.py
+++ experimenter/experimenter/targeting/constants.py
@@ -2780,6 +2780,7 @@ ANDROID_EXISTING_USERS_NOT_ACCEPTED_TERMS_OF_USE_OVER_ONE_POINT = NimbusTargetin
         "user_accepted_tou == false && "
         "days_since_install >= 28 && "
         "("
+        "("
         "tou_points == 1 && "
         "("
         "'uBlock0@raymondhill.net' in addon_ids || "
@@ -2793,7 +2794,7 @@ ANDROID_EXISTING_USERS_NOT_ACCEPTED_TERMS_OF_USE_OVER_ONE_POINT = NimbusTargetin
         "'kolesin.work@gmail.com' in addon_ids || "
         "'adblocker@pcmatic.com' in addon_ids || "
         "'{73a6fe31-595d-460b-a920-fcc0f8843232}' in addon_ids"
-        "))"
+        ")) || tou_points > 1)"
     ),
     desktop_telemetry="",
     sticky_required=False,
