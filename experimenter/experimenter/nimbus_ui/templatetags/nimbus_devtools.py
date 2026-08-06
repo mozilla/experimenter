bfc06a6b4bbf44d517314b74c05fcb3271377ced
Rana Al-Khulaidi <ralkhulaidi@gmail.com>
2026-08-06 02:27:32 -0400

    feat(nimbus): Add nimbus-devtools support to new rollouts UI
    Because:
    
    - rollouts on the new UI aren't integrated with nimbus-devtools yet
    
    This commit:
    
    - wires devtools metadata, recipe JSON, force-enroll, message feature preview, and toasts into the new rollouts page
    
    Fixes #16623

--- experimenter/experimenter/nimbus_ui/templatetags/nimbus_devtools.py
+++ experimenter/experimenter/nimbus_ui/templatetags/nimbus_devtools.py
@@ -1,11 +1,13 @@
 from django import template
 from django.utils.html import json_script
 
+from experimenter.experiments.models import NimbusExperiment
+
 register = template.Library()
 
 
 @register.simple_tag
-def devtools_metadata(experiment):
+def devtools_metadata(experiment: NimbusExperiment) -> str:
     """Embed metadata about a given experiment in a page for nimbus-devtools to
     consume.
     """
@@ -16,4 +18,22 @@ def devtools_metadata(experiment):
             "localizations": experiment.localizations,
         },
         element_id="nimbus-devtools-experiment-metadata",
+    )
+
+
+@register.simple_tag
+def devtools_integration(experiment: NimbusExperiment, page: str) -> str:
+    """Embed metadata about a given experiment and the current page for
+    nimbus-devtools to consume.
+    """
+    return json_script(
+        {
+            "experimentMetadata": {
+                "application": experiment.application,
+                "isLocalized": experiment.is_localized,
+                "localizations": experiment.localizations,
+            },
+            "page": page,
+        },
+        element_id="nimbus-devtools-integration",
     )
