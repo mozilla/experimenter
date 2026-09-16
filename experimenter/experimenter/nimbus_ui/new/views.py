f208edfcb9963cb79f230da276096e6d7d750b8e
Rana Al-Khulaidi <ralkhulaidi@gmail.com>
2026-09-16 15:01:10 +0000

    fix(nimbus): Only persist rollout schedule changes on save (#17287)
    Because:
    
    - changing the rollout schedule applied its phases to the database
    immediately so cancelling didn’t restore the original phases
    
    This commit:
    
    - ensures cancelling restores the original schedule and only persists
    the changes on save
    
    Fixes #17002

--- experimenter/experimenter/nimbus_ui/new/views.py
+++ experimenter/experimenter/nimbus_ui/new/views.py
@@ -795,43 +795,47 @@ class NewRolloutScheduleUpdateView(NewCardUpdateView):
         context["rollout_phase_population_estimates"] = (
             get_rollout_phase_population_estimates(self.object)
         )
-        selected_plan = self.request.POST.get("template_name") or self.request.POST.get(
-            "rollout_plan"
-        )
-        if selected_plan:
-            context["form"].initial["rollout_plan"] = selected_plan
         return context
 
     def can_edit(self):
         return self.object.is_draft or self.object.is_rolling_out
 
 
+class RolloutSchedulePreviewMixin:
+    def form_valid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+
 class NewRolloutPhaseCreateView(
-    RenderParentDBResponseMixin, NewRolloutScheduleUpdateView
+    RolloutSchedulePreviewMixin, NewRolloutScheduleUpdateView
 ):
     form_class = RolloutPhaseCreateForm
 
 
-class NewRolloutPhaseDeleteView(CardMutationMixin, NewRolloutScheduleUpdateView):
+class NewRolloutPhaseDeleteView(
+    RolloutSchedulePreviewMixin, NewRolloutScheduleUpdateView
+):
     form_class = RolloutPhaseDeleteForm
 
-    def mutate(self, form):
-        phase_id = self.request.POST.get("phase_id")
-        if not phase_id:
-            return
-        if int(phase_id) not in form.locked_phase_ids:
-            self.object.rollout_phases.filter(id=phase_id).delete()
 
-
-class NewRolloutPlanCreateView(RenderParentDBResponseMixin, NewRolloutScheduleUpdateView):
-    form_class = RolloutPlanCreateForm
-
-
-class NewRolloutPlanApplyView(CardMutationMixin, NewRolloutScheduleUpdateView):
+class NewRolloutPlanApplyView(RolloutSchedulePreviewMixin, NewRolloutScheduleUpdateView):
     form_class = RolloutPlanApplyForm
 
-    def mutate(self, form):
-        form.apply_plan()
+
+class NewRolloutPlanCreateView(NewRolloutScheduleUpdateView):
+    form_class = RolloutPlanCreateForm
+
+    def form_valid(self, form):
+        form.save()
+        data = self.request.POST.copy()
+        data["rollout_plan"] = form.cleaned_data["template_name"]
+        return self.render_to_response(
+            self.get_context_data(
+                form=RolloutScheduleForm(
+                    data=data, instance=self.object, request=self.request
+                )
+            )
+        )
 
 
 class NewSubscribeView(NimbusExperimentViewMixin, RequestFormMixin, UpdateView):
